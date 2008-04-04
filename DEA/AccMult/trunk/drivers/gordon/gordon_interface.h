bce5926deba7876c7e6febdc85a3ec138cc8d522
Cédric Augonnet <cedric.augonnet@inria.fr>
2008-04-04 18:46:31 +0000

    still very little support for gordon, modifying the repo properties to solve the issues with the externals repo ..

---
+++ DEA/AccMult/trunk/drivers/gordon/gordon_interface.h
@@ -0,0 +1,7 @@
+#ifndef __GORDON_INTERFACE_H__
+#define __GORDON_INTERFACE_H__
+
+#include "externals/scalp/util/util.h"
+#include "externals/scalp/cell/gordon/gordon.h"
+
+#endif // __GORDON_INTERFACE_H__
--- DEA/AccMult/trunk/drivers/gordon/mult_gordon.c
+++ DEA/AccMult/trunk/drivers/gordon/mult_gordon.c
@@ -5,10 +5,56 @@ void *gordon_worker(void *arg)
 {
 	struct gordon_worker_arg_t* args = (struct gordon_worker_arg_t*)arg;
 
+#ifndef DONTBIND
+	/* fix the thread on the correct cpu */
+	cpu_set_t aff_mask;
+	CPU_ZERO(&aff_mask);
+	CPU_SET(args->bindid, &aff_mask);
+	sched_setaffinity(0, sizeof(aff_mask), &aff_mask);
+#endif
+
+	/* TODO set_local_memory_node per SPU */
+
 	gordon_init(args->nspus);	
 
 	/* tell the core that gordon is ready */
 	args->ready_flag = 1;
+
+	int res;
+	job_t j;
+	do {
+		j = pop_task();
+		if (j == NULL) continue;
+
+		/* is gordon able to do that ? */
+		if (!GORDON_MAY_PERFORM(j))
+		{
+			push_task(j);
+			continue;
+		}
+
+		/* XXX that model is not really appropriated for gordon */
+		res = execute_job_on_gordon_non_blocking(j);
+		if (res != OK) {
+			switch (res) {
+				case TRYAGAIN:
+					push_task(j);
+					break;
+				case OK:
+				case FATAL:
+				default:
+					assert(0);
+			}
+		}
+
+		if (j->cb)
+			j->cb(j->argcb);
+
+		/* TODO add counters */
+
+		job_delete(j);
+
+	} while(1);
 
 	return NULL;
 }
