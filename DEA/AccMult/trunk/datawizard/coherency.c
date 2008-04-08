284d1aab8ccc3239ca2a81c2e392b0db81825343
Cédric Augonnet <cedric.augonnet@inria.fr>
2008-04-08 12:35:43 +0000

    move  monitor_new_data from coherency.c to hierarchy.c

--- DEA/AccMult/trunk/datawizard/coherency.c
+++ DEA/AccMult/trunk/datawizard/coherency.c
@@ -16,52 +16,5 @@ void release_lock(data_lock *lock)
 	lock->taken = FREE;
 }
 
-void monitor_new_data(data_state *state, uint32_t home_node, 
-			uintptr_t ptr, size_t length)
-{
-	ASSERT(state);
-
-	/* initialize the new lock */
-	state->lock.taken = FREE;
-#ifdef USE_SPU
-	state->ea_data_state = (uintptr_t)&state;
-	state->lock.ea_taken = (uintptr_t)&state->lock.taken;
-#endif
-
-	/* first take care to properly lock the data */
-	take_lock(&state->lock);
-
-	/* we assume that all nodes may use that data */
-	state->nnodes = MAXNODES;
-
-	/* make sure we do have a valid copy */
-	ASSERT(home_node < MAXNODES);
-
-	/* that new data is invalid from all nodes perpective except for the
-	 * home node */
-	unsigned node;
-	for (node = 0; node < MAXNODES; node++)
-	{
-		if (node == home_node) {
-			/* this is the home node with the only valid copy */
-			state->per_node[node].state = OWNER;
-			state->per_node[node].ptr = ptr;
-			state->per_node[node].allocated = 1;
-			state->per_node[node].automatically_allocated = 0;
-		}
-		else {
-			/* the value is not available here yet */
-			state->per_node[node].state = INVALID;
-			state->per_node[node].ptr = 0;
-			state->per_node[node].allocated = 0;
-		}
-	}
-
-	state->length = length;
-
-	/* now the data is available ! */
-	release_lock(&state->lock);
-}
-
 /* we do have the same code for PPU and SPU in the case of a Cell */
 #include "coherency_common.c"
--- DEA/AccMult/trunk/datawizard/hierarchy.c
+++ DEA/AccMult/trunk/datawizard/hierarchy.c
@@ -1,5 +1,52 @@
 #include "hierarchy.h"
 
+void monitor_new_data(data_state *state, uint32_t home_node, 
+			uintptr_t ptr, size_t length)
+{
+	ASSERT(state);
+
+	/* initialize the new lock */
+	state->lock.taken = FREE;
+#ifdef USE_SPU
+	state->ea_data_state = (uintptr_t)&state;
+	state->lock.ea_taken = (uintptr_t)&state->lock.taken;
+#endif
+
+	/* first take care to properly lock the data */
+	take_lock(&state->lock);
+
+	/* we assume that all nodes may use that data */
+	state->nnodes = MAXNODES;
+
+	/* make sure we do have a valid copy */
+	ASSERT(home_node < MAXNODES);
+
+	/* that new data is invalid from all nodes perpective except for the
+	 * home node */
+	unsigned node;
+	for (node = 0; node < MAXNODES; node++)
+	{
+		if (node == home_node) {
+			/* this is the home node with the only valid copy */
+			state->per_node[node].state = OWNER;
+			state->per_node[node].ptr = ptr;
+			state->per_node[node].allocated = 1;
+			state->per_node[node].automatically_allocated = 0;
+		}
+		else {
+			/* the value is not available here yet */
+			state->per_node[node].state = INVALID;
+			state->per_node[node].ptr = 0;
+			state->per_node[node].allocated = 0;
+		}
+	}
+
+	state->length = length;
+
+	/* now the data is available ! */
+	release_lock(&state->lock);
+}
+
 /*
  * For now, we assume that partitionned_data is already properly allocated;
  * at least by the filter function !
@@ -107,3 +154,4 @@ void unpartition_data(data_state *root_data, uint32_t gathering_node)
 	/* now the parent may be used again so we release the lock */
 	release_lock(&root_data->lock);
 }
+
