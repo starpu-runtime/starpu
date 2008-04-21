7ccc293b81b03f1795374a41ab132f3fefc72f60
Cédric Augonnet <cedric.augonnet@inria.fr>
2008-04-21 08:59:07 +0000

    had forgotten to add that file : get rid of the macro-style for timing

---
+++ DEA/AccMult/trunk/common/timing.c
@@ -0,0 +1,52 @@
+#include "timing.h"
+
+#define TICK_RAW_DIFF(t1, t2) ((t2).tick - (t1).tick)
+#define TICK_DIFF(t1, t2) (TICK_RAW_DIFF(t1, t2) - residual)
+#define TIMING_DELAY(t1, t2) tick2usec(TICK_DIFF(t1, t2))
+
+static double scale = 0.0;
+static unsigned long long residual = 0;
+
+static int inited = 0;
+
+void __attribute__ ((unused)) timing_init(void)
+{
+  static tick_t t1, t2;
+  int i;
+
+  if (inited) return;
+
+  residual = (unsigned long long)1 << 63;
+  
+  for(i = 0; i < 20; i++)
+    {
+      GET_TICK(t1);
+      GET_TICK(t2);
+      residual = min(residual, TICK_RAW_DIFF(t1, t2));
+    }
+  
+  {
+    struct timeval tv1,tv2;
+    
+    GET_TICK(t1);
+    gettimeofday(&tv1,0);
+    usleep(500000);
+    GET_TICK(t2);
+    gettimeofday(&tv2,0);
+    scale = ((tv2.tv_sec*1e6 + tv2.tv_usec) -
+	     (tv1.tv_sec*1e6 + tv1.tv_usec)) / 
+      (double)(TICK_DIFF(t1, t2));
+  }
+
+  inited = 1;
+}
+
+double __attribute__ ((unused)) tick2usec(long long t)
+{
+  return (double)(t)*scale;
+}
+
+double __attribute__ ((unused)) timing_delay(tick_t *t1, tick_t *t2)
+{
+	return TIMING_DELAY(*t1, *t2);
+}
