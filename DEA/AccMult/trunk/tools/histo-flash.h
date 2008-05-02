189c647a6c28b837d9fcf02f5efaa5ace9888b04
Cédric Augonnet <cedric.augonnet@inria.fr>
2008-05-02 17:36:01 +0000

    forgot to add a file

---
+++ DEA/AccMult/trunk/tools/histo-flash.h
@@ -0,0 +1,22 @@
+#ifndef __HISTO_FLASH_H__
+#define __HISTO_FLASH_H__
+
+#include <stdint.h>
+#include <stdlib.h>
+#include <ming.h>
+#include <math.h>
+
+#include "fxt-tool.h"
+
+#define WIDTH	800
+#define HEIGHT	600
+
+#define THICKNESS	50
+#define GAP		10
+
+#define BORDERX		100
+#define BORDERY		100
+
+void flash_engine_init(void);
+
+#endif // __HISTO_FLASH_H__
