4399775cbfd7451dfdc112ad086061e3cc8a46b2
Cédric Augonnet <cedric.augonnet@inria.fr>
2008-05-05 09:08:07 +0000

    improve the gflops.sh example for the heat problem... possibly more than one run per experiment.

--- DEA/AccMult/trunk/tests/heat/gflops.gp
+++ DEA/AccMult/trunk/tests/heat/gflops.gp
@@ -5,6 +5,7 @@ set output "gflops.eps"
 set title "Facto LU : impact of granularity"
 set xlabel "problem size (unknowns)"
 set ylabel "GFlops"
+set logscale x
 set key right bottom
 set datafile missing 'x'
 plot "timings/gflops.data" usi 1:(2*$1*$1*$1 / (3*$2* 1000000)) with lines title "tile 128"  ,\
--- DEA/AccMult/trunk/tests/heat/gflops.sh
+++ DEA/AccMult/trunk/tests/heat/gflops.sh
@@ -6,8 +6,39 @@ TIMINGDIR=$DIR/timings/
 mkdir -p $TIMINGDIR
 filename=$TIMINGDIR/gflops.data
 
-tilelist="128 256 512 1024 2048"
+tilelist="128 256 512 1024"
 sizelist="256 512 1024 2048 3072 4096 5120 6144 7168 8192 9216 10240 11264 12288 13312 14336 15360 16384 17408 18432 19456 20480 21504 22528 23552 24576 25600"
+
+#tilelist="1024"
+#sizelist="1024 2048 4096 8192 16384 20480 24576 25600"
+
+heat_ret=0
+
+measure_heat()
+{
+	thick=$1
+	theta=$2
+	nblocks=$3
+	size=$4
+
+	if [ $size -le 2048 ] 
+	then
+		nsample=1
+	else
+		nsample=1
+	fi
+
+	total=0
+
+	for i in `seq 1 $nsample`
+	do
+		echo "iter $i/$nsample"
+		val=`$ROOTDIR/examples/heat -nthick $thick -ntheta $theta -nblocks $nblocks 2>/dev/null`
+		total=`echo "$val + $total" |bc -l`
+	done
+
+	heat_ret=`echo "$total / $nsample"|bc -l`
+}
 
 trace_header()
 {
@@ -35,9 +66,11 @@ trace_size()
 		theta=$(($(($size / 32)) + 2))
 		thick=34
 
-		if [ $tile -lt $size -a $nblocks -lt 32 -a $(($size % $tile)) == 0 ];
+		if [ $tile -le $size -a $nblocks -le 32 -a $(($size % $tile)) == 0 ];
 		then
-			timing=`$ROOTDIR/examples/heat -nthick $thick -ntheta $theta -nblocks $nblocks 2>/dev/null`
+			echo "$ROOTDIR/examples/heat -nthick $thick -ntheta $theta -nblocks $nblocks"
+			measure_heat $thick $theta $nblocks $size;
+			timing=$heat_ret
 		else
 			timing="x"
 		fi
