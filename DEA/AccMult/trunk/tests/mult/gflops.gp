c2d319e3767621bb93085db32adce13231d168ad
Cédric Augonnet <cedric.augonnet@inria.fr>
2008-05-15 17:45:13 +0000

    add some gflops timing for the mult example

---
+++ DEA/AccMult/trunk/tests/mult/gflops.gp
@@ -0,0 +1,14 @@
+#!/usr/bin/gnuplot -persist
+
+set term postscript eps enhanced color
+set output "gflops.eps"
+set title "Facto LU : impact of granularity"
+set xlabel "problem size (unknowns)"
+set ylabel "GFlops"
+set logscale x
+set key right bottom
+set datafile missing 'x'
+plot "timings/gflops.data" usi 1:(2*$1*$1*$1 / ($2* 1000000)) with lines title "tile 256"  ,\
+     "timings/gflops.data" usi 1:(2*$1*$1*$1 / ($3* 1000000)) with lines title "tile 512"  ,\
+     "timings/gflops.data" usi 1:(2*$1*$1*$1 / ($4* 1000000)) with lines title "tile 1024" ,\
+     "timings/gflops.data" usi 1:(2*$1*$1*$1 / ($5* 1000000)) with lines title "tile 2048"
---
+++ DEA/AccMult/trunk/tests/mult/gflops.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+
+DIR=$PWD
+ROOTDIR=$DIR/../..
+TIMINGDIR=$DIR/timings/
+mkdir -p $TIMINGDIR
+filename=$TIMINGDIR/gflops.data
+
+tilelist="256 512 1024 2048"
+sizelist="256 512 1024 2048 4096 8192"
+
+trace_header()
+{
+	line="# size 	"
+	for tile in $tilelist
+	do
+		line="$line	$tile"
+	done
+
+	echo "$line" > $filename
+}
+
+trace_size()
+{
+	size=$1
+
+	echo "Computing size $size"
+	
+	line="$size"
+
+	for tile in $tilelist
+	do
+		nblocks=$(($size / $tile))
+
+		if [ $tile -lt $size -a $nblocks -lt 32 -a $(($size % $tile)) == 0 ];
+		then
+			echo "start tile $tile size $size nblocks $nblocks  "
+			timing=`$ROOTDIR/examples/dw_mult -x $size -y $size -z $size -nblocks $nblocks 2>/dev/null`
+		else
+			timing="x"
+		fi
+	#	timing=`$ROOTDIR/examples/heat -nthick $thick -ntheta $theta -nblocks $nblocks 2>/dev/null`
+
+		echo "size : $size tile $tile => $timing us"
+
+		line="$line	$timing"
+
+	done
+
+	echo "$line" >> $filename
+}
+
+cd $ROOTDIR
+
+make clean 1> /dev/null 2> /dev/null
+make ATLAS=1 CUBLAS=1 CPUS=3 1> /dev/null 2> /dev/null
+
+cd $DIR
+
+trace_header 
+for size in $sizelist
+do
+	trace_size $size;
+done
