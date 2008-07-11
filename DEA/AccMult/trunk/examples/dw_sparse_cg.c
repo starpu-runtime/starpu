7a3b388e69cb3e7eb5e300a57e191730be602e05
Cédric Augonnet <cedric.augonnet@inria.fr>
2008-07-11 15:00:41 +0000

    the sparse conjugate gradient example is mostly written, ... does not work yet

--- DEA/AccMult/trunk/examples/dw_sparse_cg.c
+++ DEA/AccMult/trunk/examples/dw_sparse_cg.c
@@ -2,33 +2,9 @@
  * Conjugate gradients for Sparse matrices
  */
 
-#include <semaphore.h>
-#include <core/jobs.h>
-#include <core/workers.h>
-#include <core/tags.h>
-#include <common/timing.h>
-#include <common/util.h>
-#include <string.h>
-#include <stdint.h>
-#include <math.h>
-#include <sys/types.h>
-#include <pthread.h>
-#include <signal.h>
-#include <cblas.h>
-#include <common/timing.h>
-
-#include <datawizard/coherency.h>
-#include <datawizard/hierarchy.h>
-#include <datawizard/interfaces/blas_interface.h>
-#include <datawizard/interfaces/blas_filters.h>
-#include <datawizard/interfaces/csr_interface.h>
+#include "dw_sparse_cg.h"
 
 tick_t start,end;
-
-/* First a Matrix-Vector product (SpMV) */
-
-unsigned blocks = 512;
-unsigned grids  = 8;
 
 #ifdef USE_CUDA
 /* CUDA spmv codelet */
@@ -63,226 +39,247 @@ void initialize_cuda(void)
 
 #endif // USE_CUDA
 
-
-sem_t sem;
-uint32_t size = 4194304;
-
-data_state sparse_matrix;
-data_state vector_in, vector_out;
-
-float *sparse_matrix_nzval;
-uint32_t *sparse_matrix_colind;
-uint32_t *sparse_matrix_rowptr;
-
-float *vector_in_ptr;
-float *vector_out_ptr;
-
-unsigned usecpu = 0;
-
-
-void parse_args(int argc, char **argv)
-{
-	int i;
-	for (i = 1; i < argc; i++) {
-		if (strcmp(argv[i], "-size") == 0) {
-			char *argptr;
-			size = strtol(argv[++i], &argptr, 10);
-		}
-
-		if (strcmp(argv[i], "-block") == 0) {
-			char *argptr;
-			blocks = strtol(argv[++i], &argptr, 10);
-		}
-
-		if (strcmp(argv[i], "-grid") == 0) {
-			char *argptr;
-			grids = strtol(argv[++i], &argptr, 10);
-		}
-
-		if (strcmp(argv[i], "-cpu") == 0) {
-			usecpu = 1;
-		}
-	}
-}
-
-void core_spmv(data_interface_t *descr, __attribute__((unused))  void *arg)
-{
-	printf("CORE codelet\n");
-
-	float *nzval = (float *)descr[0].csr.nzval;
-	uint32_t *colind = descr[0].csr.colind;
-	uint32_t *rowptr = descr[0].csr.rowptr;
-
-	float *vecin = (float *)descr[1].blas.ptr;
-	float *vecout = (float *)descr[2].blas.ptr;
-
-	uint32_t nnz;
-	uint32_t nrow;
-
-	nnz = descr[0].csr.nnz;
-	nrow = descr[0].csr.nrow;
-
-	ASSERT(nrow == descr[1].blas.nx);
-	ASSERT(nrow == descr[2].blas.nx);
-
-	unsigned row;
-	for (row = 0; row < nrow; row++)
-	{
-		float tmp = 0.0f;
-		unsigned index;
-
-		unsigned firstindex = rowptr[row];
-		unsigned lastindex = (row < nrow - 1)?rowptr[row+1]:nnz;
-
-		for (index = firstindex; index < lastindex; index++)
-		{
-			unsigned col;
-
-			col = colind[index];
-			tmp += nzval[index]*vecin[col];
-		}
-
-		vecout[row] = tmp;
-	}
-
-}
-
-void create_data(void)
-{
-	/* we need a sparse symetric (definite positive ?) matrix and a "dense" vector */
-	
-	/* example of 3-band matrix */
-	float *nzval;
-	uint32_t nnz;
-	uint32_t *colind;
-	uint32_t *rowptr;
-
-	nnz = 3*size-2;
-
-	nzval = malloc(nnz*sizeof(float));
-	colind = malloc(nnz*sizeof(uint32_t));
-	rowptr = malloc(size*sizeof(uint32_t));
-
-	assert(nzval);
-	assert(colind);
-	assert(rowptr);
-
-	/* fill the matrix */
-	unsigned row;
-	unsigned pos = 0;
-	for (row = 0; row < size; row++)
-	{
-		rowptr[row] = pos;
-
-		if (row > 0) {
-			nzval[pos] = 1.0f;
-			colind[pos] = row-1;
-			pos++;
-		}
-		
-		nzval[pos] = 5.0f;
-		colind[pos] = row;
-		pos++;
-
-		if (row < size - 1) {
-			nzval[pos] = 1.0f;
-			colind[pos] = row+1;
-			pos++;
-		}
-	}
-
-	ASSERT(pos == nnz);
-	
-	monitor_csr_data(&sparse_matrix, 0, nnz, size, (uintptr_t)nzval, colind, rowptr, 0, sizeof(float));
-
-	sparse_matrix_nzval = nzval;
-	sparse_matrix_colind = colind;
-	sparse_matrix_rowptr = rowptr;
-
-	/* initiate the 2 vectors */
-	float *invec, *outvec;
-	invec = malloc(size*sizeof(float));
-	assert(invec);
-
-	outvec = malloc(size*sizeof(float));
-	assert(outvec);
-
-	/* fill those */
-	unsigned ind;
-	for (ind = 0; ind < size; ind++)
-	{
-		invec[ind] = 2.0f;
-		outvec[ind] = 0.0f;
-	}
-
-	monitor_blas_data(&vector_in, 0, (uintptr_t)invec, size, size, 1, sizeof(float));
-	monitor_blas_data(&vector_out, 0, (uintptr_t)outvec, size, size, 1, sizeof(float));
-
-	vector_in_ptr = invec;
-	vector_out_ptr = outvec;
-
-}
-
-void init_problem_callback(void *arg __attribute__((unused)))
-{
-	sem_post(&sem);
-
-	GET_TICK(end);
-
-}
-
-void call_spmv_codelet(void)
-{
-	job_t job;
-	codelet *cl = malloc(sizeof(codelet));
-
-	cl->cl_arg = NULL;
-	cl->core_func =  core_spmv;
-#ifdef USE_CUDA
-	cl->cuda_func = &cuda_codelet;
-#endif
-
-	job = job_create();
-#ifdef USE_CUDA
-	job->where = usecpu?CORE:CUDA;
-#else
-	job->where = CORE;
-#endif
-	job->cb = init_problem_callback;
-	job->argcb = NULL;
-	job->cl = cl;
-
-	job->nbuffers = 3;
-	job->buffers[0].state = &sparse_matrix;
-	job->buffers[0].mode  = R;
-	job->buffers[1].state = &vector_in;
-	job->buffers[1].mode = R;
-	job->buffers[2].state = &vector_out;
-	job->buffers[2].mode = W;
-
-
-	GET_TICK(start);
-
-	push_task(job);
-}
-
 void init_problem(void)
 {
 	/* create the sparse input matrix */
-	create_data();
+	float *nzval;
+	float *vecb;
+	float *vecx;
+	uint32_t nnz;
+	uint32_t nrow;
+	uint32_t *colind;
+	uint32_t *rowptr;
 
-	/* create a new codelet that will perform a SpMV on it */
-	call_spmv_codelet();
+	create_data(&nzval, &vecb, &vecx, &nnz, &nrow, &colind, &rowptr);
+
+	conjugate_gradient(nzval, vecb, vecx, nnz, nrow, colind, rowptr);
 }
 
-void print_results(void)
-{
-	unsigned row;
+/*
+ *	cg initialization phase 
+ */
 
-	for (row = 0; row < MIN(size, 16); row++)
+void init_cg(struct cg_problem *problem) 
+{
+	problem->i = 0;
+
+	/* r = b  - A x */
+	job_t job1 = create_job(1UL);
+	job1->where = CORE;
+	job1->cl->core_func = core_codelet_func_1;
+	job1->nbuffers = 4;
+		job1->buffers[0].state = problem->ds_matrixA;
+		job1->buffers[0].mode = R;
+		job1->buffers[1].state = problem->ds_vecx;
+		job1->buffers[0].mode = R;
+		job1->buffers[2].state = problem->ds_vecr;
+		job1->buffers[0].mode = W;
+		job1->buffers[3].state = problem->ds_vecb;
+		job1->buffers[3].mode = R;
+
+	/* d = r */
+	job_t job2 = create_job(2UL);
+	job2->where = CORE;
+	job2->cl->core_func = core_codelet_func_2;
+	job2->nbuffers = 2;
+		job1->buffers[0].state = problem->ds_vecd;
+		job1->buffers[0].mode = W;
+		job1->buffers[1].state = problem->ds_vecr;
+		job1->buffers[1].mode = R;
+	
+	tag_declare_deps(2UL, 1, 1UL);
+
+	/* delta_new = trans(r) r */
+	job_t job3 = create_job(3UL);
+	job3->where = CUBLAS;
+	job3->cl->cublas_func = cublas_codelet_func_3;
+	job3->nbuffers = 1;
+		job1->buffers[0].state = problem->ds_vecr;
+		job1->buffers[0].mode = R;
+
+	job3->cb = iteration_cg;
+	job3->argcb = problem;
+	
+	/* XXX 3 should only depend on 1 ... */
+	tag_declare_deps(3UL, 1, 2UL);
+
+	/* launch the computation now */
+	push_task(job1);
+}
+
+/*
+ *	the inner iteration of the cg algorithm 
+ *		the codelet code launcher is its own callback !
+ */
+
+void launch_new_cg_iteration(struct cg_problem *problem)
+{
+	unsigned iter = problem->i;
+
+	unsigned long long maskiter = (iter*1024);
+
+	/* q = A d */
+	job_t job4 = create_job(maskiter | 4UL);
+	job4->where = CORE;
+	job4->cl->core_func = core_codelet_func_4;
+	job4->nbuffers = 3;
+		job4->buffers[0].state = problem->ds_matrixA;
+		job4->buffers[0].mode = R;
+		job4->buffers[1].state = problem->ds_vecd;
+		job4->buffers[1].mode = R;
+		job4->buffers[2].state = problem->ds_vecq;
+		job4->buffers[2].mode = W;
+
+	/* alpha = delta_new / ( trans(d) q )*/
+	job_t job5 = create_job(maskiter | 5UL);
+	job5->where = CUBLAS;
+	job5->cl->cublas_func = cublas_codelet_func_5;
+	job5->nbuffers = 2;
+		job5->buffers[0].state = problem->ds_vecd;
+		job5->buffers[0].mode = R;
+		job5->buffers[1].state = problem->ds_vecq;
+		job5->buffers[1].mode = R;
+
+	tag_declare_deps(maskiter | 5UL, 1, maskiter | 4UL);
+
+	/* x = x + alpha d */
+	job_t job6 = create_job(maskiter | 6UL);
+	job6->where = CUBLAS;
+	job6->cl->cublas_func = cublas_codelet_func_6;
+	job6->nbuffers = 2;
+		job6->buffers[0].state = problem->ds_vecx;
+		job6->buffers[0].mode = RW;
+		job6->buffers[1].state = problem->ds_vecd;
+		job6->buffers[1].mode = R;
+
+	tag_declare_deps(maskiter | 6UL, 1, maskiter | 5UL);
+
+	/* r = r - alpha q */
+	job_t job7 = create_job(maskiter | 7UL);
+	job7->where = CUBLAS;
+	job7->cl->cublas_func = cublas_codelet_func_7;
+	job7->nbuffers = 2;
+		job7->buffers[0].state = problem->ds_vecr;
+		job7->buffers[0].mode = RW;
+		job7->buffers[1].state = problem->ds_vecq;
+		job7->buffers[1].mode = R;
+
+	tag_declare_deps(maskiter | 7UL, 1, maskiter | 6UL);
+
+	/* update delta_* and compute beta */
+	job_t job8 = create_job(maskiter | 8UL);
+	job8->where = CUBLAS;
+	job8->cl->cublas_func = cublas_codelet_func_8;
+	job8->nbuffers = 1;
+		job8->buffers[0].state = problem->ds_vecr;
+		job8->buffers[0].mode = R;
+
+	tag_declare_deps(maskiter | 8UL, 1, maskiter | 7UL);
+
+	/* d = r + beta d */
+	job_t job9 = create_job(maskiter | 9UL);
+	job9->where = CUBLAS;
+	job9->cl->cublas_func = cublas_codelet_func_9;
+	job9->nbuffers = 2;
+		job9->buffers[0].state = problem->ds_vecd;
+		job9->buffers[0].mode = RW;
+		job9->buffers[1].state = problem->ds_vecr;
+		job9->buffers[1].mode = R;
+
+	tag_declare_deps(maskiter | 9UL, 1, maskiter | 8UL);
+
+	job9->cb = iteration_cg;
+	job9->argcb = problem;
+	
+	/* launch the computation now */
+	push_task(job4);
+}
+
+void *iteration_cg(void *problem)
+{
+	struct cg_problem *pb = problem;
+
+	if ((pb->i++ < MAXITER) && 
+		(pb->delta_new > pb->epsilon) )
 	{
-		printf("%2.2f\t%2.2f\n", vector_in_ptr[row], vector_out_ptr[row]);
+		/* we did not reach the stop condition yet */
+		launch_new_cg_iteration(problem);
 	}
+	else {
+		/* we may stop */
+		printf("We are done ... \n");
+		sem_post(pb->sem);
+	}
+
+	return NULL;
+}
+
+/*
+ *	initializing the problem 
+ */
+
+void conjugate_gradient(float *nzvalA, float *vecb, float *vecx, uint32_t nnz,
+			unsigned nrow, uint32_t *colind, uint32_t *rowptr)
+{
+	/* first declare all the data structures to the runtime */
+
+	struct data_state_t ds_matrixA;
+	struct data_state_t ds_vecx, ds_vecb;
+	struct data_state_t ds_vecr, ds_vecd, ds_vecq; 
+
+	/* first the user-allocated data */
+	monitor_csr_data(&ds_matrixA, 0, nnz, nrow, 
+			(uintptr_t)nzvalA, colind, rowptr, 0, sizeof(float));
+	monitor_blas_data(&ds_vecx, 0, (uintptr_t)vecx,
+			nrow, nrow, 1, sizeof(float));
+	monitor_blas_data(&ds_vecb, 0, (uintptr_t)vecb,
+			nrow, nrow, 1, sizeof(float));
+
+	/* then allocate the algorithm intern data */
+	float *ptr_vecr, *ptr_vecd, *ptr_vecq;
+
+	unsigned i;
+	ptr_vecr = malloc(nrow*sizeof(float));
+	ptr_vecd = malloc(nrow*sizeof(float));
+	ptr_vecq = malloc(nrow*sizeof(float));
+
+	for (i = 0; i < nrow; i++)
+	{
+		ptr_vecr[i] = 0.0f;
+		ptr_vecd[i] = 0.0f;
+		ptr_vecq[i] = 0.0f;
+	}
+
+	/* and declare them as well */
+	monitor_blas_data(&ds_vecr, 0, (uintptr_t)ptr_vecr, 
+			nrow, nrow, 1, sizeof(float));
+	monitor_blas_data(&ds_vecd, 0, (uintptr_t)ptr_vecd, 
+			nrow, nrow, 1, sizeof(float));
+	monitor_blas_data(&ds_vecq, 0, (uintptr_t)ptr_vecq, 
+			nrow, nrow, 1, sizeof(float));
+
+	/* we now have the complete problem */
+	struct cg_problem problem;
+
+	problem.ds_matrixA = &ds_matrixA;
+	problem.ds_vecx    = &ds_vecx;
+	problem.ds_vecb    = &ds_vecb;
+	problem.ds_vecr    = &ds_vecr;
+	problem.ds_vecd    = &ds_vecd;
+	problem.ds_vecq    = &ds_vecq;
+
+	/* XXX set me */
+	problem.epsilon = 0.01f;
+
+	/* we need a semaphore to synchronize with callbacks */
+	sem_t sem;
+	sem_init(&sem, 0, 0U);
+	problem.sem  = &sem;
+
+	init_cg(&problem);
+
+	sem_wait(&sem);
+	sem_destroy(&sem);
 }
 
 int main(__attribute__ ((unused)) int argc,
@@ -296,7 +293,6 @@ int main(__attribute__ ((unused)) int argc,
 	init_machine();
 	init_workers();
 
-	sem_init(&sem, 0, 0U);
 
 #ifdef USE_CUDA
 	initialize_cuda();
@@ -304,8 +300,7 @@ int main(__attribute__ ((unused)) int argc,
 
 	init_problem();
 
-	sem_wait(&sem);
-	sem_destroy(&sem);
+
 
 	print_results();
 
---
+++ DEA/AccMult/trunk/examples/dw_sparse_cg.h
@@ -0,0 +1,196 @@
+#ifndef __DW_SPARSE_CG_H__
+#define __DW_SPARSE_CG_H__
+
+#include <semaphore.h>
+#include <core/jobs.h>
+#include <core/workers.h>
+#include <core/tags.h>
+#include <common/timing.h>
+#include <common/util.h>
+#include <string.h>
+#include <stdint.h>
+#include <math.h>
+#include <sys/types.h>
+#include <pthread.h>
+#include <signal.h>
+#include <cblas.h>
+#include <common/timing.h>
+
+#include <datawizard/coherency.h>
+#include <datawizard/hierarchy.h>
+#include <datawizard/interfaces/blas_interface.h>
+#include <datawizard/interfaces/blas_filters.h>
+#include <datawizard/interfaces/csr_interface.h>
+
+#define MAXITER	100
+
+/* code parameters */
+static uint32_t size = 4194304;
+static unsigned usecpu = 0;
+static unsigned blocks = 512;
+static unsigned grids  = 8;
+
+struct cg_problem {
+	struct data_state_t *ds_matrixA;
+	struct data_state_t *ds_vecx;
+	struct data_state_t *ds_vecb;
+	struct data_state_t *ds_vecr;
+	struct data_state_t *ds_vecd;
+	struct data_state_t *ds_vecq;
+
+	sem_t *sem;
+	
+	float alpha;
+	float beta;
+	float delta_0;
+	float delta_old;
+	float delta_new;
+	float epsilon;
+
+	int i;
+};
+
+/* some useful functions */
+static void parse_args(int argc, char **argv)
+{
+	int i;
+	for (i = 1; i < argc; i++) {
+		if (strcmp(argv[i], "-size") == 0) {
+			char *argptr;
+			size = strtol(argv[++i], &argptr, 10);
+		}
+
+		if (strcmp(argv[i], "-block") == 0) {
+			char *argptr;
+			blocks = strtol(argv[++i], &argptr, 10);
+		}
+
+		if (strcmp(argv[i], "-grid") == 0) {
+			char *argptr;
+			grids = strtol(argv[++i], &argptr, 10);
+		}
+
+		if (strcmp(argv[i], "-cpu") == 0) {
+			usecpu = 1;
+		}
+	}
+}
+
+
+static void print_results(void)
+{
+}
+
+static void create_data(float **_nzvalA, float **_vecb, float **_vecx, uint32_t *_nnz, uint32_t *_nrow, uint32_t **_colind, uint32_t **_rowptr)
+{
+	/* we need a sparse symetric (definite positive ?) matrix and a "dense" vector */
+	
+	/* example of 3-band matrix */
+	float *nzval;
+	uint32_t nnz;
+	uint32_t *colind;
+	uint32_t *rowptr;
+
+	nnz = 3*size-2;
+
+	nzval = malloc(nnz*sizeof(float));
+	colind = malloc(nnz*sizeof(uint32_t));
+	rowptr = malloc(size*sizeof(uint32_t));
+
+	assert(nzval);
+	assert(colind);
+	assert(rowptr);
+
+
+	/* fill the matrix */
+	unsigned row;
+	unsigned pos = 0;
+	for (row = 0; row < size; row++)
+	{
+		rowptr[row] = pos;
+
+		if (row > 0) {
+			nzval[pos] = 1.0f;
+			colind[pos] = row-1;
+			pos++;
+		}
+		
+		nzval[pos] = 5.0f;
+		colind[pos] = row;
+		pos++;
+
+		if (row < size - 1) {
+			nzval[pos] = 1.0f;
+			colind[pos] = row+1;
+			pos++;
+		}
+	}
+
+	*_nnz = nnz;
+	*_nrow = size;
+	*_nzvalA = nzval;
+	*_colind = colind;
+	*_rowptr = rowptr;
+
+	ASSERT(pos == nnz);
+	
+	//sparse_matrix_nzval = nzval;
+	//sparse_matrix_colind = colind;
+	//sparse_matrix_rowptr = rowptr;
+
+	/* initiate the 2 vectors */
+	float *invec, *outvec;
+	invec = malloc(size*sizeof(float));
+	assert(invec);
+
+	outvec = malloc(size*sizeof(float));
+	assert(outvec);
+
+	/* fill those */
+	unsigned ind;
+	for (ind = 0; ind < size; ind++)
+	{
+		invec[ind] = 2.0f;
+		outvec[ind] = 0.0f;
+	}
+
+//	monitor_blas_data(&vector_in, 0, (uintptr_t)invec, size, size, 1, sizeof(float));
+//	monitor_blas_data(&vector_out, 0, (uintptr_t)outvec, size, size, 1, sizeof(float));
+//
+//	vector_in_ptr = invec;
+//	vector_out_ptr = outvec;
+
+	*_vecb = invec;
+	*_vecx = outvec;
+}
+
+static job_t create_job(tag_t id)
+{
+	codelet *cl = malloc(sizeof(codelet));
+	cl->cl_arg = NULL;
+
+	job_t j = job_create();
+		j->where = ANY;
+		j->cl = cl;
+
+	tag_declare(id, j);
+
+	return j;
+}
+
+void core_codelet_func_1(data_interface_t *descr, void *arg);
+void core_codelet_func_2(data_interface_t *descr, void *arg);
+void cublas_codelet_func_3(data_interface_t *descr, void *arg);
+void core_codelet_func_4(data_interface_t *descr, void *arg);
+void cublas_codelet_func_5(data_interface_t *descr, void *arg);
+void cublas_codelet_func_6(data_interface_t *descr, void *arg);
+void cublas_codelet_func_7(data_interface_t *descr, void *arg);
+void cublas_codelet_func_8(data_interface_t *descr, void *arg);
+void cublas_codelet_func_9(data_interface_t *descr, void *arg);
+
+void *iteration_cg(void *problem);
+
+void conjugate_gradient(float *nzvalA, float *vecb, float *vecx, uint32_t nnz,
+			unsigned nrow, uint32_t *colind, uint32_t *rowptr);
+
+#endif // __DW_SPARSE_CG_H__
