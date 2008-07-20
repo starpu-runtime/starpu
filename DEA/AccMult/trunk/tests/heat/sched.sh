256f9d13a8691943cb44a7fd50f415433ef460ea
Cédric Augonnet <cedric.augonnet@inria.fr>
2008-07-20 17:24:48 +0000

    some new test to measure the differences between scheduling policies

---
+++ DEA/AccMult/trunk/tests/heat/sched.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+
+maxiter=10
+
+DIR=$PWD
+ROOTDIR=$DIR/../..
+TIMINGDIR=$DIR/timings/
+mkdir -p $TIMINGDIR
+
+cd $ROOTDIR
+filename=$TIMINGDIR/sched.greedy.data
+rm -f $filename
+make clean 1> /dev/null 2> /dev/null
+make ATLAS=1 CPUS=4 1> /dev/null 2> /dev/null
+cd $DIR
+
+for iter in `seq 1 $maxiter`
+do
+	echo "$iter / $maxiter"
+	 val=`$ROOTDIR/examples/heat 2> /dev/null`
+	 echo "$val" >> $filename
+done
+
+
+
+cd $ROOTDIR
+filename=$TIMINGDIR/sched.greedy.noprio.data
+rm -f $filename
+make clean 1> /dev/null 2> /dev/null
+make ATLAS=1 CPUS=4 NO_PRIO=1 1> /dev/null 2> /dev/null
+cd $DIR
+
+for iter in `seq 1 $maxiter`
+do
+	echo "$iter / $maxiter"
+	 val=`$ROOTDIR/examples/heat 2> /dev/null`
+	 echo "$val" >> $filename
+done
+
+cd $ROOTDIR
+filename=$TIMINGDIR/sched.greedy.ws.data
+rm -f $filename
+make clean 1> /dev/null 2> /dev/null
+make ATLAS=1 CPUS=4 1> /dev/null 2> /dev/null
+cd $DIR
+
+for iter in `seq 1 $maxiter`
+do
+	echo "$iter / $maxiter"
+	 val=`SCHED=ws $ROOTDIR/examples/heat 2> /dev/null`
+	 echo "$val" >> $filename
+done
+
+
+
+cd $ROOTDIR
+filename=$TIMINGDIR/sched.greedy.noprio.ws.data
+rm -f $filename
+make clean 1> /dev/null 2> /dev/null
+make ATLAS=1 CPUS=4 NO_PRIO=1 1> /dev/null 2> /dev/null
+cd $DIR
+
+for iter in `seq 1 $maxiter`
+do
+	echo "$iter / $maxiter"
+	 val=`SCHED=ws $ROOTDIR/examples/heat 2> /dev/null`
+	 echo "$val" >> $filename
+done
