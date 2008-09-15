bf763e078e4efc2450922ff5705c4ef6f5bbd95a
Cédric Augonnet <cedric.augonnet@inria.fr>
2008-09-15 17:49:57 +0000

    the R script to measure the effect of perturbation is a little better

--- DEA/AccMult/trunk/tests/heat/perturbate.r
+++ DEA/AccMult/trunk/tests/heat/perturbate.r
@@ -23,12 +23,33 @@ parse <- function (size, ampl)
 	return(NULL);
 }
 
+parse_ref <- function (size)
+{
+	filename = paste("timings-sched/sched.greedy", size, sep=".");
+	
+	if (file.exists(filename))
+	{
+		ret <- scan(filename);
+		return(ret);
+	};
+
+	return(NULL);
+}
+
 handle_size <- function (size, ampl)
 {
 	gflops <- gflops(parse(size, ampl), size);
 
 	return(gflops);
 }
+
+handle_size_ref <- function (size)
+{
+	gflops <- gflops(parse_ref(size), size);
+
+	return(gflops);
+}
+
 
 
 handle_ampl <- function(ampl)
@@ -64,37 +85,62 @@ handle_ampl_mean <- function(ampl)
 	);
 }
 
+handle_ref_mean <- function()
+{
+	meantab <- NULL;
+	sizetab <- NULL;
+
+	for (size in sizelist) {
+		list <- mean(handle_size_ref(size));
+		meantab <- c(meantab, list);
+		sizetab <- c(sizetab, array(size, c(length(list))));
+	}
+
+	return(
+		data.frame(gflops=meantab, size=sizetab)
+#		meantab
+	);
+}
+
+
 trace_ampl <- function(ampl, color)
 {
 	#points(handle_ampl(ampl)$size, handle_ampl(ampl)$gflops, col=color);
-	lines(handle_ampl_mean(ampl)$size, handle_ampl_mean(ampl)$gflops, col=color);
+	lines(handle_ampl_mean(ampl)$size, handle_ampl_mean(ampl)$gflops, col=color, lwd= 2, lty=1);
+}
+
+trace_ref <- function(color)
+{
+	lines(handle_ref_mean()$size, handle_ref_mean()$gflops, col=color, lwd=3, lty=2);
 }
 
 display_ampl <- function()
 {
-#	xlist <- range(c(0,1));
 	xlist <- range(sizelist);
 	ylist <- range(c(0,80));
 
 	plot.new();
 	plot.window(xlist, ylist);
 
+	trace_ref("black");
+
 	trace_ampl("0.0", "orange");
 	trace_ampl("0.1", "green");
-	trace_ampl("0.2", "blue");
-	trace_ampl("0.3", "green");
 	trace_ampl("0.25", "red");
-	trace_ampl("0.4", "green");
 	trace_ampl("0.50", "blue");
-	trace_ampl("0.75", "black");
-	trace_ampl("0.95", "red");
 	trace_ampl("1.0", "orange");
+
+
+	labels <- c("greedy", "0 %", "10 %", "25 %", "50 %", "100 %")
+	legend("topleft", inset=.05, title="Perturbation", labels, lwd=2, lty=c(2, 1, 1, 1, 1, 1), col=c("black", "orange", "green", "red", "blue", "orange"))
 
 
 	axis(1, at=sizelist)
 	axis(2, at=seq(0, 100, 10), tck=1)
 #	axis(4, at=seq(0, 100, 10))
 	box(bty="u")
+
+
 
 	mtext("size", side=1, line=2, cex=0.8)
 	mtext("GFlops", side=2, line=2, las=0, cex=0.8)
