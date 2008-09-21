2c1ebfda782e73ed17f225978a640dbc7d87d878
Cédric Augonnet <cedric.augonnet@inria.fr>
2008-09-21 09:01:49 +0000

    actually generate the BCSR representation given the intermediate list of blocks

--- DEA/AccMult/trunk/examples/spmv/matrix-market/mm_to_bcsr.c
+++ DEA/AccMult/trunk/examples/spmv/matrix-market/mm_to_bcsr.c
@@ -1,10 +1,7 @@
-
+#include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include "mmio.h"
-
-
-
 
 /* convert a matrix stored in a file with the matrix market format into the 
  * BCSR format */
@@ -19,6 +16,18 @@ typedef struct tmp_block {
 	float *val;
 
 } tmp_block_t;
+
+typedef struct {
+	unsigned r,c;
+	unsigned nnz_blocks;
+	unsigned nrows_blocks;
+
+	float *val;
+	unsigned *colind;
+	unsigned *rowptr;
+} bcsr_t;
+
+/* Some debug functions */
 
 void print_block(tmp_block_t *block, unsigned r, unsigned c)
 {
@@ -42,8 +51,47 @@ void print_all_blocks(tmp_block_t *block_list, unsigned r, unsigned c)
 
 		current_block = current_block->next;
 	};
-
 }
+
+void print_bcsr(bcsr_t *bcsr)
+{
+	fprintf(stderr, "** BSCR **\n");
+	fprintf(stderr, "non zero - blocks = %d\n", bcsr->nnz_blocks);
+	fprintf(stderr, "nrows - blocks = %d\n", bcsr->nrows_blocks);
+	fprintf(stderr, "block size : c %d r %d\n", bcsr->c, bcsr->r);
+}
+
+unsigned count_blocks(tmp_block_t *block_list)
+{
+	unsigned count = 0;
+	tmp_block_t *current_block = block_list;
+
+	while(current_block) {
+		count++;
+		current_block = current_block->next;
+	};
+
+	return count;
+}
+
+unsigned count_row_blocks(tmp_block_t *block_list)
+{
+	unsigned maxrow = 0;
+	tmp_block_t *current_block = block_list;
+
+	while(current_block) {
+		if (current_block->j > maxrow)
+			maxrow = current_block->j;
+
+		current_block = current_block->next;
+	};
+
+	return (maxrow+1);
+}
+
+
+
+/* Find the block that corresponds to (i,j) if it exists in the list */
 
 tmp_block_t *search_block(tmp_block_t *block_list, unsigned i, unsigned j)
 {
@@ -71,10 +119,10 @@ tmp_block_t *create_block(unsigned c, unsigned r)
 	block = malloc(sizeof(tmp_block_t));
 	block->val = calloc(c*r, sizeof(float));
 
-
 	return block;
 }
 
+/* determine if next block is bigger in lexical order */
 unsigned next_block_is_bigger(tmp_block_t *block, unsigned i, unsigned j)
 {
 	tmp_block_t *next = block->next;
@@ -96,6 +144,7 @@ unsigned next_block_is_bigger(tmp_block_t *block, unsigned i, unsigned j)
 	return 1;
 }
 
+/* we insert a block in the list, directly at the appropriate place */
 void insert_block(tmp_block_t *block, tmp_block_t **block_list, unsigned i, unsigned j)
 {
 	///* insert block at the beginning of the list */
@@ -127,6 +176,7 @@ void insert_block(tmp_block_t *block, tmp_block_t **block_list, unsigned i, unsi
 	/* should not be reached ! */
 }
 
+/* we add an element to the list of blocks, it is either added to an existing block or in a block specifically created if there was none */
 void insert_elem(tmp_block_t **block_list, unsigned abs_i, unsigned abs_j, float val, unsigned c, unsigned r)
 {
 	/* we are looking for the block that contains (abs_i, abs_j) (abs = absolute) */
@@ -161,13 +211,10 @@ void insert_elem(tmp_block_t **block_list, unsigned abs_i, unsigned abs_j, float
 	local_index = local_j * c + local_i;
 	
 	block->val[local_index] = val;
-
-	printf("prout %p\n", *block_list);
 }
 
-
-/* matrix market to BCSR */
-tmp_block_t * mm_to_bcsr(int nz, unsigned *I, unsigned *J, float *val, unsigned c, unsigned r)
+/* transform a list of values (with coordinates) into a list of blocks that are easily processed into BCSR */
+tmp_block_t * mm_to_blocks(int nz, unsigned *I, unsigned *J, float *val, unsigned c, unsigned r)
 {
 	int elem;
 
@@ -182,6 +229,68 @@ tmp_block_t * mm_to_bcsr(int nz, unsigned *I, unsigned *J, float *val, unsigned 
 	return block_list;
 }
 
+void fill_bcsr(tmp_block_t *block_list, unsigned c, unsigned r, bcsr_t *bcsr)
+{
+	unsigned block = 0;
+	unsigned current_offset = 0;
+	size_t block_size = c*r*sizeof(float);
+
+	tmp_block_t *current_block = block_list;
+
+	while(current_block) {
+		/* copy the val from the block to the contiguous area in the BCSR */
+		memcpy(&bcsr->val[current_offset], current_block->val, block_size);
+
+		/* write the the index of the block 
+		 * XXX should it be in blocks ? */
+		bcsr->colind[block] = current_block->i;
+
+		if (bcsr->rowptr[current_block->j] == 0)
+		{
+			/* this is the first element of the line */
+			bcsr->rowptr[current_block->j] = current_offset;
+		}
+
+		block++;
+		current_offset = block*c*r;
+		current_block = current_block->next;
+	};
+
+	/* for all lines where there were no block at all (XXX), fill the 0 in rowptr */
+	/* the first row must start at 0 ? */
+	unsigned row;
+	for (row = 1; row < bcsr->nrows_blocks; row++)
+	{
+		if (bcsr->rowptr[row] == 0) 
+			bcsr->rowptr[row] = bcsr->rowptr[row-1];
+	}
+}
+
+bcsr_t * blocks_to_bcsr(tmp_block_t *block_list, unsigned c, unsigned r)
+{
+	unsigned nblocks;
+
+	//print_all_blocks(block_list, r, c);
+
+	nblocks = count_blocks(block_list);
+
+	bcsr_t *bcsr = malloc(sizeof(bcsr_t));
+
+	bcsr->nnz_blocks = nblocks;
+	bcsr->r = r;
+	bcsr->c = c;
+	
+	unsigned nrows_blocks = count_row_blocks(block_list);
+	bcsr->nrows_blocks = nrows_blocks;
+
+	bcsr->val = malloc(nblocks*r*c*sizeof(float));
+	bcsr->colind = malloc(nblocks*sizeof(unsigned));
+	bcsr->rowptr = calloc((nrows_blocks + 1), sizeof(unsigned));
+
+	fill_bcsr(block_list, c, r, bcsr);
+
+	return bcsr;
+}
 
 int main(int argc, char *argv[])
 {
@@ -252,13 +361,22 @@ int main(int argc, char *argv[])
    // for (i=0; i<nz; i++)
    //     fprintf(stdout, "%d %d %20.19g\n", I[i]+1, J[i]+1, val[i]);
 
-	unsigned r = 4;
-	unsigned c = 6;
+	unsigned r = 128;
+	unsigned c = 128;
+	
+   printf("nz = %d \n", nz);
 
    tmp_block_t *block_list;
-   block_list = mm_to_bcsr(nz, I, J, val, c, r);
+   block_list = mm_to_blocks(nz, I, J, val, c, r);
 
-   print_all_blocks(block_list, r, c);
+   free(I);
+   free(J);
+   free(val);
+
+   bcsr_t *bcsr;
+   bcsr = blocks_to_bcsr(block_list, c, r);
+
+   print_bcsr(bcsr);
 
 	return 0;
 }
