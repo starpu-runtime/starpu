b1c27710f8a6b5605daa643d259a5a164e918bec
Cédric Augonnet <cedric.augonnet@inria.fr>
2009-01-11 23:46:17 +0000

    The gordon driver starts to be a little more asynchronous as it is possible to submit different tasks at the same time. There still exists an issue if the driver is blocked on a fetch_input if the data is to be released by the gordon driver itself.

--- drivers/gordon/driver_gordon.c
+++ drivers/gordon/driver_gordon.c
@@ -1,27 +1,44 @@
 #include <pthread.h>
+#include <semaphore.h>
 #include "driver_gordon.h"
 #include "gordon_interface.h"
 #include <core/policies/sched_policy.h>
 
 pthread_t progress_thread;
-unsigned progress_thread_is_ready;
+sem_t progress_sem;
 
-char hellobuf[128];
+struct gordon_task_wrapper_s {
+	/* who has executed that ? */
+	struct worker_s *worker;
+
+	/* for now, we only have a single job at a time */
+	job_t j;	/* StarPU */
+	struct gordon_ppu_job_s *gordon_job; /* gordon*/
+};
+
 
 void *gordon_worker_progress(void *arg)
 {
 	fprintf(stderr, "gordon_worker_progress\n");
 
-	progress_thread_is_ready = 1;
+	sem_post(&progress_sem);
 
 	while (1) {
 		/* the Gordon runtime needs to make sure that we poll it 
 		 * so that we handle jobs that are done */
 
-		/* wait for one task termination */
-		gordon_wait(1);
+		gordon_poll();
+		wake_all_blocked_workers();
 
-		/* possibly wake the thread that injects work */
+//		/* wait for one task termination */
+//		int ret = gordon_wait(1);
+//		if (ret)
+//		{
+////			fprintf(stderr, "1 task completed ret = %d !\n", ret);
+//			wake_all_blocked_workers();
+//		}
+//
+//		/* possibly wake the thread that injects work */
 		// TODO
 	}
 
@@ -98,10 +115,14 @@ static void starpu_to_gordon_buffers(job_t j, struct gordon_ppu_job_s *gordon_jo
 
 /* we assume the data are already available so that the data interface fields are 
  * already filled */
-static struct gordon_ppu_job_s *starpu_to_gordon_job(job_t j)
+static struct gordon_task_wrapper_s *starpu_to_gordon_job(job_t j)
 {
-
 	struct gordon_ppu_job_s *gordon_job = gordon_alloc_jobs(1, 0);
+	struct gordon_task_wrapper_s *task_wrapper =
+				malloc(sizeof(struct gordon_task_wrapper_s));
+
+	task_wrapper->gordon_job = gordon_job;
+	task_wrapper->j = j;
 
 	gordon_job->index = j->cl->gordon_func;
 
@@ -109,21 +130,62 @@ static struct gordon_ppu_job_s *starpu_to_gordon_job(job_t j)
 	unsigned memory_node = 0;
 	starpu_to_gordon_buffers(j, gordon_job, memory_node);
 
-	return gordon_job;
+	return task_wrapper;
+}
+
+void handle_terminated_job(job_t j)
+{
+	push_codelet_output(j->buffers, j->nbuffers, 0);
+	handle_job_termination(j);
+	job_delete(j);
+	wake_all_blocked_workers();
+}
+
+void handle_terminated_job_per_worker(struct worker_s *worker)
+{
+
+	if (!worker->worker_is_running)
+		return;
+
+//	fprintf(stderr, " handle_terminated_job_per_worker worker %p worker->terminated_jobs %p \n", worker, worker->terminated_jobs);
+
+	while (!job_list_empty(worker->terminated_jobs))
+	{
+		job_t j;
+		j = job_list_pop_front(worker->terminated_jobs);
+//		fprintf(stderr, "handle_terminated_job %p\n", j);
+		handle_terminated_job(j);
+	}
+}
+
+static void handle_terminated_jobs(struct worker_set_s *arg)
+{
+//	fprintf(stderr, "handle_terminated_jobs\n");
+
+	/* terminate all the pending jobs and remove 
+ 	 * them from the terminated_jobs lists */
+	unsigned spu;
+	for (spu = 0; spu < arg->nworkers; spu++)
+	{
+		handle_terminated_job_per_worker(&arg->workers[spu]);
+	}
 }
 
 static void gordon_callback_func(void *arg)
 {
-	struct job_s *j = arg; 
+	struct gordon_task_wrapper_s *task_wrapper = arg; 
 
-	push_codelet_output(j->buffers, j->nbuffers, 0);
+	/* we don't know who will execute that codelet : so we actually defer the
+ 	 * execution of the StarPU codelet and the job termination later */
+	struct worker_s *worker = task_wrapper->worker;
+	STARPU_ASSERT(worker);
 
-	handle_job_termination(j);
-
-	job_delete(j);
+//	fprintf(stderr, "gordon callback : push job j %p\n", task_wrapper->j);
+	job_list_push_back(worker->terminated_jobs, task_wrapper->j);
+	wake_all_blocked_workers();
 }
 
-int inject_task(job_t j)
+int inject_task(job_t j, struct worker_s *worker)
 {
 	int ret = fetch_codelet_input(j->buffers, j->interface, j->nbuffers, 0);
 
@@ -133,8 +195,11 @@ int inject_task(job_t j)
 		return TRYAGAIN;
 	}
 
-	struct gordon_ppu_job_s *gordon_job = starpu_to_gordon_job(j);
-	gordon_pushjob(&gordon_job[0], gordon_callback_func, j);
+	struct gordon_task_wrapper_s *task_wrapper = starpu_to_gordon_job(j);
+
+	task_wrapper->worker = worker;
+
+	gordon_pushjob(task_wrapper->gordon_job, gordon_callback_func, task_wrapper);
 
 	return 0;
 }
@@ -144,9 +209,11 @@ void *gordon_worker_inject(struct worker_set_s *arg)
 	job_t j;
 
 	while(machine_is_running()) {
+		/* make gordon driver progress */
+		handle_terminated_jobs(arg);
+
 		if (gordon_busy_enough()) {
 			/* gordon already has enough work, wait a little TODO */
-			//gordon_wait(1);
 		}
 		else {
 			/* do some progression */
@@ -154,12 +221,12 @@ void *gordon_worker_inject(struct worker_set_s *arg)
 
 			/* gordon should accept a little more work */
 			j =  pop_task();
-			fprintf(stderr, "pop task %p\n", j);
+	//		fprintf(stderr, "pop task %p\n", j);
 			if (j) {
 				if (GORDON_MAY_PERFORM(j)) {
-					/* inject that task TODO */
-					inject_task(j);
-					gordon_wait(1);
+					/* inject that task */
+					/* XXX we hardcore &arg->workers[0] for now */
+					inject_task(j, &arg->workers[0]);
 				}
 				else {
 					push_task(j);
@@ -185,21 +252,23 @@ void *gordon_worker(void *arg)
  	 * gordon is progressing (and performs the callbacks).
 	 */
 
-//	/* launch the progression thread */
-//	progress_thread_is_ready = 0;
-//	pthread_create(&progress_thread, NULL, gordon_worker_progress, gordon_set_arg);
-//
-//	/* wait for the progression thread to be ready */
-//	while(!progress_thread_is_ready) {}
-//
-	//fprintf(stderr, "progress thread is running ... \n");
+	/* launch the progression thread */
+	sem_init(&progress_sem, 0, 0);
+	pthread_create(&progress_thread, NULL, gordon_worker_progress, gordon_set_arg);
+
+	/* wait for the progression thread to be ready */
+	sem_wait(&progress_sem);
+
+	fprintf(stderr, "progress thread is running ... \n");
 	
 	/* tell the core that gordon is ready */
 	sem_post(&gordon_set_arg->ready_sem);
 
 	gordon_worker_inject(gordon_set_arg);
 
+	fprintf(stderr, "gordon deinit...\n");
 	gordon_deinit();
+	fprintf(stderr, "gordon was deinited\n");
 
-	return NULL;
+	pthread_exit((void *)0x42);
 }
