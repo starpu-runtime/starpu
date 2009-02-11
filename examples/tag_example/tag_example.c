c2ac9ad5cff03f4efb9f914e932da1a28b03c058
Cédric Augonnet <cedric.augonnet@inria.fr>
2009-02-11 14:36:19 +0000

    make the tag example stress starpu a little more

--- examples/tag_example/tag_example.c
+++ examples/tag_example/tag_example.c
@@ -10,70 +10,175 @@
 #include <signal.h>
 #include <core/dependencies/tags.h>
 
-void callback_core(void *argcb __attribute__ ((unused)))
+#define TAG(i, j, iter)	((uint64_t) ( ((iter)*262144) |  ((j)*8192) | (i)) )
+
+sem_t sem;
+codelet cl;
+
+#define Ni	1024
+#define Nj	32
+#define Nk	512
+
+typedef struct coord_s {
+	int i;
+	int j;
+} coord_t;
+
+//coord_t coords[Ni][Nj];
+
+static unsigned callback_cnt;
+static unsigned iter = 0;
+
+void callback_core(void *argcb);
+static void express_deps(unsigned nj, unsigned i, unsigned j, unsigned iter);
+
+static void tag_cleanup_grid(unsigned ni, unsigned nj, unsigned iter)
 {
-	printf("callback core\n");
+	unsigned i,j;
+
+	for (j = 0; j < nj; j++)
+	for (i = 0; i < ni; i++)
+	{
+		tag_remove(TAG(i,j,iter));
+	}
+
+
+} 
+
+static void create_task_grid(unsigned ni, unsigned nj, unsigned iter)
+{
+	unsigned i, j;
+
+	fprintf(stderr, "start iter %d...\n", iter);
+
+	callback_cnt = (Ni*Nj);
+
+	/* create non-entry tasks */
+	for (j = 0; j < nj; j++)
+//	for (i = ni -1; i > 0; i--)
+	for (i = 1; i < ni; i++)
+	{
+		//coords[i][j].i = i;
+		//coords[i][j].j = j;
+
+		/* create a new task */
+		job_t jb = job_create();
+		jb->where = CORE;
+		jb->cb = callback_core;
+		//jb->argcb = &coords[i][j];
+		jb->cl = &cl;
+
+		tag_declare(TAG(i,j, iter), jb);
+//		fprintf(stderr, "job %p TAG %lx\n", jb, TAG(i,j));
+
+		/* express deps : (i,j) depends on (i-1, j-1) & (i-1, j+1) */		
+		if (i == 0) {
+			/* this is an entry task */
+			push_task(jb);
+		}
+		else {
+			express_deps(nj, i, j, iter);
+		}
+	}
+
+	/* create entry tasks */
+	for (j = 0; j < nj; j++)
+	{
+		//coords[0][j].i = 0;
+		//coords[0][j].j = j;
+
+		/* create a new task */
+		job_t jb = job_create();
+		jb->where = CORE;
+		jb->cb = callback_core;
+		//jb->argcb = &coords[0][j];
+		jb->cl = &cl;
+
+		tag_declare(TAG(0,j, iter), jb);
+//		fprintf(stderr, "job %p TAG %lx\n", jb, TAG(i,j));
+
+		/* this is an entry task */
+		push_task(jb);
+	}
+
 }
 
-void last_callback_core(void *argcb __attribute__ ((unused)))
+
+void callback_core(void *argcb)
 {
-	printf("callback core\n");
+//	struct coord_s *c = argcb;
+	unsigned newcnt = ATOMIC_ADD(&callback_cnt, -1);	
+
+//	printf("callback core %d (i = %d, j = %d)\n", newcnt, c->i, c->j);
+	if (newcnt == 0)
+	{
+		
+		iter++;
+		if (iter < Nk)
+		{
+			/* cleanup old grids ... */
+			if (iter > 2)
+				tag_cleanup_grid(Ni, Nj, iter-2);
+
+			/* create a new iteration */
+			create_task_grid(Ni, Nj, iter);
+		}
+		else {
+			sem_post(&sem);
+		}
+	}
 }
 
 void core_codelet(void *_args __attribute__ ((unused)))
 {
-	printf("codelet core\n");
+//	printf("execute task\n");
 }
 
-
+static void express_deps(unsigned nj, unsigned i, unsigned j, unsigned iter)
+{
+	if (j > 0) {
+		/* (i,j-1) exists */
+		if (j < nj - 1)
+		{
+			/* (i,j+1) exists */
+			tag_declare_deps(TAG(i,j,iter), 2, TAG(i-1,j-1,iter), TAG(i-1,j+1,iter));
+		}
+		else 
+		{
+			/* (i,j+1) does not exist */
+			tag_declare_deps(TAG(i,j,iter), 1, TAG(i-1,j-1,iter));
+		}
+	}
+	else {
+		/* (i, (j-1) does not exist */
+		if (j < nj - 1)
+		{
+			/* (i,j+1) exists */
+			tag_declare_deps(TAG(i,j,iter), 1, TAG(i-1,j+1,iter));
+		}
+		else 
+		{
+			/* (i,j+1) does not exist */
+			STARPU_ASSERT(0);
+		}
+	}
+}
 
 int main(int argc __attribute__((unused)) , char **argv __attribute__((unused)))
 {
 	init_machine();
 
-	codelet cl;
-	codelet cl2;
-	codelet cl3;
-
-	job_t j = job_create();
-	j->where = ANY;
-	j->cb = callback_core;
-	j->cl = &cl;
-
-	job_t j2 = job_create();
-	j2->where = CORE;
-	j2->cb = callback_core;
-	j2->cl = &cl2;
-
-	job_t j3 = job_create();
-	j3->type = CODELET;
-	j3->where = CORE;
-	j3->cb = last_callback_core;
-	j3->cl = &cl3;
-
-
-
 	cl.cl_arg = NULL;
 	cl.core_func = core_codelet;
+	cl.cublas_func = core_codelet;
 
-	cl2.cl_arg = NULL;
-	cl2.core_func = core_codelet;
+	sem_init(&sem, 0, 0);
 
-	cl3.cl_arg = NULL;
-	cl3.core_func = core_codelet;
+	create_task_grid(Ni, Nj, 0);
 
-	tag_declare(42, j);
-	tag_declare(1664, j2);
-	tag_declare(10000000, j3);
+	sem_wait(&sem);
 
-	tag_declare_deps(1664, 1, 42);
-	tag_declare_deps(10000000, 2, 42, 1664);
-
-	push_task_sync(j);
-
-	tag_remove(10000000);
-	tag_remove(1664);
-	tag_remove(42);
+	fprintf(stderr, "TEST DONE ...\n");
 
 	return 0;
 }
