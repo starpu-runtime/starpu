9b251b3205b2ec5cd8868a91ba868faec1da76c1
Cédric Augonnet <cedric.augonnet@inria.fr>
2009-03-31 14:32:54 +0000

    StarPU is now licensed under LGPL.

--- tests/heat/model-perturbation.sh
+++ tests/heat/model-perturbation.sh
@@ -1,5 +1,22 @@
 #!/bin/bash
 
+#
+# StarPU
+# Copyright (C) INRIA 2008-2009 (see AUTHORS file)
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation; either version 2.1 of the License, or (at
+# your option) any later version.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+#
+# See the GNU Lesser General Public License in COPYING.LGPL for more details.
+#
+
+
 #ampllist="0.0 0.1 0.2 0.25 0.3 0.4 0.50 0.6 0.7 0.75 0.8 0.9 0.95 1.0 "
 #ampllist="1.0 0.50 0.25 0.1 0.0"
 ampllist="1.0 0.5"
