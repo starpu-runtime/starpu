9c58b47408dfaab19e30add31af0ac8b354942ce
Cédric Augonnet <cedric.augonnet@inria.fr>
2009-04-12 14:19:30 +0000

    add missing headers

--- src/common/htable32.c
+++ src/common/htable32.c
@@ -14,6 +14,8 @@
  * See the GNU Lesser General Public License in COPYING.LGPL for more details.
  */
 
+#include <starpu.h>
+#include <common/config.h>
 #include <common/htable32.h>
 #include <stdint.h>
 #include <string.h>
