626b22ba6f358bd360ea3cf8080164505834aa34
Cédric Augonnet <cedric.augonnet@inria.fr>
2009-04-27 23:04:53 +0000

    introduce a new example "downscaler", no real stuff for the moment

---
+++ examples/ppm-downscaler/ppm-downscaler.h
@@ -0,0 +1,29 @@
+/*
+ * StarPU
+ * Copyright (C) INRIA 2008-2009 (see AUTHORS file)
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 2.1 of the License, or (at
+ * your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ * See the GNU Lesser General Public License in COPYING.LGPL for more details.
+ */
+
+/* we make the asumption that there are 256 color levels at most */
+struct ppm_color {
+	unsigned char r;
+	unsigned char g;
+	unsigned char b;
+};
+
+struct ppm_image {
+	int nlines;
+	int ncols;
+	int coldepth;
+	struct ppm_color *data;
+};
