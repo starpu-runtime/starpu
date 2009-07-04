5cdd7aec2f690f0c5042b0be1a13b7175209bc52
Cédric Augonnet <cedric.augonnet@inria.fr>
2009-07-04 12:09:27 +0000

    (re)implement a spinlock in case pthread_spinlock_t is not available.

---
+++ src/common/starpu-spinlock.c
@@ -0,0 +1,72 @@
+/*
+ * StarPU
+ * Copyright (C) INRIA 2008-2009 (see AUTHORS file)
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 2.1 of the License, or (at
+ * your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ * See the GNU Lesser General Public License in COPYING.LGPL for more details.
+ */
+
+#include <common/starpu-spinlock.h>
+#include <common/config.h>
+
+int starpu_spin_init(starpu_spinlock_t *lock)
+{
+#ifdef HAVE_PTHREAD_SPIN_LOCK
+	return pthread_spin_init(&lock->lock, 0);
+#else
+	lock->taken = 0;
+	return 0;
+#endif
+}
+
+int starpu_spin_destroy(starpu_spinlock_t *lock)
+{
+#ifdef HAVE_PTHREAD_SPIN_LOCK
+	return pthread_spin_destroy(&lock->lock);
+#else
+	/* we don't do anything */
+	return 0;
+#endif
+}
+
+int starpu_spin_lock(starpu_spinlock_t *lock)
+{
+#ifdef HAVE_PTHREAD_SPIN_LOCK
+	return pthread_spin_lock(&lock->lock);
+#else
+	uint32_t prev;
+	do {
+		prev = __sync_lock_test_and_set(&lock->taken, 1);
+	} while (prev);
+	return 0;
+#endif
+}
+
+int starpu_spin_trylock(starpu_spinlock_t *lock)
+{
+#ifdef HAVE_PTHREAD_SPIN_LOCK
+	return pthread_spin_trylock(&lock->lock);
+#else
+	uint32_t prev;
+	prev = __sync_lock_test_and_set(&lock->taken, 1);
+	return (prev == 0)?0:EBUSY;
+#endif
+}
+
+int starpu_spin_unlock(starpu_spinlock_t *lock)
+{
+#ifdef HAVE_PTHREAD_SPIN_LOCK
+	return pthread_spin_unlock(&lock->lock);
+#else
+	lock->taken = 0;
+	return 0;
+#endif
+}
---
+++ src/common/starpu-spinlock.h
@@ -0,0 +1,40 @@
+/*
+ * StarPU
+ * Copyright (C) INRIA 2008-2009 (see AUTHORS file)
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 2.1 of the License, or (at
+ * your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ * See the GNU Lesser General Public License in COPYING.LGPL for more details.
+ */
+#ifndef __STARPU_SPINLOCK_H__
+#define __STARPU_SPINLOCK_H__
+
+#include <errno.h>
+#include <stdint.h>
+#include <pthread.h>
+#include <common/config.h>
+
+typedef struct starpu_spinlock_s {
+#ifdef HAVE_PTHREAD_SPIN_LOCK
+	pthread_spinlock_t lock;
+#else
+	/* we only have a trivial implementation yet ! */
+	volatile uint32_t taken __attribute__ ((aligned(16)));
+#endif
+} starpu_spinlock_t;
+
+int starpu_spin_init(starpu_spinlock_t *lock);
+int starpu_spin_destroy(starpu_spinlock_t *lock);
+
+int starpu_spin_lock(starpu_spinlock_t *lock);
+int starpu_spin_trylock(starpu_spinlock_t *lock);
+int starpu_spin_unlock(starpu_spinlock_t *lock);
+
+#endif // __STARPU_SPINLOCK_H__
