60d2b403d5bb98d5578743aac168244b9a037bd0
Cédric Augonnet <cedric.augonnet@inria.fr>
2009-09-06 10:32:28 +0000

    forgot this declaration in a previous commit

--- src/datawizard/coherency.h
+++ src/datawizard/coherency.h
@@ -154,5 +154,6 @@ inline void set_data_requested_flag_if_needed(data_state *state, uint32_t node);
 int prefetch_task_input_on_node(struct starpu_task *task, uint32_t node);
 
 uint32_t select_node_to_handle_request(uint32_t src_node, uint32_t dst_node);
+uint32_t select_src_node(data_state *state);
 
 #endif // __COHERENCY__H__
