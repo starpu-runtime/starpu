82e7cee3653b669ebdf18b458219b20738fbe002
Cédric Augonnet <cedric.augonnet@inria.fr>
2009-10-01 16:03:43 +0000

    improve the gnuplot output (as found in TPDS)

--- tests/heat/dmda_gnuplot.sh
+++ tests/heat/dmda_gnuplot.sh
@@ -11,12 +11,11 @@ set output "dmda.eps"
 set grid y
 set grid x
 #set logscale x
-set xtics 4096,4096,30760
 set size 0.65
 
 set xlabel "Matrix size"
 
-set xtics 4096,4096,30760
+set xtics 0,8192,49152
 
 set size 0.7,0.8
 
@@ -24,19 +23,19 @@ set multiplot
 
 set size 0.7,0.4
 set origin 0.0,0.0
-set ylabel "Cache miss ratio"
+set ylabel "Data accesses requiring\n a memory transfer"
 set format y "%.0f %%"
-set key box right top title "Penality for non-local accesses"
-plot "$outputfile" usi 1:2 with linespoint lt rgb "black"  lw 4 title "with" ,\
-     "$outputfile" usi 1:3 with linespoint lt rgb "black"  lw 4 title "without"
+set key box right top title "Policy"
+plot "$outputfile" usi 1:2 with linespoint lt rgb "black"  lw 4 title "heft-tm-pr" ,\
+     "$outputfile" usi 1:3 with linespoint lt rgb "black"  lw 4 title "heft-tmdp-pr"
 
 set size 0.7,0.4
 set origin 0.0,0.4
-set ylabel "Avg. activity on bus (GB/s)"
+set ylabel "Avg. activity on bus (GB/s)\n"
 set format y "%.1f"
-set key  box right bottom title "Penality for non-local accesses"
-plot "$outputfile" usi 1:((\$4/\$6)) with linespoint lt rgb "black" lw 4 title "with",\
-     "$outputfile" usi 1:((\$5/\$7)) with linespoint lt rgb "black"  lw 4 title "without"
+set key  box right bottom title "Policy"
+plot "$outputfile" usi 1:((\$4/\$6)) with linespoint lt rgb "black" lw 4 title "heft-tm-pr",\
+     "$outputfile" usi 1:((\$5/\$7)) with linespoint lt rgb "black"  lw 4 title "heft-tmdp-pr"
 
 unset multiplot
 
