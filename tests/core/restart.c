8d7aa3fcba6970e48b9fdd75b8bf95c5ef3e2fa3
Cédric Augonnet <cedric.augonnet@inria.fr>
2009-10-02 13:28:37 +0000

    make sure that it is possible to initialize and to stop StarPU multiple times

---
+++ tests/core/restart.c
@@ -0,0 +1,36 @@
+/*
+ * StarPU
+ * Copyright (C) INRIA 2008-2009 (see AUTHORS file)
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 2.1 of the License, or (at
+ * your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ * See the GNU Lesser General Public License in COPYING.LGPL for more details.
+ */
+
+#include <stdio.h>
+#include <unistd.h>
+#include <errno.h>
+#include <starpu.h>
+#include <stdlib.h>
+
+#define N	16
+
+int main(int argc, char **argv)
+{
+	unsigned iter;
+
+	for (iter = 0; iter < N; iter++)
+	{
+		starpu_init(NULL);
+		starpu_shutdown();
+	}
+
+	return 0;
+}
