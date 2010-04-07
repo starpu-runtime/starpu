5218cd26394053386b6bef72cb41ce2e0a3852bc
Nathalie Furmento <nathalie.furmento@labri.fr>
2010-04-07 13:22:09 +0000

    StarPU: Use character _ instead of - in filenames

--- examples/ppm-downscaler/ppm_downscaler.c
+++ examples/ppm-downscaler/ppm_downscaler.c
@@ -14,7 +14,7 @@
  * See the GNU Lesser General Public License in COPYING.LGPL for more details.
  */
 
-#include "ppm-downscaler.h"
+#include "ppm_downscaler.h"
 
 #include <starpu.h>
 
--- examples/ppm-downscaler/yuv_downscaler.c
+++ examples/ppm-downscaler/yuv_downscaler.c
@@ -23,7 +23,7 @@
 #include <assert.h>
 #include <stdio.h>
 
-#include "yuv-downscaler.h"
+#include "yuv_downscaler.h"
 
 struct timeval start;
 struct timeval end;
@@ -40,8 +40,8 @@ void parse_args(int argc, char **argv)
 		strcpy(filename_out, argv[2]);
 	}
 	else {
-		sprintf(filename_in, "%s/examples/ppm-downscaler/%s", STARPU_DIR, filename_in_default);
-		sprintf(filename_out, "%s/examples/ppm-downscaler/%s", STARPU_DIR, filename_out_default);
+		sprintf(filename_in, "%s/examples/ppm_downscaler/%s", STARPU_DIR, filename_in_default);
+		sprintf(filename_out, "%s/examples/ppm_downscaler/%s", STARPU_DIR, filename_out_default);
 	}
 }
 
