d954eafd0761550137d07bdb80170f307e728d98
Cédric Augonnet <cedric.augonnet@inria.fr>
2010-04-26 11:47:24 +0000

    Add missing header

---
+++ src/core/dependencies/implicit_data_deps.h
@@ -0,0 +1,26 @@
+/*
+ * StarPU
+ * Copyright (C) INRIA 2008-2009 (see AUTHORS file)
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 2.1 of the License, or (at
+ * your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ * See the GNU Lesser General Public License in COPYING.LGPL for more details.
+ */
+
+#ifndef __IMPLICIT_DATA_DEPS_H__
+#define __IMPLICIT_DATA_DEPS_H__
+
+#include <starpu.h>
+#include <common/config.h>
+
+void _starpu_detect_implicit_data_deps(struct starpu_task *task);
+
+#endif // __IMPLICIT_DATA_DEPS_H__
+
