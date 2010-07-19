41cbb6182c139460850563f600b7481851eecbd9
Nathalie Furmento <nathalie.furmento@labri.fr>
2010-07-19 09:56:13 +0000

    Filter example: fix copyright

--- examples/filters/fblock_cuda.cu
+++ examples/filters/fblock_cuda.cu
@@ -1,6 +1,6 @@
 /*
  * StarPU
- * Copyright (C) INRIA 2008-2009 (see AUTHORS file)
+ * Copyright (C) INRIA 2008-2010 (see AUTHORS file)
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
@@ -12,10 +12,6 @@
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
  *
  * See the GNU Lesser General Public License in COPYING.LGPL for more details.
- */
-
-/*
- * This example complements vector_scale.c: here we implement a CUDA version.
  */
 
 #include <starpu.h>
