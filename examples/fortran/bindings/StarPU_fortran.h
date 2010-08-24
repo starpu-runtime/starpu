952534293d29965100dc27787f2f9c11e2d4fb9f
Cédric Augonnet <cedric.augonnet@inria.fr>
2010-08-24 20:54:16 +0000

    Update copyright date

--- examples/fortran/bindings/StarPU_fortran.h
+++ examples/fortran/bindings/StarPU_fortran.h
@@ -1,6 +1,6 @@
 C
 C StarPU
-C Copyright (C) Université Bordeaux 1, CNRS 2008-2009 (see AUTHORS file)
+C Copyright (C) Université Bordeaux 1, CNRS 2008-2010 (see AUTHORS file)
 C
 C This program is free software; you can redistribute it and/or modify
 C it under the terms of the GNU Lesser General Public License as published by
