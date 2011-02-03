0ac31350d7e3e3eed9227dc579aacae83ecbc6ab
Samuel Thibault <samuel.thibault@labri.fr>
2011-02-03 13:04:33 +0000

    Correction copyright header fortran

--- examples/fortran/bindings/StarPU_fortran.h
+++ examples/fortran/bindings/StarPU_fortran.h
@@ -1,29 +1,14 @@
-/* StarPU --- Runtime system for heterogeneous multicore architectures.
- *
- * Copyright (C) 2010  Université de Bordeaux 1
- * Copyright (C) 2010  Centre National de la Recherche Scientifique
- *
- * StarPU is free software; you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as published by
- * the Free Software Foundation; either version 2.1 of the License, or (at
- * your option) any later version.
- *
- * StarPU is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
- *
- * See the GNU Lesser General Public License in COPYING.LGPL for more details.
- */
+C StarPU --- Runtime system for heterogeneous multicore architectures.
 C
-C StarPU
-C Copyright (C) Université Bordeaux 1, CNRS 2008-2010 (see AUTHORS file)
+C Copyright (C) 2010  Université de Bordeaux 1
+C Copyright (C) 2010  Centre National de la Recherche Scientifique
 C
-C This program is free software; you can redistribute it and/or modify
+C StarPU is free software; you can redistribute it and/or modify
 C it under the terms of the GNU Lesser General Public License as published by
 C the Free Software Foundation; either version 2.1 of the License, or (at
 C your option) any later version.
 C
-C This program is distributed in the hope that it will be useful, but
+C StarPU is distributed in the hope that it will be useful, but
 C WITHOUT ANY WARRANTY; without even the implied warranty of
 C MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
 C
