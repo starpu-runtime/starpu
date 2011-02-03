a59c85d82ab186a3bb329c65ba9d67d22ee5aa0b
Cédric Augonnet <cedric.augonnet@inria.fr>
2011-02-03 17:33:53 +0000

    The parallel greedy ("pgreedy") scheduling policy is a variation of the greedy strategy that aggregates CPUs so that they can execute parallel tasks. Note that if 8 CPUs are aggregated, and that we have a task using 4 CPUs, the 4 remaining CPUs are idle.

--- src/core/sched_policy.c
+++ src/core/sched_policy.c
@@ -46,8 +46,9 @@ extern struct starpu_sched_policy_s _starpu_sched_dmda_ready_policy;
 extern struct starpu_sched_policy_s _starpu_sched_dmda_sorted_policy;
 extern struct starpu_sched_policy_s _starpu_sched_eager_policy;
 extern struct starpu_sched_policy_s _starpu_sched_parallel_heft_policy;
+extern struct starpu_sched_policy_s _starpu_sched_pgreedy_policy;
 
-#define NPREDEFINED_POLICIES	10
+#define NPREDEFINED_POLICIES	11
 
 static struct starpu_sched_policy_s *predefined_policies[NPREDEFINED_POLICIES] = {
 	&_starpu_sched_ws_policy,
@@ -59,7 +60,8 @@ static struct starpu_sched_policy_s *predefined_policies[NPREDEFINED_POLICIES] =
 	&_starpu_sched_dmda_sorted_policy,
 	&_starpu_sched_random_policy,
 	&_starpu_sched_eager_policy,
-	&_starpu_sched_parallel_heft_policy
+	&_starpu_sched_parallel_heft_policy,
+	&_starpu_sched_pgreedy_policy
 };
 
 struct starpu_sched_policy_s *_starpu_get_sched_policy(void)
