c7cf97c9607211b5e596b0752950ac04062381b9
Nathalie Furmento <nathalie.furmento@labri.fr>
2011-02-14 10:20:00 +0000

    mpi/tests/insert_task_owner2.c: update test to show how to get copies of values modified by MPI which were registered within StarPU with -1 and NULL

--- mpi/tests/insert_task_owner2.c
+++ mpi/tests/insert_task_owner2.c
@@ -15,6 +15,7 @@
  */
 
 #include <starpu_mpi.h>
+#include <starpu_mpi_datatype.h>
 #include <math.h>
 
 void func_cpu(void *descr[], __attribute__ ((unused)) void *_args)
@@ -30,9 +31,10 @@ void func_cpu(void *descr[], __attribute__ ((unused)) void *_args)
 //        *y = 144;
 //
         fprintf(stderr, "-------> CODELET VALUES: %d %d %d %d\n", *x0, *x1, *x2, *y);
-        *y = (*x0 + *x1) * -100;
+        *y = (*x0 + *x1) * 100;
         *x1 = 12;
         *x2 = 24;
+        *x0 = 36;
         fprintf(stderr, "-------> CODELET VALUES: %d %d %d %d\n", *x0, *x1, *x2, *y);
 }
 
@@ -52,27 +54,33 @@ int main(int argc, char **argv)
 	starpu_init(NULL);
 	starpu_mpi_initialize_extended(1, &rank, &size);
 
+        if (rank > 1) {
+                starpu_mpi_shutdown();
+                starpu_shutdown();
+                return 0;
+        }
+
         if (rank == 0) {
                 for(i=0 ; i<3 ; i++) {
-                        x[i] = -10*(i+1);
+                        x[i] = 10*(i+1);
                         starpu_variable_data_register(&data_handles[i], 0, (uintptr_t)&x[i], sizeof(x[i]));
                         starpu_data_set_rank(data_handles[i], rank);
                 }
-                y = -40;
+                y = -1;
                 starpu_variable_data_register(&data_handles[3], -1, (uintptr_t)NULL, sizeof(int));
                 starpu_data_set_rank(data_handles[3], 1);
         }
         else if (rank == 1) {
                 for(i=0 ; i<3 ; i++) {
-                        x[i] = -20;
+                        x[i] = -1;
                         starpu_variable_data_register(&data_handles[i], -1, (uintptr_t)NULL, sizeof(int));
                         starpu_data_set_rank(data_handles[i], 0);
                 }
-                y=-20;
+                y=200;
                 starpu_variable_data_register(&data_handles[3], 0, (uintptr_t)&y, sizeof(int));
                 starpu_data_set_rank(data_handles[3], rank);
         }
-        fprintf(stderr, "[%d] VALUES: %d %d %d %d\n", rank, x[0], x[1], x[2], y);
+        fprintf(stderr, "[%d][init] VALUES: %d %d %d %d\n", rank, x[0], x[1], x[2], y);
 
         err = starpu_mpi_insert_task(MPI_COMM_WORLD, &mycodelet,
                                      STARPU_R, data_handles[0], STARPU_RW, data_handles[1],
@@ -82,15 +90,14 @@ int main(int argc, char **argv)
         assert(err == 0);
         starpu_task_wait_for_all();
 
-        if (rank == 0) {
-                for(i=0 ; i<3 ; i++) {
-                        starpu_data_acquire(data_handles[i], STARPU_R);
-                }
+        int *values = malloc(4 * sizeof(int *));
+        for(i=0 ; i<4 ; i++) {
+                starpu_mpi_get_data_on_node(MPI_COMM_WORLD, data_handles[i], 0);
+                starpu_data_acquire(data_handles[i], STARPU_R);
+                values[i] = *((int *)starpu_mpi_handle_to_ptr(data_handles[i]));
         }
-        else if (rank == 1) {
-                starpu_data_acquire(data_handles[3], STARPU_R);
-        }
-        fprintf(stderr, "[%d] VALUES: %d %d %d %d\n", rank, x[0], x[1], x[2], y);
+        fprintf(stderr, "[%d][local ptr] VALUES: %d %d %d %d\n", rank, values[0], values[1], values[2], values[3]);
+        fprintf(stderr, "[%d][end] VALUES: %d %d %d %d\n", rank, x[0], x[1], x[2], y);
 
 	starpu_mpi_shutdown();
 	starpu_shutdown();
