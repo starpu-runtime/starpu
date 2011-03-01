cc2384986e5861aef37423080569ad4166c6dd11
Nathalie Furmento <nathalie.furmento@labri.fr>
2011-03-01 12:22:15 +0000

    copyright fixes

--- tests/core/multithreaded.c
+++ tests/core/multithreaded.c
@@ -2,6 +2,7 @@
  *
  * Copyright (C) 2009, 2010  Université de Bordeaux 1
  * Copyright (C) 2010, 2011  Centre National de la Recherche Scientifique
+ * Copyright (C) 2011  Centre National de la Recherche Scientifique
  *
  * StarPU is free software; you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
