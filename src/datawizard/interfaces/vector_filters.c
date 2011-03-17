f32a02d56f1d7e7118bad5b5517fec4e405b54d0
Nathalie Furmento <nathalie.furmento@labri.fr>
2011-03-17 10:29:59 +0000

    src/datawizard/interfaces/vector_filters.c: fix typo. loop from 0 to id-1

--- src/datawizard/interfaces/vector_filters.c
+++ src/datawizard/interfaces/vector_filters.c
@@ -2,7 +2,7 @@
  *
  * Copyright (C) 2009, 2010  Université de Bordeaux 1
  * Copyright (C) 2010  Mehdi Juhoor <mjuhoor@gmail.com>
- * Copyright (C) 2010  Centre National de la Recherche Scientifique
+ * Copyright (C) 2010, 2011  Centre National de la Recherche Scientifique
  *
  * StarPU is free software; you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
@@ -107,7 +107,7 @@ void starpu_vector_list_filter_func(void *father_interface, void *child_interfac
 	if (vector_father->ptr) {
 	  /* compute the current position */
 	  unsigned i;
-	  for (i = 0; i <= id; i++) 
+	  for (i = 0; i < id; i++) 
 	    current_pos += length_tab[i];
 	  
 	  vector_child->ptr = vector_father->ptr + current_pos*elemsize;
