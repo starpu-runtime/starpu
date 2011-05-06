8e988c81803da1052dde8926472bdc208fe806c3
Samuel Thibault <samuel.thibault@labri.fr>
2011-05-06 14:45:25 +0000

    Warn the user when the scheduling policy was not found

--- src/core/sched_policy.c
+++ src/core/sched_policy.c
@@ -1,7 +1,7 @@
 /* StarPU --- Runtime system for heterogeneous multicore architectures.
  *
- * Copyright (C) 2010  Université de Bordeaux 1
- * Copyright (C) 2010, 2011  Centre National de la Recherche Scientifique
+ * Copyright (C) 2010-2011  Université de Bordeaux 1
+ * Copyright (C) 2010-2011  Centre National de la Recherche Scientifique
  *
  * StarPU is free software; you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
@@ -119,6 +119,7 @@ static struct starpu_sched_policy_s *find_sched_policy_from_name(const char *pol
 			}
 		}
 	}
+	fprintf(stderr, "Warning: scheduling policy \"%s\" was not found, try \"help\" to get a list\n", policy_name);
 
 	/* nothing was found */
 	return NULL;
