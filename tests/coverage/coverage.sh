133839b017a7a5b18b761a5732879d2a485f3a9c
Nathalie Furmento <nathalie.furmento@labri.fr>
2011-06-06 09:33:15 +0000

    tests/coverage/coverage.sh: remove un-needed test

--- tests/coverage/coverage.sh
+++ tests/coverage/coverage.sh
@@ -3,7 +3,7 @@
 # StarPU --- Runtime system for heterogeneous multicore architectures.
 # 
 # Copyright (C) 2010  Université de Bordeaux 1
-# Copyright (C) 2010  Centre National de la Recherche Scientifique
+# Copyright (C) 2010, 2011  Centre National de la Recherche Scientifique
 # 
 # StarPU is free software; you can redistribute it and/or modify
 # it under the terms of the GNU Lesser General Public License as published by
@@ -86,9 +86,9 @@ apps()
 	STARPU_SCHED="dm" $exampledir/heat/heat -ntheta 66 -nthick 130 -nblocks 8 -v2
 	check_success $?
 
-	echo "heat.dm.8k.v2.no.prio"
-	STARPU_SCHED="no-prio" $exampledir/heat/heat -ntheta 66 -nthick 130 -nblocks 8 -pin -v2
-	check_success $?
+#	echo "heat.dm.8k.v2.no.prio"
+#	STARPU_SCHED="no-prio" $exampledir/heat/heat -ntheta 66 -nthick 130 -nblocks 8 -pin -v2
+#	check_success $?
 
 	echo "heat.dm.8k.v2.random"
 	STARPU_SCHED="random" $exampledir/heat/heat -ntheta 66 -nthick 130 -nblocks 8 -pin -v2
