47fa9111ba5a34ae57d6b8628ffb57c6e0339985
Nathalie Furmento <nathalie.furmento@labri.fr>
2011-06-06 15:05:46 +0000

    core/workers.h: revert wrong commit

--- src/core/workers.h
+++ src/core/workers.h
@@ -1,7 +1,7 @@
 /* StarPU --- Runtime system for heterogeneous multicore architectures.
  *
  * Copyright (C) 2009, 2010-2011  Université de Bordeaux 1
- * Copyright (C) 2010, 2011  Centre National de la Recherche Scientifique
+ * Copyright (C) 2010  Centre National de la Recherche Scientifique
  *
  * StarPU is free software; you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
@@ -76,7 +76,7 @@ struct starpu_worker_s {
 	unsigned worker_is_running;
 	unsigned worker_is_initialized;
 	starpu_worker_status status; /* what is the worker doing now ? (eg. CALLBACK) */
-	char name[64];
+	char name[32];
 
 #ifdef __GLIBC__
 	cpu_set_t initial_cpu_set;
