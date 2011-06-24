d884c6b015241d36997573f817ac06e2f1484e80
Samuel Thibault <samuel.thibault@labri.fr>
2011-06-24 13:46:11 +0000

    revert r3914 to fix fxt build

--- src/drivers/driver_common/driver_common.c
+++ src/drivers/driver_common/driver_common.c
@@ -1,7 +1,7 @@
 /* StarPU --- Runtime system for heterogeneous multicore architectures.
  *
  * Copyright (C) 2010, 2011  Université de Bordeaux 1
- * Copyright (C) 2010, 2011  Centre National de la Recherche Scientifique
+ * Copyright (C) 2010  Centre National de la Recherche Scientifique
  *
  * StarPU is free software; you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
@@ -67,6 +67,7 @@ void _starpu_driver_end_job(struct starpu_worker_s *args, starpu_job_t j, struct
 	int starpu_top=starpu_top_status_get();
 	int workerid = args->workerid;
 	unsigned calibrate_model = 0;
+	enum starpu_perf_archtype archtype = args->perf_arch;
 
 	STARPU_TRACE_END_CODELET_BODY(j, archtype);
 
