992afb35cfb73b120178303be084d1d345ce9096
Samuel Thibault <samuel.thibault@labri.fr>
2011-07-25 10:14:11 +0000

    fix loading push_task_notify in load_sched_policy

--- src/core/sched_policy.c
+++ src/core/sched_policy.c
@@ -90,6 +90,7 @@ static void load_sched_policy(struct starpu_sched_policy_s *sched_policy)
 	policy.init_sched = sched_policy->init_sched;
 	policy.deinit_sched = sched_policy->deinit_sched;
 	policy.push_task = sched_policy->push_task;
+	policy.push_task_notify = sched_policy->push_task_notify;
 	policy.pop_task = sched_policy->pop_task;
         policy.post_exec_hook = sched_policy->post_exec_hook;
 	policy.pop_every_task = sched_policy->pop_every_task;
