c55b7479180d06886017dce9af9e11f446c1d261
Nicolas Collin <nicolas.collin@inria.fr>
2011-09-07 16:23:14 +0000

    Check the STARPU_SINGLE_COMBINED_WORKER environment variable in case the user didn't call starpu_conf_init().

--- src/sched_policies/detect_combined_workers.c
+++ src/sched_policies/detect_combined_workers.c
@@ -171,12 +171,11 @@ void _starpu_sched_find_worker_combinations(struct starpu_machine_topology_s *to
 {
 	struct starpu_machine_config_s *config = _starpu_get_machine_config();
 
-	if (config->user_conf && config->user_conf->single_combined_worker)
+	if ((config->user_conf && config->user_conf->single_combined_worker) || starpu_get_env_number("STARPU_SINGLE_COMBINED_WORKER"))
 		combine_all_cpu_workers(topology);
 	else {
 #ifdef STARPU_HAVE_HWLOC
 		find_combinations_with_hwloc(topology);
-		//find_combinations_without_hwloc(topology);
 #else
 		find_combinations_without_hwloc(topology);
 #endif
