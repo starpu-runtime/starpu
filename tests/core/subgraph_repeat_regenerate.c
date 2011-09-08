38b8d7ac298652760713b8fa95028c7f4e57490e
Samuel Thibault <samuel.thibault@labri.fr>
2011-09-08 16:13:05 +0000

    Do not regenerate taskD, since starpu will be shutting down

--- tests/core/subgraph_repeat_regenerate.c
+++ tests/core/subgraph_repeat_regenerate.c
@@ -1,6 +1,6 @@
 /* StarPU --- Runtime system for heterogeneous multicore architectures.
  *
- * Copyright (C) 2010  Université de Bordeaux 1
+ * Copyright (C) 2010-2011  Université de Bordeaux 1
  * Copyright (C) 2010, 2011  Centre National de la Recherche Scientifique
  *
  * StarPU is free software; you can redistribute it and/or modify
@@ -65,6 +65,7 @@ static void callback_task_D(void *arg __attribute__((unused)))
 	if (loop_cnt == niter)
 	{
 		/* We are done */
+		taskD.regenerate = 0;
 		pthread_mutex_lock(&mutex);
 		pthread_cond_signal(&cond);
 		pthread_mutex_unlock(&mutex);
