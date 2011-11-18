238cb6c0d765d324c559bf728020e200ace32598
Nathalie Furmento <nathalie.furmento@labri.fr>
2011-11-18 12:46:07 +0000

    tools/dev: add copyright

--- tools/dev/functions_undocumented.sh
+++ tools/dev/functions_undocumented.sh
@@ -1,5 +1,20 @@
 #!/bin/bash
 
+# StarPU --- Runtime system for heterogeneous multicore architectures.
+#
+# Copyright (C) 2011  Centre National de la Recherche Scientifique
+#
+# StarPU is free software; you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation; either version 2.1 of the License, or (at
+# your option) any later version.
+#
+# StarPU is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+#
+# See the GNU Lesser General Public License in COPYING.LGPL for more details.
+
 stcolor=$(tput sgr0)
 redcolor=$(tput setaf 1)
 
