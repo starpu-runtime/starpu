1c42006ca213f59fea278eb0e1613f06fa88769b
Samuel Thibault <samuel.thibault@labri.fr>
2011-12-12 15:51:58 +0000

    Also interpret STARPU_HOME

--- src/core/perfmodel/perfmodel.c
+++ src/core/perfmodel/perfmodel.c
@@ -330,7 +330,9 @@ void _starpu_get_perf_model_dir(char *path, size_t maxlen)
 	snprintf(path, maxlen, "%s", STARPU_PERF_MODEL_DIR);
 #else
 	/* by default, we use $HOME/.starpu/sampling */
-	const char *home_path = getenv("HOME");
+	const char *home_path = getenv("STARPU_HOME");
+	if (!home_path)
+		home_path = getenv("HOME");
 	if (!home_path)
 		home_path = getenv("USERPROFILE");
 	if (!home_path)
