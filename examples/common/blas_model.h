c3a5171174dea18aa1e8acfe74845e74e6820c37
Nathalie Furmento <nathalie.furmento@labri.fr>
2012-01-04 14:59:50 +0000

    examples/common/blas_model.h: fix function prototype

--- examples/common/blas_model.h
+++ examples/common/blas_model.h
@@ -1,7 +1,7 @@
 /* StarPU --- Runtime system for heterogeneous multicore architectures.
  *
  * Copyright (C) 2009-2012  Université de Bordeaux 1
- * Copyright (C) 2010, 2011  Centre National de la Recherche Scientifique
+ * Copyright (C) 2010, 2011, 2012  Centre National de la Recherche Scientifique
  *
  * StarPU is free software; you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
@@ -20,7 +20,7 @@
 
 #include <starpu.h>
 
-double gemm_cost(struct starpu_buffer_descr *descr, unsigned nimpl);
+double gemm_cost(struct starpu_task *task, unsigned nimpl);
 
 static struct starpu_perfmodel starpu_sgemm_model =
 {
