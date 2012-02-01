726ccf2ea7d992db8b1203e17ce694f734ce3855
Cyril Roelandt <cyril.roelandt@inria.fr>
2012-02-01 14:47:41 +0000

    Replace "if STARPU_HAVE_VALGRIND_H" by "ifdef STARPU_HAVE_VALGRIND_H".

--- tests/helper.h
+++ tests/helper.h
@@ -33,7 +33,7 @@
 //#define STARPU_TEST_OUTPUT
 #define FPRINTF(ofile, fmt, args ...) do { if (!getenv("STARPU_SSILENT")) {fprintf(ofile, fmt, ##args); }} while(0)
 
-#if STARPU_HAVE_VALGRIND_H
+#ifdef STARPU_HAVE_VALGRIND_H
 #define STARPU_SKIP_IF_VALGRIND do {                                                               \
 if(RUNNING_ON_VALGRIND) {                                                                          \
 	FPRINTF(stderr, "Running on valgrind, skipping the actual computations in %s\n", __func__);\
