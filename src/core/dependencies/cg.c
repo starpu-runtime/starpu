5b25d475bb011b0407362f7564fbdfccc1a941b4
Nathalie Furmento <nathalie.furmento@labri.fr>
2012-02-16 10:35:45 +0000

    src/core/dependencies/cg.c: include core/task.h for _starpu_task_destroy

--- src/core/dependencies/cg.c
+++ src/core/dependencies/cg.c
@@ -1,7 +1,7 @@
 /* StarPU --- Runtime system for heterogeneous multicore architectures.
  *
  * Copyright (C) 2010-2012  Université de Bordeaux 1
- * Copyright (C) 2010, 2011  Centre National de la Recherche Scientifique
+ * Copyright (C) 2010, 2011, 2012  Centre National de la Recherche Scientifique
  * Copyright (C) 2012 inria
  *
  * StarPU is free software; you can redistribute it and/or modify
@@ -20,6 +20,7 @@
 #include <common/config.h>
 #include <common/utils.h>
 #include <core/jobs.h>
+#include <core/task.h>
 #include <core/dependencies/cg.h>
 #include <core/dependencies/tags.h>
 
