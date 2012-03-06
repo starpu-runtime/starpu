298d68f6fa5757ba6cf85da3a697aa31a363a9ee
Cyril Roelandt <cyril.roelandt@inria.fr>
2012-03-06 18:34:09 +0000

    Make examples/tag_example/tag_example2 work with OpenCL.

--- examples/tag_example/tag_example2.c
+++ examples/tag_example/tag_example2.c
@@ -134,10 +134,10 @@ int main(int argc __attribute__((unused)) , char **argv __attribute__((unused)))
 
 	cl.cpu_funcs[0] = cpu_codelet;
 	cl.cuda_funcs[0] = cpu_codelet;
+	cl.opencl_funcs[0] = cpu_codelet;
 #ifdef STARPU_USE_GORDON
 	cl.gordon_func = gordon_null_kernel;
 #endif
-	cl.where = STARPU_CPU|STARPU_CUDA|STARPU_GORDON;
 	cl.nbuffers = 0;
 
 	FPRINTF(stderr, "ITER : %u\n", nk);
