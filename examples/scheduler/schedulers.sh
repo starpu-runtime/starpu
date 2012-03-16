59a647893204ade62693139f7b93d71726d5fa0d
Nathalie Furmento <nathalie.furmento@labri.fr>
2012-03-16 14:30:58 +0000

    examples/scheduler/schedulers.sh: add script file to test all schedulers on cholesky application

---
+++ examples/scheduler/schedulers.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# StarPU --- Runtime system for heterogeneous multicore architectures.
+#
+# Copyright (C) 2012  Centre National de la Recherche Scientifique
+#
+# StarPU is free software; you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation; either version 2.1 of the License, or (at
+# your option) any later version.
+#
+# StarPU is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+#
+# See the GNU Lesser General Public License in COPYING.LGPL for more details.
+
+check_success()
+{
+    if [ $1 != 0 ] ; then
+	echo "failure"
+        exit $1
+    fi
+}
+
+SCHEDULERS=$(STARPU_SCHED="help" ./basic_examples/hello_world 2>&1 | awk '/->/ {print $1}')
+
+for sched in $SCHEDULERS
+do
+    echo "cholesky.$sched"
+    STARPU_SCHED=$sched ./cholesky/cholesky_tag
+    check_success $?
+done
+
+exit 1
