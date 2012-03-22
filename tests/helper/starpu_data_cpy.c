143ef752ae98ac0f8e04d34dcb2878d6c6a6f014
Nathalie Furmento <nathalie.furmento@labri.fr>
2012-03-22 10:00:01 +0000

    tests/helper/starpu_data_cpy.c: return EXIT_FAILURE instead of aborting

--- tests/helper/starpu_data_cpy.c
+++ tests/helper/starpu_data_cpy.c
@@ -17,19 +17,18 @@
 #include <starpu.h>
 #include "../helper.h"
 
-int var1, var2;
-starpu_data_handle_t var1_handle, var2_handle;
-
 int main(int argc, char **argv)
 {
 	int ret;
+	int var1, var2;
+	starpu_data_handle_t var1_handle, var2_handle;
 
 	ret = starpu_init(NULL);
 	if (ret == -ENODEV) return STARPU_TEST_SKIPPED;
 	STARPU_CHECK_RETURN_VALUE(ret, "starpu_init");
 
 	var1 = 42;
-	var2 = 0;
+	var2 = 12;
 
 	starpu_variable_data_register(&var1_handle, 0, (uintptr_t)&var1, sizeof(var1));
 	starpu_variable_data_register(&var2_handle, 0, (uintptr_t)&var2, sizeof(var2));
@@ -38,12 +37,17 @@ int main(int argc, char **argv)
 	STARPU_CHECK_RETURN_VALUE(ret, "starpu_data_cpy");
 
 	starpu_data_acquire(var2_handle, STARPU_R);
-	STARPU_ASSERT(var2 == 42);
+	ret = EXIT_SUCCESS;
+	if (var2 != var1)
+	{
+	     FPRINTF(stderr, "var2 is %d but it should be %d\n", var2, var1);
+	     ret = EXIT_FAILURE;
+	}
 	starpu_data_release(var2_handle);
 
 	starpu_data_unregister(var1_handle);
 	starpu_data_unregister(var2_handle);
 	starpu_shutdown();
 
-	STARPU_RETURN(EXIT_SUCCESS);
+	STARPU_RETURN(ret);
 }
