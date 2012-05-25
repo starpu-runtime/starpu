e2f04033e0121174cd5e7da2a22422b57c6cce57
Sylvain Henry <sylvain.henry@inria.fr>
2012-05-25 13:49:29 +0000

    core/sched_policy.c: use memcpy to copy scheduling policy structure

--- src/core/sched_policy.c
+++ src/core/sched_policy.c
@@ -88,16 +88,7 @@ static void load_sched_policy(struct starpu_sched_policy *sched_policy)
 	}
 #endif
 
-	policy.policy_name = sched_policy->policy_name;
-	policy.policy_description = sched_policy->policy_description;
-	policy.init_sched = sched_policy->init_sched;
-	policy.deinit_sched = sched_policy->deinit_sched;
-	policy.push_task = sched_policy->push_task;
-	policy.push_task_notify = sched_policy->push_task_notify;
-	policy.pop_task = sched_policy->pop_task;
-        policy.pre_exec_hook = sched_policy->pre_exec_hook;
-        policy.post_exec_hook = sched_policy->post_exec_hook;
-	policy.pop_every_task = sched_policy->pop_every_task;
+	memcpy(&policy, sched_policy, sizeof(policy));
 }
 
 static struct starpu_sched_policy *find_sched_policy_from_name(const char *policy_name)
