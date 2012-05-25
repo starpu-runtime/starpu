7894eed66c9909ad7797a94d8ad8487263b2772a
Samuel Thibault <samuel.thibault@labri.fr>
2012-05-25 13:16:41 +0000

    Fix list of implementations

--- examples/basic_examples/vector_scal.c
+++ examples/basic_examples/vector_scal.c
@@ -1,7 +1,7 @@
 /* StarPU --- Runtime system for heterogeneous multicore architectures.
  *
  * Copyright (C) 2010, 2011, 2012  Centre National de la Recherche Scientifique
- * Copyright (C) 2010, 2011  Université de Bordeaux 1
+ * Copyright (C) 2010-2012  Université de Bordeaux 1
  *
  * StarPU is free software; you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
@@ -66,6 +66,7 @@ static struct starpu_codelet cl =
 		, scal_sse_func_icc
 #endif
 #endif
+		, NULL
 	},
 #ifdef STARPU_USE_CUDA
 	/* CUDA implementation of the codelet */
