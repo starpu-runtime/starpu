606087f38960c21afce42b0c0bef43bc2a465e56
Samuel Thibault <samuel.thibault@labri.fr>
2012-06-21 09:34:30 +0000

    Fix copyright years

--- examples/pipeline/pipeline.c
+++ examples/pipeline/pipeline.c
@@ -1,7 +1,7 @@
 /* StarPU --- Runtime system for heterogeneous multicore architectures.
  *
- * Copyright (C) 2010, 2011, 2012  Centre National de la Recherche Scientifique
- * Copyright (C) 2010-2012  Université de Bordeaux 1
+ * Copyright (C) 2012  Centre National de la Recherche Scientifique
+ * Copyright (C) 2012  Université de Bordeaux 1
  *
  * StarPU is free software; you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
