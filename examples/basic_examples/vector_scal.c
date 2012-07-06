59cb5ccbb995e33319eb0b0a9cc7555140b76735
Cyril Roelandt <cyril.roelandt@inria.fr>
2012-07-06 22:16:36 +0000

    Fix build on systems that do not have nearbyintf().
    Try to use rintf() on such systems; if neither nearbyintf nor rintf are available, give an explicit answer message to the user.

--- examples/basic_examples/vector_scal.c
+++ examples/basic_examples/vector_scal.c
@@ -23,6 +23,7 @@
  *  3- how a kernel can manipulate the data (buffers[0].vector.ptr)
  */
 
+#include <config.h>
 #include <starpu.h>
 #include <starpu_opencl.h>
 #include <stdlib.h>
@@ -88,8 +89,15 @@ struct starpu_opencl_program opencl_program;
 
 static int approximately_equal(float a, float b)
 {
+#ifdef STARPU_HAVE_NEARBYINTF
 	int ai = (int) nearbyintf(a * 1000.0);
 	int bi = (int) nearbyintf(b * 1000.0);
+#elif defined(STARPU_HAVE_RINTF)
+	int ai = (int) rintf(a * 1000.0);
+	int bi = (int) rintf(b * 1000.0);
+#else
+#error "Please define either nearbyintf or rintf."
+#endif
 	return ai == bi;
 }
 
