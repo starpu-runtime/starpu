f413aaa1cd01277323d8ae7878bde76610da4ad2
Nathalie Furmento <nathalie.furmento@labri.fr>
2012-07-23 09:18:39 +0000

    src/util/starpu_cublas.c: add missing #ifdef

--- src/util/starpu_cublas.c
+++ src/util/starpu_cublas.c
@@ -1,7 +1,7 @@
 /* StarPU --- Runtime system for heterogeneous multicore architectures.
  *
  * Copyright (C) 2009-2012  Université de Bordeaux 1
- * Copyright (C) 2010, 2011  Centre National de la Recherche Scientifique
+ * Copyright (C) 2010, 2011, 2012  Centre National de la Recherche Scientifique
  *
  * StarPU is free software; you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
@@ -32,9 +32,10 @@ static void init_cublas_func(void *args STARPU_ATTRIBUTE_UNUSED)
 
 static void shutdown_cublas_func(void *args STARPU_ATTRIBUTE_UNUSED)
 {
+#ifdef STARPU_USE_CUDA
 	cublasShutdown();
-}
 #endif
+}
 
 void starpu_helper_cublas_init(void)
 {
