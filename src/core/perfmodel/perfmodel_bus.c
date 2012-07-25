41b76f990d3ee1ce94c947da0c12fb155c1707eb
Nathalie Furmento <nathalie.furmento@labri.fr>
2012-07-25 09:54:09 +0000

    src/core/perfmodel/perfmodel_bus.c: only display header when some cuda or opencl devices are available

--- src/core/perfmodel/perfmodel_bus.c
+++ src/core/perfmodel/perfmodel_bus.c
@@ -1133,7 +1133,8 @@ void starpu_bus_print_bandwidth(FILE *f)
 	}
 
 #if defined(STARPU_USE_CUDA) || defined(STARPU_USE_OPENCL)
-	fprintf(f, "\nGPU\tCPU in preference order (logical index), host-to-device, device-to-host\n");
+	if (ncuda != 0 || nopencl != 0)
+		fprintf(f, "\nGPU\tCPU in preference order (logical index), host-to-device, device-to-host\n");
 	for (src = 1; src <= maxnode; src++)
 	{
 		struct dev_timing *timing;
