0de74d0e6314307b8d1f7f329e8627e17643be97
Nathalie Furmento <nathalie.furmento@labri.fr>
2012-09-21 12:20:45 +0000

    include/starpu_hash.h: fix macro name to avoid double inclusion

--- include/starpu_hash.h
+++ include/starpu_hash.h
@@ -1,7 +1,7 @@
 /* StarPU --- Runtime system for heterogeneous multicore architectures.
  *
  * Copyright (C) 2009-2011  Université de Bordeaux 1
- * Copyright (C) 2010  Centre National de la Recherche Scientifique
+ * Copyright (C) 2010, 2012  Centre National de la Recherche Scientifique
  *
  * StarPU is free software; you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
@@ -15,8 +15,8 @@
  * See the GNU Lesser General Public License in COPYING.LGPL for more details.
  */
 
-#ifndef __HASH_H__
-#define __HASH_H__
+#ifndef __STARPU_HASH_H__
+#define __STARPU_HASH_H__
 
 #include <stdint.h>
 #include <stddef.h>
@@ -36,4 +36,4 @@ uint32_t starpu_crc32_be(uint32_t input, uint32_t inputcrc);
  * computation. */
 uint32_t starpu_crc32_string(char *str, uint32_t inputcrc);
 
-#endif // __HASH_H__
+#endif // __STARPU_HASH_H__
