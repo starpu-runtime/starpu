56da695b31f306bbf1c6d890b431395045e8f56d
Nathalie Furmento <nathalie.furmento@labri.fr>
2012-11-15 10:01:56 +0000

    src/profiling/profiling.c: use named variable instead of hard-coded value

--- src/profiling/profiling.c
+++ src/profiling/profiling.c
@@ -1,7 +1,7 @@
 /* StarPU --- Runtime system for heterogeneous multicore architectures.
  *
  * Copyright (C) 2010-2012  Université de Bordeaux 1
- * Copyright (C) 2010, 2011  Centre National de la Recherche Scientifique
+ * Copyright (C) 2010, 2011, 2012  Centre National de la Recherche Scientifique
  *
  * StarPU is free software; you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
@@ -104,7 +104,7 @@ void _starpu_profiling_init(void)
 		_starpu_worker_reset_profiling_info(worker);
 	}
 	if ((env = getenv("STARPU_PROFILING")) && atoi(env))
-		_starpu_profiling = 1;
+		_starpu_profiling = STARPU_PROFILING_ENABLE;
 }
 
 void _starpu_profiling_terminate(void)
