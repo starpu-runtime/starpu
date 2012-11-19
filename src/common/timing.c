28a00cfbbda587a92ea5d0ff9447dd9e06c80363
Nathalie Furmento <nathalie.furmento@labri.fr>
2012-11-19 20:30:02 +0000

    src/common/timing.c: include math.h for floor

--- src/common/timing.c
+++ src/common/timing.c
@@ -1,7 +1,7 @@
 /* StarPU --- Runtime system for heterogeneous multicore architectures.
  *
  * Copyright (C) 2009-2012  Université de Bordeaux 1
- * Copyright (C) 2010, 2011  Centre National de la Recherche Scientifique
+ * Copyright (C) 2010, 2011, 2012  Centre National de la Recherche Scientifique
  *
  * StarPU is free software; you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
@@ -20,6 +20,7 @@
 #include <common/config.h>
 #include <profiling/profiling.h>
 #include <common/timing.h>
+#include <math.h>
 
 #ifdef STARPU_SIMGRID
 #include <msg/msg.h>
