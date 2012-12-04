aee51207519423cdf16957219d4609a749f902e4
Cyril Roelandt <cyril.roelandt@inria.fr>
2012-12-04 16:53:32 +0000

    dmda/heft merge: update the Changelog and the Copyrights.

--- src/sched_policies/deque_modeling_policy_data_aware.c
+++ src/sched_policies/deque_modeling_policy_data_aware.c
@@ -3,6 +3,7 @@
  * Copyright (C) 2010-2012  Université de Bordeaux 1
  * Copyright (C) 2010, 2011, 2012  Centre National de la Recherche Scientifique
  * Copyright (C) 2011  Télécom-SudParis
+ * Copyright (C) 2012  inria
  *
  * StarPU is free software; you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
