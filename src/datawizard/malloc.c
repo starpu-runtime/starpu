0ceb536b41265b5eddf6cf846ca2c57ee9e757d7
Samuel Thibault <samuel.thibault@labri.fr>
2012-12-06 15:05:09 +0000

    properly merge malloc.c

--- src/datawizard/malloc.c
+++ src/datawizard/malloc.c
@@ -106,7 +106,7 @@ int starpu_malloc(void **A, size_t dim)
 
 		_starpu_exclude_task_from_dag(task);
 
-		push_res = starpu_task_submit(task);
+		push_res = _starpu_task_submit_internally(task);
 		STARPU_ASSERT(push_res != -ENODEV);
 #endif
 #endif
@@ -132,7 +132,7 @@ int starpu_malloc(void **A, size_t dim)
 //
 //		_starpu_exclude_task_from_dag(task);
 //
-//		push_res = starpu_task_submit(task);
+//		push_res = _starpu_task_submit_internally(task);
 //		STARPU_ASSERT(push_res != -ENODEV);
 //#endif
 //        }
@@ -219,7 +219,7 @@ int starpu_free(void *A)
 
 		_starpu_exclude_task_from_dag(task);
 
-		push_res = starpu_task_submit(task);
+		push_res = _starpu_task_submit_internally(task);
 		STARPU_ASSERT(push_res != -ENODEV);
 	}
 #endif
