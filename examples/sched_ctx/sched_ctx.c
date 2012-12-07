d1507f5b2c42820820da2c8f0af3be7bcc1ca5eb
Nathalie Furmento <nathalie.furmento@labri.fr>
2012-12-07 12:00:45 +0000

    examples/sched_ctx/sched_ctx.c: add warning

--- examples/sched_ctx/sched_ctx.c
+++ examples/sched_ctx/sched_ctx.c
@@ -99,6 +99,8 @@ int main(int argc, char **argv)
 	/* tell starpu when you finished submitting tasks to this context
 	   in order to allow moving resources from this context to the inheritor one
 	   when its corresponding tasks finished executing */
+
+#warning TODO: to be fixed
 //	starpu_sched_ctx_finished_submit(sched_ctx1);
 
 	for (i = 0; i < ntasks/2; i++)
@@ -113,7 +115,7 @@ int main(int argc, char **argv)
 		STARPU_CHECK_RETURN_VALUE(ret, "starpu_task_submit");
 	}
 
-
+#warning TODO: to be fixed
 //	starpu_sched_ctx_finished_submit(sched_ctx2);
 
 	/* wait for all tasks at the end*/
