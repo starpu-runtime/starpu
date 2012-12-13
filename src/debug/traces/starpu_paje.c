bffb42eae4bc09d712f8daa188b1f1759022b616
Samuel Thibault <samuel.thibault@labri.fr>
2012-12-13 09:36:18 +0000

    poti_headers now has a parameter to keep compatibility with vite 1.2

--- src/debug/traces/starpu_paje.c
+++ src/debug/traces/starpu_paje.c
@@ -26,7 +26,7 @@ void _starpu_fxt_write_paje_header(FILE *file)
 {
 	unsigned i;
 #ifdef STARPU_HAVE_POTI
-	poti_header(1); /* 1 as parameter means basic, no extended events */
+	poti_header(1, 1); /* 1 as parameter means basic, no extended events */
 #else
 	fprintf(file, "%%EventDef	PajeDefineContainerType	1\n");
 	fprintf(file, "%%	Alias	string\n");
