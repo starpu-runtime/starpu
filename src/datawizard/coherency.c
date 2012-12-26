3cbd60b79ba2dce9290b812448c401118211af6e
Samuel Thibault <samuel.thibault@labri.fr>
2012-12-26 15:51:54 +0000

    Comment that in GPUDirect mode, peer transfer is probably better when issued from the destination too

--- src/datawizard/coherency.c
+++ src/datawizard/coherency.c
@@ -197,7 +197,7 @@ static int link_supports_direct_transfers(starpu_data_handle_t handle, unsigned 
 	}
 #endif
 
-	/* Note: with CUDA, performance seems a bit better when issuing the transfer from the destination (tested without GPUDirect) */
+	/* Note: with CUDA, performance seems a bit better when issuing the transfer from the destination (tested without GPUDirect, but GPUDirect probably behave the same) */
 	if (worker_supports_direct_access(src_node, dst_node))
 	{
 		*handling_node = dst_node;
