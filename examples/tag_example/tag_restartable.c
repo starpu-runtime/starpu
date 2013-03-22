fff88f27de4904567fc007950e0ef4ba17d9047f
Samuel Thibault <samuel.thibault@labri.fr>
2013-03-22 10:06:24 +0000

    Add missing task cleaning

--- examples/tag_example/tag_restartable.c
+++ examples/tag_example/tag_restartable.c
@@ -1,6 +1,6 @@
 /* StarPU --- Runtime system for heterogeneous multicore architectures.
  *
- * Copyright (C) 2009, 2010  Université de Bordeaux 1
+ * Copyright (C) 2009, 2010, 2013  Université de Bordeaux 1
  * Copyright (C) 2010, 2011, 2012, 2013  Centre National de la Recherche Scientifique
  *
  * StarPU is free software; you can redistribute it and/or modify
@@ -163,6 +163,7 @@ int main(int argc __attribute__((unused)) , char **argv __attribute__((unused)))
 enodev:
 	for (i = 0; i < Nrolls; i++)
 	{
+		starpu_task_clean(tasks[i]);
 		free(tasks[i]);
 	}
 
