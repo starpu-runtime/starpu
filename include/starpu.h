21d6e04544f2a1ee5d9bf2134023183a5654ddef
Nathalie Furmento <nathalie.furmento@labri.fr>
2013-04-04 19:18:42 +0000

    include/starpu.h: define uintptr_t if needed

--- include/starpu.h
+++ include/starpu.h
@@ -27,6 +27,7 @@ typedef unsigned char uint8_t;
 typedef unsigned short uint16_t;
 typedef unsigned long uint32_t;
 typedef unsigned long long uint64_t;
+typedef unsigned long int uintptr_t;
 #endif
 
 #include <starpu_config.h>
