8d5bc2004a6cec087d00ada927b279442a576049
Nathalie Furmento <nathalie.furmento@labri.fr>
2013-04-04 11:56:49 +0000

    examples/ppm_downscaler/yuv_downscaler.c: remove un-necessary include pthread.h

--- examples/ppm_downscaler/yuv_downscaler.c
+++ examples/ppm_downscaler/yuv_downscaler.c
@@ -2,7 +2,7 @@
  *
  * Copyright (C) 2010-2011  Université de Bordeaux 1
  * Copyright (C) 2010  Mehdi Juhoor <mjuhoor@gmail.com>
- * Copyright (C) 2010, 2011, 2012  Centre National de la Recherche Scientifique
+ * Copyright (C) 2010, 2011, 2012, 2013  Centre National de la Recherche Scientifique
  *
  * StarPU is free software; you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
@@ -24,7 +24,6 @@
 #include <unistd.h>
 #include <assert.h>
 #include <stdio.h>
-#include <pthread.h>
 
 #include "yuv_downscaler.h"
 
