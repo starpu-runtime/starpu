da91717417c5ca79f2f8740fba88a6f3f71b5134
Nathalie Furmento <nathalie.furmento@labri.fr>
2013-04-05 09:27:56 +0000

    merge trunk@9144: Fix build with ayudame

--- src/core/debug.c
+++ src/core/debug.c
@@ -1,6 +1,6 @@
 /* StarPU --- Runtime system for heterogeneous multicore architectures.
  *
- * Copyright (C) 2009-2012  Université de Bordeaux 1
+ * Copyright (C) 2009-2013  Université de Bordeaux 1
  * Copyright (C) 2010, 2011, 2013  Centre National de la Recherche Scientifique
  *
  * StarPU is free software; you can redistribute it and/or modify
@@ -76,7 +76,7 @@ struct ayudame_codelet
 	struct starpu_codelet *cl;
 } *codelets;
 static unsigned ncodelets, ncodelets_alloc;
-static starpu_pthread_mutex_t ayudame_mutex = _STARPU_PTHREAD_MUTEX_INITIALIZER;
+static starpu_pthread_mutex_t ayudame_mutex = STARPU_PTHREAD_MUTEX_INITIALIZER;
 int64_t _starpu_ayudame_get_func_id(struct starpu_codelet *cl)
 {
 	unsigned i;
