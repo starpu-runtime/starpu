39bce84ba62bb1cdeb3f093e37115396c0d57974
Nathalie Furmento <nathalie.furmento@labri.fr>
2013-04-12 07:49:51 +0000

    merge trunk@9209: tools/msvc: batch and project files to execute StarPU applications under windows

---
+++ tools/msvc/starpu_clean.bat
@@ -0,0 +1,24 @@
+REM StarPU --- Runtime system for heterogeneous multicore architectures.
+REM
+REM Copyright (C) 2013  Centre National de la Recherche Scientifique
+REM
+REM StarPU is free software; you can redistribute it and/or modify
+REM it under the terms of the GNU Lesser General Public License as published by
+REM the Free Software Foundation; either version 2.1 of the License, or (at
+REM your option) any later version.
+REM
+REM StarPU is distributed in the hope that it will be useful, but
+REM WITHOUT ANY WARRANTY; without even the implied warranty of
+REM MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+REM
+REM See the GNU Lesser General Public License in COPYING.LGPL for more details.
+
+@ECHO OFF
+TITLE MSVC StarPU Cleaning
+ECHO.
+ECHO MSVC StarPU Cleaning
+ECHO.
+
+FOR %%d in (debug starpu\debug ipch) DO IF EXIST %%d RMDIR /S /Q %%d 
+DEL starpu.sdf
+
