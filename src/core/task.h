0bd5b79704c461c5cb451ed79bdbf2b34387ed89
Nathalie Furmento <nathalie.furmento@labri.fr>
2013-04-26 17:20:00 +0000

    src/core/task.h: fix parentheses

--- src/core/task.h
+++ src/core/task.h
@@ -77,9 +77,9 @@ starpu_opencl_func_t _starpu_task_get_opencl_nth_implementation(struct starpu_co
 #define _STARPU_TASK_SET_HANDLE(task, handle, i) do { if (task->dyn_handles) task->dyn_handles[i] = handle; else task->handles[i] = handle; } while(0)
 
 #define _STARPU_TASK_SET_INTERFACE(task, interface, i) do { if (task->dyn_handles) task->dyn_interfaces[i] = interface; else task->interfaces[i] = interface;} while(0)
-#define _STARPU_TASK_GET_INTERFACES(task) (task->dyn_handles) ? task->dyn_interfaces : task->interfaces
+#define _STARPU_TASK_GET_INTERFACES(task) ((task->dyn_handles) ? task->dyn_interfaces : task->interfaces)
 
-#define _STARPU_CODELET_GET_MODE(codelet, i) (codelet->dyn_modes) ? codelet->dyn_modes[i] : codelet->modes[i]
+#define _STARPU_CODELET_GET_MODE(codelet, i) ((codelet->dyn_modes) ? codelet->dyn_modes[i] : codelet->modes[i])
 #define _STARPU_CODELET_SET_MODE(codelet, mode, i) do { if (codelet->dyn_modes) codelet->dyn_modes[i] = mode; else codelet->modes[i] = mode; } while(0)
 
 #endif // __CORE_TASK_H__
