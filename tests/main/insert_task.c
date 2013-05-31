2236457de0ecee33831b62f77bed8b2e548c37b1
Samuel Thibault <samuel.thibault@labri.fr>
2013-05-31 09:46:43 +0000

    No need to allocate data dynamically

--- tests/main/insert_task.c
+++ tests/main/insert_task.c
@@ -50,25 +50,22 @@ struct starpu_codelet mycodelet =
 
 int main(int argc, char **argv)
 {
-        int *x; float *f;
+        int x; float f;
         int i, ret;
 	int ifactor=12;
 	float ffactor=10.0;
         starpu_data_handle_t data_handles[2];
 
-	ret = starpu_initialize(NULL, &argc, &argv);
+	ret = starpu_init(NULL);
 	if (ret == -ENODEV) return STARPU_TEST_SKIPPED;
 	STARPU_CHECK_RETURN_VALUE(ret, "starpu_init");
 
-	starpu_malloc((void**)&x, sizeof(*x));
-	*x = 1;
-	starpu_variable_data_register(&data_handles[0], 0, (uintptr_t)x, sizeof(*x));
+	x = 1;
+	starpu_variable_data_register(&data_handles[0], 0, (uintptr_t)&x, sizeof(x));
+	f = 2.0;
+	starpu_variable_data_register(&data_handles[1], 0, (uintptr_t)&f, sizeof(f));
 
-	starpu_malloc((void**)&f, sizeof(*f));
-	*f = 2.0;
-	starpu_variable_data_register(&data_handles[1], 0, (uintptr_t)f, sizeof(*f));
-
-        FPRINTF(stderr, "VALUES: %d (%d) %f (%f)\n", *x, ifactor, *f, ffactor);
+        FPRINTF(stderr, "VALUES: %d (%d) %f (%f)\n", x, ifactor, f, ffactor);
 
         ret = starpu_insert_task(&mycodelet,
 				 STARPU_VALUE, &ifactor, sizeof(ifactor),
@@ -86,7 +83,7 @@ int main(int argc, char **argv)
                 ret = starpu_data_acquire(data_handles[i], STARPU_R);
 		STARPU_CHECK_RETURN_VALUE(ret, "starpu_data_acquire");
         }
-	FPRINTF(stderr, "VALUES: %d %f\n", *x, *f);
+        FPRINTF(stderr, "VALUES: %d %f\n", x, f);
 
         for(i=0 ; i<2 ; i++)
 	{
@@ -114,16 +111,13 @@ int main(int argc, char **argv)
                 ret = starpu_data_acquire(data_handles[i], STARPU_R);
 		STARPU_CHECK_RETURN_VALUE(ret, "starpu_data_acquire");
         }
-	FPRINTF(stderr, "VALUES: %d %f\n", *x, *f);
+        FPRINTF(stderr, "VALUES: %d %f\n", x, f);
 
         for(i=0 ; i<2 ; i++)
 	{
                 starpu_data_release(data_handles[i]);
 		starpu_data_unregister(data_handles[i]);
         }
-
-	starpu_free(x);
-	starpu_free(f);
 
 	starpu_shutdown();
 
