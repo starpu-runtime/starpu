320e1681afebde8f4b5268bb9a772694a620204e
Andra Hugo <andra.hugo@inria.fr>
2013-06-06 11:35:16 +0000

    fixes + refact in the hyp

--- sc_hypervisor/src/hypervisor_policies/teft_lp_policy.c
+++ sc_hypervisor/src/hypervisor_policies/teft_lp_policy.c
@@ -168,7 +168,7 @@ static void teft_lp_handle_poped_task(unsigned sched_ctx, int worker, struct sta
 			return;
 		}
 
-		if(sc_hypervisor_has_velocity_gap_btw_ctxs())
+		if(sc_hypervisor_criteria_fulfilled(sched_ctx, worker))
 		{
 			int ns = sc_hypervisor_get_nsched_ctxs();
 			int nw = starpu_worker_get_count(); /* Number of different workers */
