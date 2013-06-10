3b82bf6e305892e02e2a7de48453e82533a18ddc
Nathalie Furmento <nathalie.furmento@labri.fr>
2013-06-10 12:39:25 +0000

    merge trunk@10007: include/starpu_task_util.h: remove documentation as it is defined elsewhere

--- include/starpu_task_util.h
+++ include/starpu_task_util.h
@@ -35,28 +35,23 @@ void starpu_create_sync_task(starpu_tag_t sync_tag, unsigned ndeps, starpu_tag_t
 				void (*callback)(void *), void *callback_arg);
 
 /* Constants used by the starpu_insert_task helper to determine the different types of argument */
-#define STARPU_VALUE		(1<<4)	/* Pointer to a constant value */
-#define STARPU_CALLBACK		(1<<5)	/* Callback function */
-#define STARPU_CALLBACK_WITH_ARG	(1<<6)	/* Callback function */
-#define STARPU_CALLBACK_ARG	(1<<7)	/* Argument of the callback function (of type void *) */
-#define STARPU_PRIORITY		(1<<8)	/* Priority associated to the task */
+#define STARPU_VALUE		(1<<4)
+#define STARPU_CALLBACK		(1<<5)
+#define STARPU_CALLBACK_WITH_ARG (1<<6)
+#define STARPU_CALLBACK_ARG	(1<<7)
+#define STARPU_PRIORITY		(1<<8)
 #define STARPU_EXECUTE_ON_NODE	(1<<9)	/* Used by MPI to define which task is going to execute the codelet */
-#define STARPU_EXECUTE_ON_DATA	(1<<10)	/* Used by MPI to define which task is going to execute the codelet */
-#define STARPU_DATA_ARRAY       (1<<11) /* Array of data handles */
-#define STARPU_TAG              (1<<12) /* Tag */
-#define STARPU_HYPERVISOR_TAG	(1<<13)	/* Used to tag a task after whose execution we'll execute  a code */
-#define STARPU_FLOPS	        (1<<14)	/* Used to specify the number of flops needed to be executed by a task */
-#define STARPU_SCHED_CTX	(1<<15)	/* Used to specify the sched_ctx to which the task will be submitted */
+#define STARPU_EXECUTE_ON_DATA	(1<<10)
+#define STARPU_DATA_ARRAY       (1<<11)
+#define STARPU_TAG              (1<<12)
+#define STARPU_HYPERVISOR_TAG	(1<<13)
+#define STARPU_FLOPS	        (1<<14)
+#define STARPU_SCHED_CTX	(1<<15)
 
-/* Wrapper to create a task. */
 int starpu_insert_task(struct starpu_codelet *cl, ...);
 
-/* Retrieve the arguments of type STARPU_VALUE associated to a task
- * automatically created using starpu_insert_task. */
 void starpu_codelet_unpack_args(void *cl_arg, ...);
 
-/* Pack arguments of type STARPU_VALUE into a buffer which can be
- * given to a codelet and later unpacked with starpu_codelet_unpack_args */
 void starpu_codelet_pack_args(void **arg_buffer, size_t *arg_buffer_size, ...);
 
 #ifdef __cplusplus
