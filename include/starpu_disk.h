5d44fafa29c10c27bbece8f90859397d13484227
Corentin Salingue <corentin.salingue@inria.fr>
2013-06-20 14:02:02 +0000

    name of functions

--- include/starpu_disk.h
+++ include/starpu_disk.h
@@ -38,9 +38,9 @@ struct starpu_disk_ops {
 extern struct starpu_disk_ops starpu_disk_stdio_ops;
 
 /*functions to add an existing memory */
-void _starpu_disk_close(unsigned node, void *obj, size_t size);
+void starpu_disk_close(unsigned node, void *obj, size_t size);
 
-void * _starpu_disk_open(unsigned node, void *pos, size_t size);
+void * starpu_disk_open(unsigned node, void *pos, size_t size);
 
 /* interface to create and to free a memory disk */
 int starpu_disk_register(struct starpu_disk_ops * func, void *parameter, size_t size);
