172b4fa57517ba2e52a09f16af0ef16ae950b400
Nathalie Furmento <nathalie.furmento@labri.fr>
2013-07-02 08:44:24 +0000

    mpi/tests/helper.h: check STARPU_SSILENT instead of STARPU_SILENT

--- mpi/tests/helper.h
+++ mpi/tests/helper.h
@@ -19,7 +19,7 @@
 #define STARPU_TEST_SKIPPED 77
 
 #define FPRINTF(ofile, fmt, ...) do { if (!getenv("STARPU_SSILENT")) {fprintf(ofile, fmt, ## __VA_ARGS__); }} while(0)
-#define FPRINTF_MPI(fmt, ...) do { if (!getenv("STARPU_SILENT")) { \
+#define FPRINTF_MPI(fmt, ...) do { if (!getenv("STARPU_SSILENT")) { \
     						int _disp_rank; MPI_Comm_rank(MPI_COMM_WORLD, &_disp_rank);       \
                                                 fprintf(stderr, "[%d][starpu_mpi][%s] " fmt , _disp_rank, __starpu_func__ ,## __VA_ARGS__); \
                                                 fflush(stderr); }} while(0);
