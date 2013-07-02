525236462da56907803377c496e0959d4267b6c9
Samuel Thibault <samuel.thibault@labri.fr>
2013-07-02 13:51:21 +0000

    fix comments spuriously commited

--- src/drivers/cpu/driver_cpu.c
+++ src/drivers/cpu/driver_cpu.c
@@ -207,8 +207,10 @@ static size_t _starpu_cpu_get_global_mem_size(int devid, struct _starpu_machine_
 	if (depth_node == HWLOC_TYPE_DEPTH_UNKNOWN)
 	     global_mem = hwloc_get_root_obj(topology->hwtopology)->memory.total_memory;
 	else
+#ifdef STARPU_DEVEL
+#warning devid looks wrong
+#endif
 	     global_mem = hwloc_get_obj_by_depth(topology->hwtopology, depth_node, devid)->memory.local_memory;
-	/* devid ?! */
 
 #else /* STARPU_HAVE_HWLOC */
 #ifdef STARPU_DEVEL
