855d879585c16dbd0856e71b95933e8a3ef940fe
Thibaud Lambert <thibaud.lambert@inria.fr>
2013-08-12 14:28:13 +0000

    fix bug when using mp without mic

--- src/core/workers.h
+++ src/core/workers.h
@@ -123,7 +123,7 @@ struct _starpu_combined_worker
 	int worker_size;
 	unsigned memory_node; /* which memory node is associated that worker to ? */
 	int combined_workerid[STARPU_NMAXWORKERS];
-#ifdef STARPU_USE_MIC 
+#ifdef STARPU_USE_MP
 	int count;
 	pthread_mutex_t count_mutex;
 #endif
