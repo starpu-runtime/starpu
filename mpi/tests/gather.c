c5bdf82215d07c466f4686e6eb36021b9f0ec2ff
Nathalie Furmento <nathalie.furmento@labri.fr>
2013-09-23 12:38:00 +0000

    mpi/tests/gather.c: skip test when less than 3 nodes

--- mpi/tests/gather.c
+++ mpi/tests/gather.c
@@ -27,14 +27,14 @@ int main(int argc, char **argv)
 	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 	MPI_Comm_size(MPI_COMM_WORLD, &size);
 
-//	if (size<3)
-//	{
-//		if (rank == 0)
-//			FPRINTF(stderr, "We need more than 2 processes.\n");
-//
-//		MPI_Finalize();
-//		return STARPU_TEST_SKIPPED;
-//	}
+	if (size<3)
+	{
+		if (rank == 0)
+			FPRINTF(stderr, "We need more than 2 processes.\n");
+
+		MPI_Finalize();
+		return STARPU_TEST_SKIPPED;
+	}
 
 	ret = starpu_init(NULL);
 	STARPU_CHECK_RETURN_VALUE(ret, "starpu_init");
