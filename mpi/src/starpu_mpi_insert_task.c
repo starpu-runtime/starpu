65f72772b35ab32305aa178d250e459f5c69387e
Nathalie Furmento <nathalie.furmento@labri.fr>
2013-09-26 09:10:58 +0000

    mpi/src/starpu_mpi_insert_task.c: set task::cl_arg_free to 1 when calling starpu_mpi_insert_task, it will clean the memory allocated to pack the codelet arguments

--- mpi/src/starpu_mpi_insert_task.c
+++ mpi/src/starpu_mpi_insert_task.c
@@ -596,7 +596,10 @@ int starpu_mpi_insert_task(MPI_Comm comm, struct starpu_codelet *codelet, ...)
 
 		_STARPU_MPI_DEBUG(1, "Execution of the codelet %p (%s)\n", codelet, codelet->name);
 		va_start(varg_list, codelet);
+
 		struct starpu_task *task = starpu_task_create();
+		task->cl_arg_free = 1;
+
 		if (codelet->nbuffers > STARPU_NMAXBUFS)
 		{
 			task->dyn_handles = malloc(codelet->nbuffers * sizeof(starpu_data_handle_t));
