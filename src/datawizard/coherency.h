37a29f7ebaf01a93deb2ab2ed36c247ff5c752d5
Samuel Thibault <samuel.thibault@labri.fr>
2013-10-08 16:40:34 +0000

    Use a hash table to speed up allocation cache reuse by hashing through the data footprint

--- src/datawizard/coherency.h
+++ src/datawizard/coherency.h
@@ -1,6 +1,6 @@
 /* StarPU --- Runtime system for heterogeneous multicore architectures.
  *
- * Copyright (C) 2009-2012  Université de Bordeaux 1
+ * Copyright (C) 2009-2013  Université de Bordeaux 1
  * Copyright (C) 2010, 2011, 2012, 2013  Centre National de la Recherche Scientifique
  *
  * StarPU is free software; you can redistribute it and/or modify
