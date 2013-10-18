df8ac1c5a089113cef52a59d479652d0bb903f6e
Nathalie Furmento <nathalie.furmento@labri.fr>
2013-10-18 09:38:01 +0000

    tests/main/multithreaded.c: add error message

--- tests/main/multithreaded.c
+++ tests/main/multithreaded.c
@@ -61,7 +61,7 @@ void *thread_func(void *arg STARPU_ATTRIBUTE_UNUSED)
 		task->callback_arg = NULL;
 
 		ret = starpu_task_submit(task);
-		STARPU_ASSERT(!ret);
+		STARPU_ASSERT_MSG(!ret, "task submission failed with error code %d", ret);
 	}
 
 	ret = starpu_task_wait_for_all();
