fa5940809a575954bb2dd337275f4a0a27beb35c
Samuel Thibault <samuel.thibault@labri.fr>
2013-10-18 15:28:47 +0000

    port r 11459 from 1.1: Do not force calibration systematically when a history-based model is used. Let the scheduler trigger it when it actually needs an expected performance

--- src/core/perfmodel/perfmodel_history.c
+++ src/core/perfmodel/perfmodel_history.c
@@ -1014,12 +1014,6 @@ void _starpu_load_history_based_model(struct starpu_perfmodel *model, unsigned s
 		else
 		{
 			_STARPU_DEBUG("File does not exists\n");
-			if (!calibrate_flag)
-			{
-				_STARPU_DISP("Warning: model %s is not calibrated, forcing calibration for this run. Use the STARPU_CALIBRATE environment variable to control this.\n", model->symbol);
-				_starpu_set_calibrate_flag(1);
-				model->benchmarking = 1;
-			}
 		}
 
 		_STARPU_DEBUG("Performance model file %s for model %s is loaded\n", path, model->symbol);
