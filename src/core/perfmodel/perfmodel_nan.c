a8b5026bdb993ec003ba6c954bfe897bccd0fdcc
Nathalie Furmento <nathalie.furmento@labri.fr>
2013-11-07 09:35:16 +0000

    src/core/perfmodel/perfmodel_nan.c: we need to read the spaces after the nan value

--- src/core/perfmodel/perfmodel_nan.c
+++ src/core/perfmodel/perfmodel_nan.c
@@ -24,6 +24,22 @@
 #include <config.h>
 #include <core/perfmodel/perfmodel.h>
 
+#ifdef STARPU_HAVE_WINDOWS
+static
+void _starpu_read_spaces(FILE *f)
+{
+	int c = getc(f);
+	if (c == ' ')
+	{
+		while (c == ' ') c = getc(f);
+	}
+	else
+	{
+		ungetc(c, f);
+	}
+}
+#endif /* STARPU_HAVE_WINDOWS */
+
 int _starpu_read_double(FILE *f, char *format, double *val)
 {
 #ifdef STARPU_HAVE_WINDOWS
@@ -36,6 +52,7 @@ int _starpu_read_double(FILE *f, char *format, double *val)
 	     int x3 = getc(f);
 	     if (x2 == 'a' && x3 == 'n')
 	     {
+		     _starpu_read_spaces(f);
 		     *val = NAN;
 		     return 1;
 	     }
