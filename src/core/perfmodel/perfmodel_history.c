7a8d6e0a7a1f75fc9421f95e00e7ca1dbdd0778c
Samuel Thibault <samuel.thibault@labri.fr>
2013-11-08 13:06:00 +0000

    More readable arch name

--- src/core/perfmodel/perfmodel_history.c
+++ src/core/perfmodel/perfmodel_history.c
@@ -1142,7 +1142,7 @@ char* starpu_perfmodel_get_archtype_name(enum starpu_worker_archtype archtype)
 
 void starpu_perfmodel_get_arch_name(struct starpu_perfmodel_arch* arch, char *archname, size_t maxlen,unsigned nimpl)
 {
-	snprintf(archname, maxlen, "%s_%dncore_%dimpl_%u",
+	snprintf(archname, maxlen, "%s%d_ncore%d_impl%u",
 			starpu_perfmodel_get_archtype_name(arch->type),
 			arch->devid,
 			arch->ncore,
