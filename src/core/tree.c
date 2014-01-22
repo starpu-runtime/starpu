d83b5d8ee8e759a6787a18dc109b14acb501c6d5
Nathalie Furmento <nathalie.furmento@labri.fr>
2014-01-22 08:56:05 +0000

    src/core/tree.c: add missing include

--- src/core/tree.c
+++ src/core/tree.c
@@ -14,6 +14,7 @@
  * See the GNU Lesser General Public License in COPYING.LGPL for more details.
  */
 
+#include <stdlib.h>
 #include "starpu_tree.h"
 
 void starpu_tree_reset_visited(struct starpu_tree *tree, int *visited)
