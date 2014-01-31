a66130b75c2b0d3eefd54f1943c78cbff6649949
Nathalie Furmento <nathalie.furmento@labri.fr>
2014-01-31 13:22:37 +0000

    src/core/perfmodel/perfmodel_history.c: fix error message

--- src/core/perfmodel/perfmodel_history.c
+++ src/core/perfmodel/perfmodel_history.c
@@ -1,7 +1,7 @@
 /* StarPU --- Runtime system for heterogeneous multicore architectures.
  *
  * Copyright (C) 2009-2014  Université de Bordeaux 1
- * Copyright (C) 2010, 2011, 2012, 2013  Centre National de la Recherche Scientifique
+ * Copyright (C) 2010, 2011, 2012, 2013, 2014  Centre National de la Recherche Scientifique
  * Copyright (C) 2011  Télécom-SudParis
  *
  * StarPU is free software; you can redistribute it and/or modify
@@ -542,7 +542,7 @@ static void dump_model_file(FILE *f, struct starpu_perfmodel *model)
 							max_impl = nimpl + 1;
 				}
 				else
-					STARPU_ASSERT_MSG(0, "Unknown history-based performance model %u", archtype);
+					STARPU_ASSERT_MSG(0, "Unknown history-based performance model %u", model->type);
 
 				fprintf(f, "##########\n");
 				fprintf(f, "# %u worker(s) in parallel\n", nc+1);
