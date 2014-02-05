565d38105cf9eb99ff2b478922db686249500a2f
Samuel Thibault <samuel.thibault@labri.fr>
2014-02-05 12:44:22 +0000

    backport r12164 from trunk: Never save performance models when simulating

--- src/core/perfmodel/perfmodel_history.c
+++ src/core/perfmodel/perfmodel_history.c
@@ -704,6 +704,7 @@ static void save_history_based_model(struct starpu_perfmodel *model)
 
 static void _starpu_dump_registered_models(void)
 {
+#ifndef STARPU_SIMGRID
 	STARPU_PTHREAD_RWLOCK_WRLOCK(&registered_models_rwlock);
 
 	struct _starpu_perfmodel_list *node;
@@ -718,6 +719,7 @@ static void _starpu_dump_registered_models(void)
 	}
 
 	STARPU_PTHREAD_RWLOCK_UNLOCK(&registered_models_rwlock);
+#endif
 }
 
 void _starpu_initialize_registered_performance_models(void)
