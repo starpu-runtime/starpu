456903cb107e3d88df381df232f95909082c8d05
Olivier Aumage <olivier.aumage@inria.fr>
2014-04-01 14:54:09 +0000

    - add partial support for OpenMP environment parsing

--- src/util/openmp_runtime_support.h
+++ src/util/openmp_runtime_support.h
@@ -21,6 +21,64 @@
 
 #ifdef STARPU_OPENMP
 
+/*
+ * Arbitrary limit on the number of nested parallel sections
+ */
+#define STARPU_OMP_MAX_ACTIVE_LEVELS 4
+
+/*
+ * Proc bind modes defined by the OpenMP spec
+ */
+enum starpu_omp_bind_mode
+{
+	starpu_omp_bind_false  = 0,
+	starpu_omp_bind_true   = 1,
+	starpu_omp_bind_master = 2,
+	starpu_omp_bind_close  = 3,
+	starpu_omp_bind_spread = 4
+};
+
+enum starpu_omp_schedule_mode
+{
+	starpu_omp_schedule_static  = 0,
+	starpu_omp_schedule_dynamic = 1,
+	starpu_omp_schedule_guided  = 2,
+	starpu_omp_schedule_auto    = 3
+};
+
+/*
+ * Possible abstract names for OpenMP places
+ */
+enum starpu_omp_place_name
+{
+	starpu_omp_place_undefined = 0,
+	starpu_omp_place_threads   = 1,
+	starpu_omp_place_cores     = 2,
+	starpu_omp_place_sockets   = 3,
+	starpu_omp_place_numerical = 4 /* place specified numerically */
+};
+
+struct starpu_omp_numeric_place
+{
+	int excluded_place;
+	int *included_numeric_items;
+	int nb_included_numeric_items;
+	int *excluded_numeric_items;
+	int nb_excluded_numeric_items;
+};
+
+/*
+ * OpenMP place for thread afinity, defined by the OpenMP spec
+ */
+struct starpu_omp_place
+{
+	int abstract_name;
+	int abstract_excluded;
+	int abstract_length;
+	struct starpu_omp_numeric_place *numeric_places;
+	int nb_numeric_places;
+};
+
 /* 
  * Internal Control Variables (ICVs) declared following
  * OpenMP 4.0.0 spec section 2.3.1
@@ -75,6 +133,7 @@ struct starpu_omp_initial_icv_values
 	int nest_var;
 	int *nthreads_var;
 	int run_sched_var;
+	int run_sched_chunk_var;
 	int def_sched_var;
 	int *bind_var;
 	int stacksize_var;
@@ -86,6 +145,9 @@ struct starpu_omp_initial_icv_values
 	int place_partition_var;
 	int cancel_var;
 	int default_device_var;
+
+	/* not a real ICV, but needed to store the contents of OMP_PLACES */
+	struct starpu_omp_place places;
 };
 
 extern struct starpu_omp_global_icvs *starpu_omp_global_icvs;
