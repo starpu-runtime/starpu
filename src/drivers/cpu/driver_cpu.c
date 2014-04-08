49be1f3fb66036bc8624d629579d9dcd7ed77df9
Olivier Aumage <olivier.aumage@inria.fr>
2014-04-08 16:18:55 +0000

    - update institution list

--- src/drivers/cpu/driver_cpu.c
+++ src/drivers/cpu/driver_cpu.c
@@ -4,6 +4,7 @@
  * Copyright (C) 2010  Mehdi Juhoor <mjuhoor@gmail.com>
  * Copyright (C) 2010-2014  Centre National de la Recherche Scientifique
  * Copyright (C) 2011  Télécom-SudParis
+ * Copyright (C) 2014  Inria
  *
  * StarPU is free software; you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
