d480a253d161e51abcc95dfd5ddbb4938fac6d9b
Samuel Thibault <samuel.thibault@labri.fr>
2014-08-20 15:15:16 +0000

    backport r13273 from trunk: free data outside critical section

--- src/core/sched_ctx.c
+++ src/core/sched_ctx.c
@@ -595,10 +595,10 @@ void starpu_sched_ctx_delete(unsigned sched_ctx_id)
 
 	}
 
+	STARPU_PTHREAD_RWLOCK_UNLOCK(&changing_ctx_mutex[sched_ctx_id]);
 	/* workerids is malloc-ed in starpu_sched_ctx_get_workers_list, don't forget to free it when
 	   you don't use it anymore */
 	free(workerids);
-	STARPU_PTHREAD_RWLOCK_UNLOCK(&changing_ctx_mutex[sched_ctx_id]);
 	return;
 }
 
