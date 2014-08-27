cf392a304b38eb21594351064a53d06590487fe3
Samuel Thibault <samuel.thibault@labri.fr>
2014-08-27 11:07:20 +0000

    backport r13297 from trunk: drop spurious include

--- examples/pi/SobolQRNG/sobol_gold.c
+++ examples/pi/SobolQRNG/sobol_gold.c
@@ -1,6 +1,6 @@
 /* StarPU --- Runtime system for heterogeneous multicore architectures.
  *
- * Copyright (C) 2010-2011  Université de Bordeaux 1
+ * Copyright (C) 2010-2011, 2014  Université de Bordeaux 1
  *
  * StarPU is free software; you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
@@ -55,7 +55,6 @@
 #include <stdlib.h>
 #include <math.h>
 #include <string.h>
-#include <strings.h>
 
 #include "sobol.h"
 #include "sobol_gold.h"
