1e1a9d4ca8466bc9d6aed9151d0a1d906f3b720c
Samuel Thibault <samuel.thibault@labri.fr>
2014-09-17 09:42:27 +0000

    Do not check for stream synchronization when we do it ourselves

--- src/drivers/cuda/driver_cuda.c
+++ src/drivers/cuda/driver_cuda.c
@@ -474,10 +474,8 @@ static void execute_job_on_cuda(struct starpu_task *task, struct _starpu_worker 
 	{
 		if (worker->pipeline_length == 0)
 		{
+			/* Forced synchronous execution */
 			cudaStreamSynchronize(starpu_cuda_get_local_stream());
-#if defined(STARPU_DEBUG) && !defined(STARPU_SIMGRID)
-			STARPU_ASSERT_MSG(cudaStreamQuery(starpu_cuda_get_local_stream()) == cudaSuccess, "CUDA codelets have to wait for termination of their kernels on the starpu_cuda_get_local_stream() stream");
-#endif
 			finish_job_on_cuda(j, worker);
 		}
 		else
