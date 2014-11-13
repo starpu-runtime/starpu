51c32ebe172a0e8a863745e46e51ce5d7f3d5e7e
Nathalie Furmento <nathalie.furmento@labri.fr>
2014-11-13 13:46:59 +0000

    examples: add testcase to run application sched_ctx/sched_ctx with all defined schedulers

---
+++ examples/scheduler/schedulers_context.sh
@@ -0,0 +1,44 @@
+#!/bin/sh
+
+# StarPU --- Runtime system for heterogeneous multicore architectures.
+#
+# Copyright (C) 2012, 2014  Centre National de la Recherche Scientifique
+#
+# StarPU is free software; you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation; either version 2.1 of the License, or (at
+# your option) any later version.
+#
+# StarPU is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+#
+# See the GNU Lesser General Public License in COPYING.LGPL for more details.
+
+check_success()
+{
+    if [ $1 -ne 0 ] ; then
+	echo "failure" >&2
+        exit $1
+    fi
+}
+
+if test ! -x ./sched_ctx/sched_ctx
+then
+    echo "Application ./sched_ctx/sched_ctx unavailable"
+    exit 77
+fi
+
+SCHEDULERS=`STARPU_SCHED="help" ./basic_examples/hello_world 2>&1 | awk '/\t->/ {print $1}'`
+
+for sched in $SCHEDULERS
+do
+    # XXX peager often hangs, we have to fix it.
+    # Let's just disable it for now.
+    if [ "$sched" = "peager" ] ; then
+        continue
+    fi
+    echo "sched_ctx.$sched"
+    STARPU_SCHED=$sched ./sched_ctx/sched_ctx
+    check_success $?
+done
