f1b7d5e33652494fbecb0d1c439aea85fb3f2138
Samuel Thibault <samuel.thibault@labri.fr>
2014-12-03 11:38:19 +0000

    backport r14062 from trunk: assume double support is available in simgrid mode

--- examples/lu/xlu_kernels.c
+++ examples/lu/xlu_kernels.c
@@ -111,6 +111,10 @@ static int can_execute(unsigned workerid, struct starpu_task *task, unsigned nim
 	if (starpu_worker_get_type(workerid) == STARPU_CPU_WORKER)
 		return 1;
 
+#ifdef STARPU_SIMGRID
+	/* We don't know, let's assume it can */
+	return 1;
+#else
 	/* Cuda device */
 	const struct cudaDeviceProp *props;
 	props = starpu_cuda_get_device_properties(workerid);
@@ -124,6 +128,7 @@ static int can_execute(unsigned workerid, struct starpu_task *task, unsigned nim
 		/* Old card does not support doubles */
 		return 0;
 	}
+#endif
 }
 #endif
 
