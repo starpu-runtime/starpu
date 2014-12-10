cc8bb47d9a26ea7b47d963889c381ce2619a621b
Nathalie Furmento <nathalie.furmento@labri.fr>
2014-12-10 10:24:01 +0000

    merge trunk@14178: examples: remove call to starpu_shutdown() when starpu_init() returns ENODEV

--- examples/matvecmult/matvecmult.c
+++ examples/matvecmult/matvecmult.c
@@ -1,7 +1,7 @@
 /* StarPU --- Runtime system for heterogeneous multicore architectures.
  *
  * Copyright (C) 2010, 2011-2012  Université de Bordeaux 1
- * Copyright (C) 2010, 2011, 2012, 2013  Centre National de la Recherche Scientifique
+ * Copyright (C) 2010, 2011, 2012, 2013, 2014  Centre National de la Recherche Scientifique
  *
  * StarPU is free software; you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
@@ -164,7 +164,6 @@ int main(int argc, char **argv)
 	if (STARPU_UNLIKELY(ret == -ENODEV))
 	{
                 FPRINTF(stderr, "This application requires an OpenCL worker.\n");
-		starpu_shutdown();
 		return 77;
 	}
 
