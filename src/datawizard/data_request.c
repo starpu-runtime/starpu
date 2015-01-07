d350844aa9eae9d1e49a4c6c63b2664f7d184e42
Samuel Thibault <samuel.thibault@labri.fr>
2015-01-07 18:34:15 +0000

    backport r14268 from trunk: Fix longer transfer times in simgrid when a handle lock is busy

--- src/datawizard/data_request.c
+++ src/datawizard/data_request.c
@@ -1,7 +1,7 @@
 /* StarPU --- Runtime system for heterogeneous multicore architectures.
  *
- * Copyright (C) 2009-2014  Université de Bordeaux 1
- * Copyright (C) 2010, 2011, 2012, 2013  Centre National de la Recherche Scientifique
+ * Copyright (C) 2009-2015  Université de Bordeaux
+ * Copyright (C) 2010, 2011, 2012, 2013, 2014  Centre National de la Recherche Scientifique
  *
  * StarPU is free software; you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
@@ -675,9 +675,15 @@ static void _handle_pending_node_data_requests(unsigned src_node, unsigned force
 
 		starpu_data_handle_t handle = r->handle;
 
+#ifndef STARPU_SIMGRID
 		if (force)
 			/* Have to wait for the handle, whatever it takes */
+#endif
+			/* Or when running in simgrid, in which case we can not
+			 * afford going to sleep, since nobody would wake us
+			 * up. */
 			_starpu_spin_lock(&handle->header_lock);
+#ifndef STARPU_SIMGRID
 		else
 			if (_starpu_spin_trylock(&handle->header_lock))
 			{
@@ -686,6 +692,7 @@ static void _handle_pending_node_data_requests(unsigned src_node, unsigned force
 				kept++;
 				continue;
 			}
+#endif
 
 		/* This shouldn't be too hard to acquire */
 		_starpu_spin_lock(&r->lock);
