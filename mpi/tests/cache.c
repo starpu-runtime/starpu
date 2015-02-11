94efe6a142d9b8bcd7de1000c778c956fb8df5f3
Samuel Thibault <samuel.thibault@labri.fr>
2015-02-11 14:39:15 +0000

    Free data

--- mpi/tests/cache.c
+++ mpi/tests/cache.c
@@ -48,10 +48,12 @@ void test(struct starpu_codelet *codelet, enum starpu_data_access_mode mode, sta
 {
 	void *ptr;
 	int ret;
+	struct starpu_task *task;
 
 	// We call starpu_mpi_task_build() and starpu_mpi_task_post_build() instead of
 	// starpu_mpi_task_insert() to avoid executing the codelet as we just want to test the cache mechanism
-	starpu_mpi_task_build(MPI_COMM_WORLD, codelet, mode, data, STARPU_EXECUTE_ON_NODE, 1, 0);
+	task = starpu_mpi_task_build(MPI_COMM_WORLD, codelet, mode, data, STARPU_EXECUTE_ON_NODE, 1, 0);
+	starpu_task_destroy(task);
 	ret = starpu_mpi_task_post_build(MPI_COMM_WORLD, codelet, mode, data, STARPU_EXECUTE_ON_NODE, 1, 0);
 	STARPU_CHECK_RETURN_VALUE(ret, "starpu_mpi_task_post_build");
 
