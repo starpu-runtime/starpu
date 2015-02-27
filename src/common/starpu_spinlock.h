594c834fac5d688df41fb3da9b05dc748701487c
Nathalie Furmento <nathalie.furmento@labri.fr>
2015-02-27 13:26:45 +0000

    src/common/starpu_spinlock.h: add missing include

--- src/common/starpu_spinlock.h
+++ src/common/starpu_spinlock.h
@@ -1,7 +1,7 @@
 /* StarPU --- Runtime system for heterogeneous multicore architectures.
  *
  * Copyright (C) 2010-2014  Université de Bordeaux
- * Copyright (C) 2010, 2011, 2013, 2014  Centre National de la Recherche Scientifique
+ * Copyright (C) 2010, 2011, 2013, 2014, 2015  Centre National de la Recherche Scientifique
  *
  * StarPU is free software; you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
@@ -20,6 +20,7 @@
 #include <errno.h>
 #include <stdint.h>
 #include <common/config.h>
+#include <common/fxt.h>
 #include <starpu.h>
 
 struct _starpu_spinlock
