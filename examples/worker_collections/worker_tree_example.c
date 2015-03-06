1b72cf7d9822d92286912f87bba330c07b4cce25
Samuel Thibault <samuel.thibault@labri.fr>
2015-03-06 08:30:57 +0000

    port r14766 from trunk: free leaked memory

--- examples/worker_collections/worker_tree_example.c
+++ examples/worker_collections/worker_tree_example.c
@@ -1,6 +1,6 @@
 /* StarPU --- Runtime system for heterogeneous multicore architectures.
  *
- * Copyright (C) 2010-2014  Université de Bordeaux
+ * Copyright (C) 2010-2015  Université de Bordeaux
  * Copyright (C) 2010-2014  Centre National de la Recherche Scientifique
  *
  * StarPU is free software; you can redistribute it and/or modify
@@ -96,6 +96,7 @@ int main()
 
 	co->deinit(co);
 	starpu_shutdown();
+	free(co);
 
 	return 0;
 }
