18d1eea5dbe50109e7179517c326ffe025297477
Samuel Thibault <samuel.thibault@labri.fr>
2015-03-17 12:00:30 +0000

    Add starpu_memory_pin and starpu_memory_unpin

--- include/starpu_stdlib.h
+++ include/starpu_stdlib.h
@@ -1,6 +1,6 @@
 /* StarPU --- Runtime system for heterogeneous multicore architectures.
  *
- * Copyright (C) 2010-2014  Université de Bordeaux
+ * Copyright (C) 2010-2015  Université de Bordeaux
  * Copyright (C) 2010, 2011, 2012, 2013  Centre National de la Recherche Scientifique
  *
  * StarPU is free software; you can redistribute it and/or modify
@@ -36,6 +36,9 @@ int starpu_free(void *A);
 
 int starpu_malloc_flags(void **A, size_t dim, int flags);
 int starpu_free_flags(void *A, size_t dim, int flags);
+
+int starpu_memory_pin(void *addr, size_t size);
+int starpu_memory_unpin(void *addr, size_t size);
 
 starpu_ssize_t starpu_memory_get_total(unsigned node);
 starpu_ssize_t starpu_memory_get_available(unsigned node);
