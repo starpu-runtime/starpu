fbee05e6fe4b9d84fd314187105e02afbdd86536
Samuel Thibault <samuel.thibault@labri.fr>
2015-03-23 12:10:31 +0000

    use STARPU_MAIN_RAM instead of hardcoded 0

--- src/datawizard/malloc.c
+++ src/datawizard/malloc.c
@@ -99,9 +99,9 @@ int starpu_malloc_flags(void **A, size_t dim, int flags)
 				size_t freed;
 				size_t reclaim = 2 * dim;
 				_STARPU_DEBUG("There is not enough memory left, we are going to reclaim %ld\n", reclaim);
-				_STARPU_TRACE_START_MEMRECLAIM(0,0);
-				freed = _starpu_memory_reclaim_generic(0, 0, reclaim);
-				_STARPU_TRACE_END_MEMRECLAIM(0,0);
+				_STARPU_TRACE_START_MEMRECLAIM(STARPU_MAIN_RAM,0);
+				freed = _starpu_memory_reclaim_generic(STARPU_MAIN_RAM, 0, reclaim);
+				_STARPU_TRACE_END_MEMRECLAIM(STARPU_MAIN_RAM,0);
 				if (freed < dim)
 				{
 					// We could not reclaim enough memory
