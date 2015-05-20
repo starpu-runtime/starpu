6b13ca908ebe6106089cb332e6610213cce05de7
Olivier Aumage <olivier.aumage@inria.fr>
2015-05-20 14:47:36 +0000

    - backport r15480 from trunk: integrate Fortran90 example contributed by ONERA

---
+++ examples/fortran90/mod_interface.f90
@@ -0,0 +1,71 @@
+! StarPU --- Runtime system for heterogeneous multicore architectures.
+!
+! Copyright (C) 2015  ONERA
+! Copyright (C) 2015  Inria
+!
+! StarPU is free software; you can redistribute it and/or modify
+! it under the terms of the GNU Lesser General Public License as published by
+! the Free Software Foundation; either version 2.1 of the License, or (at
+! your option) any later version.
+!
+! StarPU is distributed in the hope that it will be useful, but
+! WITHOUT ANY WARRANTY; without even the implied warranty of
+! MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+!
+! See the GNU Lesser General Public License in COPYING.LGPL for more details.
+
+MODULE mod_interface
+
+  INTERFACE
+     SUBROUTINE starpu_register_element_c(Neq,Np,Ng,ro,dro,basis,ro_h,dro_h,basis_h) BIND(C)
+       USE iso_c_binding
+       INTEGER(KIND=C_INT),VALUE             :: Neq,Np,Ng
+       REAL(KIND=C_DOUBLE),DIMENSION(Neq,Np) :: ro,dro
+       REAL(KIND=C_DOUBLE),DIMENSION(Np,Ng)  :: basis
+       TYPE(C_PTR), INTENT(OUT)              :: ro_h, dro_h, basis_h
+     END SUBROUTINE starpu_register_element_c
+  END INTERFACE
+
+  INTERFACE
+     SUBROUTINE starpu_unregister_element_c( &
+               ro_h,dro_h,basis_h) BIND(C)
+       USE iso_c_binding
+       TYPE(C_PTR), INTENT(IN)               :: ro_h, dro_h, basis_h
+     END SUBROUTINE starpu_unregister_element_c
+  END INTERFACE
+
+  INTERFACE
+     SUBROUTINE starpu_loop_element_task_c(coeff, &
+               ro_h,dro_h,basis_h) BIND(C)
+       USE iso_c_binding
+       REAL(KIND=C_DOUBLE),VALUE             :: coeff
+       TYPE(C_PTR), INTENT(IN)               :: ro_h, dro_h, basis_h
+     END SUBROUTINE starpu_loop_element_task_c
+  END INTERFACE
+
+  INTERFACE
+     SUBROUTINE starpu_copy_element_task_c( &
+               ro_h,dro_h) BIND(C)
+       USE iso_c_binding
+       TYPE(C_PTR), INTENT(IN)               :: ro_h, dro_h
+     END SUBROUTINE starpu_copy_element_task_c
+  END INTERFACE
+
+  INTERFACE
+     FUNCTION starpu_init_c() BIND(C)
+       USE iso_c_binding
+       INTEGER(KIND=C_INT)                   :: main_starpu_init_c
+     END FUNCTION starpu_init_c
+  END INTERFACE
+
+  INTERFACE starpu_task_wait_for_all_c
+     SUBROUTINE starpu_task_wait_for_all() BIND(C)
+     END SUBROUTINE starpu_task_wait_for_all
+  END INTERFACE
+
+  INTERFACE starpu_shutdown_c
+     SUBROUTINE starpu_shutdown() BIND(C)
+     END SUBROUTINE starpu_shutdown
+  END INTERFACE
+
+END MODULE mod_interface
---
+++ examples/fortran90/mod_types.f90
@@ -0,0 +1,37 @@
+! StarPU --- Runtime system for heterogeneous multicore architectures.
+!
+! Copyright (C) 2015  ONERA
+! Copyright (C) 2015  Inria
+!
+! StarPU is free software; you can redistribute it and/or modify
+! it under the terms of the GNU Lesser General Public License as published by
+! the Free Software Foundation; either version 2.1 of the License, or (at
+! your option) any later version.
+!
+! StarPU is distributed in the hope that it will be useful, but
+! WITHOUT ANY WARRANTY; without even the implied warranty of
+! MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+!
+! See the GNU Lesser General Public License in COPYING.LGPL for more details.
+
+MODULE mod_types
+
+  USE iso_c_binding
+
+  TYPE type_numpar
+     REAL(KIND=C_DOUBLE)                        :: coeff
+     INTEGER(KIND=C_INT)                        :: Neq_max
+  END TYPE type_numpar
+
+  TYPE type_mesh_elt
+     INTEGER(KIND=C_INT)                        :: Ng, Np
+     REAL(KIND=C_DOUBLE),POINTER,DIMENSION(:,:) :: ro, dro
+     REAL(KIND=C_DOUBLE),POINTER,DIMENSION(:,:) :: basis
+     TYPE(C_PTR)                                :: ro_h, dro_h, basis_h
+  END TYPE type_mesh_elt
+
+  TYPE type_mesh
+     TYPE(type_mesh_elt), POINTER, DIMENSION(:) :: elt
+  END TYPE type_mesh
+
+END MODULE mod_types
