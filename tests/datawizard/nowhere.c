a8c50bb8f1d3dc6f0145122d6cdf6f9a51cf4dce
Samuel Thibault <samuel.thibault@labri.fr>
2015-08-06 15:29:19 +0000

    fix build

--- tests/datawizard/nowhere.c
+++ tests/datawizard/nowhere.c
@@ -70,7 +70,7 @@ int main(int argc, char **argv)
 	starpu_data_handle_t handle_x, handle_y;
 	int ret;
 
-	ret = starpu_initialize(NULL, &argc, &argv);
+	ret = starpu_init(NULL);
 	if (ret == -ENODEV) return STARPU_TEST_SKIPPED;
 	STARPU_CHECK_RETURN_VALUE(ret, "starpu_init");
 
