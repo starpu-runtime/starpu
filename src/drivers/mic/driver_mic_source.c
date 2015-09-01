4cd34556fd3349426bb00b9d39d8abf4e7565574
Nathalie Furmento <nathalie.furmento@labri.fr>
2015-09-01 13:19:31 +0000

    merge trunk@15704: src/drivers/mic/driver_mic_source.c: remove wrong code

--- src/drivers/mic/driver_mic_source.c
+++ src/drivers/mic/driver_mic_source.c
@@ -548,11 +548,6 @@ void *_starpu_mic_src_worker(void *arg)
 
 	_starpu_src_common_worker(worker_set, baseworkerid, mic_nodes[devid]);
 
-	_STARPU_TRACE_WORKER_DEINIT_START;
-
-	worker->worker_is_initialized = 0;
-	_STARPU_TRACE_WORKER_DEINIT_END(_STARPU_FUT_CUDA_KEY);
-
 	return NULL;
 
 }
