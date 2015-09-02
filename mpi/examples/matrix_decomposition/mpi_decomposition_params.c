94ac280a9be5f188e99ad7495f5019ce40f66ac8
Samuel Thibault <samuel.thibault@labri.fr>
2015-09-02 13:34:39 +0000

    backport r16025 from trunk: Make mpi cholesky use a default 240x240 tile size instead of 256

--- mpi/examples/matrix_decomposition/mpi_decomposition_params.c
+++ mpi/examples/matrix_decomposition/mpi_decomposition_params.c
@@ -1,6 +1,6 @@
 /* StarPU --- Runtime system for heterogeneous multicore architectures.
  *
- * Copyright (C) 2009, 2010  Université de Bordeaux
+ * Copyright (C) 2009, 2010, 2015  Université de Bordeaux
  * Copyright (C) 2010, 2011, 2012, 2013  Centre National de la Recherche Scientifique
  *
  * StarPU is free software; you can redistribute it and/or modify
@@ -20,7 +20,7 @@
 #include <stdio.h>
 #include <math.h>
 
-unsigned size = 4*1024;
+unsigned size = 4*960;
 unsigned nblocks = 16;
 unsigned nbigblocks = 2;
 unsigned noprio = 0;
