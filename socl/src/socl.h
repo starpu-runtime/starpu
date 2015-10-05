da1f9f2d7329187fd8e414bc901979c85658f220
Nathalie Furmento <nathalie.furmento@labri.fr>
2015-10-05 09:40:00 +0000

    merge trunk@16187: socl/src/socl.h: declare variables as extern

--- socl/src/socl.h
+++ socl/src/socl.h
@@ -1,7 +1,7 @@
 /* StarPU --- Runtime system for heterogeneous multicore architectures.
  *
  * Copyright (C) 2010-2012 University of Bordeaux
- * Copyright (C) 2012,2014 CNRS
+ * Copyright (C) 2012,2014,2015 CNRS
  *
  * StarPU is free software; you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
@@ -103,12 +103,12 @@ struct _cl_device_id {
    RETURN_EVENT(ev,event);\
 
 /* Constants */
-const char * SOCL_PROFILE;
-const char * SOCL_VERSION;
-const char * SOCL_PLATFORM_NAME;
-const char * SOCL_VENDOR;
-const char * SOCL_PLATFORM_EXTENSIONS;
-const char * SOCL_PLATFORM_ICD_SUFFIX_KHR;
+extern const char * SOCL_PROFILE;
+extern const char * SOCL_VERSION;
+extern const char * SOCL_PLATFORM_NAME;
+extern const char * SOCL_VENDOR;
+extern const char * SOCL_PLATFORM_EXTENSIONS;
+extern const char * SOCL_PLATFORM_ICD_SUFFIX_KHR;
 
 struct _cl_context {
   CL_ENTITY;
@@ -289,7 +289,7 @@ struct _cl_kernel {
  * This allows us to disable StarPU profiling it
  * is equal to 0
  */
-int profiling_queue_count;
+extern int profiling_queue_count;
 
 /***************************************************************************/
 
@@ -778,9 +778,9 @@ soclIcdGetPlatformIDsKHR(cl_uint          /* num_entries */,
                  cl_uint *        /* num_platforms */) CL_EXT_SUFFIX__VERSION_1_0;
 
 
-struct _cl_icd_dispatch socl_master_dispatch;
-struct _cl_platform_id socl_platform;
-struct _cl_device_id * socl_devices;
+extern struct _cl_icd_dispatch socl_master_dispatch;
+extern struct _cl_platform_id socl_platform;
+extern struct _cl_device_id * socl_devices;
 extern unsigned int socl_device_count;
 
 #endif /* SOCL_H */
