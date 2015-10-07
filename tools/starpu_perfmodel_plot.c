78cf7009417200b6490efb057715ed91d85c9728
Olivier Aumage <olivier.aumage@inria.fr>
2015-10-07 15:15:10 +0000

    - take into account the combined worker identification in the grep expression - give some more helpful feedback to the user when specifiying both the gflops option and a trace file

--- tools/starpu_perfmodel_plot.c
+++ tools/starpu_perfmodel_plot.c
@@ -222,10 +222,14 @@ static void display_perf_model(FILE *gnuplot_file, struct starpu_perfmodel_arch*
 	starpu_perfmodel_get_arch_name(arch, arch_name, 256, impl);
 
 #ifdef STARPU_USE_FXT
-	if (!options->gflops && options->with_fxt_file && impl == 0)
+	if (options->with_fxt_file && impl == 0)
 	{
-		print_comma(gnuplot_file, first);
-		fprintf(gnuplot_file, "\"< grep -w \\^%s %s\" using 2:3 title \"Profiling %s\"", arch_name, options->data_file_name, replace_char(arch_name, '_', '-'));
+		if (options->gflops) {
+                        _STARPU_DISP("gflops unit selected, ignoring fxt trace\n");
+		} else {
+			print_comma(gnuplot_file, first);
+			fprintf(gnuplot_file, "\"< grep '^%s' %s\" using 3:4 title \"Profiling %s\"", arch_name, options->data_file_name, replace_char(arch_name, '_', '-'));
+		}
 	}
 #endif
 
@@ -425,7 +429,7 @@ static void display_selected_models(FILE *gnuplot_file, struct starpu_perfmodel 
 
 	/* If no input data is given to gnuplot, we at least need to specify an
 	 * arbitrary range. */
-	if (options->with_fxt_file == 0)
+	if (options->with_fxt_file == 0 || options->gflops)
 		fprintf(gnuplot_file, "set xrange [1:10**9]\n\n");
 
 	int first = 1;
