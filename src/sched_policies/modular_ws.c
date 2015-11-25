1918c0abc6b3dace93dc46fffbaef6a8d93255de
Samuel Pitoiset <samuel.pitoiset@inria.fr>
2015-11-25 10:47:32 +0000

    sched_policies: add missing files for modular-ws sched
    I don't know if it works but let include these files instead of removing
    them for now.

--- src/sched_policies/modular_ws.c
+++ src/sched_policies/modular_ws.c
@@ -28,7 +28,7 @@ static void initialize_ws_center_policy(unsigned sched_ctx_id)
 
 	unsigned i;
 	for(i = 0; i < starpu_worker_get_count() + starpu_combined_worker_get_count(); i++)
-		starpu_sched_component_connect(t->root, starpu_sched_component_worker_get(sched_ctx_id, i);
+		starpu_sched_component_connect(t->root, starpu_sched_component_worker_get(sched_ctx_id, i));
 
 	starpu_sched_tree_update_workers(t);
 	starpu_sched_ctx_set_policy_data(sched_ctx_id, (void*)t);
