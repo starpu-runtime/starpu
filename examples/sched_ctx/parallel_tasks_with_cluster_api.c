5072adf4a1598fc6fb86ebb3870fd2f44a771a72
Terry Cojean <terry.cojean@inria.fr>
2015-12-14 10:12:44 +0000

    Add a cluster management API. The purpose is to facilitate a partition of the machine and set it to process parallel tasks. It is a first version and can change in the future.

---
+++ examples/sched_ctx/parallel_tasks_with_cluster_api.c
@@ -0,0 +1,124 @@
+/* StarPU --- Runtime system for heterogeneous multicore architectures.
+ *
+ * Copyright (C) 2015 Université de Bordeaux
+ * Copyright (C) 2015 INRIA
+ * Copyright (C) 2015 CNRS
+ *
+ * StarPU is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 2.1 of the License, or (at
+ * your option) any later version.
+ *
+ * StarPU is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ * See the GNU Lesser General Public License in COPYING.LGPL for more details.
+ */
+
+#include <starpu.h>
+#include <omp.h>
+
+#ifdef STARPU_QUICK_CHECK
+#define NTASKS 8
+#else
+#define NTASKS 32
+#endif
+
+
+#define SIZE 4000
+
+/* Codelet SUM */
+static void sum_cpu(void * descr[], void *cl_arg)
+{
+	double * v_dst = (double *) STARPU_VECTOR_GET_PTR(descr[0]);
+	double * v_src0 = (double *) STARPU_VECTOR_GET_PTR(descr[1]);
+	double * v_src1 = (double *) STARPU_VECTOR_GET_PTR(descr[1]);
+
+	int size;
+	starpu_codelet_unpack_args(cl_arg, &size);
+	int i, k;
+	for (k=0;k<10;k++)
+	{
+#pragma omp parallel for
+		for (i=0; i<size; i++)
+		{
+			v_dst[i]+=v_src0[i]+v_src1[i];
+		}
+	}
+}
+
+static struct starpu_codelet sum_cl =
+{
+	.cpu_funcs = {sum_cpu, NULL},
+	.nbuffers = 3,
+	.modes={STARPU_RW,STARPU_R, STARPU_R}
+};
+
+int main(int argc, char **argv)
+{
+	int ntasks = NTASKS;
+	int ret, i;
+	struct starpu_cluster_machine *clusters;
+
+	ret = starpu_init(NULL);
+	if (ret == -ENODEV)
+		return 77;
+	STARPU_CHECK_RETURN_VALUE(ret, "starpu_init");
+
+	/* We regroup resources under each sockets into a cluster. We express a partition
+	 * of one socket to create two internal clusters */
+	clusters = starpu_cluster_machine(HWLOC_OBJ_SOCKET,
+									  STARPU_CLUSTER_PARTITION_ONE, STARPU_CLUSTER_NB, 2,
+									  0);
+	starpu_cluster_print(clusters);
+
+	/* Data preparation */
+	double array1[SIZE];
+	double array2[SIZE];
+
+	memset(array1, 0, sizeof(double));
+	for (i=0;i<SIZE;i++)
+	{
+		array2[i]=i*2;
+	}
+
+	starpu_data_handle_t handle1;
+	starpu_data_handle_t handle2;
+
+	starpu_vector_data_register(&handle1, 0, (uintptr_t)array1, SIZE, sizeof(double));
+	starpu_vector_data_register(&handle2, 0, (uintptr_t)array2, SIZE, sizeof(double));
+
+	int size = SIZE;
+
+	for (i = 0; i < ntasks; i++)
+	{
+		struct starpu_task * t;
+		t=starpu_task_build(&sum_cl,
+				    STARPU_RW,handle1,
+				    STARPU_R,handle2,
+				    STARPU_R,handle1,
+				    STARPU_VALUE,&size,sizeof(int),
+				    0);
+		t->destroy = 1;
+		t->possibly_parallel = 1;
+
+		ret=starpu_task_submit(t);
+		if (ret == -ENODEV)
+			goto out;
+		STARPU_CHECK_RETURN_VALUE(ret, "starpu_task_submit");
+	}
+
+
+
+out:
+	/* wait for all tasks at the end*/
+	starpu_task_wait_for_all();
+
+	starpu_data_unregister(handle1);
+	starpu_data_unregister(handle2);
+	starpu_uncluster_machine(clusters);
+
+	starpu_shutdown();
+	return 0;
+}
---
+++ src/util/starpu_clusters_create.h
@@ -0,0 +1,118 @@
+/* StarPU --- Runtime system for heterogeneous multicore architectures.
+ *
+ * Copyright (C) 2015  Université de Bordeaux
+ * Copyright (C) 2015  INRIA
+ * Copyright (C) 2015  CNRS
+ *
+ * StarPU is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 2.1 of the License, or (at
+ * your option) any later version.
+ *
+ * StarPU is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ * See the GNU Lesser General Public License in COPYING.LGPL for more details.
+ */
+
+#ifndef __STARPU_CLUSTERS_CREATE_H__
+#define __STARPU_CLUSTERS_CREATE_H__
+
+#include "starpu_clusters_util.h"
+#include <core/workers.h>
+#include <common/list.h>
+#include <string.h>
+#ifdef STARPU_OPENMP
+#include <omp.h>
+#endif
+#ifdef STARPU_MKL
+#include <mkl_service.h>
+#endif
+
+#ifdef __cplusplus
+extern
+#endif
+
+struct _starpu_cluster_parameters
+{
+		int min_nb;
+		int max_nb;
+		int nb;
+		char* sched_policy_name;
+		struct starpu_sched_policy* sched_policy_struct;
+		unsigned keep_homogeneous;
+		unsigned prefere_min;
+		void (*create_func)(void*);
+		void* create_func_arg;
+		int type;
+		unsigned awake_workers;
+};
+
+LIST_TYPE(_starpu_cluster_group,
+		  unsigned id;
+		  hwloc_obj_t group_obj;
+		  int nclusters;
+		  struct _starpu_cluster_list* clusters;
+		  struct starpu_cluster_machine* father;
+		  struct _starpu_cluster_parameters* params;
+		);
+
+LIST_TYPE(_starpu_cluster,
+		  unsigned id;
+		  hwloc_cpuset_t cpuset;
+		  int ncores;
+		  int* cores;
+		  int* workerids;
+		  struct _starpu_cluster_group* father;
+		  struct _starpu_cluster_parameters* params;
+		);
+
+
+/* Machine discovery and cluster creation main funcitons */
+void _starpu_cluster_machine(hwloc_obj_type_t cluster_level,
+							 struct starpu_cluster_machine* machine);
+void _starpu_cluster_topology(hwloc_obj_type_t cluster_level,
+							  struct starpu_cluster_machine* machine);
+void _starpu_cluster_group(hwloc_obj_type_t cluster_level,
+						   struct starpu_cluster_machine* machine);
+void _starpu_cluster(struct _starpu_cluster_group* group);
+
+/* Parameter functions */
+void _starpu_cluster_init_parameters(struct _starpu_cluster_parameters* globals);
+void _starpu_cluster_copy_parameters(struct _starpu_cluster_parameters* src,
+									 struct _starpu_cluster_parameters* dst);
+int _starpu_cluster_analyze_parameters(struct _starpu_cluster_parameters* params, int npus);
+
+/* Cluster helper functions */
+void _starpu_cluster_init(struct _starpu_cluster* cluster, struct _starpu_cluster_group* father);
+void _starpu_cluster_create(struct _starpu_cluster* cluster);
+
+int _starpu_cluster_bind(struct _starpu_cluster* cluster);
+int _starpu_cluster_remove(struct _starpu_cluster_list* cluster_list,
+						   struct _starpu_cluster* cluster);
+
+/* Cluster group helper function */
+void _starpu_cluster_group_init(struct _starpu_cluster_group* group,
+								struct starpu_cluster_machine* father);
+void _starpu_cluster_group_create(struct _starpu_cluster_group* group);
+int _starpu_cluster_group_remove(struct _starpu_cluster_group_list* group_list,
+								 struct _starpu_cluster_group* group);
+
+/* Binding helpers */
+void _starpu_cluster_noop(void* buffers[], void* cl_arg)
+{
+}
+
+static struct starpu_codelet _starpu_cluster_bind_cl=
+{
+		.cpu_funcs = {_starpu_cluster_noop},
+		.nbuffers = 0,
+		.name = "cluster internal runtime init"
+};
+
+
+typedef void (*starpu_binding_function)(void*);
+starpu_binding_function _starpu_cluster_type_get_func(starpu_cluster_types type);
+
+#endif /* __STARPU_CLUSTERS_CREATE_H__ */
