3242b495fd872dc2304655ce1f036ab1cfc493fd
Samuel Thibault <samuel.thibault@labri.fr>
2016-03-17 09:46:18 +0000

    Document the new behavior

--- src/util/starpu_task_insert.c
+++ src/util/starpu_task_insert.c
@@ -1,6 +1,6 @@
 /* StarPU --- Runtime system for heterogeneous multicore architectures.
  *
- * Copyright (C) 2010, 2012, 2014-2015  Université de Bordeaux
+ * Copyright (C) 2010, 2012, 2014-2016  Université de Bordeaux
  * Copyright (C) 2011, 2012, 2013, 2014, 2015  CNRS
  *
  * StarPU is free software; you can redistribute it and/or modify
@@ -50,7 +50,7 @@ void starpu_codelet_unpack_args(void *_cl_arg, ...)
 	{
 		void *argptr = va_arg(varg_list, void *);
 
-		// If not reading all cl_args
+		/* If not reading all cl_args */
 		if(argptr == NULL)
 			break;
 
