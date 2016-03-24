0f4d88859395a5504eb6a0b001136ffa19109d2b
Samuel Thibault <samuel.thibault@labri.fr>
2016-03-24 17:33:31 +0000

    avoid expensive loop when not using contexts

--- src/core/sched_ctx.c
+++ src/core/sched_ctx.c
@@ -1946,6 +1946,9 @@ unsigned starpu_sched_ctx_master_get_context(int masterid)
 
 struct _starpu_sched_ctx *_starpu_sched_ctx_get_sched_ctx_for_worker_and_job(struct _starpu_worker *worker, struct _starpu_job *j)
 {
+	if (_starpu_get_nsched_ctxs() == 1)
+		return _starpu_get_sched_ctx_struct(0);
+
 	struct _starpu_sched_ctx_elt *e = NULL;
 	struct _starpu_sched_ctx_list_iterator list_it;
 	struct _starpu_sched_ctx *sched_ctx = NULL;
