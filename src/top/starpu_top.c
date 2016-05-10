09c16bf44b805e577087a5ae4947f5c19489d579
Samuel Thibault <samuel.thibault@labri.fr>
2016-05-10 12:48:10 +0000

    fix memory leak in starpu_top

--- src/top/starpu_top.c
+++ src/top/starpu_top.c
@@ -220,6 +220,16 @@ void starpu_top_init_and_wait(const char* server_name)
 	sem_wait(&starpu_top_wait_for_go);
 }
 
+void _starpu_top_shutdown(void)
+{
+	struct starpu_top_data * cur = starpu_top_first_data, * next;
+	while (cur) {
+		next = cur->next;
+		free(cur);
+		cur = next;
+	}
+}
+
 static
 void starpu_top_enqueue_data(struct starpu_top_data * data)
 {
