8babce9b4cfb3a7ec39c104b721e948fdcb77c7e
Samuel Thibault <samuel.thibault@labri.fr>
2016-05-11 11:34:40 +0000

    port r17418 from 1.2: catch usan runtime errors

--- starpu.mk
+++ starpu.mk
@@ -4,6 +4,7 @@ showcheck:
 	! grep -q "WARNING: AddressSanitizer: " $(TEST_LOGS) /dev/null
 	! grep -q "ERROR: ThreadSanitizer: " $(TEST_LOGS) /dev/null
 	! grep -q "WARNING: ThreadSanitizer: " $(TEST_LOGS) /dev/null
+	! grep -q " runtime error: " $(TEST_LOGS) /dev/null
 	RET=0 ; \
 	for i in $(SUBDIRS) ; do \
 		make -C $$i showcheck || RET=1 ; \
