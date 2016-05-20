fb19bfd5d51f0d6eac15218690bc6a7a7703d030
Nathalie Furmento <nathalie.furmento@labri.fr>
2016-05-20 14:38:26 +0000

    tools/valgrind: update suppression files

--- tools/valgrind/valgrind.sh
+++ tools/valgrind/valgrind.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 SUPPRESSIONS=$(for f in $(dirname $0)/*.suppr ; do echo "--suppressions=$f" ; done)
-valgrind $SUPPRESSIONS --leak-check=full --show-leak-kinds=all --gen-suppressions=all $*
+valgrind $SUPPRESSIONS -v --num-callers=42 --error-exitcode=42 --track-origins=yes --leak-check=full --show-reachable=yes --errors-for-leak-kinds=all --show-leak-kinds=all --gen-suppressions=all $*
