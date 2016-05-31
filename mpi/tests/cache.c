cf15571eb6309aad272c896b7f09051a77a58529
Nathalie Furmento <nathalie.furmento@labri.fr>
2016-05-31 14:53:40 +0000

    merge trunk@17509: mpi/tests: properly allocate memory

--- mpi/tests/cache.c
+++ mpi/tests/cache.c
@@ -1,6 +1,6 @@
 /* StarPU --- Runtime system for heterogeneous multicore architectures.
  *
- * Copyright (C) 2015  CNRS
+ * Copyright (C) 2015, 2016  CNRS
  *
  * StarPU is free software; you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
@@ -85,7 +85,7 @@ int main(int argc, char **argv)
 {
 	int rank, n;
 	int ret;
-	unsigned val;
+	unsigned val = 42;
 	starpu_data_handle_t data;
 
 	ret = starpu_init(NULL);
