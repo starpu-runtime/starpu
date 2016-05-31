7257c1eef6726df161b724a3e0e1a8597aae7e1f
Nathalie Furmento <nathalie.furmento@labri.fr>
2016-05-31 13:51:31 +0000

    merge trunk@17488: tools/valgrind: add suppression rules

--- tools/valgrind/valgrind.sh
+++ tools/valgrind/valgrind.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 SUPPRESSIONS=$(for f in $(dirname $0)/*.suppr ; do echo "--suppressions=$f" ; done)
-valgrind $SUPPRESSIONS --leak-check=full --show-leak-kinds=all $*
+valgrind $SUPPRESSIONS --leak-check=full --show-leak-kinds=all --gen-suppressions=all $*
