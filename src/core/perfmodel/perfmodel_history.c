417813cce098040f8436e93ff4a1ce1776c26a52
Samuel Thibault <samuel.thibault@labri.fr>
2016-06-04 09:00:13 +0000

    Fix unlocking model

--- src/core/perfmodel/perfmodel_history.c
+++ src/core/perfmodel/perfmodel_history.c
@@ -1478,6 +1478,7 @@ void _starpu_update_perfmodel_history(struct _starpu_job *j, struct starpu_perfm
 		if (f == NULL)
 		{
 			_STARPU_DISP("Error <%s> when opening file <%s>\n", strerror(errno), per_arch_model->debug_path);
+			STARPU_PTHREAD_RWLOCK_UNLOCK(&model->state->model_rwlock);
 			return;
 		}
 		_starpu_fwrlock(f);
