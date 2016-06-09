d1fd7dbfc9644aa13bf75b2530fc0e5fd5923ef4
Samuel Thibault <samuel.thibault@labri.fr>
2016-06-09 12:27:53 +0000

    '-' can not appear in function name

--- starpu-mynvcc.mk
+++ starpu-mynvcc.mk
@@ -8,4 +8,4 @@ V_mynvcc_1 =
 V_mynvcc = $(V_mynvcc_$(V))
 .cu.o:
 	@$(MKDIR_P) `dirname $@`
-	$(V_mynvcc)grep 'extern *"C" *void *' $< | sed -ne 's/extern *"C" *void *\([a-zA-Z0-9_-]*\) *(.*/void \1(void) {}/p' | $(CC) -x c - -o $@ -c
+	$(V_mynvcc)grep 'extern *"C" *void *' $< | sed -ne 's/extern *"C" *void *\([a-zA-Z0-9_]*\) *(.*/void \1(void) {}/p' | $(CC) -x c - -o $@ -c
