f736e9c094a58b3b9828248233f49239f9ed3121
Nathalie Furmento <nathalie.furmento@labri.fr>
2016-06-14 11:49:36 +0000

    tests/main/insert_task_pack.c: remove unreachable code

--- tests/main/insert_task_pack.c
+++ tests/main/insert_task_pack.c
@@ -53,11 +53,4 @@ int main(int argc, char **argv)
 
 	FPRINTF(stderr, "Task %p\n", task);
 	return (task==NULL)?0:1;
-
-enodev:
-	starpu_shutdown();
-	fprintf(stderr, "WARNING: No one can execute this task\n");
-	/* yes, we do not perform the computation but we did detect that no one
- 	 * could perform the kernel, so this is not an error from StarPU */
-	return STARPU_TEST_SKIPPED;
 }
