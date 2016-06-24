581ff6326d8a557c826dc1a8412b24f1b59cde21
Samuel Thibault <samuel.thibault@labri.fr>
2016-06-24 19:55:35 +0000

    port r18004 from trunk: Make sure to have both v3 and v4 simgrid platform files, since we don't know at that point which version will be needed

--- src/core/perfmodel/perfmodel_bus.c
+++ src/core/perfmodel/perfmodel_bus.c
@@ -2405,7 +2405,7 @@ static void check_bus_platform_file(void)
 
 	res = access(path, F_OK);
 
-	if (res)
+	if (!res)
 	{
 		_starpu_simgrid_get_platform_path(3, path, sizeof(path));
 		res = access(path, F_OK);
