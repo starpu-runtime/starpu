2987ab7461ae483be821b7b58d86721afdebd5cc
Samuel Thibault <samuel.thibault@labri.fr>
2016-07-05 16:01:12 +0000

    port r18176 from trunk: Fix starpu mpi simgrid fxt traces

--- mpi/src/starpu_mpi.c
+++ mpi/src/starpu_mpi.c
@@ -1293,7 +1293,6 @@ static void *_starpu_mpi_progress_thread_func(void *arg)
 #endif //STARPU_USE_FXT
 	}
 
-	_starpu_mpi_add_sync_point_in_fxt();
 	_starpu_mpi_comm_amounts_init(argc_argv->comm);
 	_starpu_mpi_cache_init(argc_argv->comm);
 	_starpu_mpi_select_node_init();
@@ -1323,6 +1322,7 @@ static void *_starpu_mpi_progress_thread_func(void *arg)
 	smpi_process_set_user_data(calloc(MAX_TSD, sizeof(void*)));
 #endif
 #endif
+	_starpu_mpi_add_sync_point_in_fxt();
 
 	STARPU_PTHREAD_MUTEX_LOCK(&mutex);
 
@@ -1536,6 +1536,10 @@ static int hookid = - 1;
 static void _starpu_mpi_add_sync_point_in_fxt(void)
 {
 #ifdef STARPU_USE_FXT
+	STARPU_PTHREAD_MUTEX_LOCK(&_starpu_fxt_started_mutex);
+	while (!_starpu_fxt_started)
+		STARPU_PTHREAD_COND_WAIT(&_starpu_fxt_started_cond, &_starpu_fxt_started_mutex);
+	STARPU_PTHREAD_MUTEX_UNLOCK(&_starpu_fxt_started_mutex);
 	int rank;
 	int worldsize;
 	int ret;
