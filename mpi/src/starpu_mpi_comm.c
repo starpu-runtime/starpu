260a2ed5570adb0add9e7752808e99ce8c798e96
Samuel Thibault <samuel.thibault@labri.fr>
2016-08-05 08:58:03 +0000

    Reduce the granularity of simulation of CUDA and MPI probes, it is not that expensive.

--- mpi/src/starpu_mpi_comm.c
+++ mpi/src/starpu_mpi_comm.c
@@ -1,7 +1,7 @@
 /* StarPU --- Runtime system for heterogeneous multicore architectures.
  *
  * Copyright (C) 2011, 2012, 2013, 2014, 2015, 2016  CNRS
- * Copyright (C) 2011-2015  Université de Bordeaux
+ * Copyright (C) 2011-2016  Université de Bordeaux
  * Copyright (C) 2014 INRIA
  *
  * StarPU is free software; you can redistribute it and/or modify
@@ -130,7 +130,7 @@ int _starpu_mpi_comm_test_recv(MPI_Status *status, struct _starpu_mpi_envelope *
 		{
 			/* test whether an envelope has arrived. */
 #ifdef STARPU_SIMGRID
-			MSG_process_sleep(0.000010);
+			MSG_process_sleep(0.000001);
 #endif
 			res = MPI_Test(&_comm->request, &flag, status);
 			STARPU_ASSERT(res == MPI_SUCCESS);
