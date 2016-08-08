1f61da76846c117eb9737091c4ec3c3b88caf04e
Samuel Thibault <samuel.thibault@labri.fr>
2016-08-08 17:22:37 +0000

    Add microbenchmarks from Bérangère Subervie's internship

---
+++ tests/microbenchs/parallel_independent_homogeneous_tasks_data.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# StarPU --- Runtime system for heterogeneous multicore architectures.
+#
+# Copyright (C) 2016  Université de Bordeaux
+#
+# StarPU is free software; you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation; either version 2.1 of the License, or (at
+# your option) any later version.
+#
+# StarPU is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+#
+# See the GNU Lesser General Public License in COPYING.LGPL for more details.
+
+source $srcdir/microbenchs/microbench.sh
+
+XFAIL="modular-eager modular-eager-prefetching modular-prio modular-prio-prefetching modular-random modular-random-prio modular-random-prefetching modular-random-prio-prefetching modular-ws modular-heft modular-heft2 eager prio random lws ws dm dmda dmdar dmdas dmdasd pheft peager heteroprio graph_test"
+
+test_scheds parallel_independent_homogeneous_tasks_data
