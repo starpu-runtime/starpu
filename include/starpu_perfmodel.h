dfd51e27402dc42e40c5c0acee8ff85989b067f7
Nathalie Furmento <nathalie.furmento@labri.fr>
2016-09-05 11:54:42 +0000

    merge trunk@18631: doc: add missing documentation and remove now-deleted stuff

--- include/starpu_perfmodel.h
+++ include/starpu_perfmodel.h
@@ -1,7 +1,7 @@
 /* StarPU --- Runtime system for heterogeneous multicore architectures.
  *
  * Copyright (C) 2010-2014, 2016  Université de Bordeaux
- * Copyright (C) 2010, 2011, 2012, 2013, 2014, 2015  CNRS
+ * Copyright (C) 2010, 2011, 2012, 2013, 2014, 2015, 2016  CNRS
  * Copyright (C) 2011  Télécom-SudParis
  *
  * StarPU is free software; you can redistribute it and/or modify
@@ -38,8 +38,8 @@ struct starpu_data_descr;
 struct starpu_perfmodel_device
 {
 	enum starpu_worker_archtype type;
-	int devid;	/* identifier of the precise device */
-	int ncores;	/* number of execution in parallel, minus 1 */
+	int devid;
+	int ncores;
 };
 
 struct starpu_perfmodel_arch
