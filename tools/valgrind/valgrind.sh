42d4a53afd9a083b160e8057d9e9868a3fc3bdba
Nathalie Furmento <nathalie.furmento@labri.fr>
2016-09-12 14:24:32 +0000

    tools/valgrind: modify script to start valgrind or helgrind

--- tools/valgrind/valgrind.sh
+++ tools/valgrind/valgrind.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
+EXEC=$(basename $0 .sh)
 SUPPRESSIONS=$(for f in $(dirname $0)/*.suppr ; do echo "--suppressions=$f" ; done)
-valgrind -v --num-callers=42 --error-exitcode=42 --track-origins=yes --leak-check=full --show-reachable=yes --errors-for-leak-kinds=all --show-leak-kinds=all --gen-suppressions=all $SUPPRESSIONS $*
+$EXEC -v --num-callers=42 --error-exitcode=42 --track-origins=yes --leak-check=full --show-reachable=yes --errors-for-leak-kinds=all --show-leak-kinds=all --gen-suppressions=all $SUPPRESSIONS $*
