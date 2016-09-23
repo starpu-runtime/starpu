1b8a30aa985a0b1f2a40c4df2589cb9ac077ecff
Nathalie Furmento <nathalie.furmento@labri.fr>
2016-09-23 10:52:18 +0000

    merge trunk@18795,18796: fix copyright

--- include/pthread_win32/semaphore.h
+++ include/pthread_win32/semaphore.h
@@ -1,13 +1,14 @@
-/*
- * StarPU
- * Copyright (C) Université Bordeaux, CNRS 2010 (see AUTHORS file)
+/* StarPU --- Runtime system for heterogeneous multicore architectures.
  *
- * This program is free software; you can redistribute it and/or modify
+ * Copyright (C) 2010 Université Bordeaux
+ * Copyright (C) 2010  CNRS
+ *
+ * StarPU is free software; you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation; either version 2.1 of the License, or (at
  * your option) any later version.
  *
- * This program is distributed in the hope that it will be useful, but
+ * StarPU is distributed in the hope that it will be useful, but
  * WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
  *
--- tests/main/combined_workers/bfs/run.sh
+++ tests/main/combined_workers/bfs/run.sh
@@ -1,3 +1,21 @@
+#!/bin/bash
+#
+# StarPU --- Runtime system for heterogeneous multicore architectures.
+#
+# Copyright (C) 2014 Université de Bordeaux
+# Copyright (C) 2012 Inria
+#
+# StarPU is free software; you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation; either version 2.1 of the License, or (at
+# your option) any later version.
+#
+# StarPU is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+#
+# See the GNU Lesser General Public License in COPYING.LGPL for more details.
+
 #export STARPU_GENERATE_TRACE=1
 #export GOMP_CPU_AFFINITY="0 6 1 7 2 8 3 9 4 10 5 11"
 #export OMP_WAIT_POLICY=PASSIVE
