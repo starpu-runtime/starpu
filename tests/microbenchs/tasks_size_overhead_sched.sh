a1fbbfc7d01433e31f6c17128cbd99365c80db60
Nathalie Furmento <nathalie.furmento@labri.fr>
2016-11-04 09:41:28 +0000

    tests/microbenchs/tasks_size_overhead_sched.sh: do not fail when gnuplot unavailable

--- tests/microbenchs/tasks_size_overhead_sched.sh
+++ tests/microbenchs/tasks_size_overhead_sched.sh
@@ -3,7 +3,7 @@
 # StarPU --- Runtime system for heterogeneous multicore architectures.
 #
 # Copyright (C) 2009, 2010, 2016  Université de Bordeaux
-# Copyright (C) 2010, 2011  CNRS
+# Copyright (C) 2010, 2011, 2016  CNRS
 #
 # StarPU is free software; you can redistribute it and/or modify
 # it under the terms of the GNU Lesser General Public License as published by
@@ -20,8 +20,10 @@ ROOT=${0%.sh}
 ROOT=${ROOT%_sched}
 unset STARPU_SSILENT
 $ROOT "$@" > tasks_size_overhead.output
+ret=$?
 DIR=
 [ -z "$STARPU_BENCH_DIR" ] || DIR="$STARPU_BENCH_DIR/"
 export TERMINAL=png
 export OUTFILE=${DIR}tasks_size_overhead_${STARPU_SCHED}.png
 $ROOT.gp
+exit $ret
