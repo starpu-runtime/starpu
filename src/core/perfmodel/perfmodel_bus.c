ced5880fc30f623190ab79877f6e4036955ed8af
Samuel Thibault <samuel.thibault@labri.fr>
2016-12-01 10:25:58 +0000

    fix warning

--- src/core/perfmodel/perfmodel_bus.c
+++ src/core/perfmodel/perfmodel_bus.c
@@ -2513,7 +2513,9 @@ double starpu_transfer_predict(unsigned src_node, unsigned dst_node, size_t size
 	double latency = latency_matrix[src_node][dst_node];
 	struct _starpu_machine_topology *topology = &_starpu_get_machine_config()->topology;
 	int busid = starpu_bus_get_id(src_node, dst_node);
+#if 0
 	int direct = starpu_bus_get_direct(busid);
+#endif
 	float ngpus = topology->ncudagpus+topology->nopenclgpus;
 
 #if 0
