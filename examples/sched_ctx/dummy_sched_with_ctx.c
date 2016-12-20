9e50ed246866acf05812d6354cc6afef407d460e
Samuel Thibault <samuel.thibault@labri.fr>
2016-12-20 11:39:48 +0000

    revert part of 19482: apparently we shouldn't return NULL to the sched ctx engine, even in the non-blocking mode

--- examples/sched_ctx/dummy_sched_with_ctx.c
+++ examples/sched_ctx/dummy_sched_with_ctx.c
@@ -118,10 +118,6 @@ static struct starpu_task *pop_task_dummy(unsigned sched_ctx_id)
 	 * the calling worker. So we just take the head of the list and give it
 	 * to the worker. */
 	struct dummy_sched_data *data = (struct dummy_sched_data*)starpu_sched_ctx_get_policy_data(sched_ctx_id);
-#ifdef STARPU_NON_BLOCKING_DRIVERS
-	if (starpu_task_list_empty(&data->sched_list))
-		return NULL;
-#endif
 	STARPU_PTHREAD_MUTEX_LOCK(&data->policy_mutex);
 	struct starpu_task *task = starpu_task_list_pop_back(&data->sched_list);
 	STARPU_PTHREAD_MUTEX_UNLOCK(&data->policy_mutex);
