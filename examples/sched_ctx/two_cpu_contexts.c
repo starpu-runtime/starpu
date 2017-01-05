ecb09b0f18a5c1dc130d405dc52aa6b8dba15a6f
Nathalie Furmento <nathalie.furmento@labri.fr>
2017-01-05 10:54:19 +0000

    merge trunk@19530: examples/: move cppcheck suppression rules from source code to dedicated file

--- examples/sched_ctx/two_cpu_contexts.c
+++ examples/sched_ctx/two_cpu_contexts.c
@@ -73,7 +73,6 @@ int main(int argc, char **argv)
 	}
 
         /* create sched context 1 with default policy, by giving a NULL policy name */
-	// cppcheck-suppress varFuncNullUB
 	unsigned sched_ctx1 = starpu_sched_ctx_create(procs1, nprocs1, "ctx1", STARPU_SCHED_CTX_POLICY_NAME, NULL, 0);
         /* create sched context 2 with a user selected policy name */
 	unsigned sched_ctx2 = starpu_sched_ctx_create(procs2, nprocs2, "ctx2", STARPU_SCHED_CTX_POLICY_NAME, "eager", 0);
