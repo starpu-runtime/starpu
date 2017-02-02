36984281e295cc1361bf0e65d52b606ad787d81e
Samuel Thibault <samuel.thibault@labri.fr>
2017-02-02 15:35:20 +0000

    fix fallthrough

--- src/debug/traces/starpu_fxt.c
+++ src/debug/traces/starpu_fxt.c
@@ -2893,6 +2893,7 @@ void _starpu_fxt_parse_new_file(char *filename_in, struct starpu_fxt_options *op
 				break;
 			case _STARPU_FUT_USED_MEM:
 				handle_used_mem(&ev, options);
+				break;
 
 			case _STARPU_FUT_USER_EVENT:
 				handle_user_event(&ev, options);
