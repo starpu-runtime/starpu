8e6da2a587d51cc41b6485053209534bd27c2c56
Samuel Thibault <samuel.thibault@labri.fr>
2017-02-07 16:53:26 +0000

    track-origins is supported by valgrind only, not helgrind

--- tools/valgrind/valgrind.sh
+++ tools/valgrind/valgrind.sh
@@ -18,9 +18,9 @@
 EXEC=$(basename $0 .sh)
 if test "$EXEC" == "valgrind"
 then
-    RUN="valgrind"
+    RUN="valgrind --track-origins=yes"
 else
     RUN="valgrind --tool=$EXEC"
 fi
 SUPPRESSIONS=$(for f in $(dirname $0)/*.suppr ; do echo "--suppressions=$f" ; done)
-$RUN --num-callers=42 --error-exitcode=42 --track-origins=yes --leak-check=full --show-reachable=yes --errors-for-leak-kinds=all --show-leak-kinds=all --gen-suppressions=all $SUPPRESSIONS $*
+$RUN --num-callers=42 --error-exitcode=42 --leak-check=full --show-reachable=yes --errors-for-leak-kinds=all --show-leak-kinds=all --gen-suppressions=all $SUPPRESSIONS $*
