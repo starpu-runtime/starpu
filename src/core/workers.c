8dc167bc37258be8beec49436e2e06981e04b4ca
Samuel Thibault <samuel.thibault@labri.fr>
2017-02-22 15:49:42 +0000

    Label the CUDA driver just 'CUDA' one using just one thread

--- src/core/workers.c
+++ src/core/workers.c
@@ -714,7 +714,7 @@ static void _starpu_launch_drivers(struct _starpu_machine_config *pconfig)
 
 
 				STARPU_PTHREAD_CREATE_ON(
-					workerarg->name,
+					conf->topology.cuda_th_per_dev ? "CUDA" : workerarg->name,
 					&worker_set->worker_thread,
 					NULL,
 					_starpu_cuda_worker,
