11cf8bbbe66965184d080e1bdd6b317d7fb2337a
Samuel Thibault <samuel.thibault@labri.fr>
2017-02-24 09:21:31 +0000

    Fix build with nopeer

--- src/datawizard/malloc.c
+++ src/datawizard/malloc.c
@@ -1,6 +1,6 @@
 /* StarPU --- Runtime system for heterogeneous multicore architectures.
  *
- * Copyright (C) 2009-2010, 2012-2016  Université de Bordeaux
+ * Copyright (C) 2009-2010, 2012-2017  Université de Bordeaux
  * Copyright (C) 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017  CNRS
  *
  * StarPU is free software; you can redistribute it and/or modify
@@ -27,6 +27,7 @@
 #include <datawizard/memory_nodes.h>
 #include <datawizard/malloc.h>
 #include <core/simgrid.h>
+#include <core/task.h>
 
 #ifdef STARPU_SIMGRID
 #include <sys/mman.h>
