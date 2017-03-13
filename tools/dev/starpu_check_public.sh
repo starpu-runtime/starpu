e0ee6e1c0cd1a5fb8c8eeb4933af9dfc4fd2a9bb
Nathalie Furmento <nathalie.furmento@labri.fr>
2017-03-13 10:39:59 +0000

    merge trunk@19929: add missing copyright

--- tools/dev/starpu_check_public.sh
+++ tools/dev/starpu_check_public.sh
@@ -4,7 +4,7 @@
 
 # StarPU --- Runtime system for heterogeneous multicore architectures.
 #
-# Copyright (C) 2011, 2012, 2015 CNRS
+# Copyright (C) 2011, 2012, 2015, 2017 CNRS
 # Copyright (C) 2011 INRIA
 #
 # StarPU is free software; you can redistribute it and/or modify
@@ -22,43 +22,62 @@ stcolor=$(tput sgr0)
 redcolor=$(tput setaf 1)
 greencolor=$(tput setaf 2)
 
-functions=$(spatch -very_quiet -sp_file doc/doxygen/dev/starpu_funcs.cocci $(find include -name '*.h'))
-for func in $functions ; do
+test_()
+{
+    INCLUDE=$1
+    INCLUDE_FILES=$(find $INCLUDE -name '*.h')
+    shift
+    echo "Check include files in directory <$INCLUDE> against $*"
+    ok=1
+
+    functions=$(spatch -very_quiet -sp_file doc/doxygen/dev/starpu_funcs.cocci $INCLUDE_FILES)
+    for func in $functions ; do
 	fname=$(echo $func|awk -F ',' '{print $1}')
 	location=$(echo $func|awk -F ',' '{print $2}')
-	x=$(grep -rs "$fname" examples tests mpi starpufft gcc-plugin tools src/sched_policies)
+	x=$(grep -rs "$fname" $*)
 	if test "$x" == "" ; then
+	    ok=0
 	    echo "function ${redcolor}${fname}${stcolor} at location ${redcolor}$location${stcolor} is not used in any examples or tests"
 	fi
-done
+    done
 
-echo
+    echo
 
-structs=$(grep "struct starpu" $(find include -name '*.h') | grep -v "[;|,|(|)]" | awk '{print $2}')
-for struct in $structs ; do
-    x=$(grep -rs "struct $struct" examples tests mpi starpufft gcc-plugin tools src/sched_policies)
-    if test "$x" == "" ; then
-	echo "struct ${redcolor}${struct}${stcolor} is not used in any examples or tests"
+    structs=$(grep "struct starpu" $INCLUDE_FILES | grep -v "[;|,|(|)]" | awk '{print $2}')
+    for struct in $structs ; do
+	x=$(grep -rs "struct $struct" $*)
+	if test "$x" == "" ; then
+	    ok=0
+	    echo "struct ${redcolor}${struct}${stcolor} is not used in any examples or tests"
+	fi
+    done
+
+    echo
+
+    enums=$(grep "enum starpu" $INCLUDE_FILES | grep -v "[;|,|(|)]" | awk '{print $2}')
+    for enum in $enums ; do
+	x=$(grep -rs "enum $enum" $*)
+	if test "$x" == "" ; then
+	    ok=0
+	    echo "enum ${redcolor}${enum}${stcolor} is not used in any examples or tests"
+	fi
+    done
+
+    echo
+
+    macros=$(grep "define\b" $INCLUDE_FILES|grep -v deprecated|grep "#" | grep -v "__" | sed 's/#[ ]*/#/g' | awk '{print $2}' | awk -F'(' '{print $1}' | sort|uniq)
+    for macro in $macros ; do
+	x=$(grep -rs "$macro" $*)
+	if test "$x" == "" ; then
+	    ok=0
+	    echo "macro ${redcolor}${macro}${stcolor} is not used in any examples or tests"
+	fi
+    done
+
+    if test "$ok" == "1" ; then
+	echo "All OK"
     fi
-done
+}
 
-echo
-
-enums=$(grep "enum starpu" $(find include -name '*.h') | grep -v "[;|,|(|)]" | awk '{print $2}')
-for enum in $enums ; do
-    x=$(grep -rs "enum $enum" examples tests mpi starpufft gcc-plugin tools src/sched_policies)
-    if test "$x" == "" ; then
-	echo "enum ${redcolor}${enum}${stcolor} is not used in any examples or tests"
-    fi
-done
-
-echo
-
-macros=$(grep "define\b" include/*|grep -v deprecated|grep "#" | grep -v "__" | sed 's/#[ ]*/#/g' | awk '{print $2}' | awk -F'(' '{print $1}' | sort|uniq)
-for macro in $macros ; do
-    x=$(grep -rs "$macro" examples tests mpi starpufft gcc-plugin tools src/sched_policies)
-    if test "$x" == "" ; then
-	echo "macro ${redcolor}${macro}${stcolor} is not used in any examples or tests"
-    fi
-done
-
+test_ include examples tests mpi/src starpufft gcc-plugin tools src/sched_policies
+test_ mpi/include mpi/src mpi/examples
