b319a80d953f89c1e64e757bf75f74f4176f7f4d
Samuel Thibault <samuel.thibault@labri.fr>
2017-03-29 14:52:57 +0000

    reenable test

--- examples/scheduler/heteroprio_test.c
+++ examples/scheduler/heteroprio_test.c
@@ -110,9 +110,6 @@ int main(int argc, char** argv)
 	assert(ret == 0);
 
 	conf.sched_policy_name = "heteroprio";
-#warning "heteroprio needs update with new synchro scheme"
-	return 77;
-
 	conf.sched_policy_init = &initSchedulerCallback;
 	ret = starpu_init(&conf);
 	if (ret == -ENODEV)
