ab648e8c9e6d16f6d0d6ba87f1d252ed852271ec
Nathalie Furmento <nathalie.furmento@labri.fr>
2017-03-29 08:36:58 +0000

    merge trunk@18895: drop useless continue

--- examples/cg/cg.c
+++ examples/cg/cg.c
@@ -408,7 +408,6 @@ static void parse_args(int argc, char **argv)
 		{
 			FPRINTF(stderr, "usage: %s [-h] [-nblocks #blocks] [-n problem_size] [-no-reduction] [-maxiter i]\n", argv[0]);
 			exit(-1);
-			continue;
 		}
         }
 }
