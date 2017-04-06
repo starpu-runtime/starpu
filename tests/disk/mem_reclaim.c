2fefc380538c6b06b09e619622b1af42f2909de2
Nathalie Furmento <nathalie.furmento@labri.fr>
2017-04-06 13:23:08 +0000

    merge trunk@20978: tests/disk/mem_reclaim.c: test return value for rmdir

--- tests/disk/mem_reclaim.c
+++ tests/disk/mem_reclaim.c
@@ -1,7 +1,7 @@
 /* StarPU --- Runtime system for heterogeneous multicore architectures.
  *
  * Copyright (C) 2013 Corentin Salingue
- * Copyright (C) 2015, 2016 CNRS
+ * Copyright (C) 2015, 2016, 2017 CNRS
  *
  * StarPU is free software; you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
@@ -225,7 +225,9 @@ int main(void)
 	ret = merge_result(ret, dotest(&starpu_disk_unistd_o_direct_ops, s, starpu_vector_data_register));
 	ret = merge_result(ret, dotest(&starpu_disk_unistd_o_direct_ops, s, starpu_my_vector_data_register));
 #endif
-	rmdir(s);
+
+	ret = rmdir(s);
+	STARPU_CHECK_RETURN_VALUE(ret, "rmdir '%s'\n", s);
 	return ret;
 }
 #endif
