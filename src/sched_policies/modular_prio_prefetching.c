3440c016b187e8e13746c8c8df8f31a36a84928d
Samuel Thibault <samuel.thibault@labri.fr>
2017-08-09 13:28:18 +0000

    Add more missing notification that schedulers handle any priority

--- src/sched_policies/modular_prio_prefetching.c
+++ src/sched_policies/modular_prio_prefetching.c
@@ -48,6 +48,12 @@ static void initialize_prio_prefetching_center_policy(unsigned sched_ctx_id)
 	}
 	starpu_sched_tree_update_workers(t);
 	starpu_sched_ctx_set_policy_data(sched_ctx_id, (void*)t);
+
+	/* The application may use any integer */
+	if (starpu_sched_ctx_min_priority_is_set(sched_ctx_id) == 0)
+		starpu_sched_ctx_set_min_priority(sched_ctx_id, INT_MIN);
+	if (starpu_sched_ctx_max_priority_is_set(sched_ctx_id) == 0)
+		starpu_sched_ctx_set_max_priority(sched_ctx_id, INT_MAX);
 }
 
 static void deinitialize_prio_prefetching_center_policy(unsigned sched_ctx_id)
