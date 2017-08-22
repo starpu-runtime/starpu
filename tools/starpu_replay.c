975cab9648687c03fff8a1ffe54d5b62aae79ef5
Samuel Thibault <samuel.thibault@labri.fr>
2017-08-22 14:45:06 +0000

    fix frenchism

--- tools/starpu_replay.c
+++ tools/starpu_replay.c
@@ -370,7 +370,7 @@ int main(int argc, char **argv)
 
 	if (argc <= 1)
 	{
-		fprintf(stderr,"Usage: %s tasks.rec [ordo.rec]\n", argv[0]);
+		fprintf(stderr,"Usage: %s tasks.rec [sched.rec]\n", argv[0]);
 		exit(EXIT_FAILURE);
 	}
 
