7080e79d2b8c6e9ae89c6ae245e9f1c3eb465bbf
Samuel Thibault <samuel.thibault@labri.fr>
2017-10-04 16:28:51 +0000

    port r22280 from trunk: documentation improvements

--- examples/basic_examples/vector_scal.c
+++ examples/basic_examples/vector_scal.c
@@ -1,7 +1,7 @@
 /* StarPU --- Runtime system for heterogeneous multicore architectures.
  *
  * Copyright (C) 2010, 2011, 2012, 2013, 2015  CNRS
- * Copyright (C) 2010-2016  Université de Bordeaux
+ * Copyright (C) 2010-2017  Université de Bordeaux
  *
  * StarPU is free software; you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
@@ -169,6 +169,7 @@ int main(int argc, char **argv)
 	 * COPY of the argument */
 	task->cl_arg = &factor;
 	task->cl_arg_size = sizeof(factor);
+	task->name = "foo";
 
 	/* execute the task on any eligible computational ressource */
 	ret = starpu_task_submit(task);
