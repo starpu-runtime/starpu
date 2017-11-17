f42608ba91124e983138b1088c03cc506b6efd98
Samuel Thibault <samuel.thibault@labri.fr>
2017-11-17 10:21:49 +0000

    port r22485 from trunk: No need to actually allocate the matrix in simgrid mode

--- examples/cholesky/cholesky_implicit.c
+++ examples/cholesky/cholesky_implicit.c
@@ -202,8 +202,8 @@ static void execute_cholesky(unsigned size, unsigned nblocks)
 	float *mat = NULL;
 	unsigned i,j;
 
-	starpu_malloc_flags((void **)&mat, (size_t)size*size*sizeof(float), STARPU_MALLOC_PINNED|STARPU_MALLOC_SIMULATION_FOLDED);
 #ifndef STARPU_SIMGRID
+	starpu_malloc_flags((void **)&mat, (size_t)size*size*sizeof(float), STARPU_MALLOC_PINNED|STARPU_MALLOC_SIMULATION_FOLDED);
 	for (i = 0; i < size; i++)
 	{
 		for (j = 0; j < size; j++)
@@ -237,6 +237,7 @@ static void execute_cholesky(unsigned size, unsigned nblocks)
 
 	cholesky(mat, size, size, nblocks);
 
+#ifndef STARPU_SIMGRID
 #ifdef PRINT_OUTPUT
 	FPRINTF(stdout, "Results :\n");
 	for (j = 0; j < size; j++)
@@ -314,6 +315,7 @@ static void execute_cholesky(unsigned size, unsigned nblocks)
 		free(test_mat);
 	}
 	starpu_free_flags(mat, (size_t)size*size*sizeof(float), STARPU_MALLOC_PINNED|STARPU_MALLOC_SIMULATION_FOLDED);
+#endif
 }
 
 int main(int argc, char **argv)
