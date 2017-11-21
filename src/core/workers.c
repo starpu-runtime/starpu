58c1f85a980a9e1ae337d9344eb7a896b3d6791e
Samuel Thibault <samuel.thibault@labri.fr>
2017-11-21 12:22:26 +0000

    Fix basic OpenMP initialization

--- src/core/workers.c
+++ src/core/workers.c
@@ -926,10 +926,8 @@ static void _starpu_launch_drivers(struct _starpu_machine_config *pconfig)
 		struct _starpu_worker *workerarg = &pconfig->workers[worker];
 
 		_STARPU_DEBUG("waiting for worker %u initialization\n", worker);
-#ifndef STARPU_SIMGRID
 		if (!workerarg->run_by_starpu)
 			break;
-#endif
 #if defined(STARPU_USE_CUDA) || defined(STARPU_SIMGRID)
 		if (workerarg->arch == STARPU_CUDA_WORKER)
 		{
