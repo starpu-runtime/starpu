2d61a642dcaee07832f418e0434cc9563bdcf951
Nathalie Furmento <nathalie.furmento@labri.fr>
2017-12-12 13:20:05 +0000

    fix copyright holder name

--- tools/starpu_fxt_data_trace.c
+++ tools/starpu_fxt_data_trace.c
@@ -1,7 +1,7 @@
 /* StarPU --- Runtime system for heterogeneous multicore architectures.
  *
  * Copyright (C) 2013       Joris Pablo
- * Copyright (C) 2011-2014, 2016  Universite de Bordeaux
+ * Copyright (C) 2011-2014, 2016  Université de Bordeaux
  * Copyright (C) 2014, 2015, 2017       CNRS
  *
  * StarPU is free software; you can redistribute it and/or modify
