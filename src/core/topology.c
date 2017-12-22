486d531a0eb3c88fa1c9b3883af8eac44f94d7bf
Samuel Thibault <samuel.thibault@labri.fr>
2017-12-22 10:26:54 +0000

    More fixes for hwloc2: cuda & friends are in io list now

--- src/core/topology.c
+++ src/core/topology.c
@@ -646,6 +646,11 @@ _starpu_allocate_topology_userdata(hwloc_obj_t obj)
 	_STARPU_CALLOC(obj->userdata,  1, sizeof(struct _starpu_hwloc_userdata));
 	for (i = 0; i < obj->arity; i++)
 		_starpu_allocate_topology_userdata(obj->children[i]);
+#if HWLOC_API_VERSION >= 0x00020000
+	hwloc_obj_t child;
+	for (child = obj->io_first_child; child; child = child->next_sibling)
+		_starpu_allocate_topology_userdata(child);
+#endif
 }
 
 static void
@@ -658,6 +663,11 @@ _starpu_deallocate_topology_userdata(hwloc_obj_t obj)
 	free(data);
 	for (i = 0; i < obj->arity; i++)
 		_starpu_deallocate_topology_userdata(obj->children[i]);
+#if HWLOC_API_VERSION >= 0x00020000
+	hwloc_obj_t child;
+	for (child = obj->io_first_child; child; child = child->next_sibling)
+		_starpu_deallocate_topology_userdata(child);
+#endif
 }
 #endif
 #endif
