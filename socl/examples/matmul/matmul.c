d4dfa9135be62007050b730d8edb77e57448e320
Nathalie Furmento <nathalie.furmento@labri.fr>
2018-01-08 10:09:58 +0000

    socl/examples/matmul: remove ^M character

--- socl/examples/matmul/matmul.c
+++ socl/examples/matmul/matmul.c
@@ -1,7 +1,7 @@
 /* StarPU --- Runtime system for heterogeneous multicore architectures.
  *
  * Copyright (C) 2012                                     Inria
- * Copyright (C) 2012-2013,2015-2017                      CNRS
+ * Copyright (C) 2012-2013,2015-2018                      CNRS
  * Copyright (C)                                          Université de Bordeaux
  * Copyright (C) 2010-2011,2015,2017                      Université de Bordeaux
  *
