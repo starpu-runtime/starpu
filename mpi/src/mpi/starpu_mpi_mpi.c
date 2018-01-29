fe149defb8104ac347eff6b6c52c8196db8cb535
Samuel Thibault <samuel.thibault@ens-lyon.org>
2018-01-29 10:40:52 +0100

    We need to record our ID in the trace before the main thread makes any MPI call

--- mpi/src/mpi/starpu_mpi_mpi.c
+++ mpi/src/mpi/starpu_mpi_mpi.c
@@ -1270,22 +1270,18 @@ static void *_starpu_mpi_progress_thread_func(void *arg)
 	starpu_pthread_queue_register(&wait, &dontsleep);
 #endif
 
+#ifdef STARPU_USE_FXT
+	_starpu_fxt_wait_initialisation();
+	/* We need to record our ID in the trace before the main thread makes any MPI call */
+	_STARPU_MPI_TRACE_START(argc_argv->rank, argc_argv->world_size);
+	starpu_profiling_set_id(argc_argv->rank);
+#endif //STARPU_USE_FXT
+
 	/* notify the main thread that the progression thread is ready */
 	STARPU_PTHREAD_MUTEX_LOCK(&progress_mutex);
 	running = 1;
 	STARPU_PTHREAD_COND_SIGNAL(&progress_cond);
 	STARPU_PTHREAD_MUTEX_UNLOCK(&progress_mutex);
-
-#ifdef STARPU_USE_FXT
-	_starpu_fxt_wait_initialisation();
-#endif //STARPU_USE_FXT
-
-	{
-		_STARPU_MPI_TRACE_START(argc_argv->rank, argc_argv->world_size);
-#ifdef STARPU_USE_FXT
-		starpu_profiling_set_id(argc_argv->rank);
-#endif //STARPU_USE_FXT
-	}
 
 	_starpu_mpi_add_sync_point_in_fxt();
 	STARPU_PTHREAD_MUTEX_LOCK(&progress_mutex);
