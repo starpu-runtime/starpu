ea7f6d0bc66a13ff7c94724e82eb79873faebd2b
Samuel Thibault <samuel.thibault@ens-lyon.org>
2018-01-31 15:45:36 +0100

    Fix mpi_nmad build
    (cherry picked from commit 8a1e682f58a0f86ff1f6ee78016388770039ffc8)

--- mpi/src/starpu_mpi_req.c
+++ mpi/src/starpu_mpi_req.c
@@ -17,6 +17,7 @@
  * See the GNU Lesser General Public License in COPYING.LGPL for more details.
  */
 
+#include <starpu.h>
 #include <starpu_mpi_private.h>
 #if defined(STARPU_USE_MPI_MPI)
 #include <mpi/starpu_mpi_comm.h>
