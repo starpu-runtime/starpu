3011e485ff8e45f766486670b59c3dfec76abc36
Nathalie Furmento <nathalie.furmento@labri.fr>
2018-02-01 15:09:01 +0100

    src/util: minor fixes to follow coding conventions
    (cherry picked from commit 1b2d99686ecdb0f7155e063f835320a86f94a268)

--- src/util/starpu_task_insert_utils.c
+++ src/util/starpu_task_insert_utils.c
@@ -1,8 +1,8 @@
 /* StarPU --- Runtime system for heterogeneous multicore architectures.
  *
  * Copyright (C) 2011-2014,2016-2017                      Inria
- * Copyright (C) 2011-2016, 2018                          Université de Bordeaux
- * Copyright (C) 2011-2017                                CNRS
+ * Copyright (C) 2011-2018                                Université de Bordeaux
+ * Copyright (C) 2011-2018                                CNRS
  *
  * StarPU is free software; you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
@@ -466,8 +466,10 @@ int _starpu_task_insert_create(struct starpu_codelet *cl, struct starpu_task *ta
 		}
 	}
 
-	if (state.nargs) {
-		if (task->cl_arg != NULL) {
+	if (state.nargs)
+	{
+		if (task->cl_arg != NULL)
+		{
 			_STARPU_DISP("Parameters STARPU_CL_ARGS and STARPU_VALUE cannot be used in the same call\n");
 			free(state.arg_buffer);
 			return -EINVAL;
