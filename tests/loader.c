b6fc6890e18244e96b8ecdca0f242d4bdee6c748
Nathalie Furmento <nathalie.furmento@labri.fr>
2018-02-05 10:05:54 +0100

    tests/loader.c: fix when launcher_args is NULL
    (cherry picked from commit 96d49de3ef03ef60baf018d855c7410cbe3aa093)

--- tests/loader.c
+++ tests/loader.c
@@ -1,6 +1,8 @@
 /* StarPU --- Runtime system for heterogeneous multicore architectures.
  *
- * Copyright (C) 2010, 2014-2015  Université de Bordeaux
+ * Copyright (C) 2011-2012,2017                           Inria
+ * Copyright (C) 2012-2018                                CNRS
+ * Copyright (C) 2010,2014-2017                           Université de Bordeaux
  *
  * StarPU is free software; you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
@@ -284,11 +286,14 @@ int main(int argc, char *argv[])
 				launcher_argv[0] = libtool;
 				launcher_argv[1] = "--mode=execute";
 				launcher_argv[2] = launcher;
-				launcher_argv[i] = strtok(launcher_args, " ");
-				while (launcher_argv[i])
+				if (launcher_args)
 				{
-					i++;
-					launcher_argv[i] = strtok(NULL, " ");
+					launcher_argv[i] = strtok(launcher_args, " ");
+					while (launcher_argv[i])
+					{
+						i++;
+						launcher_argv[i] = strtok(NULL, " ");
+					}
 				}
 				launcher_argv[i] = test_name;
 				launcher_argv[i+1] = test_args;
