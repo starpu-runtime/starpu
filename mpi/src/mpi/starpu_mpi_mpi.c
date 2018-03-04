b7aa4ac9c90bdaebada70715c4ed1c4fd6a150b1
Samuel Thibault <samuel.thibault@ens-lyon.org>
2018-03-04 18:21:49 +0100

    Fix starpu_mpi_comm_get_attr calling convention for STARPU_MPI_TAG_UB according to MPI standard

--- mpi/src/mpi/starpu_mpi_mpi.c
+++ mpi/src/mpi/starpu_mpi_mpi.c
@@ -1527,6 +1527,7 @@ void _starpu_mpi_progress_shutdown(void **value)
         STARPU_PTHREAD_COND_DESTROY(&barrier_cond);
 }
 
+static int64_t _starpu_mpi_tag_max = INT64_MAX;
 
 int starpu_mpi_comm_get_attr(MPI_Comm comm, int keyval, void *attribute_val, int *flag)
 {
@@ -1534,7 +1535,7 @@ int starpu_mpi_comm_get_attr(MPI_Comm comm, int keyval, void *attribute_val, int
 	if (keyval == STARPU_MPI_TAG_UB)
 	{
 		*flag = 1;
-		*(int64_t *)attribute_val = INT64_MAX;
+		*(int64_t **)attribute_val = &_starpu_mpi_tag_max;
 	}
 	else
 	{
