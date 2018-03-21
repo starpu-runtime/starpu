a90eb51bf0b74b635a5c0b8074fe687747001de4
Samuel Thibault <samuel.thibault@ens-lyon.org>
2018-03-21 11:01:37 +0100

    Add more simgrid compatibility tests
    (cherry picked from commit 71c05a2d3434935dcb0c43a68ceb379450cf3b73)

--- tests/datawizard/simgrid-locality.c
+++ tests/datawizard/simgrid-locality.c
@@ -1,7 +1,7 @@
 /* StarPU --- Runtime system for heterogeneous multicore architectures.
  *
  * Copyright (C) 2016-2017                                CNRS
- * Copyright (C) 2016-2017                                Université de Bordeaux
+ * Copyright (C) 2016-2018                                Université de Bordeaux
  *
  * StarPU is free software; you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
@@ -18,7 +18,8 @@
 /* Check that defining a main makes starpu use MSG_process_attach. */
 #include "locality.c"
 #include <common/config.h>
-#if defined(HAVE_MSG_PROCESS_ATTACH) && SIMGRID_VERSION_MAJOR > 3 || (SIMGRID_VERSION_MAJOR == 3 && SIMGRID_VERSION_MINOR >= 15)
+
+#if defined(SIMGRID_VERSION) && ((defined(HAVE_MSG_PROCESS_ATTACH) || defined(MSG_process_attach)) && SIMGRID_VERSION >= 31500)
 #undef main
 int main(int argc, char *argv[])
 {
