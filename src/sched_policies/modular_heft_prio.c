28694c40fe204eb333e0a5a0569b23b34202d794
Samuel Thibault <samuel.thibault@ens-lyon.org>
2018-04-20 18:40:15 +0200

    Factorize modular scheduler creation. Add example of modular scheduling policy
    (cherry picked from commit 82900d839f8c2a1be9b5b9bc4fede17d83e25119)

--- src/sched_policies/modular_heft_prio.c
+++ src/sched_policies/modular_heft_prio.c
@@ -2,7 +2,7 @@
  *
  * Copyright (C) 2013-2015,2017                           Inria
  * Copyright (C) 2014-2015,2017                           CNRS
- * Copyright (C) 2013-2015,2017                           Université de Bordeaux
+ * Copyright (C) 2013-2015,2017-2018                           Université de Bordeaux
  * Copyright (C) 2013                                     Simon Archipoff
  *
  * StarPU is free software; you can redistribute it and/or modify
@@ -21,47 +21,6 @@
 #include <starpu_scheduler.h>
 #include <float.h>
 #include <limits.h>
-
-/* The two thresolds concerns the prio components, which contains queues
- * who can handle the priority of StarPU tasks. You can tune your
- * scheduling by benching those values and choose which one is the
- * best for your current application. 
- * The current value of the ntasks_threshold is the best we found
- * so far across several types of applications (cholesky, LU, stencil).
- */
-#define _STARPU_SCHED_NTASKS_THRESHOLD_DEFAULT 30
-#define _STARPU_SCHED_EXP_LEN_THRESHOLD_DEFAULT 1000000000.0
-
-static void initialize_heft_prio_policy(unsigned sched_ctx_id)
-{
-	unsigned i, j, n, nnodes;
-
-	/* First count how many memory nodes really have workers */
-	n = starpu_memory_nodes_get_count();
-	nnodes = 0;
-	for(i = 0; i < n; i++)
-	{
-		for(j = 0; j < starpu_worker_get_count() + starpu_combined_worker_get_count(); j++)
-			if (starpu_worker_get_memory_node(j) == i)
-				break;
-		if (j >= starpu_worker_get_count() + starpu_combined_worker_get_count())
-			/* Don't create a component for this memory node with no worker */
-			continue;
-		nnodes++;
-	}
-
-	if (nnodes == 1)
-	{
-		/* Just one memory node, we don't actually need MCT etc., just
-		 * initialize a prio scheduler */
-		return starpu_initialize_prio_center_policy(sched_ctx_id);
-	}
-
-	/* The application may use any integer */
-	if (starpu_sched_ctx_min_priority_is_set(sched_ctx_id) == 0)
-		starpu_sched_ctx_set_min_priority(sched_ctx_id, INT_MIN);
-	if (starpu_sched_ctx_max_priority_is_set(sched_ctx_id) == 0)
-		starpu_sched_ctx_set_max_priority(sched_ctx_id, INT_MAX);
 
 /* The scheduling strategy look like this :
  *
@@ -90,68 +49,17 @@ static void initialize_heft_prio_policy(unsigned sched_ctx_id)
  * component will push the task on its local queue if no one of the two thresholds
  * have been reached for it, or send a push_error signal to its parent.
  */
-	struct starpu_sched_tree * t = starpu_sched_tree_create(sched_ctx_id);
 
-	struct starpu_sched_component * window_component = starpu_sched_component_prio_create(t, NULL);
-
-	struct starpu_sched_component * perfmodel_component = starpu_sched_component_mct_create(t, NULL);
-	struct starpu_sched_component * no_perfmodel_component = starpu_sched_component_eager_create(t, NULL);
-	struct starpu_sched_component * calibrator_component = starpu_sched_component_eager_calibration_create(t, NULL);
-	
-	struct starpu_sched_component_perfmodel_select_data perfmodel_select_data =
-		{
-			.calibrator_component = calibrator_component,
-			.no_perfmodel_component = no_perfmodel_component,
-			.perfmodel_component = perfmodel_component,
-		};
-
-	struct starpu_sched_component * perfmodel_select_component = starpu_sched_component_perfmodel_select_create(t, &perfmodel_select_data);
-
-	t->root = window_component;
-	starpu_sched_component_connect(window_component, perfmodel_select_component);
-
-	starpu_sched_component_connect(perfmodel_select_component, perfmodel_component);
-	starpu_sched_component_connect(perfmodel_select_component, calibrator_component);
-	starpu_sched_component_connect(perfmodel_select_component, no_perfmodel_component);
-
-	struct starpu_sched_component_prio_data prio_data =
-		{
-			.ntasks_threshold = starpu_get_env_number_default("STARPU_NTASKS_THRESHOLD", _STARPU_SCHED_NTASKS_THRESHOLD_DEFAULT),
-			.exp_len_threshold = starpu_get_env_float_default("STARPU_EXP_LEN_THRESHOLD", _STARPU_SCHED_EXP_LEN_THRESHOLD_DEFAULT),
-		};
-
-	n = starpu_memory_nodes_get_count();
-	STARPU_ASSERT(n >= 1);
-	struct starpu_sched_component * eagers[n];
-
-	/* Create one fifo+eager component pair per memory node, below mct */
-	for(i = 0; i < n; i++)
-	{
-		for(j = 0; j < starpu_worker_get_count() + starpu_combined_worker_get_count(); j++)
-			if (starpu_worker_get_memory_node(j) == i)
-				break;
-		if (j == starpu_worker_get_count() + starpu_combined_worker_get_count())
-			/* Don't create a component for this memory node with no worker */
-			continue;
-		struct starpu_sched_component * prio_component = starpu_sched_component_prio_create(t, &prio_data);
-		eagers[i] = starpu_sched_component_eager_create(t, NULL);
-		starpu_sched_component_connect(perfmodel_component, prio_component);
-		starpu_sched_component_connect(prio_component, eagers[i]);
-	}
-
-	for(i = 0; i < starpu_worker_get_count() + starpu_combined_worker_get_count(); i++)
-	{
-		struct starpu_sched_component * worker_component = starpu_sched_component_worker_new(sched_ctx_id, i);
-		struct starpu_sched_component * impl_component = starpu_sched_component_best_implementation_create(t, NULL);
-
-		starpu_sched_component_connect(eagers[starpu_worker_get_memory_node(i)], impl_component);
-		starpu_sched_component_connect(no_perfmodel_component, impl_component);
-		starpu_sched_component_connect(calibrator_component, impl_component);
-		starpu_sched_component_connect(impl_component, worker_component);
-	}
-
-	starpu_sched_tree_update_workers(t);
-	starpu_sched_ctx_set_policy_data(sched_ctx_id, (void*)t);
+static void initialize_heft_prio_policy(unsigned sched_ctx_id)
+{
+	starpu_sched_component_initialize_simple_scheduler((starpu_sched_component_create_t) starpu_sched_component_mct_create, NULL,
+			STARPU_SCHED_SIMPLE_DECIDE_MEMNODES |
+			STARPU_SCHED_SIMPLE_PERFMODEL |
+			STARPU_SCHED_SIMPLE_FIFO_ABOVE |
+			STARPU_SCHED_SIMPLE_FIFO_ABOVE_PRIO |
+			STARPU_SCHED_SIMPLE_FIFOS_BELOW |
+			STARPU_SCHED_SIMPLE_FIFOS_BELOW_PRIO |
+			STARPU_SCHED_SIMPLE_IMPL, sched_ctx_id);
 }
 
 static void deinitialize_heft_prio_policy(unsigned sched_ctx_id)
