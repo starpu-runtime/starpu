09f6bdfd07ba6328d3b9d39525520ddd0832aa42
Nathalie Furmento <nathalie.furmento@labri.fr>
2018-05-16 10:28:18 +0200

    contrib: update tarball script to delete previous build directory

--- contrib/ci.inria.fr/job-0-tarball.sh
+++ contrib/ci.inria.fr/job-0-tarball.sh
@@ -4,7 +4,8 @@ export PKG_CONFIG_PATH=/home/ci/usr/local/lib/pkgconfig:$PKG_CONFIG_PATH
 export LD_LIBRARY_PATH=/home/ci/usr/local/lib:$LD_LIBRARY_PATH
 
 ./autogen.sh
-rm -rf build && mkdir build && cd build
+if test -d build ; then chmod -R 777 build && rm -rf build ; fi
+mkdir build && cd build
 ../configure
 make V=1
 make dist
