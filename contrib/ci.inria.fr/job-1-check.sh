cd7d88cff5dd1e81c9e91d683df98d40feb9bee2
Nathalie Furmento <nathalie.furmento@labri.fr>
2018-05-17 15:14:32 +0200

    contrib: check that tar.gz file exists

--- contrib/ci.inria.fr/job-1-check.sh
+++ contrib/ci.inria.fr/job-1-check.sh
@@ -22,8 +22,16 @@ export PKG_CONFIG_PATH=/home/ci/usr/local/lib/pkgconfig:$PKG_CONFIG_PATH
 export LD_LIBRARY_PATH=/home/ci/usr/local/lib:$LD_LIBRARY_PATH
 
 tarball=$(ls -tr starpu-*.tar.gz | tail -1)
-basename=$(basename $tarball .tar.gz)
 
+if test -z "$tarball"
+then
+    echo Error. No tar.gz file
+    ls
+    pwd
+    exit 1
+fi
+
+basename=$(basename $tarball .tar.gz)
 export STARPU_HOME=$PWD/$basename/home
 mkdir -p $basename
 cd $basename
