72d790ed3a454acd6feb02a29e5bb016f7418d70
Nathalie Furmento <nathalie.furmento@labri.fr>
2018-05-17 15:51:24 +0200

    contrib: stash file is in build directory

--- contrib/ci.inria.fr/job-1-check.sh
+++ contrib/ci.inria.fr/job-1-check.sh
@@ -21,6 +21,8 @@ set -x
 export PKG_CONFIG_PATH=/home/ci/usr/local/lib/pkgconfig:$PKG_CONFIG_PATH
 export LD_LIBRARY_PATH=/home/ci/usr/local/lib:$LD_LIBRARY_PATH
 
+mv build/*gz .
+rmdir build
 tarball=$(ls -tr starpu-*.tar.gz | tail -1)
 
 if test -z "$tarball"
