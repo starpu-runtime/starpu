c0d574915f853a5ca089b4a13810c937f145c2df
Nathalie Furmento <nathalie.furmento@labri.fr>
2018-07-02 15:36:50 +0200

    New function starpu_task_set() similar as starpu_task_build() but with a task object given as the first parameter

--- include/starpu_task_util.h
+++ include/starpu_task_util.h
@@ -66,6 +66,7 @@ void starpu_create_sync_task(starpu_tag_t sync_tag, unsigned ndeps, starpu_tag_t
 #define STARPU_TASK_COLOR       (28<<STARPU_MODE_SHIFT)
 #define STARPU_SHIFTED_MODE_MAX (29<<STARPU_MODE_SHIFT)
 
+int starpu_task_set(struct starpu_task *task, struct starpu_codelet *cl, ...);
 struct starpu_task *starpu_task_build(struct starpu_codelet *cl, ...);
 int starpu_task_insert(struct starpu_codelet *cl, ...);
 /* the function starpu_insert_task has the same semantics as starpu_task_insert, it is kept to avoid breaking old codes */
