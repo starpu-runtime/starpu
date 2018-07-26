19d8260076543ce5b9035b15d910d406de029780
Samuel Thibault <samuel.thibault@ens-lyon.org>
2018-07-26 16:44:30 +0200

    really really fix build :)
    (cherry picked from commit 03c55a7cd61f64d260f82bd9a71564e26b1aec53)

--- include/fstarpu_mod.f90
+++ include/fstarpu_mod.f90
@@ -787,7 +787,7 @@ module fstarpu_mod
                 ! void *starpu_data_pointer_is_inside(starpu_data_handle_t handle, unsigned node, void *ptr);
                 function fstarpu_data_pointer_is_inside (dh,node,ptr) bind(C,name="starpu_data_pointer_is_inside")
                         use iso_c_binding, only: c_ptr, c_int, c_ptr
-                        type(c_int) :: fstarpu_data_pointer_is_inside
+                        integer(c_int) :: fstarpu_data_pointer_is_inside
                         type(c_ptr), value, intent(in) :: dh
                         integer(c_int), value, intent(in) :: node
                         type(c_ptr), value, intent(in) :: ptr
