8ed26e6d6db0bf57f65473b01800e82b4c92f382
Samuel Thibault <samuel.thibault@ens-lyon.org>
2018-07-26 16:12:13 +0200

    Define CL_TARGET_OPENCL_VERSION to 100
    Recent version of cl_version.h are complaining that we are not defining
    it, so let's define it to the minimum value.
    
    (cherry picked from commit a796517c163a23941b4fce3f515ea48e3accedd6)

--- examples/stencil/life_opencl.c
+++ examples/stencil/life_opencl.c
@@ -2,7 +2,7 @@
  *
  * Copyright (C) 2011-2012                                Inria
  * Copyright (C) 2012-2013,2015-2017                      CNRS
- * Copyright (C) 2010-2011,2013-2014                      Université de Bordeaux
+ * Copyright (C) 2010-2011,2013-2014,2018                 Université de Bordeaux
  *
  * StarPU is free software; you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
@@ -21,6 +21,7 @@
 /* #define _externC extern "C" */
 
 #include <stencil.h>
+#define CL_TARGET_OPENCL_VERSION 100
 #ifdef __APPLE__
 #include <OpenCL/cl.h>
 #else
