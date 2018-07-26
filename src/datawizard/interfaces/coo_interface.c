66eb3e2cd1c043f33ee66c36bae0fb4ca0a5b72d
Samuel Thibault <samuel.thibault@ens-lyon.org>
2018-07-26 16:18:10 +0200

    Add starpu_data_pointer_is_inside
    (cherry picked from commit 8ca621ccb80bfda740e055aee5c6d8fb7c61dbcf)

--- src/datawizard/interfaces/coo_interface.c
+++ src/datawizard/interfaces/coo_interface.c
@@ -149,6 +149,20 @@ free_coo_buffer_on_node(void *data_interface, unsigned node)
 	starpu_free_on_node(node, coo_interface->values, n_values * elemsize);
 }
 
+static int
+coo_pointer_is_inside(void *data_interface, unsigned node, void *ptr)
+{
+	struct starpu_coo_interface *coo_interface = (struct starpu_coo_interface *) data_interface;
+	(void) node;
+
+	return ((char*) ptr >= (char*) coo_interface->columns &&
+		(char*) ptr < (char*) coo_interface->columns + coo_interface->n_values * sizeof(coo_interface->columns[0]))
+	    || ((char*) ptr >= (char*) coo_interface->rows &&
+		(char*) ptr < (char*) coo_interface->rows + coo_interface->n_values * sizeof(coo_interface->rows[0]))
+	    || ((char*) ptr >= (char*) coo_interface->values &&
+		(char*) ptr < (char*) coo_interface->values + coo_interface->n_values * coo_interface->elemsize);
+}
+
 static size_t
 coo_interface_get_size(starpu_data_handle_t handle)
 {
@@ -208,6 +222,7 @@ struct starpu_data_interface_ops starpu_interface_coo_ops =
 	.register_data_handle  = register_coo_handle,
 	.allocate_data_on_node = allocate_coo_buffer_on_node,
 	.to_pointer            = NULL,
+	.pointer_is_inside     = coo_pointer_is_inside,
 	.free_data_on_node     = free_coo_buffer_on_node,
 	.copy_methods          = &coo_copy_data_methods,
 	.get_size              = coo_interface_get_size,
--- src/datawizard/interfaces/variable_interface.c
+++ src/datawizard/interfaces/variable_interface.c
@@ -40,6 +40,7 @@ static const struct starpu_data_copy_methods variable_copy_data_methods_s =
 static void register_variable_handle(starpu_data_handle_t handle, unsigned home_node, void *data_interface);
 static starpu_ssize_t allocate_variable_buffer_on_node(void *data_interface_, unsigned dst_node);
 static void *variable_to_pointer(void *data_interface, unsigned node);
+static int variable_pointer_is_inside(void *data_interface, unsigned node, void *ptr);
 static void free_variable_buffer_on_node(void *data_interface, unsigned node);
 static size_t variable_interface_get_size(starpu_data_handle_t handle);
 static uint32_t footprint_variable_interface_crc32(starpu_data_handle_t handle);
@@ -54,6 +55,7 @@ struct starpu_data_interface_ops starpu_interface_variable_ops =
 	.register_data_handle = register_variable_handle,
 	.allocate_data_on_node = allocate_variable_buffer_on_node,
 	.to_pointer = variable_to_pointer,
+	.pointer_is_inside = variable_pointer_is_inside,
 	.free_data_on_node = free_variable_buffer_on_node,
 	.copy_methods = &variable_copy_data_methods_s,
 	.get_size = variable_interface_get_size,
@@ -72,6 +74,14 @@ static void *variable_to_pointer(void *data_interface, unsigned node)
 {
 	(void) node;
 	return (void*) STARPU_VARIABLE_GET_PTR(data_interface);
+}
+
+static int variable_pointer_is_inside(void *data_interface, unsigned node, void *ptr)
+{
+	(void) node;
+	struct starpu_variable_interface *variable_interface = data_interface;
+	return (char*) ptr >= (char*) variable_interface->ptr &&
+		(char*) ptr < (char*) variable_interface->ptr + variable_interface->elemsize;
 }
 
 static void register_variable_handle(starpu_data_handle_t handle, unsigned home_node, void *data_interface)
