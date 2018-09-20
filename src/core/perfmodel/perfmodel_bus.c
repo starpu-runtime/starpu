4b92e9d352b19fec272e8727fe7f3dfe1dfab510
Samuel Thibault <samuel.thibault@ens-lyon.org>
2018-09-20 17:25:14 +0200

    Avoid making hwloc load PCI topology when CUDA is not enabled
    It is very costly to initialize GPUs if they are not in persistent mode,
    while we don't actually use them...

--- src/core/perfmodel/perfmodel_bus.c
+++ src/core/perfmodel/perfmodel_bus.c
@@ -2318,7 +2318,7 @@ static void write_bus_platform_file_content(int version)
 	}
 #endif
 
-#if defined(HAVE_DECL_HWLOC_CUDA_GET_DEVICE_OSDEV_BY_INDEX) && HAVE_DECL_HWLOC_CUDA_GET_DEVICE_OSDEV_BY_INDEX && defined(STARPU_HAVE_CUDA_MEMCPY_PEER)
+#if defined(HAVE_DECL_HWLOC_CUDA_GET_DEVICE_OSDEV_BY_INDEX) && HAVE_DECL_HWLOC_CUDA_GET_DEVICE_OSDEV_BY_INDEX && defined(STARPU_USE_CUDA) && defined(STARPU_HAVE_CUDA_MEMCPY_PEER)
 	/* If we have enough hwloc information, write PCI bandwidths and routes */
 	if (!starpu_get_env_number_default("STARPU_PCI_FLAT", 0))
 	{
