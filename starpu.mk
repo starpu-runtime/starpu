577560deabeb9c78e0f512f69aff7aebaf2d88bc
Nathalie Furmento <nathalie.furmento@labri.fr>
2018-11-09 11:41:50 +0100

    new make rule showsuite to show only fail and skip tests
    (cherry picked from commit 0e73f14f8cc8adf4fdb7e41f34d67c081e7d8109)

--- starpu.mk
+++ starpu.mk
@@ -1,6 +1,6 @@
 # StarPU --- Runtime system for heterogeneous multicore architectures.
 #
-# Copyright (C) 2017                                     CNRS
+# Copyright (C) 2017, 2018                               CNRS
 # Copyright (C) 2016-2017                                Université de Bordeaux
 #
 # StarPU is free software; you can redistribute it and/or modify
@@ -36,5 +36,20 @@ showcheck:
 	RET=0 ; \
 	for i in $(SUBDIRS) ; do \
 		make -C $$i showcheck || RET=1 ; \
+	done ; \
+	exit $$RET
+
+showsuite:
+	-cat $(TEST_SUITE_LOG) /dev/null
+	@! grep -q "ERROR: AddressSanitizer: " $(TEST_SUITE_LOG) /dev/null
+	@! grep -q "WARNING: AddressSanitizer: " $(TEST_SUITE_LOG) /dev/null
+	@! grep -q "ERROR: ThreadSanitizer: " $(TEST_SUITE_LOG) /dev/null
+	@! grep -q "WARNING: ThreadSanitizer: " $(TEST_SUITE_LOG) /dev/null
+	@! grep -q "ERROR: LeakSanitizer: " $(TEST_SUITE_LOG) /dev/null
+	@! grep -q "WARNING: LeakSanitizer: " $(TEST_SUITE_LOG) /dev/null
+	@! grep -q " runtime error: " $(TEST_SUITE_LOG) /dev/null
+	RET=0 ; \
+	for i in $(SUBDIRS) ; do \
+		make -C $$i showsuite || RET=1 ; \
 	done ; \
 	exit $$RET
