ae02337efb2073012cc00a7b55dd9474fe10f086
Samuel Thibault <samuel.thibault@ens-lyon.org>
2019-01-14 21:29:41 +0100

    Fix gcc-9 warning
    (cherry picked from commit 8f2ae1543fa1934f43d4e37c362479bce1cc9f20)

--- include/starpu_stdlib.h
+++ include/starpu_stdlib.h
@@ -1,7 +1,7 @@
 /* StarPU --- Runtime system for heterogeneous multicore architectures.
  *
  * Copyright (C) 2010-2013,2015                           CNRS
- * Copyright (C) 2010-2016                                Université de Bordeaux
+ * Copyright (C) 2010-2016,2019                           Université de Bordeaux
  *
  * StarPU is free software; you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
@@ -36,10 +36,10 @@ extern "C"
 
 void starpu_malloc_set_align(size_t align);
 
-int starpu_malloc(void **A, size_t dim) STARPU_ATTRIBUTE_ALLOC_SIZE(2);
+int starpu_malloc(void **A, size_t dim);
 int starpu_free(void *A);
 
-int starpu_malloc_flags(void **A, size_t dim, int flags) STARPU_ATTRIBUTE_ALLOC_SIZE(2);
+int starpu_malloc_flags(void **A, size_t dim, int flags);
 int starpu_free_flags(void *A, size_t dim, int flags);
 
 int starpu_memory_pin(void *addr, size_t size);
