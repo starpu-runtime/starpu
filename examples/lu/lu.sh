8f262d6e41d33171568990ae0aa0ce95b9e5d414
Samuel Thibault <samuel.thibault@ens-lyon.org>
2019-01-16 15:19:41 +0100

    Reduce test time
    (cherry picked from commit ee9bd33e6695a806fb88ef5f3ec36e4ab91f25f6)

--- examples/lu/lu.sh
+++ examples/lu/lu.sh
@@ -2,7 +2,7 @@
 # StarPU --- Runtime system for heterogeneous multicore architectures.
 #
 # Copyright (C) 2017                                     CNRS
-# Copyright (C) 2017                                     Université de Bordeaux
+# Copyright (C) 2017,2019                                Université de Bordeaux
 #
 # StarPU is free software; you can redistribute it and/or modify
 # it under the terms of the GNU Lesser General Public License as published by
@@ -27,11 +27,11 @@ if [ -n "$STARPU_MIC_SINK_PROGRAM_PATH" ] ; then
 	[ -x "$STARPU_MIC_SINK_PROGRAM_PATH/.libs/lu_implicit_example_float" ] && STARPU_MIC_SINK_PROGRAM_NAME=$STARPU_MIC_SINK_PROGRAM_PATH/.libs/lu_implicit_example_float
 fi
 
-$PREFIX/lu_implicit_example_float -size $((960 * 4)) -nblocks 4 -piv
-$PREFIX/lu_implicit_example_float -size $((960 * 4)) -nblocks 4 -no-stride
-$PREFIX/lu_implicit_example_float -size $((960 * 4)) -nblocks 4 -bound
-$PREFIX/lu_implicit_example_float -size $((960 * 2)) -nblocks 2 -bounddeps
-$PREFIX/lu_implicit_example_float -size $((960 * 2)) -nblocks 2 -bound -bounddeps -bounddepsprio
+$PREFIX/lu_implicit_example_float -size $((160 * 4)) -nblocks 4 -piv
+$PREFIX/lu_implicit_example_float -size $((160 * 4)) -nblocks 4 -no-stride
+$PREFIX/lu_implicit_example_float -size $((160 * 4)) -nblocks 4 -bound
+$PREFIX/lu_implicit_example_float -size $((160 * 2)) -nblocks 2 -bounddeps
+$PREFIX/lu_implicit_example_float -size $((160 * 2)) -nblocks 2 -bound -bounddeps -bounddepsprio
 
 if [ -n "$STARPU_MIC_SINK_PROGRAM_PATH" ] ; then
 	STARPU_MIC_SINK_PROGRAM_NAME=$STARPU_MIC_SINK_PROGRAM_PATH/lu_example_float
@@ -39,8 +39,8 @@ if [ -n "$STARPU_MIC_SINK_PROGRAM_PATH" ] ; then
 	[ -x "$STARPU_MIC_SINK_PROGRAM_PATH/.libs/lu_example_float" ] && STARPU_MIC_SINK_PROGRAM_NAME=$STARPU_MIC_SINK_PROGRAM_PATH/.libs/lu_example_float
 fi
 
-$PREFIX/lu_example_float -size $((960 * 4)) -nblocks 4 -piv
-$PREFIX/lu_example_float -size $((960 * 4)) -nblocks 4 -no-stride
-$PREFIX/lu_example_float -size $((960 * 4)) -nblocks 4 -bound
-$PREFIX/lu_example_float -size $((960 * 2)) -nblocks 2 -bounddeps
-$PREFIX/lu_example_float -size $((960 * 2)) -nblocks 2 -bound -bounddeps -bounddepsprio
+$PREFIX/lu_example_float -size $((160 * 4)) -nblocks 4 -piv
+$PREFIX/lu_example_float -size $((160 * 4)) -nblocks 4 -no-stride
+$PREFIX/lu_example_float -size $((160 * 4)) -nblocks 4 -bound
+$PREFIX/lu_example_float -size $((160 * 2)) -nblocks 2 -bounddeps
+$PREFIX/lu_example_float -size $((160 * 2)) -nblocks 2 -bound -bounddeps -bounddepsprio
