32e12728f14cd3b102d9598821d5bb84b1087ef4
Samuel Thibault <samuel.thibault@ens-lyon.org>
2019-01-19 21:16:17 +0100

    considerably reduce tasks_size_overhead_scheds.sh duration
    (cherry picked from commit 4ad92e3514d5479e3d9c8c70836ec2d59598563c)

--- tests/microbenchs/microbench.sh
+++ tests/microbenchs/microbench.sh
@@ -1,7 +1,7 @@
 # StarPU --- Runtime system for heterogeneous multicore architectures.
 #
 # Copyright (C) 2016,2017                                CNRS
-# Copyright (C) 2016,2017                                Université de Bordeaux
+# Copyright (C) 2016,2017,2019                           Université de Bordeaux
 #
 # StarPU is free software; you can redistribute it and/or modify
 # it under the terms of the GNU Lesser General Public License as published by
@@ -28,6 +28,7 @@ SCHEDS=`$(dirname $0)/../../tools/starpu_sched_display`
 test_scheds()
 {
 	TEST=$1
+	shift
 	xfailed=""
 	failed=""
 	pass=""
@@ -43,7 +44,7 @@ test_scheds()
 	for sched in $SCHEDS;
 	do
 	    	set +e
-	    	STARPU_SCHED=$sched $(dirname $0)/$TEST
+		STARPU_SCHED=$sched $(dirname $0)/$TEST "$@"
 		ret=$?
 	    	set -e
 		if test $ret = 0
--- tests/microbenchs/tasks_size_overhead_scheds.sh
+++ tests/microbenchs/tasks_size_overhead_scheds.sh
@@ -2,7 +2,7 @@
 # StarPU --- Runtime system for heterogeneous multicore architectures.
 #
 # Copyright (C) 2016,2017                                CNRS
-# Copyright (C) 2016                                     Université de Bordeaux
+# Copyright (C) 2016,2019                                Université de Bordeaux
 #
 # StarPU is free software; you can redistribute it and/or modify
 # it under the terms of the GNU Lesser General Public License as published by
@@ -19,4 +19,4 @@
 
 XFAIL="heteroprio"
 
-test_scheds tasks_size_overhead_sched.sh
+test_scheds tasks_size_overhead_sched.sh -i 8
