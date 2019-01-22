1fc0c086a405a754cbacca91eda3ed75fd064271
Samuel Thibault <samuel.thibault@ens-lyon.org>
2019-01-22 17:41:50 +0100

    Really make sure not to use GPU memory

--- tests/disk/disk_copy_unpack.c
+++ tests/disk/disk_copy_unpack.c
@@ -50,6 +50,11 @@ int dotest(struct starpu_disk_ops *ops, void *param)
 	int ret;
 
 	/* Initialize StarPU without GPU devices to make sure the memory of the GPU devices will not be used */
+	// Ignore environment variables as we want to force the exact number of workers
+	unsetenv("STARPU_NCUDA");
+	unsetenv("STARPU_NOPENCL");
+	unsetenv("STARPU_NMIC");
+	unsetenv("STARPU_NSCC");
 	struct starpu_conf conf;
 	ret = starpu_conf_init(&conf);
 	if (ret == -EINVAL)
