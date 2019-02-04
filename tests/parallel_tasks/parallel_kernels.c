cd23be48c26df117ba2aeb412be1efa894d04589
Samuel Thibault <samuel.thibault@ens-lyon.org>
2019-02-04 17:37:17 +0100

    Reduce test time on valgrind in quickcheck mode too

--- tests/parallel_tasks/parallel_kernels.c
+++ tests/parallel_tasks/parallel_kernels.c
@@ -91,11 +91,9 @@ int main(void)
 
 	unsigned iter, worker, n;
 	n = N;
-#ifndef STARPU_QUICK_CHECK
 	if (STARPU_RUNNING_ON_VALGRIND) {
-		n = 100;
+		n /= 300;
 	}
-#endif
 	for (iter = 0; iter < n; iter++)
 	{
 		for (worker = 0; worker < nworker; worker++)
