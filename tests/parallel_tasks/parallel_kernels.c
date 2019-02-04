783eb8e7939c4fe1a7efa5ae040e2d5261290aec
Samuel Thibault <samuel.thibault@ens-lyon.org>
2019-02-04 10:41:05 +0100

    Reduce test time in valgrind runs

--- tests/parallel_tasks/parallel_kernels.c
+++ tests/parallel_tasks/parallel_kernels.c
@@ -1,6 +1,6 @@
 /* StarPU --- Runtime system for heterogeneous multicore architectures.
  *
- * Copyright (C) 2010-2016                                Université de Bordeaux
+ * Copyright (C) 2010-2016,2019                           Université de Bordeaux
  * Copyright (C) 2012,2013                                Inria
  * Copyright (C) 2010-2013,2015,2017                      CNRS
  * Copyright (C) 2013                                     Thibaut Lambert
@@ -89,8 +89,14 @@ int main(void)
 
 	unsigned nworker = starpu_worker_get_count() + starpu_combined_worker_get_count();
 
-	unsigned iter, worker;
-	for (iter = 0; iter < N; iter++)
+	unsigned iter, worker, n;
+	n = N;
+#ifndef STARPU_QUICK_CHECK
+	if (STARPU_RUNNING_ON_VALGRIND) {
+		n = 100;
+	}
+#endif
+	for (iter = 0; iter < n; iter++)
 	{
 		for (worker = 0; worker < nworker; worker++)
 		{
