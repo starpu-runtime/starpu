fa92e65c63697c5ece36d78d49f2a08e2bbe5c89
Nathalie Furmento <nathalie.furmento@labri.fr>
2019-02-14 15:39:04 +0100

    mpi/src/starpu_mpi_datatype.c: use MPI_Type_create_hvector() instead of MPI_Type_hvector() which is deprecated
    (cherry picked from commit fb55b58bb10055de332aa9415e1cb9105d0205b6)
    (cherry picked from commit b6a293d930031692e5afc6bdc33fd34dfad2a5e2)

--- mpi/src/starpu_mpi_datatype.c
+++ mpi/src/starpu_mpi_datatype.c
@@ -61,7 +61,7 @@ static void handle_to_datatype_block(starpu_data_handle_t data_handle, MPI_Datat
 	ret = MPI_Type_commit(&datatype_2dlayer);
 	STARPU_ASSERT_MSG(ret == MPI_SUCCESS, "MPI_Type_commit failed");
 
-	ret = MPI_Type_hvector(nz, 1, ldz*elemsize, datatype_2dlayer, datatype);
+	ret = MPI_Type_create_hvector(nz, 1, ldz*elemsize, datatype_2dlayer, datatype);
 	STARPU_ASSERT_MSG(ret == MPI_SUCCESS, "MPI_Type_hvector failed");
 
 	ret = MPI_Type_commit(datatype);
