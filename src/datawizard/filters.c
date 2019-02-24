1dbaeb6b8f81231d9cdb643a2d6f756960e1648e
Samuel Thibault <samuel.thibault@ens-lyon.org>
2019-02-24 20:47:15 +0100

    Do not inherit MPI data when partitioning data, application needs to provide a separate MPI tag
    (cherry picked from commit 96790a2eb1c9d99287c87d2c130d2508f5109e8a)

--- src/datawizard/filters.c
+++ src/datawizard/filters.c
@@ -238,7 +238,7 @@ static void _starpu_data_partition(starpu_data_handle_t initial_handle, starpu_d
 		child->readonly = 0;
 		child->active = inherit_state;
 		child->active_ro = 0;
-                child->mpi_data = initial_handle->mpi_data;
+                child->mpi_data = NULL;
 		child->root_handle = initial_handle->root_handle;
 		child->father_handle = initial_handle;
 		child->active_children = NULL;
