440763861aa40281400fe13c23a5f450edd5bf4e
Samuel Thibault <samuel.thibault@ens-lyon.org>
2019-07-10 11:43:06 +0200

    More use of sg_actor_self

--- include/starpu_thread.h
+++ include/starpu_thread.h
@@ -58,7 +58,11 @@ extern "C"
 
 #ifdef STARPU_SIMGRID
 
+#ifdef STARPU_HAVE_SIMGRID_ACTOR_H
+typedef sg_actor_t starpu_pthread_t;
+#else
 typedef msg_process_t starpu_pthread_t;
+#endif
 typedef int starpu_pthread_attr_t;
 
 #ifdef STARPU_HAVE_SIMGRID_ACTOR_H
