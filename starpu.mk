f27a7ea4a23eb04519b5e3d197661effcfced01d
Samuel Thibault <samuel.thibault@ens-lyon.org>
2020-02-27 23:24:14 +0100

    Factorize recheck/showfailed/showcheck/showsuite
    (cherry picked from commit 65004a1a4f2892168785b56e28c2d1ee4ef86680)
    (cherry picked from commit 6a40af26a506d0574a6743a69f9d230c00a0a265)

--- starpu.mk
+++ starpu.mk
@@ -1,3 +1,65 @@
+# StarPU --- Runtime system for heterogeneous multicore architectures.
+#
+# Copyright (C) 2017, 2018                               CNRS
+# Copyright (C) 2016-2017,2020                           Université de Bordeaux
+#
+# StarPU is free software; you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation; either version 2.1 of the License, or (at
+# your option) any later version.
+#
+# StarPU is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+#
+# See the GNU Lesser General Public License in COPYING.LGPL for more details.
+#
+
+ifeq ($(TESTS_TYPE),none)
+recheck:
+	-cat /dev/null
+
+showfailed:
+	@-cat /dev/null
+
+showcheck:
+	-cat /dev/null
+
+showsuite:
+	-cat /dev/null
+endif
+
+ifeq ($(TESTS_TYPE),subdirs)
+recheck:
+	RET=0 ; \
+	for i in $(SUBDIRS) ; do \
+		make -C $$i recheck || RET=1 ; \
+	done ; \
+	exit $$RET
+
+showfailed:
+	@RET=0 ; \
+	for i in $(SUBDIRS) ; do \
+		make -C $$i showfailed || RET=1 ; \
+	done ; \
+	exit $$RET
+
+showcheck:
+	RET=0 ; \
+	for i in $(SUBDIRS) ; do \
+		make -C $$i showcheck || RET=1 ; \
+	done ; \
+	exit $$RET
+
+showsuite:
+	RET=0 ; \
+	for i in $(SUBDIRS) ; do \
+		make -C $$i showsuite || RET=1 ; \
+	done ; \
+	exit $$RET
+endif
+
+ifeq ($(TESTS_TYPE),tests)
 showfailed:
 	@! grep "^FAIL " $(TEST_LOGS) /dev/null
 	@RET=0 ; \
@@ -35,3 +97,4 @@ showsuite:
 		make -C $$i showsuite || RET=1 ; \
 	done ; \
 	exit $$RET
+endif
