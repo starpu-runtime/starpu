a8895a494700816cc09432cc551e6ee3b63fe60e
Samuel Thibault <samuel.thibault@ens-lyon.org>
2020-02-28 15:31:04 +0100

    fix build
    (cherry picked from commit 9ff126a449746c99d2d57dae3cbb64e5753d7494)
    (cherry picked from commit 32909c4d757fa8de2fa3b95e2768fd9b648c4ab2)

--- examples/spmv/dw_block_spmv.c
+++ examples/spmv/dw_block_spmv.c
@@ -271,7 +271,7 @@ void launch_spmv_codelets(void)
 
 		ret = starpu_task_submit(&task_tab[task]);
 		if (ret == -ENODEV)
-			exit(STARPU_TEST_SKIPPED);
+			exit(77);
 		STARPU_CHECK_RETURN_VALUE(ret, "starpu_task_submit");
 	}
 
