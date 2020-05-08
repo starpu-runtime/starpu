1adfa223c0fdb8dd3e5eaaf82d002552fda5da00
Pierre Huchant <pierre.huchant@inria.fr>
2020-05-08 12:48:12 +0200

    julia: update copyright

--- julia/src/init.jl
+++ julia/src/init.jl
@@ -1,3 +1,18 @@
+# StarPU --- Runtime system for heterogeneous multicore architectures.
+#
+# Copyright (C) 2020       Université de Bordeaux, CNRS (LaBRI UMR 5800), Inria
+#
+# StarPU is free software; you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation; either version 2.1 of the License, or (at
+# your option) any later version.
+#
+# StarPU is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+#
+# See the GNU Lesser General Public License in COPYING.LGPL for more details.
+#
 """
     Must be called before any other starpu function. Field extern_task_path is the
     shared library path which will be used to find StarpuCodelet
