641c368e6cff94dcde06f358311a8b4ef2fc4555
Samuel Thibault <samuel.thibault@ens-lyon.org>
2020-05-08 11:48:33 +0200

    Fix simgrid-MC build

--- tests/model-checking/starpu_barrier.c
+++ tests/model-checking/starpu_barrier.c
@@ -67,6 +67,7 @@
 #include <xbt/synchro_core.h>
 #endif
 
+/* common/thread.c references these, but doesn't need to have them working anyway */
 int
 _starpu_simgrid_thread_start(int argc, char *argv[])
 {
@@ -77,6 +78,10 @@ size_t _starpu_default_stack_size = 8192;
 
 void
 _starpu_simgrid_set_stack_size(size_t stack_size)
+{
+}
+
+starpu_sg_host_t _starpu_simgrid_get_host_by_name(const char *name)
 {
 }
 
