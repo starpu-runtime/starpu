7943f9d4c625f79ac50f82abe14168007a82cb7b
Samuel Thibault <samuel.thibault@ens-lyon.org>
2020-05-13 17:44:18 +0200

    Make loader detect valgrind/helgrind/[altu]san execution
    Rather than hardcoding the timeout in the tests.py configure, thus the
    same for all of quick/normal/long cases.
    
    (cherry picked from commit 8324d41f092814cc90f8c9635b7a292a0ae0a0ac)
    (cherry picked from commit e87664c44d5feab047c06dc68174560d09c39b68)

--- tests/loader.c
+++ tests/loader.c
@@ -309,7 +309,17 @@ int main(int argc, char *argv[])
 	if (getenv("STARPU_TIMEOUT_ENV"))
 		timeout = strtol(getenv("STARPU_TIMEOUT_ENV"), NULL, 10);
 	if (timeout <= 0)
+	{
 		timeout = DEFAULT_TIMEOUT;
+		if ((launcher && strstr(launcher, "valgrind")) ||
+		    (launcher && strstr(launcher, "helgrind")) ||
+		    getenv("TSAN_OPTIONS") != NULL)
+			timeout *= 10;
+		if (getenv("ASAN_OPTIONS") != NULL ||
+		    getenv("USAN_OPTIONS") != NULL ||
+		    getenv("LSAN_OPTIONS") != NULL)
+			timeout *= 2;
+	}
 
 	/* set SIGALARM handler */
 	sa.sa_flags = 0;
