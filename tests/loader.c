d90b2cf7125c891526502b6a059143166e72e25e
Samuel Thibault <samuel.thibault@ens-lyon.org>
2020-05-13 19:24:06 +0200

    Harmonize starpu-1.3's loader with master
    (cherry picked from commit c1396fe246412bd8bd81786dd52729148ce046b2)

--- tests/loader.c
+++ tests/loader.c
@@ -226,6 +226,28 @@ int main(int argc, char *argv[])
 
 	test_args = NULL;
 	timeout = 0;
+
+	launcher=getenv("STARPU_CHECK_LAUNCHER");
+	launcher_args=getenv("STARPU_CHECK_LAUNCHER_ARGS");
+
+	if (getenv("STARPU_TIMEOUT_ENV"))
+	{
+		/* get user-defined iter_max value */
+		timeout = strtol(getenv("STARPU_TIMEOUT_ENV"), NULL, 10);
+	}
+	if (timeout <= 0)
+	{
+		timeout = DEFAULT_TIMEOUT;
+		if ((launcher && strstr(launcher, "valgrind")) ||
+		    (launcher && strstr(launcher, "helgrind")) ||
+		    getenv("TSAN_OPTIONS") != NULL)
+			timeout *= 10;
+		if (getenv("ASAN_OPTIONS") != NULL ||
+		    getenv("USAN_OPTIONS") != NULL ||
+		    getenv("LSAN_OPTIONS") != NULL)
+			timeout *= 2;
+	}
+
 	test_name = argv[1];
 
 	if (!test_name)
@@ -251,8 +273,6 @@ int main(int argc, char *argv[])
 	}
 
 	/* get launcher program */
-	launcher=getenv("STARPU_CHECK_LAUNCHER");
-	launcher_args=getenv("STARPU_CHECK_LAUNCHER_ARGS");
 	if (launcher_args)
 		launcher_args=strdup(launcher_args);
 
@@ -303,22 +323,6 @@ int main(int argc, char *argv[])
 
 		launcher = NULL;
 		launcher_args = NULL;
-	}
-
-	/* get user-defined iter_max value */
-	if (getenv("STARPU_TIMEOUT_ENV"))
-		timeout = strtol(getenv("STARPU_TIMEOUT_ENV"), NULL, 10);
-	if (timeout <= 0)
-	{
-		timeout = DEFAULT_TIMEOUT;
-		if ((launcher && strstr(launcher, "valgrind")) ||
-		    (launcher && strstr(launcher, "helgrind")) ||
-		    getenv("TSAN_OPTIONS") != NULL)
-			timeout *= 10;
-		if (getenv("ASAN_OPTIONS") != NULL ||
-		    getenv("USAN_OPTIONS") != NULL ||
-		    getenv("LSAN_OPTIONS") != NULL)
-			timeout *= 2;
 	}
 
 	/* set SIGALARM handler */
