bdaad3a1334dac5913e107a756e2d4ca7006af6f
Samuel Thibault <samuel.thibault@ens-lyon.org>
2020-08-10 15:44:37 +0200

    overlap: Add missing starpu_do_schedule call before blocking main
    (cherry picked from commit 78047c4ce573efedf00d723036fb73aa3180d2ed)

--- tests/overlap/overlap.c
+++ tests/overlap/overlap.c
@@ -130,6 +130,7 @@ int main(int argc, char **argv)
 		STARPU_CHECK_RETURN_VALUE(ret, "starpu_task_submit");
 	}
 
+	starpu_do_schedule();
 	STARPU_PTHREAD_MUTEX_LOCK(&mutex);
 	if (!finished)
 		STARPU_PTHREAD_COND_WAIT(&cond, &mutex);
