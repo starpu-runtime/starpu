f43d4563bf2e08b4dbca5316a516cf00f8203ef3
Samuel Thibault <samuel.thibault@ens-lyon.org>
2020-08-17 15:00:01 +0200

    interface examples: Avoid more duplicate definitions
    (cherry picked from commit af4f6c03daa3b51617459fa97aa1522f4900ba09)

--- examples/api/vector_data_interface.c
+++ examples/api/vector_data_interface.c
@@ -19,6 +19,12 @@
 
 #define starpu_interface_vector_ops my_starpu_interface_vector_ops
 #define starpu_vector_data_register my_starpu_vector_data_register
+#define starpu_vector_data_register_allocsize my_starpu_vector_data_register_allocsize
+#define starpu_vector_ptr_register my_starpu_vector_data_ptr_register
+#define starpu_vector_get_nx my_starpu_vector_get_nx
+#define starpu_vector_get_local_ptr my_starpu_vector_get_local_ptr
+#define starpu_vector_get_elemsize my_starpu_vector_get_elemsize
+#define starpu_vector_get_allocsize my_starpu_vector_get_allocsize
 #include "../../src/datawizard/interfaces/vector_interface.c"
 
 int main()
