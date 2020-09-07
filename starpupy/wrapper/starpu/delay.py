34bc9f3db74733087602277f9a921804abd9b47d
HE Kun <kun.he@inria.fr>
2020-09-07 19:25:15 +0200

    starpupy: add python main function to use starpu wrapper

--- starpupy/wrapper/starpu/delay.py
+++ starpupy/wrapper/starpu/delay.py
@@ -1,5 +1,10 @@
 from starpu import task
+import asyncio
+
 def delayed(f):
 	def submit():
-		task.task_submit(f)
+		async def fut_wait():
+			fut = task.task_submit(f)
+			await fut
+		asyncio.run(fut_wait())
 	return submit
--- starpupy/wrapper/starpu_py.py
+++ starpupy/wrapper/starpu_py.py
@@ -1,28 +1,59 @@
 import starpu
 import time
+import asyncio
 
-@starpu.delayed
-def salut():
-	time.sleep(1)
-	print ("salut, le monde")
-
-
+############################################################################
+#function no input no output print hello world
 def hello():
-	print ("print in python")
+	print ("Example 1:")
 	print ("Hello, world!")
 
-starpu.pause()
+#submit function "hello"
+async def hello_wait():
+    fut = starpu.task_submit(hello)
+    await fut
+asyncio.run(hello_wait())
 
-print("begin to submit task in python")
-fut=starpu.task_submit(hello)
-salut()
-#starpu.task_submit(hello)
-#starpu.task_submit(hello)
-print("finish to submit task in python")
-starpu.resume()
+#############################################################################
 
-#print("begin to sleep")
-#time.sleep(1)
-#print("finish to sleep")
+#function no input no output
+def func1():
+	print ("Example 2:")
+	print ("This is a function no input no output")
+
+#submit function "func1"
+async def func1_wait():
+    fut1 = starpu.task_submit(func1)
+    await fut1
+asyncio.run(func1_wait())
+
+##############################################################################
+
+#using decorator wrap the function
+@starpu.delayed
+def func1_deco():
+	#time.sleep(1)
+	print ("Example 3:")
+	print ("This is a function wrapped by the decorator function")
+
+#apply starpu.delayed(func1_deco())
+func1_deco()
+
+##############################################################################
+
+#function no input return a value
+def func2():
+	print ("Example 4:")
+	return 12
+
+#submit function "func2"
+async def func2_wait():
+    fut2 = starpu.task_submit(func2)
+    res = await fut2
+    #print the result of function
+    print("This is a function no input and the return value is", res)
+asyncio.run(func2_wait())
+
+###############################################################################
 
 starpu.task_wait_for_all()
