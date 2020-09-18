3cf244d6181f19d5d880f2f6ccf8619a28777286
Nathalie Furmento <nathalie.furmento@labri.fr>
2020-09-18 09:28:53 +0200

    src/common/fxt.c: set the process id in the fxt trace file as mentioned in the documentation
    (cherry picked from commit 426d606c7f39fb7ce135d30ec2e72edab0a59b24)

--- src/common/fxt.c
+++ src/common/fxt.c
@@ -163,6 +163,7 @@ void _starpu_fxt_init_profiling(unsigned trace_buffer_size)
 
 	_starpu_fxt_started = 1;
 	_starpu_written = 0;
+	_starpu_id = (int)getpid();
 	_starpu_profile_set_tracefile();
 
 #ifdef HAVE_FUT_SET_FILENAME
