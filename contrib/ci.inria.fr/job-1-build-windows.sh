07be2c7a5063d4ce7a40eb23e146791a46470656
Nathalie Furmento <nathalie.furmento@labri.fr>
2021-01-06 10:38:16 +0100

    contrib/ci.inria.fr/job-1-build-windows.sh: avoid timestamp errors
    (cherry picked from commit df330e415f6d7d738cb880c5a491443fc7b587eb)

--- contrib/ci.inria.fr/job-1-build-windows.sh
+++ contrib/ci.inria.fr/job-1-build-windows.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # StarPU --- Runtime system for heterogeneous multicore architectures.
 #
-# Copyright (C) 2013-2020  Université de Bordeaux, CNRS (LaBRI UMR 5800), Inria
+# Copyright (C) 2013-2021  Université de Bordeaux, CNRS (LaBRI UMR 5800), Inria
 #
 # StarPU is free software; you can redistribute it and/or modify
 # it under the terms of the GNU Lesser General Public License as published by
@@ -28,6 +28,7 @@ fi
 basename=$(basename $tarball .tar.gz)
 test -d $basename && chmod -R u+rwX $basename && rm -rf $basename
 tar xfz $tarball
+touch --date="last hour" $(find $basename)
 version=$(echo $basename | cut -d- -f2)
 winball=starpu-win32-build-${version}
 
