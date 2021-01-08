7b12deee30790567b43e00dfb6445e05adb29e06
Nathalie Furmento <nathalie.furmento@labri.fr>
2021-01-08 16:44:54 +0100

    tests/datawizard/variable_size.c: do not use keywords as variable name
    (cherry picked from commit 0da5163a0f26ff116bf7c31b60cf244f0bbf31da)

--- tests/datawizard/variable_size.c
+++ tests/datawizard/variable_size.c
@@ -100,7 +100,7 @@ void variable_size_data_register(starpu_data_handle_t *handleptr, unsigned x, un
 	/* Simulate that tiles close to the diagonal are more dense */
 	vsinterface.size = FULLSIZE * (starpu_lrand48() % 1024 + 1024) / 2048. * (N-sqrt(abs((int)x-(int)y)*N)) / N;
 	/* Round to page size */
-	vsinterface.size -= interface.size & (65536-1);
+	vsinterface.size -= vsinterface.size & (65536-1);
 
 	_starpu_simgrid_data_new(vsinterface.size);
 
