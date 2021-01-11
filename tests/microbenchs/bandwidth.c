ca9e4ba93499ca7d25aff62804ef5d590dfc8901
Samuel Thibault <samuel.thibault@ens-lyon.org>
2021-01-11 12:36:07 +0100

    lsan seems to go crazy with our 64*128MiB allocation
    (cherry picked from commit aec37ec3eff9027e7294d44c1b5963ab1cf28d7c)

--- tests/microbenchs/bandwidth.c
+++ tests/microbenchs/bandwidth.c
@@ -25,7 +25,7 @@
  * kernels and number of idle workers.
  */
 
-#ifdef STARPU_QUICK_CHECK
+#if defined(STARPU_QUICK_CHECK) || defined(STARPU_SANITIZE_LEAK)
 static size_t size = 1024;
 static unsigned cpustep = 4;
 #else
