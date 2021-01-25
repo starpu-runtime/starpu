ec47408dd53d891385b1db184d5132f67c994880
Maxime <maxime.gonthier@ens-lyon.fr>
2021-01-26 00:00:37 +0100

    data.h TODO: tout passer en extern

--- include/starpu_data_maxime.h
+++ include/starpu_data_maxime.h
@@ -4,17 +4,18 @@
 #include <starpu.h>
 
 struct starpu_task *task_currently_treated;
+//~ extern struct starpu_task *task_currently_treated;
 
 /**
    Task currently treated. 
    
-   It is set in sched_policy.c in void _starpu_sched_pre_exec_hook(struct starpu_task *task)
+   It is set in HFP.c in get_last_finished_task(struct starpu_task *task)
     
    Then used in xgemm.c in starpu_data_handle_t belady_victim_selector(unsigned node)
 */
 
-//~ struct starpu_data_handle_t * all_data_needed;
 starpu_data_handle_t * all_data_needed;
+//~ extern starpu_data_handle_t * all_data_needed;
 
 /**
    All the data needed for the current application
@@ -25,6 +26,7 @@ starpu_data_handle_t * all_data_needed;
 */
 
 int nb_different_data;
+//~ extern int nb_different_data;
 
 /**
    Number of different unique data
@@ -35,15 +37,21 @@ int nb_different_data;
 */
 
 starpu_data_handle_t * data_use_order;
+//~ extern starpu_data_handle_t * data_use_order;
 
 int total_nb_data;
+//~ extern int total_nb_data;
 
 int * task_position_in_data_use_order;
+//~ extern int * task_position_in_data_use_order;
 
 int total_nb_task;
+//~ extern int total_nb_task;
 
 int index_task_currently_treated;
+//~ extern int index_task_currently_treated;
 
 int last_index_task_currently_treated;
+//~ extern int last_index_task_currently_treated;
 
 #endif
