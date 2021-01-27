2ebc552a06581c7d2503734db109449da1e7c08a
Samuel Thibault <samuel.thibault@ens-lyon.org>
2021-01-27 19:07:28 +0100

    memalloc: Expose is_prefetch to eviction strategy

--- src/datawizard/malloc.c
+++ src/datawizard/malloc.c
@@ -164,7 +164,7 @@ int _starpu_malloc_flags_on_node(unsigned dst_node, void **A, size_t dim, int fl
 				size_t reclaim = 2 * dim;
 				_STARPU_DEBUG("There is not enough memory left, we are going to reclaim %ld\n", (long)reclaim);
 				_STARPU_TRACE_START_MEMRECLAIM(dst_node,0);
-				freed = _starpu_memory_reclaim_generic(dst_node, 0, reclaim);
+				freed = _starpu_memory_reclaim_generic(dst_node, 0, reclaim, STARPU_FETCH);
 				_STARPU_TRACE_END_MEMRECLAIM(dst_node,0);
 				if (freed < dim && !(flags & STARPU_MEMORY_WAIT))
 				{
