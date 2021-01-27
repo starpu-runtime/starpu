d18e64896479a59ff7ac0d4c9b2fdf6b65bb8932
Samuel Thibault <samuel.thibault@ens-lyon.org>
2021-01-27 22:50:05 +0100

    mpi tests: fix acquire
    (cherry picked from commit f653e1b44dde1a1ed5f20ddfd59ec10cb9d8a64c)

--- mpi/tests/mpi_earlyrecv2.c
+++ mpi/tests/mpi_earlyrecv2.c
@@ -111,7 +111,7 @@ void check_variable(starpu_data_handle_t handle, int i, int rank, int *error)
 {
 	int other_rank = rank%2 == 0 ? rank+1 : rank-1;
 
-	starpu_data_acquire(handle, STARPU_R);
+	starpu_data_acquire_on_node(handle, starpu_worker_get_local_memory_node(), STARPU_R);
 	int *rvalue = (int *)starpu_data_get_local_ptr(handle);
 	if (*rvalue != i*other_rank)
 	{
