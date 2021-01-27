793dfd9dbe3b65db294deed33bc13e57be9b4081
Samuel Thibault <samuel.thibault@ens-lyon.org>
2021-01-27 10:15:43 +0100

    loader: Clear SIGALRM signal handler on timeout so we really terminate with sigalarm
    (cherry picked from commit 33f882e8964caf4c960060fd4423e5e604be98f4)

--- tests/loader.c
+++ tests/loader.c
@@ -359,7 +359,7 @@ int main(int argc, char *argv[])
 	setenv("STARPU_OPENCL_PROGRAM_DIR", STARPU_SRC_DIR, 1);
 
 	/* set SIGALARM handler */
-	sa.sa_flags = 0;
+	sa.sa_flags = SA_RESETHAND | SA_NODEFER;
 	sigemptyset(&sa.sa_mask);
 	sa.sa_handler = test_cleaner;
 	if (-1 == sigaction(SIGALRM, &sa, NULL))
