075773532855522121d66d6121f51ef481591c50
Maxime <maxime.gonthier@ens-lyon.fr>
2021-01-28 11:33:08 +0100

    maj

--- src/datawizard/memalloc.c
+++ src/datawizard/memalloc.c
@@ -467,7 +467,7 @@ static size_t free_memory_on_node(struct _starpu_mem_chunk *mc, unsigned node)
 		if (handle)
 			STARPU_ASSERT(replicate->refcnt == 0);
 	}
-
+	
 	return freed;
 }
 
@@ -716,7 +716,7 @@ static size_t try_to_throw_mem_chunk(struct _starpu_mem_chunk *mc, unsigned node
 	    /* unlock the tree */
 	    unlock_all_subtree(handle);
 	}
-	if (freed ==1 && starpu_get_env_number_default("PRINTF",0) == 1) { printf("Eviction dans try_to_throw_mem_chunk de %p\n",handle); }
+	if (freed == 1 && starpu_get_env_number_default("PRINTF",0) == 1) { printf("Eviction dans try_to_throw_mem_chunk de %p\n",handle); }
 	return freed;
 }
 
