8a207bede1c864c4f3c6e9c500220d139a98fabf
Maxime <maxime.gonthier@ens-lyon.fr>
2021-02-15 18:21:29 +0100

    Ajout dans le script de GF_NT_Mc le temps a recuperer

--- Scripts_maxime/Experiments.sh
+++ Scripts_maxime/Experiments.sh
@@ -1,25 +1,25 @@
 #!/usr/bin/bash
 start=`date +%s`
-#~ ./configure --enable-simgrid --disable-mpi --with-simgrid-dir=/home/gonthier/simgrid
-#~ sudo make install
+./configure --enable-simgrid --disable-mpi --with-simgrid-dir=/home/gonthier/simgrid
+sudo make install
 sudo make -j4
 
 #~ bash Scripts_maxime/Matrice_ligne/GF_M_MC_NT=225_LRU_BW350.sh /home/gonthier/ /home/gonthier/these_gonthier_maxime/Starpu/ 10
-#~ bash Scripts_maxime/Matrice_ligne/GF_NT_MC_LRU_BW350_CM500.sh /home/gonthier/ /home/gonthier/these_gonthier_maxime/Starpu/ 10
-#~ echo "Scripts de matrice 2D fini"
+bash Scripts_maxime/Matrice_ligne/GF_NT_MC_LRU_BW350_CM500.sh /home/gonthier/ /home/gonthier/these_gonthier_maxime/Starpu/ 10
+echo "Scripts de matrice 2D fini"
 #~ bash Scripts_maxime/Matrice3D/GF_M_M3D_N=15_BW350.sh /home/gonthier/ /home/gonthier/these_gonthier_maxime/Starpu/ 10
 #~ bash Scripts_maxime/Matrice3D/GF_NT_M3D_BW350_CM500.sh /home/gonthier/ /home/gonthier/these_gonthier_maxime/Starpu/ 8
 #~ echo "Scripts de matrice 3D fini"
-#~ bash Scripts_maxime/Random_tasks/GF_M_MC_LRU_N=15_BW350_CM500_RANDOMTASKS.sh /home/gonthier/ /home/gonthier/these_gonthier_maxime/Starpu/ 10
-#~ bash Scripts_maxime/Random_tasks/GF_NT_MC_LRU_BW350_CM500_RANDOMTASKS.sh /home/gonthier/ /home/gonthier/these_gonthier_maxime/Starpu/ 10
-#~ echo "Scripts de random matrice 2D fini"
 #~ bash Scripts_maxime/Cholesky/GF_M_CHO_N=20_BW350.sh /home/gonthier/ /home/gonthier/these_gonthier_maxime/Starpu/ 9
 #~ bash Scripts_maxime/Cholesky/GF_NT_CHO_BW350_CM500.sh /home/gonthier/ /home/gonthier/these_gonthier_maxime/Starpu/ 7
 #~ echo "Scripts de cholesky fini"
+#~ bash Scripts_maxime/Random_tasks/GF_M_MC_LRU_N=15_BW350_CM500_RANDOMTASKS.sh /home/gonthier/ /home/gonthier/these_gonthier_maxime/Starpu/ 10
+#~ bash Scripts_maxime/Random_tasks/GF_NT_MC_LRU_BW350_CM500_RANDOMTASKS.sh /home/gonthier/ /home/gonthier/these_gonthier_maxime/Starpu/ 10
+#~ echo "Scripts de random matrice 2D fini"
 
 #~ bash Scripts_maxime/CMvsRCM/GF_NT_CMvsRCM_MC.sh /home/gonthier/ /home/gonthier/these_gonthier_maxime/Starpu/ 10
-bash Scripts_maxime/CMvsRCM/GF_NT_CMvsRCM_CHO.sh /home/gonthier/ /home/gonthier/these_gonthier_maxime/Starpu/ 7
-echo "Scripts de tests fini"
+#~ bash Scripts_maxime/CMvsRCM/GF_NT_CMvsRCM_CHO.sh /home/gonthier/ /home/gonthier/these_gonthier_maxime/Starpu/ 7
+#~ echo "Scripts de tests fini"
 
 end=`date +%s`
 runtime=$((end-start))
--- Scripts_maxime/Matrice_ligne/GF_NT_MC_LRU_BW350_CM500.sh
+++ Scripts_maxime/Matrice_ligne/GF_NT_MC_LRU_BW350_CM500.sh
@@ -9,13 +9,16 @@ ECHELLE_X=5
 START_X=0   
 FICHIER=GF_NT_MC_LRU_BW350_CM500
 FICHIER_DT=DT_NT_MC_LRU_BW350_CM500
+FICHIER_TIME=TIME_NT_MC_LRU_BW350_CM500
 FICHIER_RAW=${PATH_STARPU}/starpu/Output_maxime/GFlops_raw_out_1.txt
 FICHIER_RAW_DT=${PATH_STARPU}/starpu/Output_maxime/GFlops_raw_out_3.txt
 FICHIER_BUS=${PATH_STARPU}/starpu/Output_maxime/BUS_STATS_1.txt
+FICHIER_RAW_TIME=${PATH_STARPU}/starpu/Output_maxime/Execution_time_raw.txt
 DOSSIER=Matrice_ligne
 truncate -s 0 ${FICHIER_RAW:0}
 truncate -s 0 ${FICHIER_RAW_DT:0}
 truncate -s 0 ${FICHIER_BUS:0}
+truncate -s 0 ${FICHIER_RAW_TIME:0}
 echo "############## Random_order ##############"
 for ((i=1 ; i<=(($NB_TAILLE_TESTE)); i++))
 	do 
@@ -72,11 +75,20 @@ for ((i=1 ; i<=(($NB_TAILLE_TESTE)); i++))
 	STARPU_SCHED=HFP STARPU_NTASKS_THRESHOLD=30 BELADY=1 STARPU_MINIMUM_CLEAN_BUFFERS=0 STARPU_TARGET_CLEAN_BUFFERS=0 STARPU_CUDA_PIPELINE=4 ORDER_U=1 STARPU_SIMGRID_CUDA_MALLOC_COST=0 STARPU_LIMIT_BANDWIDTH=350 STARPU_LIMIT_CUDA_MEM=500 STARPU_BUS_STATS=1 STARPU_BUS_STATS_FILE="${FICHIER_BUS:0}" STARPU_NCPU=0 STARPU_NCUDA=1 STARPU_NOPENCL=0 STARPU_HOSTNAME=attila ./examples/mult/sgemm -xy $((960*N)) -nblocks $((N)) -iter 1 | tail -n 1 >> ${FICHIER_RAW:0}
 	sed -n '4p' ${FICHIER_BUS:0} >> ${FICHIER_RAW_DT:0}
 done
+# Tracage des data transfers
 gcc -o cut_datatransfers_raw_out cut_datatransfers_raw_out.c
 ./cut_datatransfers_raw_out $NB_TAILLE_TESTE $NB_ALGO_TESTE $ECHELLE_X $START_X ${FICHIER_RAW_DT:0} ${PATH_R}/R/Data/${DOSSIER}/${FICHIER_DT:0}.txt
 Rscript ${PATH_R}/R/ScriptR/${DOSSIER}/${FICHIER_DT:0}.R ${PATH_R}/R/Data/${DOSSIER}/${FICHIER_DT}.txt
 mv ${PATH_STARPU}/starpu/Rplots.pdf ${PATH_R}/R/Courbes/${DOSSIER}/${FICHIER_DT:0}.pdf
+
+# Tracage des GFlops
 gcc -o cut_gflops_raw_out cut_gflops_raw_out.c
 ./cut_gflops_raw_out $NB_TAILLE_TESTE $NB_ALGO_TESTE $ECHELLE_X $START_X ${FICHIER_RAW:0} ${PATH_R}/R/Data/${DOSSIER}/${FICHIER:0}.txt
 Rscript ${PATH_R}/R/ScriptR/${DOSSIER}/${FICHIER:0}.R ${PATH_R}/R/Data/${DOSSIER}/${FICHIER}.txt
 mv ${PATH_STARPU}/starpu/Rplots.pdf ${PATH_R}/R/Courbes/${DOSSIER}/${FICHIER:0}.pdf
+
+# Tracage des temps d'exec hors simulation
+gcc -o cut_gflops_raw_out cut_gflops_raw_out.c
+./cut_gflops_raw_out $NB_TAILLE_TESTE $((NB_ALGO_TESTE-1)) $ECHELLE_X $START_X ${FICHIER_RAW_TIME:0} ${PATH_R}/R/Data/${DOSSIER}/${FICHIER_TIME:0}.txt
+Rscript ${PATH_R}/R/ScriptR/${DOSSIER}/${FICHIER_TIME:0}.R ${PATH_R}/R/Data/${DOSSIER}/${FICHIER_TIME}.txt
+mv ${PATH_STARPU}/starpu/Rplots.pdf ${PATH_R}/R/Courbes/${DOSSIER}/${FICHIER_TIME:0}.pdf
