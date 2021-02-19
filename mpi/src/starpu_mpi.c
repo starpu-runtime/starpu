87ab2f3669000aeee098307581aee0a8ac5b1e36
Samuel Thibault <samuel.thibault@ens-lyon.org>
2021-02-19 11:32:38 +0100

    Fix redux_map memleak

--- mpi/src/starpu_mpi.c
+++ mpi/src/starpu_mpi.c
@@ -379,10 +379,13 @@ int starpu_mpi_barrier(MPI_Comm comm)
 
 void _starpu_mpi_data_clear(starpu_data_handle_t data_handle)
 {
+	struct _starpu_mpi_data *data = data_handle->mpi_data;
 	_mpi_backend._starpu_mpi_backend_data_clear(data_handle);
 	_starpu_mpi_cache_data_clear(data_handle);
-	_starpu_spin_destroy(&((struct _starpu_mpi_data*) data_handle->mpi_data)->coop_lock);
-	free(data_handle->mpi_data);
+	_starpu_spin_destroy(&data->coop_lock);
+	if (data->redux_map != REDUX_CONTRIB)
+		free(data->redux_map);
+	free(data);
 	data_handle->mpi_data = NULL;
 }
 
