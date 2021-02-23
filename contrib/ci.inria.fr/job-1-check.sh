fa31a88f62b5e8872b5696299d01689733ab21c1
Nathalie Furmento <nathalie.furmento@labri.fr>
2021-02-23 08:35:27 +0100

    contrib/ci.inria.fr/job-1-check.sh: fix saving env
    (cherry picked from commit 7c9a3bb663f347856291b1c82ed8b9f41b7f51e7)

--- contrib/ci.inria.fr/job-1-check.sh
+++ contrib/ci.inria.fr/job-1-check.sh
@@ -37,6 +37,11 @@ basename=$(basename $tarball .tar.gz)
 export STARPU_HOME=$PWD/$basename/home
 mkdir -p $basename
 cd $basename
+(
+    echo "oldPWD=\${PWD}"
+    env|grep -v LS_COLORS | grep '^[A-Z]'|grep -v BASH_FUNC | grep '=' | sed 's/=/=\"/'| sed 's/$/\"/' | sed 's/^/export /'
+    echo "cd \$oldPWD"
+) > ${PWD}/env
 env > $PWD/env
 
 test -d $basename && chmod -R u+rwX $basename && rm -rf $basename
