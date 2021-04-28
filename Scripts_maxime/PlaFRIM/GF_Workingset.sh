d70042f8472569f972a8524e73e5d18365164a53
Maxime <maxime.gonthier@ens-lyon.fr>
2021-04-28 20:24:56 +0200

    scripts

--- Scripts_maxime/PlaFRIM/GF_Workingset.sh
+++ Scripts_maxime/PlaFRIM/GF_Workingset.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/bash
-#bash Scripts_maxime/PlaFRIM/GF_Workingset.sh 2 Matrice_ligne
+#bash Scripts_maxime/PlaFRIM/GF_Workingset.sh 2 5 Matrice_ligne
 
 NB_TAILLE_TESTE=$1
-DOSSIER=$2
-NB_ALGO_TESTE=5
+DOSSIER=$3
+NB_ALGO_TESTE=2
+NB_TEST_MEME_SOMMET=$2
 ECHELLE_X=5
 START_X=0  
 FICHIER_RAW=Output_maxime/GFlops_raw_out_3.txt
@@ -17,34 +18,49 @@ if [ $DOSSIER = "Matrice_ligne" ]
 		for ((i=1 ; i<=(($NB_TAILLE_TESTE)); i++))
 			do 
 			N=$((START_X+i*ECHELLE_X))
-			STARPU_NCPU=0 STARPU_SCHED=eager ./examples/mult/sgemm -xy $((960*N)) -nblocks $((N)) -iter 1 | tail -n 1 >> ${FICHIER_RAW}
+			for ((j=1 ; j<=(($NB_TEST_MEME_SOMMET)); j++))
+				do 
+				STARPU_SCHED=eager STARPU_CUDA_PIPELINE=4 STARPU_LIMIT_CUDA_MEM=500 STARPU_MINIMUM_CLEAN_BUFFERS=0 STARPU_TARGET_CLEAN_BUFFERS=0 STARPU_NCPU=0 STARPU_NOPENCL=0 ./examples/mult/sgemm -xy $((960*N)) -nblocks $((N)) -iter 1 | tail -n 1 >> ${FICHIER_RAW}
+			done
 		done
 		echo "############## Dmdar ##############"
 		for ((i=1 ; i<=(($NB_TAILLE_TESTE)); i++))
 			do 
 			N=$((START_X+i*ECHELLE_X))
-			STARPU_NCPU=0 STARPU_SCHED=dmdar ./examples/mult/sgemm -xy $((960*N)) -nblocks $((N)) -iter 1 | tail -n 1 >> ${FICHIER_RAW}
+			for ((j=1 ; j<=(($NB_TEST_MEME_SOMMET)); j++))
+				do 
+				STARPU_SCHED=dmdar STARPU_CUDA_PIPELINE=4 STARPU_LIMIT_CUDA_MEM=500 STARPU_MINIMUM_CLEAN_BUFFERS=0 STARPU_TARGET_CLEAN_BUFFERS=0 STARPU_NCPU=0 STARPU_NOPENCL=0 ./examples/mult/sgemm -xy $((960*N)) -nblocks $((N)) -iter 1 | tail -n 1 >> ${FICHIER_RAW}
+			done
 		done
 		echo "############## MST ##############"
 		for ((i=1 ; i<=(($NB_TAILLE_TESTE)); i++))
 			do 
 			N=$((START_X+i*ECHELLE_X))
-			STARPU_NCPU=0 STARPU_SCHED=mst ./examples/mult/sgemm -xy $((960*N)) -nblocks $((N)) -iter 1 | tail -n 1 >> ${FICHIER_RAW}
+			for ((j=1 ; j<=(($NB_TEST_MEME_SOMMET)); j++))
+				do 
+				STARPU_SCHED=mst STARPU_NTASK_THRESHOLD=30 STARPU_CUDA_PIPELINE=4 STARPU_LIMIT_CUDA_MEM=500 STARPU_MINIMUM_CLEAN_BUFFERS=0 STARPU_TARGET_CLEAN_BUFFERS=0 STARPU_NCPU=0 STARPU_NOPENCL=0 ./examples/mult/sgemm -xy $((960*N)) -nblocks $((N)) -iter 1 | tail -n 1 >> ${FICHIER_RAW}
+			done
 		done
 		echo "############## CM ##############"
 		for ((i=1 ; i<=(($NB_TAILLE_TESTE)); i++))
 			do 
 			N=$((START_X+i*ECHELLE_X))
-			STARPU_NCPU=0 STARPU_SCHED=cuthillmckee REVERSE=1 ./examples/mult/sgemm -xy $((960*N)) -nblocks $((N)) -iter 1 | tail -n 1 >> ${FICHIER_RAW}
+			for ((j=1 ; j<=(($NB_TEST_MEME_SOMMET)); j++))
+				do 
+				STARPU_SCHED=cuthillmckee REVERSE=1 STARPU_NTASK_THRESHOLD=30 STARPU_CUDA_PIPELINE=4 STARPU_LIMIT_CUDA_MEM=500 STARPU_MINIMUM_CLEAN_BUFFERS=0 STARPU_TARGET_CLEAN_BUFFERS=0 STARPU_NCPU=0 STARPU_NOPENCL=0 ./examples/mult/sgemm -xy $((960*N)) -nblocks $((N)) -iter 1 | tail -n 1 >> ${FICHIER_RAW}
+			done
 		done
-		echo "############## HFPU + Belady ##############"
+		echo "############## HFP ##############"
 		for ((i=1 ; i<=(($NB_TAILLE_TESTE)); i++))
 			do 
 			N=$((START_X+i*ECHELLE_X))
-			STARPU_NCPU=0 STARPU_SCHED=HFP ORDER_U=1 BELADY=1 ./examples/mult/sgemm -xy $((960*N)) -nblocks $((N)) -iter 1 | tail -n 1 >> ${FICHIER_RAW}
+			for ((j=1 ; j<=(($NB_TEST_MEME_SOMMET)); j++))
+				do 
+				STARPU_SCHED=HFP BELADY=1 ORDER_U=1 STARPU_NTASK_THRESHOLD=30 STARPU_CUDA_PIPELINE=4 STARPU_LIMIT_CUDA_MEM=500 STARPU_MINIMUM_CLEAN_BUFFERS=0 STARPU_TARGET_CLEAN_BUFFERS=0 STARPU_NCPU=0 STARPU_NOPENCL=0 ./examples/mult/sgemm -xy $((960*N)) -nblocks $((N)) -iter 1 | tail -n 1 >> ${FICHIER_RAW}
+			done
 		done
 fi
 
 # Tracage des GFlops
-gcc -o cut_gflops_raw_out cut_gflops_raw_out.c
-./cut_gflops_raw_out $NB_TAILLE_TESTE $NB_ALGO_TESTE $ECHELLE_X $START_X ${FICHIER_RAW} Output_maxime/GFlops.txt
+gcc -o cut_gflops_raw_out_mean_deviation cut_gflops_raw_out_mean_deviation.c -lm
+./cut_gflops_raw_out_mean_deviation $NB_TAILLE_TESTE $NB_ALGO_TESTE $ECHELLE_X $START_X ${FICHIER_RAW} Output_maxime/GFlops.txt NB_TEST_MEME_SOMMET
