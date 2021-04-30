c39fe22cae52fdaeda2e670f5394d797c8949fd5
Maxime <maxime.gonthier@ens-lyon.fr>
2021-04-30 19:16:21 +0200

    scripts

--- Scripts_maxime/PlaFRIM/Draw.sh
+++ Scripts_maxime/PlaFRIM/Draw.sh
@@ -1,14 +1,23 @@
 #~ After an execution in PlaFRIM
-#~ bash Scripts_maxime/PlaFRIM/Draw.sh /home/gonthier/ /home/gonthier/these_gonthier_maxime/Starpu/ Matrice_ligne Workingset_PlaFRIM Sirocco09
+#~ bash Scripts_maxime/PlaFRIM/Draw.sh /home/gonthier/ /home/gonthier/these_gonthier_maxime/Starpu/ Matrice_ligne Workingset_PlaFRIM Sirocco09 PlaFRIM
+#~ bash Scripts_maxime/PlaFRIM/Draw.sh /home/gonthier/ /home/gonthier/these_gonthier_maxime/Starpu/ Matrice_ligne Workingset_PlaFRIM Sirocco09 Grid5000
 
 PATH_STARPU=$1
 PATH_R=$2
 DOSSIER=$3
 MODEL=$4
 GPU=$5
+PLATEFORME=$6
 FICHIER=${PATH_STARPU}/starpu/Output_maxime/GFlops.txt
 
-scp mgonthie@plafrim:/home/mgonthie/starpu/Output_maxime/GFlops.txt /home/gonthier/starpu/Output_maxime/
+if [ $PLATEFORME = "PlaFRIM" ]
+	then
+	scp mgonthie@plafrim:/home/mgonthie/starpu/Output_maxime/GFlops.txt /home/gonthier/starpu/Output_maxime/
+fi
+if [ $PLATEFORME = "Grid5000" ]
+	then
+	scp mgonthier@access.grid5000.fr:/home/mgonthier/lyon/starpu/Output_maxime/GFlops.txt /home/gonthier/starpu/Output_maxime/
+fi
 mv /home/gonthier/starpu/Output_maxime/GFlops.txt ${PATH_R}/R/Data/PlaFRIM/${DOSSIER}/GF_${MODEL}_${GPU}.txt
 
 # Tracage des GFlops
