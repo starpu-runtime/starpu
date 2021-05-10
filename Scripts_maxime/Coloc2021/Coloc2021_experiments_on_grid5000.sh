8bcd2f337d57c1776d220a9bde272ca828e0e248
Maxime <maxime.gonthier@ens-lyon.fr>
2021-05-10 17:32:49 +0200

    ajout chemin

--- Scripts_maxime/Coloc2021/Coloc2021_experiments_on_grid5000.sh
+++ Scripts_maxime/Coloc2021/Coloc2021_experiments_on_grid5000.sh
@@ -1,4 +1,6 @@
 NAME=$1
+PATH_R=$3
+PATH_STARPU=$2
 module load linalg/mkl
 ./configure --prefix=/home/${NAME}/starpu
 make -j 100
@@ -6,3 +8,4 @@ make install
 echo "INITIALISATION OK"
 export STARPU_PERF_MODEL_DIR=/home/${NAME}/starpu/perfmodels/sampling
 bash Scripts_maxime/PlaFRIM/GF_Workingset.sh 9 Matrice_ligne
+bash Scripts_maxime/PlaFRIM/Draw.sh ${PATH_STARPU} ${PATH_R} Matrice_ligne Workingset_PlaFRIM Gemini02 Grid5000
