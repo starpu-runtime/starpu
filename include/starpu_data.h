4ce00c42b0644c72bda7024655d2b0c6e80fb672
Maxime <maxime.gonthier@ens-lyon.fr>
2021-07-23 15:58:14 +0200

    Récupération de component dans victim_selector et register_victim_selector

--- include/starpu_data.h
+++ include/starpu_data.h
@@ -647,7 +647,7 @@ int starpu_data_can_evict(starpu_data_handle_t handle, unsigned node, enum starp
    victim with the same shape as this data, so that the buffer can simply be
    reused without any free/alloc operations (that are very costly with CUDA).
 */
-typedef starpu_data_handle_t starpu_data_victim_selector(starpu_data_handle_t toload, unsigned node, enum starpu_is_prefetch is_prefetch);
+typedef starpu_data_handle_t starpu_data_victim_selector(starpu_data_handle_t toload, unsigned node, enum starpu_is_prefetch is_prefetch, void *component);
 typedef starpu_data_handle_t starpu_data_victim_evicted(int success, starpu_data_handle_t victim);
 /**
    Register a data victim selector.
@@ -665,7 +665,7 @@ typedef starpu_data_handle_t starpu_data_victim_evicted(int success, starpu_data
 
    This is very experimental for now.
 */
-void starpu_data_register_victim_selector(starpu_data_victim_selector selector, starpu_data_victim_evicted evicted);
+void starpu_data_register_victim_selector(starpu_data_victim_selector selector, starpu_data_victim_evicted evicted, void *component);
 #define STARPU_DATA_NO_VICTIM ((starpu_data_handle_t) -1)
 
 /**
