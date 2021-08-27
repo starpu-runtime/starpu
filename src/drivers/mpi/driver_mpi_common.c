2f901e3e98bf8aabb676e62ae40359a69337ff91
Samuel Thibault <samuel.thibault@ens-lyon.org>
2021-08-27 14:24:26 +0200

    add TODO

--- src/drivers/mpi/driver_mpi_common.c
+++ src/drivers/mpi/driver_mpi_common.c
@@ -29,6 +29,7 @@ static int mpi_initialized = 0;
 static int extern_initialized = 0;
 static int src_node_id;
 
+// TODO: I don't think we need a list: there is only one MPI request per datawizard request
 LIST_TYPE(_starpu_mpi_ms_event_request,
         MPI_Request request;
 );
