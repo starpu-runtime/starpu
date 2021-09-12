a58cc125ec099676c80b29078d5a56674f0136ac
Maxime <maxime.gonthier@ens-lyon.fr>
2021-09-12 14:00:22 +0200

    Scripts

--- Scripts_maxime/script_testing_1.sh
+++ Scripts_maxime/script_testing_1.sh
@@ -44,15 +44,15 @@ make -C src/ -j 6
 #~ watch *(int *)0x5555558c2820
 #~ watch *(int *)0x5555558c2820
 
-N=40
-NGPU=3
+N=15
+NGPU=1
 ORDO="dynamic-outer"
-BW=350*3
+BW=350
 #~ BW=35
-CM=500
+CM=100
 #~ CM=500
-EVICTION=0
-READY=1
+EVICTION=1
+READY=0
 HOST=attila
 export STARPU_PERF_MODEL_DIR=tools/perfmodels/sampling
 ulimit -S -s 5000000
@@ -63,7 +63,7 @@ ulimit -S -s 5000000
 STARPU_SCHED=${ORDO} SEED=0 EVICTION_STRATEGY_DYNAMIC_OUTER=$((EVICTION)) STARPU_GENERATE_TRACE=0 PRINTF=1 PRINT_N=$((N)) STARPU_SCHED_READY=$((READY)) STARPU_NTASKS_THRESHOLD=30 STARPU_CUDA_PIPELINE=30 STARPU_SIMGRID_CUDA_MALLOC_COST=0 STARPU_LIMIT_BANDWIDTH=$((BW)) STARPU_MINIMUM_CLEAN_BUFFERS=0 STARPU_TARGET_CLEAN_BUFFERS=0 STARPU_LIMIT_CUDA_MEM=$((CM)) STARPU_NCPU=0 STARPU_NCUDA=$((NGPU)) STARPU_NOPENCL=0 STARPU_HOSTNAME=attila ./examples/mult/sgemm -xy $((960*N)) -nblocks $((N)) -iter 1 2>&1 | tee Output_maxime/terminal_output.txt
 #~ STARPU_SCHED=${ORDO} SEED=0 EVICTION_STRATEGY_DYNAMIC_OUTER=$((EVICTION)) DATA_POP_POLICY=$((POP_POLICY)) STARPU_GENERATE_TRACE=0 PRINTF=1 PRINT_N=$((N)) STARPU_SCHED_READY=$((READY)) STARPU_NTASKS_THRESHOLD=30 STARPU_CUDA_PIPELINE=30 STARPU_SIMGRID_CUDA_MALLOC_COST=0 STARPU_LIMIT_BANDWIDTH=$((BW)) STARPU_MINIMUM_CLEAN_BUFFERS=0 STARPU_TARGET_CLEAN_BUFFERS=0 STARPU_LIMIT_CUDA_MEM=$((CM)) STARPU_NCPU=0 STARPU_NCUDA=$((NGPU)) STARPU_NOPENCL=0 STARPU_HOSTNAME=attila libtool --mode=execute gdb --args ./examples/mult/sgemm -xy $((960*N)) -nblocks $((N)) -iter 1 2>&1 | tee Output_maxime/terminal_output.txt
 
-python3 /home/gonthier/these_gonthier_maxime/Code/visualisation2D.py Output_maxime/Data_coordinates_order_last_SCHEDULER.txt Output_maxime/Data_to_load_SCHEDULER.txt ${N} ${ORDO} ${NGPU} 1
+#~ python3 /home/gonthier/these_gonthier_maxime/Code/visualisation2D.py Output_maxime/Data_coordinates_order_last_SCHEDULER.txt Output_maxime/Data_to_load_SCHEDULER.txt ${N} ${ORDO} ${NGPU} 1
 
 #~ libtool --mode=execute gdb --args
 #~ setarch linux64 -R libtool --mode=execute gdb --args
