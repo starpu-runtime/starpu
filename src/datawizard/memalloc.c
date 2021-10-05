a2d9a1108d3e9bc8d0f720c4568c370711960799
Samuel Thibault <samuel.thibault@ens-lyon.org>
2021-10-05 14:44:10 +0200

    Only use reuse_data_on_node on reuse, others seem to need the whole interface
    (cherry picked from commit b7598961204d240f5d29591e5450ba07d0c262f0)

--- src/datawizard/memalloc.c
+++ src/datawizard/memalloc.c
@@ -1612,10 +1612,7 @@ void _starpu_request_mem_chunk_removal(starpu_data_handle_t handle, struct _star
 	/* Also keep the interface parameters and pointers, for later reuse
 	 * while detached, or freed */
 	_STARPU_MALLOC(mc->chunk_interface, mc->size_interface);
-	if (mc->ops->reuse_data_on_node)
-		mc->ops->reuse_data_on_node(mc->chunk_interface, replicate->data_interface, node);
-	else
-		memcpy(mc->chunk_interface, replicate->data_interface, mc->size_interface);
+	memcpy(mc->chunk_interface, replicate->data_interface, mc->size_interface);
 
 	/* This memchunk doesn't have to do with the data any more. */
 	replicate->mc = NULL;
@@ -1733,10 +1730,7 @@ static starpu_ssize_t _starpu_allocate_interface(starpu_data_handle_t handle, st
 		size = 1;
 	char data_interface[size];
 
-	if (handle->ops->reuse_data_on_node)
-		handle->ops->reuse_data_on_node(data_interface, replicate->data_interface, dst_node);
-	else
-		memcpy(data_interface, replicate->data_interface, handle->ops->interface_size);
+	memcpy(data_interface, replicate->data_interface, handle->ops->interface_size);
 
 	/* Take temporary reference on the replicate */
 	replicate->refcnt++;
@@ -1866,13 +1860,8 @@ static starpu_ssize_t _starpu_allocate_interface(starpu_data_handle_t handle, st
 		allocated_memory = 0;
 	}
 	else
-	{
 		/* Install newly-allocated interface */
-		if (handle->ops->reuse_data_on_node)
-			handle->ops->reuse_data_on_node(replicate->data_interface, data_interface, dst_node);
-		else
-			memcpy(replicate->data_interface, data_interface, handle->ops->interface_size);
-	}
+		memcpy(replicate->data_interface, data_interface, handle->ops->interface_size);
 
 out:
 	return allocated_memory;
