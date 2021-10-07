f5746e0ffb3b24c099a0a8fdb0c1fd857307173d
Maxime <maxime.gonthier@ens-lyon.fr>
2021-10-07 14:36:55 +0200

    script

--- Scripts_maxime/PlaFRIM-Grid5k/IPDPS.sh
+++ Scripts_maxime/PlaFRIM-Grid5k/IPDPS.sh
@@ -343,6 +343,17 @@ then
 		    done
 		fi
 	fi
+	if [ $MODEL == "dynamic_data_aware_compare_threshold" ]
+	then
+		ECHELLE_X=20
+		N=110
+		for ((i=1 ; i<=(($NB_TAILLE_TESTE)); i++))
+			do
+			DDA_TH=$((START_X+i*ECHELLE_X))
+			CHOOSE_BEST_DATA_THRESHOLD=$((DDA_TH)) SEED=0 STARPU_SCHED=dynamic-data-aware STARPU_SCHED_READY=0 DATA_POP_POLICY=1 EVICTION_STRATEGY_DYNAMIC_DATA_AWARE=1 STARPU_NTASKS_THRESHOLD=$((TH)) STARPU_CUDA_PIPELINE=$((CP)) STARPU_LIMIT_CUDA_MEM=$((CM)) STARPU_MINIMUM_CLEAN_BUFFERS=0 STARPU_TARGET_CLEAN_BUFFERS=0 STARPU_NCPU=0 STARPU_NCUDA=$((NGPU)) STARPU_NOPENCL=0 STARPU_BUS_STATS=1 STARPU_BUS_STATS_FILE="${FICHIER_BUS:0}" ./examples/mult/sgemm -xy $((960*N)) -nblocks $((N)) -iter $((NITER)) | tail -n 1 >> ${FICHIER_RAW:0}
+			sed -n '4,'$((NCOMBINAISONS))'p' ${FICHIER_BUS:0} >> ${FICHIER_RAW_DT:0}
+		done
+	fi
 fi
 
 #~ #Tracage data transfers
