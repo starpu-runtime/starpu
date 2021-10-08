53828be2765064e864f894f7e22a52dc714330a5
Maxime <maxime.gonthier@ens-lyon.fr>
2021-10-08 17:29:45 +0200

    autre version de la variante des data a choisir

--- include/schedulers/dynamic_data_aware.h
+++ include/schedulers/dynamic_data_aware.h
@@ -7,6 +7,7 @@
 #define NUMBER_OF_TASK_DONE_BEFORE_LIFTING_THRESHOLD /* Après ce chiffre/NGPU je retire le threshold */
 //~ #define FILL_PLANNED_TASK_LIST_THRESHOLD /* Jusqu'a où je rempli la liste des planned task. 0 = infini. Inutile en réalité car cela prends très peu de temps. */
 #define LIFT_THRESHOLD_MODE /* 0 = default, 1 = fix number,  2 = reach certain number of task out, 3 = reach certain number of task out + slow increase, 4 = percentage + slow increase */
+#define CHOOSE_BEST_DATA_TYPE /* 0 = the best one, 1 = the 10 best one, 2 = the 10 best one and I choose among them */
 
 starpu_pthread_mutex_t global_mutex;   	
 
