fe3d006f8ae75dcf4961342388de8171db62da17
Lucas Nesi <lucas.nesi@inf.ufrgs.br>
2021-10-20 19:16:39 -0300

    fxt: set ptr to NULL to avoid double free
    (cherry picked from commit 2ecc19989fc4b5480f76fce178240239f9e0cf75)

--- src/debug/traces/starpu_fxt.c
+++ src/debug/traces/starpu_fxt.c
@@ -4183,7 +4183,10 @@ void _starpu_fxt_parse_new_file(char *filename_in, struct starpu_fxt_options *op
 	}
 
 	for (i = 0; i < STARPU_NMAXWORKERS; i++)
+	{
 		free(options->worker_archtypes[i].devices);
+		options->worker_archtypes[i].devices = NULL;
+	}
 
 	/* Close the trace file */
 	if (close(fd_in))
