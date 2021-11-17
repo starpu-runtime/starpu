1f9181a4c8c09c7db4de36851fb105d4105b9d78
Maxime <maxime.gonthier@ens-lyon.fr>
2021-11-17 10:33:58 +0100

    Scrits

--- Scripts_maxime/script_testing_2.sh
+++ Scripts_maxime/script_testing_2.sh
@@ -1,42 +1,54 @@
 #!/usr/bin/bash
 start=`date +%s`
 ulimit -S -s 5000000
+#~ export STARPU_PERF_MODEL_DIR=tools/perfmodels/sampling
 
-N=35
+N=0
 
-COUNT_DO_SCHEDULE=1 PRINT_TIME=1 STARPU_SCHED=HFP STARPU_SCHED_READY=1 BELADY=1 ORDER_U=1 STARPU_NTASKS_THRESHOLD=10 STARPU_CUDA_PIPELINE=5 STARPU_LIMIT_CUDA_MEM=500 STARPU_MINIMUM_CLEAN_BUFFERS=0 STARPU_TARGET_CLEAN_BUFFERS=0 STARPU_NCPU=0 STARPU_NCUDA=1 STARPU_NOPENCL=0 STARPU_BUS_STATS=1 ./examples/cholesky/cholesky_implicit -size $((960*35)) -nblocks $((35))
+NGPU=1
 
-end=`date +%s`
+ORDO="dmdar"
+
+CM=500
+
+EVICTION=0
+
+READY=1
+
+TH=10
+
+CP=5
+
+HOST="gemini-1-fgcs"
+
+SEED=1
+
+PRINTF=0
+
+BELADY=0
+
+MULTI=0
+
+STEALING=0
+
+NITER=1
+
+NB_TAILLE_TESTE=8
+
+THRESHOLD_DMDAR=4000
+
+FICHIER=Output_maxime/test.txt
+truncate -s 0 ${FICHIER}
+
+for ((i=1 ; i<=(($NB_TAILLE_TESTE)); i++))
+	do
+	N=$((5*i))
+	DMDAR_THRESHOLD=$((THRESHOLD_DMDAR)) TASK_STEALING=$((STEALING)) STARPU_BUS_STATS=1 MULTIGPU=$((MULTI)) PRINTF=$((PRINTF)) SEED=$((SEED)) STARPU_SCHED=${ORDO} BELADY=$((BELADY)) ORDER_U=0 STARPU_SCHED_READY=$((READY)) STARPU_NTASKS_THRESHOLD=$((TH)) STARPU_CUDA_PIPELINE=$((CP)) STARPU_LIMIT_CUDA_MEM=$((CM)) STARPU_MINIMUM_CLEAN_BUFFERS=0 STARPU_TARGET_CLEAN_BUFFERS=0 STARPU_NCPU=0 STARPU_NCUDA=$((NGPU)) STARPU_NOPENCL=0 DATA_POP_POLICY=1 EVICTION_STRATEGY_DYNAMIC_DATA_AWARE=$((EVICTION)) ./examples/mult/sgemm -3d -xy $((960*N)) -nblocks $((N)) -nblocksz $((4)) -iter 1 | tail -n 1 >> ${FICHIER}
+done
+
+echo "Résultats pour" ${ORDO}
+cat ${FICHIER}
+
+end=`date +%s` 
 runtime=$((end-start))
 echo "Fin du script, l'execution a durée" $((runtime/60))" min "$((runtime%60))" sec."
-
-
-
-#~ # size	ms	GFlops
-#~ 24000	1119	4117.7
-
-#~ #---------------------
-#~ Data transfer stats:
-	#~ NUMA 0 -> CUDA 0	2.1046 GB	28.7782 MB/s	(transfers : 613 - avg 3.5156 MB)
-	#~ CUDA 0 -> NUMA 0	0.0000 GB	0.0000 MB/s	(transfers : 0 - avg -nan MB)
-#~ Total transfers: 2.1046 GB
-#~ #---------------------
-#~ Fin du script, l'execution a durée 1 min 16 sec.
-
-
-#~ PRINT_N=25 PRINT3D=1 PRINTF=1 STARPU_SCHED=dmdar STARPU_NTASKS_THRESHOLD=10 STARPU_CUDA_PIPELINE=5 STARPU_LIMIT_CUDA_MEM=500 STARPU_MINIMUM_CLEAN_BUFFERS=0 STARPU_TARGET_CLEAN_BUFFERS=0 STARPU_NCPU=0 STARPU_NCUDA=1 STARPU_NOPENCL=0 ./examples/mult/sgemm -3d -xy $((960*25)) -nblocks 25 -nblocksz $((4)) -iter 1
-#~ /./home/gonthier/these_gonthier_maxime/Code/permutation_visu_python $((N)) ${ORDO} 1 4
-#~ python3 /home/gonthier/these_gonthier_maxime/Code/visualisation2D.py Output_maxime/Data_coordinates_order_last_SCHEDULER.txt Output_maxime/Data_to_load_SCHEDULER.txt ${N} ${ORDO} ${NGPU} 4
-
-
-#~ 33600	1505659	8.4
-
-#---------------------
-#~ Data transfer stats:
-	#~ NUMA 0 -> CUDA 0	7.4879 GB	5.0788 MB/s	(transfers : 2181 - avg 3.5156 MB)
-	#~ CUDA 0 -> NUMA 0	0.0000 GB	0.0000 MB/s	(transfers : 0 - avg -nan MB)
-#~ Total transfers: 7.4879 GB
-#---------------------
-
-
-#~ 1522
