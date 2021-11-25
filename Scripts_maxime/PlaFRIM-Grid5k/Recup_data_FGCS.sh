e5e35e7b08f61a27f9396457179a43bd05056a5d
Maxime <maxime.gonthier@ens-lyon.fr>
2021-11-25 13:59:50 +0100

    Scripts

--- Scripts_maxime/PlaFRIM-Grid5k/Recup_data_FGCS.sh
+++ Scripts_maxime/PlaFRIM-Grid5k/Recup_data_FGCS.sh
@@ -1,11 +1,12 @@
 #~ Pour récup les data sur Grid5k et les process pour IPDPS
 
-#~ bash Scripts_maxime/PlaFRIM-Grid5k/Recup_data_FGCS.sh 13 Matrice_ligne HFP 1 9
 #~ bash Scripts_maxime/PlaFRIM-Grid5k/Recup_data_FGCS.sh 16 Matrice_ligne HFP 1 9
 #~ bash Scripts_maxime/PlaFRIM-Grid5k/Recup_data_FGCS.sh 10 Matrice_ligne HFP_memory 1 9
 #~ bash Scripts_maxime/PlaFRIM-Grid5k/Recup_data_FGCS.sh 8 Matrice3D HFP 1 9
 #~ bash Scripts_maxime/PlaFRIM-Grid5k/Recup_data_FGCS.sh 8 Cholesky HFP 1 9
 #~ bash Scripts_maxime/PlaFRIM-Grid5k/Recup_data_FGCS.sh 10 Matrice_ligne GET_ITERATION_TIME 1 9
+#~ bash Scripts_maxime/PlaFRIM-Grid5k/Recup_data_FGCS.sh 13 Random_task_order HFP 1 9
+#~ bash Scripts_maxime/PlaFRIM-Grid5k/Recup_data_FGCS.sh 13 Random_tasks HFP 1 9
 
 NB_TAILLE_TESTE=$1
 DOSSIER=$2
