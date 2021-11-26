89fdff6f449b00b0ed0272bd93c11bbaa6b2c6d3
Maxime <maxime.gonthier@ens-lyon.fr>
2021-11-27 00:33:02 +0100

    maj scripts

--- Scripts_maxime/FGCS2021/Simulation.sh
+++ Scripts_maxime/FGCS2021/Simulation.sh
@@ -1,8 +1,8 @@
-#!/usr/bin/bash
+# Lancer depuis starpu/ avec bash Scripts_maxime/FGCS2021/Simulation.sh path_simgrid path_starpu path_r
 PATH_SIMGRID=$1
 PATH_STARPU=$2
 PATH_R=$3
 ./configure --enable-simgrid --disable-mpi --with-simgrid-dir=${PATH_SIMGRID}
-sudo make -j 6
+sudo make -j 10
 sudo make install
 bash Scripts_maxime/HFP.sh ${PATH_STARPU} ${PATH_R} 10 Matrice3D HFP gemini-1-fgcs 1
