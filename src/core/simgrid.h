b402434cb95a200d0710ddae0e6c68d2704b9b3b
Samuel Thibault <samuel.thibault@ens-lyon.org>
2022-01-02 22:17:06 +0100

    Fix compatibility with upcoming simgrid
    (cherry picked from commit 293ed672a5d105e334dfd57a0ffffa38b78eda19)

--- src/core/simgrid.h
+++ src/core/simgrid.h
@@ -1,6 +1,6 @@
 /* StarPU --- Runtime system for heterogeneous multicore architectures.
  *
- * Copyright (C) 2012-2021  Université de Bordeaux, CNRS (LaBRI UMR 5800), Inria
+ * Copyright (C) 2012-2022  Université de Bordeaux, CNRS (LaBRI UMR 5800), Inria
  * Copyright (C) 2013       Thibaut Lambert
  *
  * StarPU is free software; you can redistribute it and/or modify
@@ -54,6 +54,12 @@ struct _starpu_pthread_args
 	void *(*f)(void*);
 	void *arg;
 };
+
+#if (SIMGRID_VERSION >= 32600)
+typedef sg_link_t starpu_sg_link_t;
+#else
+typedef SD_link_t starpu_sg_link_t;
+#endif
 
 #define MAX_TSD 16
 
