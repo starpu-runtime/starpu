6ade425cd204f75fe5d7fed78a9ec2eb96a24e9e
Samuel Thibault <samuel.thibault@ens-lyon.org>
2022-01-21 20:09:20 +0100

    Fix allocating 0 bytes
    The code would otherwise return the same pointer several times. To keep
    things simple, we have no choice but to really allocate something.
    
    (cherry picked from commit 52b8011c9963b6e6999bd262214f4cdcd5d3507d)

--- src/datawizard/malloc.c
+++ src/datawizard/malloc.c
@@ -772,6 +772,8 @@ starpu_malloc_on_node_flags(unsigned dst_node, size_t size, int flags)
 
 	/* Round up allocation to block size */
 	int nblocks = (size + CHUNK_ALLOC_MIN - 1) / CHUNK_ALLOC_MIN;
+	if (!nblocks)
+		nblocks = 1;
 
 	struct _starpu_chunk *chunk;
 	int prevblock, block;
@@ -875,6 +877,8 @@ starpu_free_on_node_flags(unsigned dst_node, uintptr_t addr, size_t size, int fl
 
 	/* Round up allocation to block size */
 	int nblocks = (size + CHUNK_ALLOC_MIN - 1) / CHUNK_ALLOC_MIN;
+	if (!nblocks)
+		nblocks = 1;
 
 	STARPU_PTHREAD_MUTEX_LOCK(&chunk_mutex[dst_node]);
 	for (chunk = _starpu_chunk_list_begin(&chunks[dst_node]);
