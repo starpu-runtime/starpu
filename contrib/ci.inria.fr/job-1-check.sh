804a5c4887206fc0624875a18ad1eb144dcd9dcd
Nathalie Furmento <nathalie.furmento@labri.fr>
2022-01-26 08:54:48 +0100

    enable configure parameter --enable-mpi-check=maybe
    (cherry picked from commit b7de9c7c1478a91a1edf9726af6bb74b7070aa66)

--- contrib/ci.inria.fr/job-1-check.sh
+++ contrib/ci.inria.fr/job-1-check.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # StarPU --- Runtime system for heterogeneous multicore architectures.
 #
-# Copyright (C) 2013-2021  Université de Bordeaux, CNRS (LaBRI UMR 5800), Inria
+# Copyright (C) 2013-2022  Université de Bordeaux, CNRS (LaBRI UMR 5800), Inria
 #
 # StarPU is free software; you can redistribute it and/or modify
 # it under the terms of the GNU Lesser General Public License as published by
@@ -87,7 +87,7 @@ then
     ARGS="--with-mpiexec-args=-oversubscribe"
 fi
 
-CONFIGURE_OPTIONS="--enable-debug --enable-verbose --enable-mpi-check --disable-build-doc $ARGS"
+CONFIGURE_OPTIONS="--enable-debug --enable-verbose --enable-mpi-check=maybe --disable-build-doc $ARGS"
 CONFIGURE_CHECK=""
 day=$(date +%u)
 if test $day -le 5
