03c2e6bb88f219825602166699596058b68174ec
Maxime <maxime.gonthier@ens-lyon.fr>
2022-02-03 15:04:21 +0100

    maj depot

---
+++ Scripts_maxime/IPDPS2021/Very_Quick_Draw.sh
@@ -0,0 +1,12 @@
+# Lancer avec bash Scripts_maxime/Draw_fgcs2021/Draw.sh name_on_grid5k path_starpu path_r
+NAME=$1
+PATH_STARPU=$2
+PATH_R=$3
+
+bash Scripts_maxime/PlaFRIM-Grid5k/Draw_fgcs.sh 2 Matrice_ligne dynamic_data_aware_no_hfp_Draw_fgcs 6 1 ${NAME} ${PATH_STARPU} ${PATH_R}
+bash Scripts_maxime/PlaFRIM-Grid5k/Draw_fgcs.sh 2 Matrice_ligne dynamic_data_aware_no_hfp_Draw_fgcs 5 2 ${NAME} ${PATH_STARPU} ${PATH_R}
+bash Scripts_maxime/PlaFRIM-Grid5k/Draw_fgcs.sh 2 Matrice_ligne dynamic_data_aware_no_hfp_Draw_fgcs 6 4 ${NAME} ${PATH_STARPU} ${PATH_R}
+bash Scripts_maxime/PlaFRIM-Grid5k/Draw_fgcs.sh 2 Random_task_order dynamic_data_aware_no_hfp_Draw_fgcs 5 2 ${NAME} ${PATH_STARPU} ${PATH_R}
+bash Scripts_maxime/PlaFRIM-Grid5k/Draw_fgcs.sh 2 Cholesky dynamic_data_aware_no_hfp 6 4 ${NAME} ${PATH_STARPU} ${PATH_R}
+bash Scripts_maxime/PlaFRIM-Grid5k/Draw_fgcs.sh 2 Sparse dynamic_data_aware_no_hfp 5 4 ${NAME} ${PATH_STARPU} ${PATH_R}
+bash Scripts_maxime/PlaFRIM-Grid5k/Draw_fgcs.sh 2 Sparse_mem_infinite dynamic_data_aware_no_hfp 6 4 ${NAME} ${PATH_STARPU} ${PATH_R}
