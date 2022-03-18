cd3bbd98b120e0e1a03496e767ed8224cd327ad5
Maxime <maxime.gonthier@ens-lyon.fr>
2022-03-18 15:55:18 +0100

    Shorter scripts

---
+++ Scripts_maxime/FGCS2021/Very_Quick_Draw.sh
@@ -0,0 +1,12 @@
+# Lancer avec bash Scripts_maxime/FGCS2021/Quick_Draw.sh name_on_grid5k path_starpu path_r
+NAME=$1
+PATH_STARPU=$2
+PATH_R=$3
+
+bash Scripts_maxime/PlaFRIM-Grid5k/Draw_fgcs.sh 2 Matrice_ligne HFP 1 10 ${NAME} ${PATH_STARPU} ${PATH_R}
+bash Scripts_maxime/PlaFRIM-Grid5k/Draw_fgcs.sh 2 Matrice_ligne HFP_memory 1 10 ${NAME} ${PATH_STARPU} ${PATH_R}
+#~ bash Scripts_maxime/PlaFRIM-Grid5k/Draw_fgcs.sh 6 Matrice3D HFP 1 10 ${NAME} ${PATH_STARPU} ${PATH_R}
+bash Scripts_maxime/PlaFRIM-Grid5k/Draw_fgcs.sh 2 Matrice3DZN HFP 1 9 ${NAME} ${PATH_STARPU} ${PATH_R}
+bash Scripts_maxime/PlaFRIM-Grid5k/Draw_fgcs.sh 2 Cholesky HFP 1 8 ${NAME} ${PATH_STARPU} ${PATH_R}
+bash Scripts_maxime/PlaFRIM-Grid5k/Draw_fgcs.sh 2 Random_task_order HFP 1 8 ${NAME} ${PATH_STARPU} ${PATH_R}
+bash Scripts_maxime/PlaFRIM-Grid5k/Draw_fgcs.sh 2 Random_tasks HFP 1 8 ${NAME} ${PATH_STARPU} ${PATH_R}
---
+++ Scripts_maxime/FGCS2021/Very_Quick_Experiments.sh
@@ -0,0 +1,31 @@
+# A lancer dans le dossier starpu/ avec bash Scripts_maxime/FGCS2021/Very_Quick_Experiments.sh name_grid5k
+NAME=$1
+./configure --prefix=/home/${NAME}/starpu
+make -j 100
+make install
+bash Scripts_maxime/PlaFRIM-Grid5k/FGCS.sh 2 Matrice_ligne HFP 1
+mv Output_maxime/GFlops_raw_out_1.txt Output_maxime/Data/GF_HFP_M2D.txt
+mv Output_maxime/GFlops_raw_out_3.txt Output_maxime/Data/DT_HFP_M2D.txt
+bash Scripts_maxime/PlaFRIM-Grid5k/FGCS.sh 2 Matrice_ligne HFP_memory 1
+mv Output_maxime/GFlops_raw_out_1.txt Output_maxime/Data/GF_HFP_MEMORY_M2D.txt
+#~ bash Scripts_maxime/PlaFRIM-Grid5k/FGCS.sh 6 Matrice3D HFP 1
+#~ mv Output_maxime/GFlops_raw_out_1.txt Output_maxime/Data/GF_HFP_M3D.txt
+#~ mv Output_maxime/GFlops_raw_out_3.txt Output_maxime/Data/DT_HFP_M3D.txt
+bash Scripts_maxime/PlaFRIM-Grid5k/FGCS.sh 2 Matrice3DZN HFP 1
+mv Output_maxime/GFlops_raw_out_1.txt Output_maxime/Data/GF_HFP_M3DZN.txt
+mv Output_maxime/GFlops_raw_out_3.txt Output_maxime/Data/DT_HFP_M3DZN.txt
+bash Scripts_maxime/PlaFRIM-Grid5k/FGCS.sh 2 Cholesky HFP 1
+mv Output_maxime/GFlops_raw_out_1.txt Output_maxime/Data/GF_HFP_CHO.txt
+mv Output_maxime/GFlops_raw_out_3.txt Output_maxime/Data/DT_HFP_CHO.txt
+bash Scripts_maxime/PlaFRIM-Grid5k/FGCS.sh 2 Random_task_order HFP 1
+mv Output_maxime/GFlops_raw_out_1.txt Output_maxime/Data/GF_HFP_M2D_RANDOM_ORDER.txt
+mv Output_maxime/GFlops_raw_out_3.txt Output_maxime/Data/DT_HFP_M2D_RANDOM_ORDER.txt
+bash Scripts_maxime/PlaFRIM-Grid5k/FGCS.sh 2 Random_tasks HFP 1
+mv Output_maxime/GFlops_raw_out_1.txt Output_maxime/Data/GF_HFP_M2D_RANDOM_TASKS.txt
+mv Output_maxime/GFlops_raw_out_3.txt Output_maxime/Data/DT_HFP_M2D_RANDOM_TASKS.txt
+#~ bash Scripts_maxime/PlaFRIM-Grid5k/FGCS.sh 4 Sparse HFP 1
+#~ mv Output_maxime/GFlops_raw_out_1.txt Output_maxime/Data/GF_HFP_SPARSE.txt
+#~ mv Output_maxime/GFlops_raw_out_3.txt Output_maxime/Data/DT_HFP_SPARSE.txt
+#~ bash Scripts_maxime/PlaFRIM-Grid5k/FGCS.sh 4 Sparse HFP_mem_infinie 1
+#~ mv Output_maxime/GFlops_raw_out_1.txt Output_maxime/Data/GF_HFP_SPARSE_INFINIE.txt
+#~ mv Output_maxime/GFlops_raw_out_3.txt Output_maxime/Data/DT_HFP_SPARSE_INFINIE.txt
