8cd658dcaec42e5a4f057caf0fb8ddf42ab3dfce
Nathalie Furmento <nathalie.furmento@labri.fr>
2022-04-08 15:03:37 +0200

    do not share the same loader.o for all the directories

---
+++ starpu-loader.mk
@@ -0,0 +1,38 @@
+# StarPU --- Runtime system for heterogeneous multicore architectures.
+#
+# Copyright (C) 2016-2022  Université de Bordeaux, CNRS (LaBRI UMR 5800), Inria
+#
+# StarPU is free software; you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation; either version 2.1 of the License, or (at
+# your option) any later version.
+#
+# StarPU is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+#
+# See the GNU Lesser General Public License in COPYING.LGPL for more details.
+#
+
+noinst_PROGRAMS		=
+
+if STARPU_HAVE_WINDOWS
+LOADER_BIN		=	$(LAUNCHER)
+else
+if !STARPU_SIMGRID
+LOADER			=	loader
+loader_CPPFLAGS 	= 	$(AM_CPPFLAGS) -I$(top_builddir)/src/
+LOADER_BIN		=	$(LAUNCHER) $(LOADER)
+noinst_PROGRAMS		+=	loader
+else
+LOADER_BIN		=	$(LAUNCHER)
+endif
+endif
+
+if STARPU_HAVE_AM111
+TESTS_ENVIRONMENT	=	$(LAUNCHER_ENV) top_builddir="$(abs_top_builddir)" top_srcdir="$(abs_top_srcdir)"
+LOG_COMPILER	 	=	$(LOADER_BIN)
+else
+TESTS_ENVIRONMENT 	=	$(LAUNCHER_ENV) top_builddir="$(abs_top_builddir)" top_srcdir="$(abs_top_srcdir)" $(LOADER_BIN)
+endif
+
