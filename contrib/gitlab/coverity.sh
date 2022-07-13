df3e4ddc15a0981052472a1d890c177bb64ade4e
Nathalie Furmento <nathalie.furmento@labri.fr>
2022-07-13 19:55:17 +0200

    CI: fix branch name setting

--- contrib/gitlab/coverity.sh
+++ contrib/gitlab/coverity.sh
@@ -15,4 +15,15 @@
 # See the GNU Lesser General Public License in COPYING.LGPL for more details.
 #
 
-./contrib/ci.inria.fr/job-1-check.sh -coverity $1
+BRANCH="unknown"
+if test -n "$CI_COMMIT_BRANCH"
+then
+    BRANCH=$CI_COMMIT_BRANCH
+fi
+if test -n "$CI_MERGE_REQUEST_SOURCE_BRANCH_NAME"
+then
+    BRANCH=$CI_MERGE_REQUEST_SOURCE_BRANCH_NAME
+fi
+
+./contrib/gitlab/coverity.sh -coverity $BRANCH
+
