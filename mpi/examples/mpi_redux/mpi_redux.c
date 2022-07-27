55c1b90e9effa552797d82729a47b7249e8e737a
Samuel Thibault <samuel.thibault@ens-lyon.org>
2022-07-28 00:12:23 +0200

    Avoid printing an uninitialized value

--- mpi/examples/mpi_redux/mpi_redux.c
+++ mpi/examples/mpi_redux/mpi_redux.c
@@ -1,6 +1,6 @@
 /* StarPU --- Runtime system for heterogeneous multicore architectures.
  *
- * Copyright (C) 2016-2021  Université de Bordeaux, CNRS (LaBRI UMR 5800), Inria
+ * Copyright (C) 2016-2022  Université de Bordeaux, CNRS (LaBRI UMR 5800), Inria
  *
  * StarPU is free software; you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
@@ -66,7 +66,7 @@ static void cl_cpu_task_init(void *handles[], void*arg)
 	(void) arg;
 	double *a = (double *)STARPU_VARIABLE_GET_PTR(handles[0]);
 	sleep(1);
-	printf("init_cl (rank:%d,worker:%d) %d (was %f)\n", starpu_mpi_world_rank(), starpu_worker_get_id(), starpu_mpi_world_rank(), *a);
+	printf("init_cl (rank:%d,worker:%d) %d (was %f)\n", starpu_mpi_world_rank(), starpu_worker_get_id(), starpu_mpi_world_rank(), STARPU_RUNNING_ON_VALGRIND ? 0. : *a);
 	*a = starpu_mpi_world_rank();
 }
 
