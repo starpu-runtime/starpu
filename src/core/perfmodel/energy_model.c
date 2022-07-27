a2d56bf28c6ac658901a30824719bd07ba3e7d81
Samuel Thibault <samuel.thibault@ens-lyon.org>
2022-07-27 21:38:47 +0200

    papi: Fix off by one

--- src/core/perfmodel/energy_model.c
+++ src/core/perfmodel/energy_model.c
@@ -278,7 +278,7 @@ static int add_event(int eventSet, int socket)
 			{
 				/* Ok, too bad */
 				_STARPU_DISP("Note: DRAM energy measurement not available\n");
-				n_recorded = i+1;
+				n_recorded = i;
 				return PAPI_OK;
 			}
 			_STARPU_DISP("cannot add event '%s': %d\n", buf, retval);
