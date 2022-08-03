35f24126dbfbb1745b9d4d0249a1b11afd8fe26e
Samuel Thibault <samuel.thibault@ens-lyon.org>
2022-08-03 14:23:35 +0200

    Fix access after free

--- examples/spmd/vector_scal_spmd.c
+++ examples/spmd/vector_scal_spmd.c
@@ -1,6 +1,6 @@
 /* StarPU --- Runtime system for heterogeneous multicore architectures.
  *
- * Copyright (C) 2010-2021  Université de Bordeaux, CNRS (LaBRI UMR 5800), Inria
+ * Copyright (C) 2010-2022  Université de Bordeaux, CNRS (LaBRI UMR 5800), Inria
  *
  * StarPU is free software; you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
@@ -158,13 +158,13 @@ int main(void)
 
 	starpu_data_unregister(vector_handle);
 
+	FPRINTF(stderr, "AFTER: First element is %f\n", vector[0]);
+	FPRINTF(stderr, "AFTER: Last element is %f\n", vector[NX-1]);
+
 	starpu_free_noflag(vector, NX*sizeof(float));
 
 	/* terminate StarPU, no task can be submitted after */
 	starpu_shutdown();
-
-	FPRINTF(stderr, "AFTER: First element is %f\n", vector[0]);
-	FPRINTF(stderr, "AFTER: Last element is %f\n", vector[NX-1]);
 
 	return ret;
 }
