dc68f97a7ab87af8dbde94c605bd668a11f498e3
Nathalie Furmento <nathalie.furmento@labri.fr>
2022-08-16 15:50:07 +0200

    tests/microbenchs: skip microbenchmarks when variable STARPU_MICROBENCHS_DISABLED is defined

--- tests/microbenchs/async_tasks_data_overhead.sh
+++ tests/microbenchs/async_tasks_data_overhead.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # StarPU --- Runtime system for heterogeneous multicore architectures.
 #
-# Copyright (C) 2020-2021  Université de Bordeaux, CNRS (LaBRI UMR 5800), Inria
+# Copyright (C) 2020-2022  Université de Bordeaux, CNRS (LaBRI UMR 5800), Inria
 #
 # StarPU is free software; you can redistribute it and/or modify
 # it under the terms of the GNU Lesser General Public License as published by
@@ -14,6 +14,9 @@
 #
 # See the GNU Lesser General Public License in COPYING.LGPL for more details.
 #
+
+if test -n "$STARPU_MICROBENCHS_DISABLED" ; then exit 77 ; fi
+
 ROOT=${0%.sh}
 ROOT=$(echo $ROOT | sed 's/tasks_data_overhead/tasks_overhead/')
 exec $STARPU_LAUNCH $ROOT -b 1 "$@"
--- tests/microbenchs/tasks_size_overhead_scheds.sh
+++ tests/microbenchs/tasks_size_overhead_scheds.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # StarPU --- Runtime system for heterogeneous multicore architectures.
 #
-# Copyright (C) 2016-2021  Université de Bordeaux, CNRS (LaBRI UMR 5800), Inria
+# Copyright (C) 2016-2022  Université de Bordeaux, CNRS (LaBRI UMR 5800), Inria
 #
 # StarPU is free software; you can redistribute it and/or modify
 # it under the terms of the GNU Lesser General Public License as published by
@@ -14,6 +14,9 @@
 #
 # See the GNU Lesser General Public License in COPYING.LGPL for more details.
 #
+
+if test -n "$STARPU_MICROBENCHS_DISABLED" ; then exit 77 ; fi
+
 . $(dirname $0)/microbench.sh
 
 XFAIL="heteroprio"
