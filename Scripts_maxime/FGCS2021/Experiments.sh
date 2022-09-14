dd58c34b5c11a8b5d1073bfc0db93098413eb467
Maxime <maxime.gonthier@ens-lyon.fr>
2022-09-14 13:03:45 +0200

    for scripts sparse

--- Scripts_maxime/FGCS2021/Experiments.sh
+++ Scripts_maxime/FGCS2021/Experiments.sh
@@ -25,27 +25,27 @@ make install
 #~ mv Output_maxime/GFlops_raw_out_3.txt Output_maxime/Data/DT_HFP_M2D_RANDOM_TASKS.txt
 
 # Pour la révision
-bash Scripts_maxime/PlaFRIM-Grid5k/FGCS.sh 18 Sparse HFP 1
+bash Scripts_maxime/PlaFRIM-Grid5k/FGCS.sh 18 Sparse HFP 1 # Sparse 10%
 mv Output_maxime/GFlops_raw_out_1.txt Output_maxime/Data/GF_HFP_SPARSE.txt
 mv Output_maxime/GFlops_raw_out_3.txt Output_maxime/Data/DT_HFP_SPARSE.txt
 
-bash Scripts_maxime/PlaFRIM-Grid5k/FGCS.sh 10 Sparse HFP_N15 1
+bash Scripts_maxime/PlaFRIM-Grid5k/FGCS.sh 10 Sparse HFP_N15 1 # Sparse de 10 à 100%
 mv Output_maxime/GFlops_raw_out_1.txt Output_maxime/Data/GF_HFP_N15_SPARSE.txt
 mv Output_maxime/GFlops_raw_out_3.txt Output_maxime/Data/DT_HFP_N15_SPARSE.txt
 
-bash Scripts_maxime/PlaFRIM-Grid5k/FGCS.sh 10 Sparse HFP_N40 1
+bash Scripts_maxime/PlaFRIM-Grid5k/FGCS.sh 10 Sparse HFP_N40 1 # Sparse de 10 à 100%
 mv Output_maxime/GFlops_raw_out_1.txt Output_maxime/Data/GF_HFP_N40_SPARSE.txt
 mv Output_maxime/GFlops_raw_out_3.txt Output_maxime/Data/DT_HFP_N40_SPARSE.txt
 
-bash Scripts_maxime/PlaFRIM-Grid5k/FGCS.sh 10 Sparse3DZN HFP 1
+bash Scripts_maxime/PlaFRIM-Grid5k/FGCS.sh 10 Sparse3DZN HFP 1 # Sparse 10%
 mv Output_maxime/GFlops_raw_out_1.txt Output_maxime/Data/GF_HFP_SPARSE_3DZN.txt
 mv Output_maxime/GFlops_raw_out_3.txt Output_maxime/Data/DT_HFP_SPARSE_3DZN.txt
 
-bash Scripts_maxime/PlaFRIM-Grid5k/FGCS.sh 10 Sparse3DZN HFP_N6 1
+bash Scripts_maxime/PlaFRIM-Grid5k/FGCS.sh 10 Sparse3DZN HFP_N6 1 # Sparse de 10 à 100%
 mv Output_maxime/GFlops_raw_out_1.txt Output_maxime/Data/GF_HFP_N6_SPARSE_3DZN.txt
 mv Output_maxime/GFlops_raw_out_3.txt Output_maxime/Data/DT_HFP_N6_SPARSE_3DZN.txt
 
-bash Scripts_maxime/PlaFRIM-Grid5k/FGCS.sh 10 Sparse3DZN HFP_N14 1
+bash Scripts_maxime/PlaFRIM-Grid5k/FGCS.sh 10 Sparse3DZN HFP_N14 1 # Sparse de 10 à 100%
 mv Output_maxime/GFlops_raw_out_1.txt Output_maxime/Data/GF_HFP_N14_SPARSE_3DZN.txt
 mv Output_maxime/GFlops_raw_out_3.txt Output_maxime/Data/DT_HFP_N14_SPARSE_3DZN.txt
 
--- Scripts_maxime/PlaFRIM-Grid5k/FGCS.sh
+++ Scripts_maxime/PlaFRIM-Grid5k/FGCS.sh
@@ -707,6 +707,7 @@ then
 			    N=$((START_X+i*ECHELLE_X))
 				SPARSE_MATRIX=$((SPARSE)) COUNT_DO_SCHEDULE=0 INVALIDATE_C_TILE=0 STARPU_SCHED=modular-eager-prefetching STARPU_NTASKS_THRESHOLD=$((TH)) STARPU_CUDA_PIPELINE=$((CP)) STARPU_LIMIT_CUDA_MEM=$((CM)) STARPU_MINIMUM_CLEAN_BUFFERS=0 STARPU_TARGET_CLEAN_BUFFERS=0 STARPU_NCPU=0 STARPU_NCUDA=$((NGPU)) STARPU_NOPENCL=0 STARPU_BUS_STATS=1 STARPU_BUS_STATS_FILE="${FICHIER_BUS:0}" ./examples/mult/sgemm -3d -xy $((960*N)) -nblocks $((N)) -z $((960*ZN)) -nblocksz $((ZN)) -iter $((NITER)) | tail -n 1 >> ${FICHIER_RAW:0}
 			    sed -n '4,'$((NCOMBINAISONS))'p' ${FICHIER_BUS:0} >> ${FICHIER_RAW_DT:0}
+			done
 		    echo "############## Dmdar ##############"
 		    for ((i=1 ; i<=(($NB_TAILLE_TESTE)); i++))
 			    do 
@@ -787,6 +788,7 @@ then
 			    SPARSE=$((START_X+i*ECHELLE_X))
 				SPARSE_MATRIX=$((SPARSE)) COUNT_DO_SCHEDULE=0 INVALIDATE_C_TILE=0 STARPU_SCHED=modular-eager-prefetching STARPU_NTASKS_THRESHOLD=$((TH)) STARPU_CUDA_PIPELINE=$((CP)) STARPU_LIMIT_CUDA_MEM=$((CM)) STARPU_MINIMUM_CLEAN_BUFFERS=0 STARPU_TARGET_CLEAN_BUFFERS=0 STARPU_NCPU=0 STARPU_NCUDA=$((NGPU)) STARPU_NOPENCL=0 STARPU_BUS_STATS=1 STARPU_BUS_STATS_FILE="${FICHIER_BUS:0}" ./examples/mult/sgemm -3d -xy $((960*N)) -nblocks $((N)) -z $((960*ZN)) -nblocksz $((ZN)) -iter $((NITER)) | tail -n 1 >> ${FICHIER_RAW:0}
 			    sed -n '4,'$((NCOMBINAISONS))'p' ${FICHIER_BUS:0} >> ${FICHIER_RAW_DT:0}
+			done
 		    echo "############## Dmdar ##############"
 		    for ((i=1 ; i<=(($NB_TAILLE_TESTE)); i++))
 			    do 
@@ -859,6 +861,7 @@ then
 			    SPARSE=$((START_X+i*ECHELLE_X))
 				SPARSE_MATRIX=$((SPARSE)) COUNT_DO_SCHEDULE=0 INVALIDATE_C_TILE=0 STARPU_SCHED=modular-eager-prefetching STARPU_NTASKS_THRESHOLD=$((TH)) STARPU_CUDA_PIPELINE=$((CP)) STARPU_LIMIT_CUDA_MEM=$((CM)) STARPU_MINIMUM_CLEAN_BUFFERS=0 STARPU_TARGET_CLEAN_BUFFERS=0 STARPU_NCPU=0 STARPU_NCUDA=$((NGPU)) STARPU_NOPENCL=0 STARPU_BUS_STATS=1 STARPU_BUS_STATS_FILE="${FICHIER_BUS:0}" ./examples/mult/sgemm -3d -xy $((960*N)) -nblocks $((N)) -z $((960*ZN)) -nblocksz $((ZN)) -iter $((NITER)) | tail -n 1 >> ${FICHIER_RAW:0}
 			    sed -n '4,'$((NCOMBINAISONS))'p' ${FICHIER_BUS:0} >> ${FICHIER_RAW_DT:0}
+			done
 		    echo "############## Dmdar ##############"
 		    for ((i=1 ; i<=(($NB_TAILLE_TESTE)); i++))
 			    do 
