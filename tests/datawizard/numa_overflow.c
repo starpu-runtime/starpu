a48dbf4c01815fe821067f14dc08fbccacabc221
Samuel Thibault <samuel.thibault@ens-lyon.org>
2022-09-22 11:13:13 +0200

    reduce execution time in asan

--- tests/datawizard/numa_overflow.c
+++ tests/datawizard/numa_overflow.c
@@ -1,6 +1,6 @@
 /* StarPU --- Runtime system for heterogeneous multicore architectures.
  *
- * Copyright (C) 2020-2021  Université de Bordeaux, CNRS (LaBRI UMR 5800), Inria
+ * Copyright (C) 2020-2022  Université de Bordeaux, CNRS (LaBRI UMR 5800), Inria
  *
  * StarPU is free software; you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
@@ -29,8 +29,13 @@ int main(void)
 }
 #else
 
+#ifdef STARPU_SANITIZE_ADDRESS
+#define ITER 2
+#define N 2
+#else
 #define ITER 10
 #define N 10
+#endif
 #define SIZE (10*1024*1024)
 
 /*
