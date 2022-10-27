a353b6193f17ae6347de25ae8942bf8bb09c7e27
Loris Lucido <loris.lucido@atos.net>
2022-10-27 16:09:54 +0200

    remove auto-generated file

--- src/common/config-src-build.h
+++
@@ -1,3 +0,0 @@
-/* src/common/config-src-build.h.  Generated from config-src-build.h.in by configure.  */
-#define STARPU_SRC_DIR "/home_nfs/blucidol/src/hpl/starpu/starpu-hiprocm/."
-#define STARPU_BUILD_DIR "/home_nfs/blucidol/src/hpl/starpu/starpu-hiprocm"
