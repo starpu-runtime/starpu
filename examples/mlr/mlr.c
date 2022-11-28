40d287241a2d4d3acaa29c6f98d940758f12515e
Samuel Thibault <samuel.thibault@ens-lyon.org>
2022-11-28 20:26:50 +0100

    Fix tests which require CPUs
    (cherry picked from commit 4b13a0999629eb323439a30d6caba406ea027b99)

--- examples/mlr/mlr.c
+++ examples/mlr/mlr.c
@@ -1,6 +1,6 @@
 /* StarPU --- Runtime system for heterogeneous multicore architectures.
  *
- * Copyright (C) 2016-2021  Université de Bordeaux, CNRS (LaBRI UMR 5800), Inria
+ * Copyright (C) 2016-2021, 2024  Université de Bordeaux, CNRS (LaBRI UMR 5800), Inria
  *
  * StarPU is free software; you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
@@ -160,7 +160,16 @@ int main(void)
 	unsigned i;
 	int ret;
 
-	ret = starpu_init(NULL);
+	struct starpu_conf conf;
+	starpu_conf_init(&conf);
+	conf.precedence_over_environment_variables = 1;
+	conf.ncpus = -1;
+	conf.ncuda = 0;
+	conf.nopencl = 0;
+	conf.nmic = 0;
+	conf.nmpi_ms = 0;
+
+	ret = starpu_init(&conf);
 	if (ret == -ENODEV)
 		return 77;
 
