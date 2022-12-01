96e803e1475f600dada6c3beb6938bb4c5c5abba
Nathalie Furmento <nathalie.furmento@labri.fr>
2022-12-01 13:35:51 +0100

    mpi/src/starpu_mpi_task_insert.c: fix code

--- mpi/src/starpu_mpi_task_insert.c
+++ mpi/src/starpu_mpi_task_insert.c
@@ -608,6 +608,18 @@ int _starpu_mpi_task_build_v(MPI_Comm comm, struct starpu_codelet *codelet, stru
 	if (ret < 0)
 		return ret;
 
+	if (xrank_p)
+		*xrank_p = xrank;
+	if (nb_data_p)
+		*nb_data_p = nb_data;
+	if (prio_p)
+		*prio_p = prio;
+
+	if (descrs_p)
+		*descrs_p = descrs;
+	else
+		free(descrs);
+
 	if (do_execute == 1)
 	{
 		va_list varg_list_copy;
@@ -649,19 +661,6 @@ int _starpu_mpi_task_build_v(MPI_Comm comm, struct starpu_codelet *codelet, stru
 	{
 		_starpu_mpi_exchange_data_before_execution(descrs[i].handle, descrs[i].mode, me, xrank, do_execute, prio, comm);
 	}
-
-	if (xrank_p)
-		*xrank_p = xrank;
-	if (nb_data_p)
-		*nb_data_p = nb_data;
-	if (prio_p)
-		*prio_p = prio;
-
-	if (descrs_p)
-		*descrs_p = descrs;
-	else
-		free(descrs);
-
 
 	_STARPU_TRACE_TASK_MPI_PRE_END();
 
