1f9e0651a128a7685710402a3a9ee7555b19fb4b
Nathalie Furmento <nathalie.furmento@labri.fr>
2022-12-12 14:53:29 +0100

    tools/dev/checker/starpu_check_macros.sh: fix python code

--- tools/dev/checker/starpu_check_macros.sh
+++ tools/dev/checker/starpu_check_macros.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # StarPU --- Runtime system for heterogeneous multicore architectures.
 #
-# Copyright (C) 2016-2021  Université de Bordeaux, CNRS (LaBRI UMR 5800), Inria
+# Copyright (C) 2016-2022  Université de Bordeaux, CNRS (LaBRI UMR 5800), Inria
 #
 # StarPU is free software; you can redistribute it and/or modify
 # it under the terms of the GNU Lesser General Public License as published by
@@ -19,7 +19,7 @@ dirname=$(dirname $0)
 INC_DIRS=$(find $dirname/../../../ -name include -type d)
 STARPU_H_FILES=$(find $INC_DIRS -name '*.h')
 STARPU_CONFIG=$dirname/../../../include/starpu_config.h.in
-STARPU_CONFIG_DISPLAY=$(python3 -c "import os.path; print os.path.relpath('$STARPU_CONFIG', '.')")
+STARPU_CONFIG_DISPLAY=$(python3 -c "import os.path; print(os.path.relpath('$STARPU_CONFIG', '.'))")
 macros1=$(grep 'ifdef' $STARPU_H_FILES|grep STARPU|awk '{print $NF}')
 macros2=$(grep defined $STARPU_H_FILES | tr ' (' '\012' | grep STARPU | sed 's/defined//' | tr -d '()!,')
 macros=$(echo $macros1 $macros2 | tr ' ' '\012' | sort | uniq)
