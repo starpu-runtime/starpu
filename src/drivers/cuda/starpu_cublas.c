f4c7e7d1b60dca22626b682b869c22551290a84b
Samuel Thibault <samuel.thibault@ens-lyon.org>
2023-01-19 11:26:02 +0100

    Include cublas_v2.h only
    CUDA 12 does not let include both cublas.h and cublas_v2.h
    
    Applications that include starpu_cublas_v2.h will include cublas_v2.h as
    well, so starpu_cublas_v2.h has to use cublas_v2.h to get
    cublasHandle_t. starpu_cublas.c then has to include cublas_v2.h too to
    avoid including both.
    
    (cherry picked from commit 575b5e03bd6a922130dd735db80f37c5576d2ae0)
    (cherry picked from commit ae26f8d563323e92b31eb121fe2b293be8609b04)

--- src/drivers/cuda/starpu_cublas.c
+++ src/drivers/cuda/starpu_cublas.c
@@ -1,6 +1,6 @@
 /* StarPU --- Runtime system for heterogeneous multicore architectures.
  *
- * Copyright (C) 2009-2021  Université de Bordeaux, CNRS (LaBRI UMR 5800), Inria
+ * Copyright (C) 2009-2023  Université de Bordeaux, CNRS (LaBRI UMR 5800), Inria
  *
  * StarPU is free software; you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
@@ -19,7 +19,7 @@
 #include <common/config.h>
 
 #ifdef STARPU_USE_CUDA
-#include <cublas.h>
+#include <cublas_v2.h>
 #include <starpu_cublas_v2.h>
 
 static cublasHandle_t cublas_handles[STARPU_NMAXWORKERS];
