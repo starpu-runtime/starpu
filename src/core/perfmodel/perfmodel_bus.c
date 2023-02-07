dd2ac429e24cdc3f0941bdcfc75524288f60067f
Samuel Thibault <samuel.thibault@ens-lyon.org>
2023-02-07 16:39:03 +0100

    nvlinks are supported already

--- src/core/perfmodel/perfmodel_bus.c
+++ src/core/perfmodel/perfmodel_bus.c
@@ -2898,9 +2898,6 @@ static void write_bus_platform_file_content(int version)
 #endif
 
 #if HAVE_DECL_HWLOC_CUDA_GET_DEVICE_OSDEV_BY_INDEX && defined(STARPU_USE_CUDA) && defined(STARPU_HAVE_CUDA_MEMCPY_PEER)
-#ifdef STARPU_DEVEL
-#warning TODO: use libnvml to get NVLink links, otherwise numbers will be bogusly propagated through PCI topology
-#endif
 	/* If we have enough hwloc information, write PCI bandwidths and routes */
 	if (!starpu_getenv_number_default("STARPU_PCI_FLAT", 0) && ncuda > 0)
 	{
