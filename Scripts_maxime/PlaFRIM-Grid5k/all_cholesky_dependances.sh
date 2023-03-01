f889285f9e3e60ad34c27146e4790fd15111769e
MaximeGonthier <maxjp.gr2@gmail.com>
2023-03-02 00:38:50 +0100

    correction valeur de mémoire

--- Scripts_maxime/PlaFRIM-Grid5k/all_cholesky_dependances.sh
+++ Scripts_maxime/PlaFRIM-Grid5k/all_cholesky_dependances.sh
@@ -6,15 +6,15 @@ bash Scripts_maxime/PlaFRIM-Grid5k/cholesky_dependances.sh 2 1920 12 2000 opti
 bash Scripts_maxime/PlaFRIM-Grid5k/cholesky_dependances.sh 4 1920 12 2000 opti
 bash Scripts_maxime/PlaFRIM-Grid5k/cholesky_dependances.sh 8 1920 12 2000 opti
 
-bash Scripts_maxime/PlaFRIM-Grid5k/cholesky_dependances.sh 1 2880 12 2000 opti
-bash Scripts_maxime/PlaFRIM-Grid5k/cholesky_dependances.sh 2 2880 12 2000 opti
-bash Scripts_maxime/PlaFRIM-Grid5k/cholesky_dependances.sh 4 2880 12 2000 opti
-bash Scripts_maxime/PlaFRIM-Grid5k/cholesky_dependances.sh 8 2880 12 2000 opti
+bash Scripts_maxime/PlaFRIM-Grid5k/cholesky_dependances.sh 1 2880 12 4500 opti
+bash Scripts_maxime/PlaFRIM-Grid5k/cholesky_dependances.sh 2 2880 12 4500 opti
+bash Scripts_maxime/PlaFRIM-Grid5k/cholesky_dependances.sh 4 2880 12 4500 opti
+bash Scripts_maxime/PlaFRIM-Grid5k/cholesky_dependances.sh 8 2880 12 4500 opti
 
-bash Scripts_maxime/PlaFRIM-Grid5k/cholesky_dependances.sh 1 3840 12 2000 opti
-bash Scripts_maxime/PlaFRIM-Grid5k/cholesky_dependances.sh 2 3840 12 2000 opti
-bash Scripts_maxime/PlaFRIM-Grid5k/cholesky_dependances.sh 4 3840 12 2000 opti
-bash Scripts_maxime/PlaFRIM-Grid5k/cholesky_dependances.sh 8 3840 12 2000 opti
+bash Scripts_maxime/PlaFRIM-Grid5k/cholesky_dependances.sh 1 3840 12 8000 opti
+bash Scripts_maxime/PlaFRIM-Grid5k/cholesky_dependances.sh 2 3840 12 8000 opti
+bash Scripts_maxime/PlaFRIM-Grid5k/cholesky_dependances.sh 4 3840 12 8000 opti
+bash Scripts_maxime/PlaFRIM-Grid5k/cholesky_dependances.sh 8 3840 12 8000 opti
 
 # Opti sans limit de mem
 bash Scripts_maxime/PlaFRIM-Grid5k/cholesky_dependances.sh 1 1920 12 0 opti
