10d7fbe1242f5d8d85fd5f64e9d914aeae429ea9
Maxime Gonthier <mgonthier@gemini-1.lyon.grid5000.fr>
2023-03-03 14:18:10 +0100

    minor

--- Scripts_maxime/PlaFRIM-Grid5k/all_cholesky_dependances.sh
+++ Scripts_maxime/PlaFRIM-Grid5k/all_cholesky_dependances.sh
@@ -1,12 +1,12 @@
 # bash Scripts_maxime/PlaFRIM-Grid5k/all_cholesky_dependances.sh
 
 # Opti
-bash Scripts_maxime/PlaFRIM-Grid5k/cholesky_dependances.sh 1 1920 12 2000 opti
-bash Scripts_maxime/PlaFRIM-Grid5k/cholesky_dependances.sh 2 1920 12 2000 opti
-bash Scripts_maxime/PlaFRIM-Grid5k/cholesky_dependances.sh 4 1920 12 2000 opti
-bash Scripts_maxime/PlaFRIM-Grid5k/cholesky_dependances.sh 8 1920 12 2000 opti
+#bash Scripts_maxime/PlaFRIM-Grid5k/cholesky_dependances.sh 1 1920 12 2000 opti
+#bash Scripts_maxime/PlaFRIM-Grid5k/cholesky_dependances.sh 2 1920 12 2000 opti
+#bash Scripts_maxime/PlaFRIM-Grid5k/cholesky_dependances.sh 4 1920 12 2000 opti
+#bash Scripts_maxime/PlaFRIM-Grid5k/cholesky_dependances.sh 8 1920 12 2000 opti
 
-bash Scripts_maxime/PlaFRIM-Grid5k/cholesky_dependances.sh 1 2880 12 4500 opti
+#bash Scripts_maxime/PlaFRIM-Grid5k/cholesky_dependances.sh 1 2880 12 4500 opti
 bash Scripts_maxime/PlaFRIM-Grid5k/cholesky_dependances.sh 2 2880 12 4500 opti
 bash Scripts_maxime/PlaFRIM-Grid5k/cholesky_dependances.sh 4 2880 12 4500 opti
 bash Scripts_maxime/PlaFRIM-Grid5k/cholesky_dependances.sh 8 2880 12 4500 opti
@@ -17,12 +17,12 @@ bash Scripts_maxime/PlaFRIM-Grid5k/cholesky_dependances.sh 4 3840 12 8000 opti
 bash Scripts_maxime/PlaFRIM-Grid5k/cholesky_dependances.sh 8 3840 12 8000 opti
 
 # Opti sans limit de mem
-bash Scripts_maxime/PlaFRIM-Grid5k/cholesky_dependances.sh 1 1920 12 0 opti
+#bash Scripts_maxime/PlaFRIM-Grid5k/cholesky_dependances.sh 1 1920 12 0 opti
 bash Scripts_maxime/PlaFRIM-Grid5k/cholesky_dependances.sh 2 1920 12 0 opti
-bash Scripts_maxime/PlaFRIM-Grid5k/cholesky_dependances.sh 4 1920 12 0 opti
+#bash Scripts_maxime/PlaFRIM-Grid5k/cholesky_dependances.sh 4 1920 12 0 opti
 bash Scripts_maxime/PlaFRIM-Grid5k/cholesky_dependances.sh 8 1920 12 0 opti
 
-bash Scripts_maxime/PlaFRIM-Grid5k/cholesky_dependances.sh 1 2880 12 0 opti
+#bash Scripts_maxime/PlaFRIM-Grid5k/cholesky_dependances.sh 1 2880 12 0 opti
 bash Scripts_maxime/PlaFRIM-Grid5k/cholesky_dependances.sh 2 2880 12 0 opti
 bash Scripts_maxime/PlaFRIM-Grid5k/cholesky_dependances.sh 4 2880 12 0 opti
 bash Scripts_maxime/PlaFRIM-Grid5k/cholesky_dependances.sh 8 2880 12 0 opti
