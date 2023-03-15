c07deb8a44aee51931202bf0fff8bfac28413d44
Nathalie Furmento <nathalie.furmento@labri.fr>
2023-03-15 09:06:45 +0100

    starpupy: only keep 1 version of execute.sh script and make it possible to pass parameters after the program filename
    (cherry picked from commit 5e97e7c8b34f3d07adf8a7d3e2491023493ce3b2)

--- starpupy/examples/starpu_py_partition.sh
+++ starpupy/examples/starpu_py_partition.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # StarPU --- Runtime system for heterogeneous multicore architectures.
 #
-# Copyright (C) 2020-2021  Université de Bordeaux, CNRS (LaBRI UMR 5800), Inria
+# Copyright (C) 2020-2023  Université de Bordeaux, CNRS (LaBRI UMR 5800), Inria
 #
 # StarPU is free software; you can redistribute it and/or modify
 # it under the terms of the GNU Lesser General Public License as published by
@@ -15,5 +15,5 @@
 # See the GNU Lesser General Public License in COPYING.LGPL for more details.
 #
 
-exec $(dirname $0)/execute.sh starpu_py_partition.py $*
+$(dirname $0)/../execute.sh examples/starpu_py_partition.py $*
 
