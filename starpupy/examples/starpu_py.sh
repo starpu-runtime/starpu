d38369fbe6aa2a46379a918d62d2c8cc8105a314
Nathalie Furmento <nathalie.furmento@labri.fr>
2023-04-11 08:53:08 +0200

    fix copyright

--- starpupy/examples/starpu_py.sh
+++ starpupy/examples/starpu_py.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # StarPU --- Runtime system for heterogeneous multicore architectures.
 #
-# Copyright (C) 2020-2021, 2023  Université de Bordeaux, CNRS (LaBRI UMR 5800), Inria
+# Copyright (C) 2020-2023  Université de Bordeaux, CNRS (LaBRI UMR 5800), Inria
 #
 # StarPU is free software; you can redistribute it and/or modify
 # it under the terms of the GNU Lesser General Public License as published by
