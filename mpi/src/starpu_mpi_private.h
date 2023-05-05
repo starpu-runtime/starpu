bf9bafcb84731ff3cbac46a71d1663976185968f
Nathalie Furmento <nathalie.furmento@labri.fr>
2023-05-05 14:26:15 +0000

    MPI: fix function starpu_wait_for_all() to properly wait for detached requests

--- mpi/src/starpu_mpi_private.h
+++ mpi/src/starpu_mpi_private.h
@@ -328,7 +328,6 @@ void _starpu_mpi_submit_coop_sends(struct _starpu_mpi_coop_sends *coop_sends, in
  */
 void _starpu_mpi_redux_fill_post_sync_jobid(const void * const redux_data_args, long * const post_sync_jobid);
 
-void _starpu_mpi_submit_ready_request_inc(struct _starpu_mpi_req *req);
 void _starpu_mpi_request_init(struct _starpu_mpi_req **req);
 struct _starpu_mpi_req * _starpu_mpi_request_fill(starpu_data_handle_t data_handle,
 						  int srcdst, starpu_mpi_tag_t data_tag, MPI_Comm comm,
