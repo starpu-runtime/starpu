17bf8b4ecccd608155284c290d9518cae4c54f6c
Nathalie Furmento <nathalie.furmento@labri.fr>
2023-05-30 21:21:24 +0200

    tools/dev/valgrind: update file to be up-to-date with master

--- tools/dev/valgrind/valgrind.sh
+++ tools/dev/valgrind/valgrind.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # StarPU --- Runtime system for heterogeneous multicore architectures.
 #
-# Copyright (C) 2016-2020  Université de Bordeaux, CNRS (LaBRI UMR 5800), Inria
+# Copyright (C) 2016-2023  Université de Bordeaux, CNRS (LaBRI UMR 5800), Inria
 #
 # StarPU is free software; you can redistribute it and/or modify
 # it under the terms of the GNU Lesser General Public License as published by
@@ -15,11 +15,26 @@
 # See the GNU Lesser General Public License in COPYING.LGPL for more details.
 #
 EXEC=$(basename $0 .sh)
+DIRNAME=$(dirname $0)
+
+CLIMIT=$(ulimit -c)
+if [ "$CLIMIT" = unlimited ]
+then
+	# valgrind cores are often *huge*, 100MB will already be quite big...
+	ulimit -c 100000
+fi
+
 if test "$EXEC" == "valgrind"
 then
-    RUN="valgrind --track-origins=yes --show-reachable=yes --leak-check=full --errors-for-leak-kinds=all --show-leak-kinds=all"
+    RUN="valgrind --track-origins=yes --show-reachable=yes --leak-check=full --errors-for-leak-kinds=all --show-leak-kinds=all --error-exitcode=42 $STARPU_VALGRIND_OPTIONS"
+elif test "$EXEC" == "valgrind_xml"
+then
+    mkdir -p ${DIRNAME}/../../../valgrind
+    XML_FILE=$(mktemp -p ${DIRNAME}/../../../valgrind starpu-valgrind_XXXXXXXXXX.xml)
+    RUN="valgrind --track-origins=yes --show-reachable=yes --leak-check=full --errors-for-leak-kinds=all --show-leak-kinds=all --xml=yes --xml-file=${XML_FILE} $STARPU_VALGRIND_OPTIONS"
 else
-    RUN="valgrind --tool=$EXEC"
+    RUN="valgrind --tool=$EXEC --error-exitcode=42"
 fi
-SUPPRESSIONS=$(for f in $(dirname $0)/*.suppr ; do echo "--suppressions=$f" ; done)
-$RUN --keep-debuginfo=yes --num-callers=42 --error-exitcode=42 --gen-suppressions=all $SUPPRESSIONS $*
+SUPPRESSIONS=$(for f in $(dirname $0)/*.suppr /usr/share/hwloc/hwloc-valgrind.supp; do if test -f $f ; then echo "--suppressions=$f" ; fi ; done)
+
+exec $RUN --keep-debuginfo=yes --num-callers=42 --error-limit=no --gen-suppressions=all $SUPPRESSIONS $*
