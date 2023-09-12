e9685781e7853edc8e24252d9d407438a2c7c511
Nathalie Furmento <nathalie.furmento@labri.fr>
2023-09-12 11:14:44 +0200

    examples: new layout version of the mult/gemm application
    (cherry picked from commit 6eb500f84e99ed25ed1fe2970f985815b14a92ae)

--- examples/mult/xgemm.c
+++ examples/mult/xgemm.c
@@ -21,94 +21,7 @@
  * dimensions, and the input matrices in the corresponding dimension, and
  * perform the output computations in parallel.
  */
-#ifndef TYPE
-#error "Do not compile xgemm.c directly, compile sgemm.c or dgemm.c"
-#endif
-
-#include <limits.h>
-#include <string.h>
-#include <unistd.h>
-#include <math.h>
-#include <sys/types.h>
-#include <starpu.h>
-#include <starpu_fxt.h>
-
-#ifdef STARPU_HAVE_BLAS
-#include <common/blas.h>
-#endif
-
-#ifdef STARPU_USE_CUDA
-#include <cuda.h>
-#include <starpu_cublas_v2.h>
-static const TYPE p1 = 1.0;
-static const TYPE m1 = -1.0;
-static const TYPE v0 = 0.0;
-#endif
-
-#ifdef STARPU_USE_HIP
-#include <hip/hip_runtime.h>
-#include <starpu_hipblas.h>
-static const TYPE p1 = 1.0;
-static const TYPE m1 = -1.0;
-static const TYPE v0 = 0.0;
-#endif
-
-#ifdef STARPU_QUICK_CHECK
-static unsigned niter = 2;
-#else
-static unsigned niter = 10;
-#endif
-static unsigned nsleeps = 1;
-static unsigned nslicesx = 4;
-static unsigned nslicesy = 4;
-static unsigned nslicesz = 4;
-#if defined(STARPU_QUICK_CHECK) && !defined(STARPU_SIMGRID)
-static unsigned xdim = 256;
-static unsigned ydim = 256;
-static unsigned zdim = 64;
-#else
-static unsigned xdim = 960*4;
-static unsigned ydim = 960*4;
-static unsigned zdim = 960*4;
-#endif
-static unsigned size_set = 0;
-static unsigned check = 0;
-static unsigned bound = 0;
-static unsigned print_hostname = 0;
-static unsigned tiled = 0;
-
-static TYPE *A, *B, *C;
-static starpu_data_handle_t A_handle, B_handle, C_handle;
-
-#define FPRINTF(ofile, fmt, ...) do { if (!getenv("STARPU_SSILENT")) {fprintf(ofile, fmt, ## __VA_ARGS__); }} while(0)
-#define PRINTF(fmt, ...) do { if (!getenv("STARPU_SSILENT")) {printf(fmt, ## __VA_ARGS__); fflush(stdout); }} while(0)
-
-#ifdef STARPU_HAVE_BLAS
-static int check_output(void)
-{
-	/* compute C = C - AB */
-	CPU_GEMM("N", "N", ydim, xdim, zdim, (TYPE)-1.0f, A, ydim, B, zdim, (TYPE)1.0f, C, ydim);
-
-	/* make sure C = 0 */
-	TYPE err;
-	err = CPU_ASUM(xdim*ydim, C, 1);
-
-	if (err < EPSILON*xdim*ydim*zdim)
-	{
-		FPRINTF(stderr, "Results are OK\n");
-		return 0;
-	}
-	else
-	{
-		int max;
-		max = CPU_IAMAX(xdim*ydim, C, 1);
-
-		FPRINTF(stderr, "There were errors ... err = %f\n", err);
-		FPRINTF(stderr, "Max error : %e\n", C[max]);
-		return 1;
-	}
-}
-#endif
+#include "xgemm.h"
 
 static void init_problem_data(void)
 {
@@ -152,12 +65,9 @@ static void partition_mult_data(void)
 {
 	unsigned x, y, z;
 
-	starpu_matrix_data_register(&A_handle, STARPU_MAIN_RAM, (uintptr_t)A,
-		ydim, ydim, zdim, sizeof(TYPE));
-	starpu_matrix_data_register(&B_handle, STARPU_MAIN_RAM, (uintptr_t)B,
-		zdim, zdim, xdim, sizeof(TYPE));
-	starpu_matrix_data_register(&C_handle, STARPU_MAIN_RAM, (uintptr_t)C,
-		ydim, ydim, xdim, sizeof(TYPE));
+	starpu_matrix_data_register(&A_handle, STARPU_MAIN_RAM, (uintptr_t)A, ydim, ydim, zdim, sizeof(TYPE));
+	starpu_matrix_data_register(&B_handle, STARPU_MAIN_RAM, (uintptr_t)B, zdim, zdim, xdim, sizeof(TYPE));
+	starpu_matrix_data_register(&C_handle, STARPU_MAIN_RAM, (uintptr_t)C, ydim, ydim, xdim, sizeof(TYPE));
 
 	struct starpu_data_filter vert;
 	memset(&vert, 0, sizeof(vert));
@@ -229,58 +139,12 @@ static void cublas_mult(void *descr[], void *arg, const TYPE *beta)
 	unsigned ldC = STARPU_MATRIX_GET_LD(descr[2]);
 
 	cublasStatus_t status = CUBLAS_GEMM(starpu_cublas_get_local_handle(),
-			CUBLAS_OP_N, CUBLAS_OP_N,
-			nxC, nyC, nyA,
-			&p1, subA, ldA, subB, ldB,
-			beta, subC, ldC);
+					    CUBLAS_OP_N, CUBLAS_OP_N,
+					    nxC, nyC, nyA,
+					    &p1_cuda, subA, ldA, subB, ldB,
+					    beta, subC, ldC);
 	if (status != CUBLAS_STATUS_SUCCESS)
 		STARPU_CUBLAS_REPORT_ERROR(status);
-}
-
-static void cublas_gemm0(void *descr[], void *arg)
-{
-	cublas_mult(descr, arg, &v0);
-}
-
-static void cublas_gemm(void *descr[], void *arg)
-{
-	cublas_mult(descr, arg, &p1);
-}
-#endif
-
-#ifdef STARPU_USE_HIP
-static void hipblas_mult(void *descr[], void *arg, const TYPE *beta)
-{
-        (void)arg;
-        TYPE *subA = (TYPE *)STARPU_MATRIX_GET_PTR(descr[0]);
-        TYPE *subB = (TYPE *)STARPU_MATRIX_GET_PTR(descr[1]);
-        TYPE *subC = (TYPE *)STARPU_MATRIX_GET_PTR(descr[2]);
-
-        unsigned nxC = STARPU_MATRIX_GET_NX(descr[2]);
-        unsigned nyC = STARPU_MATRIX_GET_NY(descr[2]);
-        unsigned nyA = STARPU_MATRIX_GET_NY(descr[0]);
-
-        unsigned ldA = STARPU_MATRIX_GET_LD(descr[0]);
-        unsigned ldB = STARPU_MATRIX_GET_LD(descr[1]);
-        unsigned ldC = STARPU_MATRIX_GET_LD(descr[2]);
-
-        hipblasStatus_t status = HIPBLAS_GEMM(starpu_hipblas_get_local_handle(),
-                        HIPBLAS_OP_N, HIPBLAS_OP_N,
-                        nxC, nyC, nyA,
-                        &p1, subA, ldA, subB, ldB,
-                        beta, subC, ldC);
-        if (status != HIPBLAS_STATUS_SUCCESS)
-                STARPU_HIPBLAS_REPORT_ERROR(status);
-}
-
-static void hipblas_gemm0(void *descr[], void *arg)
-{
-        hipblas_mult(descr, arg, &v0);
-}
-
-static void hipblas_gemm(void *descr[], void *arg)
-{
-        hipblas_mult(descr, arg, &p1);
 }
 #endif
 
@@ -323,23 +187,7 @@ void cpu_mult(void *descr[], void *arg, TYPE beta)
 		CPU_GEMM("N", "N", nxC, new_nyC, nyA, (TYPE)1.0, subA, ldA, new_subB, ldB, beta, new_subC, ldC);
 	}
 }
-
-void cpu_gemm0(void *descr[], void *arg)
-{
-	cpu_mult(descr, arg, 0.);
-}
-
-void cpu_gemm(void *descr[], void *arg)
-{
-	cpu_mult(descr, arg, 1.);
-}
 #endif
-
-static struct starpu_perfmodel starpu_gemm_model =
-{
-	.type = STARPU_HISTORY_BASED,
-	.symbol = STARPU_GEMM_STR(gemm)
-};
 
 static struct starpu_codelet cl_gemm0 =
 {
@@ -388,6 +236,8 @@ static struct starpu_codelet cl_gemm =
 static void parse_args(int argc, char **argv)
 {
 	int i;
+	int size_set = 0;
+
 	for (i = 1; i < argc; i++)
 	{
 		if (strcmp(argv[i], "-3d") == 0)
@@ -560,14 +410,6 @@ static void parse_args(int argc, char **argv)
 			exit(EXIT_FAILURE);
 		}
 	}
-}
-
-int main(int argc, char **argv)
-{
-	double start, end;
-	int ret;
-
-	parse_args(argc, argv);
 
 #ifndef STARPU_SIMGRID
 	if (check && !size_set)
@@ -583,19 +425,10 @@ int main(int argc, char **argv)
 	if(niter==0)
 		niter=1;
 #endif
+}
 
-	starpu_fxt_autostart_profiling(0);
-	ret = starpu_init(NULL);
-	if (ret == -ENODEV)
-		return 77;
-	STARPU_CHECK_RETURN_VALUE(ret, "starpu_init");
-
-	starpu_cublas_init();
-	starpu_hipblas_init();
-
-	init_problem_data();
-	partition_mult_data();
-
+static int run_data(void)
+{
 	PRINTF("# ");
 	if (print_hostname)
 		PRINTF("node\t");
@@ -611,7 +444,7 @@ int main(int argc, char **argv)
 			starpu_bound_start(0, 0);
 
 	        starpu_fxt_start_profiling();
-                start = starpu_timing_now();
+                double start = starpu_timing_now();
 
 		unsigned x, y, z, iter;
 		for (iter = 0; iter < niter; iter++)
@@ -625,17 +458,16 @@ int main(int argc, char **argv)
 					for (z = 0; z < nslicesz; z++)
 					{
 						struct starpu_codelet *cl = z == 0 ? &cl_gemm0 : &cl_gemm;
-						ret = starpu_task_insert(cl,
-									 cl->modes[0], starpu_data_get_sub_data(A_handle, 2, z, y),
-									 cl->modes[1], starpu_data_get_sub_data(B_handle, 2, x, z),
-									 cl->modes[2], Ctile,
-									 STARPU_FLOPS, (double) (2ULL * (xdim/nslicesx) * (ydim/nslicesy) * (zdim/nslicesz)),
-									 0);
+						int ret = starpu_task_insert(cl,
+									     cl->modes[0], starpu_data_get_sub_data(A_handle, 2, z, y),
+									     cl->modes[1], starpu_data_get_sub_data(B_handle, 2, x, z),
+									     cl->modes[2], Ctile,
+									     STARPU_FLOPS, (double) (2ULL * (xdim/nslicesx) * (ydim/nslicesy) * (zdim/nslicesz)),
+									     0);
 						if (ret == -ENODEV)
 						{
 							check = 0;
-							ret = 77;
-							goto enodev;
+							return 77;
 						}
 						STARPU_CHECK_RETURN_VALUE(ret, "starpu_task_insert");
 					}
@@ -647,17 +479,16 @@ int main(int argc, char **argv)
 				for (x = 0; x < nslicesx; x++)
 				for (y = 0; y < nslicesy; y++)
 				{
-					ret = starpu_task_insert(&cl_gemm0,
-								 cl_gemm0.modes[0], starpu_data_get_sub_data(A_handle, 1, y),
-								 cl_gemm0.modes[1], starpu_data_get_sub_data(B_handle, 1, x),
-								 cl_gemm0.modes[2], starpu_data_get_sub_data(C_handle, 2, x, y),
-								 STARPU_FLOPS, (double) (2ULL * (xdim/nslicesx) * (ydim/nslicesy) * zdim),
-								 0);
+					int ret = starpu_task_insert(&cl_gemm0,
+								     cl_gemm0.modes[0], starpu_data_get_sub_data(A_handle, 1, y),
+								     cl_gemm0.modes[1], starpu_data_get_sub_data(B_handle, 1, x),
+								     cl_gemm0.modes[2], starpu_data_get_sub_data(C_handle, 2, x, y),
+								     STARPU_FLOPS, (double) (2ULL * (xdim/nslicesx) * (ydim/nslicesy) * zdim),
+								     0);
 					if (ret == -ENODEV)
 					{
 						check = 0;
-						ret = 77;
-						goto enodev;
+						return 77;
 					}
 					STARPU_CHECK_RETURN_VALUE(ret, "starpu_task_insert");
 					starpu_data_wont_use(starpu_data_get_sub_data(C_handle, 2, x, y));
@@ -667,7 +498,7 @@ int main(int argc, char **argv)
 			starpu_task_wait_for_all();
 		}
 
-		end = starpu_timing_now();
+		double end = starpu_timing_now();
 		starpu_fxt_stop_profiling();
 
 		if (bound)
@@ -697,30 +528,6 @@ int main(int argc, char **argv)
 			sleep(10);
 		}
 	}
-
-enodev:
-	starpu_data_unpartition(C_handle, STARPU_MAIN_RAM);
-	starpu_data_unpartition(B_handle, STARPU_MAIN_RAM);
-	starpu_data_unpartition(A_handle, STARPU_MAIN_RAM);
-
-	starpu_data_unregister(A_handle);
-	starpu_data_unregister(B_handle);
-	starpu_data_unregister(C_handle);
-
-#ifdef STARPU_HAVE_BLAS
-#ifndef STARPU_SIMGRID
-	if (check)
-		ret = check_output();
-#endif
-#endif
-
-	starpu_free_flags(A, zdim*ydim*sizeof(TYPE), STARPU_MALLOC_PINNED|STARPU_MALLOC_SIMULATION_FOLDED);
-	starpu_free_flags(B, xdim*zdim*sizeof(TYPE), STARPU_MALLOC_PINNED|STARPU_MALLOC_SIMULATION_FOLDED);
-	starpu_free_flags(C, xdim*ydim*sizeof(TYPE), STARPU_MALLOC_PINNED|STARPU_MALLOC_SIMULATION_FOLDED);
-
-	starpu_cublas_shutdown();
-	starpu_hipblas_shutdown();
-	starpu_shutdown();
-
-	return ret;
+	return 0;
 }
+
