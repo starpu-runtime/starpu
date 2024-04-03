541548c7f764f2f97824f0552586272d9912471d
Nathalie Furmento <nathalie.furmento@labri.fr>
2024-04-03 11:03:47 +0200

    hierarchical tasks are now named recursive tasks
    (cherry picked from commit cdec58d9f4ceac15a775adf7a651d7fc652c080e)

--- include/starpu_task.h
+++ include/starpu_task.h
@@ -210,13 +210,13 @@ typedef void (*starpu_opencl_func_t)(void **, void *);
 typedef void (*starpu_max_fpga_func_t)(void **, void *);
 
 /**
-   @ingroup API_Bubble Hierarchical Dags
+   @ingroup API_Bubble Recursive Tasks
    Bubble decision function
 */
-typedef int (*starpu_bubble_func_t)(struct starpu_task *t, void *arg);
+typedef int (*starpu_bubble_func_t)(struct starpu_task *, void *);
 
 /**
-   @ingroup API_Bubble Hierarchical Dags
+   @ingroup API_Bubble Recursive Tasks
    Bubble DAG generation function
 */
 typedef void (*starpu_bubble_gen_dag_func_t)(struct starpu_task *t, void *arg);
@@ -1423,31 +1423,31 @@ struct starpu_task
 #endif
 
 	/**
-	   When using hierarchical dags, the job identifier of the
+	   When using recursive tasks, the job identifier of the
 	   bubble task which created the current task
 	*/
 	unsigned long bubble_parent;
 
 	/**
-	   When using hierarchical dags, a pointer to the bubble
+	   When using recursive tasks, a pointer to the bubble
 	   decision function
 	*/
 	starpu_bubble_func_t bubble_func;
 
 	/**
-	   When using hierarchical dags, a pointer to an argument to
+	   When using recursive tasks, a pointer to an argument to
 	   be given when calling the bubble decision function
 	*/
 	void *bubble_func_arg;
 
 	/**
-	   When using hierarchical dags, a pointer to the bubble
+	   When using recursive tasks, a pointer to the bubble
 	   DAG generation function
 	*/
 	starpu_bubble_gen_dag_func_t bubble_gen_dag_func;
 
 	/**
-	   When using hierarchical dags, a pointer to an argument to
+	   When using recursive tasks, a pointer to an argument to
 	   be given when calling the bubble DAG generation function
 	 */
 	void *bubble_gen_dag_func_arg;
