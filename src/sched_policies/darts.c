eeddb4c0e4691bf1f07c04512fd7d720a867ae7d
Maxime Gonthier <mgonthier@chiclet-8.lille.grid5000.fr>
2024-04-10 22:59:41 +0200

    darts: remove wrong comment

--- src/sched_policies/darts.c
+++ src/sched_policies/darts.c
@@ -522,12 +522,11 @@ static void _if_found_erase_data_from_data_not_used_yet_of_all_pu(starpu_data_ha
 				_starpu_darts_gpu_data_not_used_delete(e);
 				
 				/* Liberating user_data as well */
-			/*	free(hud->nb_task_in_pulled_task);
+				free(hud->nb_task_in_pulled_task);
 				free(hud->nb_task_in_planned_task);
 				free(hud->last_check_to_choose_from);
 				free(hud->is_present_in_data_not_used_yet);
 				free(e->D->user_data);
-*/
 				break;
 			}
 		}
