61a159c830867e5279e65ac9a966423af5223748
Samuel Thibault <samuel.thibault@ens-lyon.org>
2024-08-31 01:53:42 +0200

    Fix crash on shutdown with several workers per gpu
    (cherry picked from commit 214f3638fdfe0dd9b606970d8aab6057b51aef08)

--- src/drivers/cuda/driver_cuda.c
+++ src/drivers/cuda/driver_cuda.c
@@ -354,7 +354,7 @@ static void init_device_context(unsigned devid, unsigned memnode)
 		for (workerid = 0; workerid < nworkers; workerid++)
 		{
 			struct _starpu_worker *worker = _starpu_get_worker_struct(workerid);
-			if (worker->arch == STARPU_CUDA_WORKER && worker->devid != devid)
+			if (worker->arch == STARPU_CUDA_WORKER && worker->subworkerid == 0 && worker->devid != devid)
 			{
 				int can;
 				cures = cudaDeviceCanAccessPeer(&can, devid, worker->devid);
@@ -413,7 +413,7 @@ static void init_device_context(unsigned devid, unsigned memnode)
 	for (workerid = 0; workerid < nworkers; workerid++)
 	{
 		struct _starpu_worker *worker = _starpu_get_worker_struct(workerid);
-		if (worker->arch == STARPU_CUDA_WORKER)
+		if (worker->arch == STARPU_CUDA_WORKER && worker->subworkerid == 0)
 		{
 			cures = starpu_cudaStreamCreate(&in_peer_transfer_streams[worker->devid][devid]);
 			if (STARPU_UNLIKELY(cures))
@@ -468,7 +468,7 @@ static void deinit_device_context(unsigned devid)
 	for (workerid = 0; workerid < nworkers; workerid++)
 	{
 		struct _starpu_worker *worker = _starpu_get_worker_struct(workerid);
-		if (worker->arch == STARPU_CUDA_WORKER)
+		if (worker->arch == STARPU_CUDA_WORKER && worker->subworkerid == 0)
 		{
 			cudaStreamDestroy(in_peer_transfer_streams[worker->devid][devid]);
 		}
