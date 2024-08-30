35afb2b5e2710b22979c5d459cf5a9699982849c
Samuel Thibault <samuel.thibault@ens-lyon.org>
2024-08-30 16:59:16 +0200

    Don't free handle mc if it's still in use on invalidation
    (cherry picked from commit a5a00bb0cb1a668c4c69193ffbacf7192680694c)
    (cherry picked from commit bb5897c8e468227f757ec06fa478b69290f49350)

--- src/datawizard/interfaces/data_interface.c
+++ src/datawizard/interfaces/data_interface.c
@@ -1055,6 +1055,10 @@ static void _starpu_data_invalidate(void *data)
 	{
 		struct _starpu_data_replicate *local = &handle->per_node[node];
 
+		if (local->refcnt)
+			/* Avoid freeing chunk still in use */
+			continue;
+
 		if (local->mc && local->allocated && local->automatically_allocated)
 		{
 			_starpu_data_unregister_ram_pointer(handle, node);
@@ -1073,7 +1077,7 @@ static void _starpu_data_invalidate(void *data)
 		{
 			struct _starpu_data_replicate *local = &handle->per_worker[worker];
 
-			if (local->mc && local->allocated && local->automatically_allocated)
+			if (local->mc && local->allocated && local->automatically_allocated && !local->refcnt)
 				/* free the data copy in a lazy fashion */
 				_starpu_request_mem_chunk_removal(handle, local, starpu_worker_get_memory_node(worker), size);
 		}
