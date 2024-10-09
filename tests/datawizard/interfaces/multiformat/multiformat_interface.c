175fa67a837850261a25ee1878650b9b3d81a7ca
Nathalie Furmento <nathalie.furmento@labri.fr>
2024-10-09 11:35:26 +0200

    tests/datawizard/interfaces/multiformat/multiformat_interface.c: cannot run without CPUs

--- tests/datawizard/interfaces/multiformat/multiformat_interface.c
+++ tests/datawizard/interfaces/multiformat/multiformat_interface.c
@@ -148,6 +148,11 @@ int main(int argc, char **argv)
 	ret = starpu_initialize(&conf, &argc, &argv);
 	if (ret == -ENODEV) return STARPU_TEST_SKIPPED;
 	STARPU_CHECK_RETURN_VALUE(ret, "starpu_init");
+	if (starpu_cpu_worker_get_count() == 0)
+	{
+		starpu_shutdown();
+		return STARPU_TEST_SKIPPED;
+	}
 
 	register_data();
 
