adc054531e1d67690f8c7e3635fd4c43ce9fa45d
Samuel Thibault <samuel.thibault@ens-lyon.org>
2024-10-28 13:18:56 +0100

    Fix memory leak when returning ENODEV
    (cherry picked from commit 9c5daefc600b96ae216284e6ffaa01957c5688f1)

--- src/core/workers.c
+++ src/core/workers.c
@@ -1478,6 +1478,12 @@ int starpu_initialize(struct starpu_conf *user_conf, int *argc, char ***argv)
 
 		_starpu_destroy_machine_config(&_starpu_config);
 
+		free((char*) _starpu_config.conf.sched_policy_name);
+		if (_starpu_config.conf.n_cuda_opengl_interoperability)
+			free(_starpu_config.conf.cuda_opengl_interoperability);
+		if (_starpu_config.conf.n_not_launched_drivers)
+			free(_starpu_config.conf.not_launched_drivers);
+
 #ifdef STARPU_USE_MPI_MASTER_SLAVE
                 if (_starpu_mpi_common_is_mp_initialized())
                         _starpu_mpi_common_mp_deinit();
