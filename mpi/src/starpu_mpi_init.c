b4349f402141f5db6ccec42c049a1d987039c218
Samuel Thibault <samuel.thibault@ens-lyon.org>
2025-02-17 23:20:53 +0100

    Fix build with gcc-15
    (cherry picked from commit 8477129e37731df1bb54dfd22704f5563fb9fd8b)

--- mpi/src/starpu_mpi_init.c
+++ mpi/src/starpu_mpi_init.c
@@ -255,7 +255,7 @@ int starpu_mpi_shutdown(void)
 
 	_starpu_mpi_comm_amounts_display(stderr, rank);
 	_starpu_mpi_comm_amounts_shutdown();
-	_starpu_mpi_cache_shutdown(world_size);
+	_starpu_mpi_cache_shutdown();
 
 	_mpi_backend._starpu_mpi_backend_shutdown();
 
