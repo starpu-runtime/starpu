951ac75374781a6e13c7b00fadac8431a8d47422
Nathalie Furmento <nathalie.furmento@labri.fr>
2025-03-17 12:56:59 +0100

    tools/gpus: add makefile and check_hip program

---
+++ tools/gpus/check_hip.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <hip/hip_runtime.h>
+#include <hip/hip_runtime_api.h>
+
+int main(void)
+{
+	int i, cnt;
+	hipError_t hipres;
+	hipres = hipGetDeviceCount(&cnt);
+	if (hipres)
+	{
+		fprintf(stderr,"hip error %d\n", hipres);
+		exit(1);
+	}
+	printf("number of hip devices: %d\n", cnt);
+	for (i = 0; i < cnt; i++)
+	{
+		struct hipDeviceProp_t props;
+		printf("HIP%d\n", i);
+		hipres = hipGetDeviceProperties(&props, i);
+		if (hipres)
+		{
+			fprintf(stderr,"HIP error %d\n", hipres);
+			continue;
+		}
+		printf("%s\n", props.name);
+		printf("%0.3f GB\n", (float) props.totalGlobalMem / (1<<30));
+		printf("%u MP\n", props.multiProcessorCount);
+		printf("\n");
+	}
+	return 0;
+}
