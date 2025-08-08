0be55e6a2ade857d6057c9795e73e0f0eeb7a009
Nathalie Furmento <nathalie.furmento@labri.fr>
2025-08-08 16:42:41 +0200

    src: disable hwloc components

--- src/common/utils.c
+++ src/common/utils.c
@@ -762,6 +762,7 @@ void starpu_display_bindings(void)
 #if defined(STARPU_HAVE_HWLOC) && !defined(STARPU_SIMGRID)
 	int value = starpu_getenv_number_default("STARPU_DISPLAY_BINDINGS", 0);
 	int ret = 0;
+	setenv("HWLOC_COMPONENTS", "linux,windows,darwin,stop");
 	if (value == 2)
 		ret = system("lstopo --ps -");
 	else
