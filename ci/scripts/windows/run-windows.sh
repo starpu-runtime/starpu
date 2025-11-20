5aa8e4a57f0752452784d8567f6a42e955a7532d
Nathalie Furmento <nathalie.furmento@labri.fr>
2025-11-20 08:19:37 +0100

    ci: add step to run windows applications

---
+++ ci/scripts/windows/run-windows.sh
@@ -0,0 +1,45 @@
+#!/bin/sh
+# StarPU --- Runtime system for heterogeneous multicore architectures.
+#
+# Copyright (C) 2025-2025   University of Bordeaux, CNRS (LaBRI UMR 5800), Inria
+#
+# StarPU is free software; you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation; either version 2.1 of the License, or (at
+# your option) any later version.
+#
+# StarPU is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+#
+# See the GNU Lesser General Public License in COPYING.LGPL for more details.
+#
+
+set -e
+set -x
+
+export LC_ALL=C
+oldPATH=$PATH
+# Add both PATHS for msys and cygdrive
+export PATH="/c/Program Files/Microsoft Visual Studio/2022/Community/VC/bin":"/c/Program Files/Microsoft Visual Studio/2022/Community/Common7/IDE":"/c/Program Files/Microsoft Visual Studio/2022/Community/VC/Tools/MSVC/14.42.34433/bin/Hostx64/x64":$oldPATH
+export PATH="/cygdrive/c/Program Files/Microsoft Visual Studio/2022/Community/VC/bin":"/cygdrive/c/Program Files/Microsoft Visual Studio/2022/Community/Common7/IDE":"/cygdrive/c/Program Files/Microsoft Visual Studio/2022/Community/VC/Tools/MSVC/14.42.34433/bin/Hostx64/x64":$PATH
+
+cd artifacts
+zipball=$(ls -tr starpu*.zip | tail -1)
+if test -z "$zipball" ; then
+    echo Zipball not available
+    exit 2
+fi
+
+basename=$(basename $zipball .zip)
+test -d $basename && chmod -R u+rwX $basename && rm -rf $basename
+unzip $zipball
+
+cd $basename
+export STARPU_PATH=$PWD
+cd share/doc/starpu/tutorial/
+make
+./hello_world.exe
+./vector_scal.exe
+
+
