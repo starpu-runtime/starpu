5cf3438f601004ece78c5c76adf48a42ce1f6c6d
Nathalie Furmento <nathalie.furmento@labri.fr>
2025-12-10 14:05:11 +0100

    ci/scripts/releases/new-release.sh: create tmp directory in home directory
    (cherry picked from commit 202ecbfbbd68f8f9e14a90ba9ee6b6cffe142268)

--- contrib/releases/new-release.sh
+++ contrib/releases/new-release.sh
@@ -146,6 +146,6 @@ done
 make clean
 
 # execute on the frontal node to upload release files on files.inria.fr/starpu
-TMP_DIR=$(ssh luckyluke mktemp -d 2>/dev/null)
+TMP_DIR=$(ssh luckyluke mktemp -p \$HOME -d 2>/dev/null)
 scp -pr ${ARTIFACTS} luckyluke:${TMP_DIR}
 ssh luckyluke $HOME/softs/starpu/starpu-scripts/mirror/scripts/uploadRelease.sh ${TMP_DIR}/${ARTIFACTS} ${release} ${DATE}
