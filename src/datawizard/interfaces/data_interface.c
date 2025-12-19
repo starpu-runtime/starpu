1a2f40037a521bc951d062429ebe4da3dd3d548d
Nathalie Furmento <nathalie.furmento@labri.fr>
2025-12-19 08:14:32 +0100

    src: fix code format

--- src/datawizard/interfaces/data_interface.c
+++ src/datawizard/interfaces/data_interface.c
@@ -517,40 +517,41 @@ void *starpu_data_handle_to_pointer(starpu_data_handle_t handle, unsigned node)
 
 void *starpu_data_handle_to_pointer_ref(starpu_data_handle_t handle, unsigned node)
 {
-    void *ptr = NULL;
-    _starpu_spin_lock(&handle->header_lock);
-    /* Check whether the operation is supported and the node has actually
-     * been allocated.  */
-    if (!starpu_data_test_if_allocated_on_node(handle, node)) {
-        _starpu_spin_unlock(&handle->header_lock);
-        return NULL;
-    }
-    if (handle->ops->to_pointer)
-    {
-        ptr = handle->ops->to_pointer(starpu_data_get_interface_on_node(handle, node), node);
-    }
-    else if (handle->ops->handle_to_pointer)
-    {
-        /* Deprecated */
-        ptr = handle->ops->handle_to_pointer(handle, node);
-    }
-    /* Keep a reference on the replicate while the application is using it,
-     * until it calls starpu_data_handle_to_pointer_unref */
-    handle->per_node[node].refcnt++;
-    handle->busy_count++;
-    _starpu_spin_unlock(&handle->header_lock);
+	void *ptr = NULL;
+	_starpu_spin_lock(&handle->header_lock);
+	/* Check whether the operation is supported and the node has actually
+	 * been allocated.  */
+	if (!starpu_data_test_if_allocated_on_node(handle, node))
+	{
+		_starpu_spin_unlock(&handle->header_lock);
+		return NULL;
+	}
+	if (handle->ops->to_pointer)
+	{
+		ptr = handle->ops->to_pointer(starpu_data_get_interface_on_node(handle, node), node);
+	}
+	else if (handle->ops->handle_to_pointer)
+	{
+		/* Deprecated */
+		ptr = handle->ops->handle_to_pointer(handle, node);
+	}
+	/* Keep a reference on the replicate while the application is using it,
+	 * until it calls starpu_data_handle_to_pointer_unref */
+	handle->per_node[node].refcnt++;
+	handle->busy_count++;
+	_starpu_spin_unlock(&handle->header_lock);
 
-    return ptr;
+	return ptr;
 }
 
 void starpu_data_handle_to_pointer_unref(starpu_data_handle_t handle, unsigned node)
 {
-    _starpu_spin_lock(&handle->header_lock);
-    /* Release reference taken in starpu_data_handle_to_pointer_ref */
-    handle->per_node[node].refcnt--;
-    handle->busy_count--;
-    if (!_starpu_data_check_not_busy(handle))
-        _starpu_spin_unlock(&handle->header_lock);
+	_starpu_spin_lock(&handle->header_lock);
+	/* Release reference taken in starpu_data_handle_to_pointer_ref */
+	handle->per_node[node].refcnt--;
+	handle->busy_count--;
+	if (!_starpu_data_check_not_busy(handle))
+		_starpu_spin_unlock(&handle->header_lock);
 }
 
 void *starpu_data_get_local_ptr(starpu_data_handle_t handle)
