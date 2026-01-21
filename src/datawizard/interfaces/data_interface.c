378e140840f9c3ca520cb200136bcd9b0299fe01
Samuel Thibault <samuel.thibault@ens-lyon.org>
2026-01-21 10:39:30 +0100

    Check nb_tasks_prefetch on data unregistration
    To verify that we didn't miss clearing task_prefetch pinning that would
    clutter memory.

--- src/datawizard/interfaces/data_interface.c
+++ src/datawizard/interfaces/data_interface.c
@@ -1,6 +1,6 @@
 /* StarPU --- Runtime system for heterogeneous multicore architectures.
  *
- * Copyright (C) 2009-2025  University of Bordeaux, CNRS (LaBRI UMR 5800), Inria
+ * Copyright (C) 2009-2026  University of Bordeaux, CNRS (LaBRI UMR 5800), Inria
  *
  * StarPU is free software; you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
@@ -910,6 +910,7 @@ retry_busy:
 	{
 		struct _starpu_data_replicate *local = &handle->per_node[node];
 		STARPU_ASSERT(!local->refcnt);
+		STARPU_ASSERT(!local->nb_tasks_prefetch);
 		if (local->allocated)
 		{
 		/* free the data copy in a lazy fashion */
