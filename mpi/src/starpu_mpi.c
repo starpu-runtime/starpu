d43b383c9e86461ff1555a061289d6e97508cbb4
Tristan Riehs <tristan.riehs@inria.fr>
2026-01-23 08:30:06 +0100

    mpi: remove aggressive assert

--- mpi/src/starpu_mpi.c
+++ mpi/src/starpu_mpi.c
@@ -237,7 +237,6 @@ static void _starpu_mpi_acquired_callback(void *arg, int *nodep, enum starpu_dat
 
 	if (req->early_node != (unsigned) node)
 	{
-		STARPU_MPI_ASSERT_MSG(0, "no acquired callback yet");
 		/* Data location changed since the soon callback was called. If
 		 * an early prefetch was requested, then it shall be
 		 * cancelled */
