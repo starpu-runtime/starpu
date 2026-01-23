2c87768bb4b6312b5c83cb865f3bced3c0217510
Tristan Riehs <tristan.riehs@inria.fr>
2026-01-23 08:24:42 +0100

    mpi: revert to basic handle_to_pointer
    This will likely have to change. But for now since I do not know where
    to call handle_to_pointer_unref, I cannot use handle_to_pointer_ref.

--- mpi/src/starpu_mpi.c
+++ mpi/src/starpu_mpi.c
@@ -188,7 +188,7 @@ static void _starpu_mpi_soon_callback(void *arg, STARPU_ATTRIBUTE_UNUSED double 
 	req->early_node = req->node;
 	_starpu_mpi_datatype_allocate(req->data_handle, req);
 	req->count = 1;
-	req->ptr = starpu_data_handle_to_pointer_ref(req->data_handle, req->early_node);
+	req->ptr = starpu_data_handle_to_pointer(req->data_handle, req->early_node);
 	_mpi_backend._starpu_mpi_backend_early_prefetch_func(req);
 	_STARPU_MPI_LOG_OUT();
 }
@@ -246,7 +246,7 @@ static void _starpu_mpi_acquired_callback(void *arg, int *nodep, enum starpu_dat
 		req->early_node = node;
 		_starpu_mpi_datatype_allocate(req->data_handle, req);
 		req->count = 1;
-		req->ptr = starpu_data_handle_to_pointer_ref(req->data_handle, node);
+		req->ptr = starpu_data_handle_to_pointer(req->data_handle, node);
 		_mpi_backend._starpu_mpi_backend_early_prefetch_func(req);
 	}
 	_STARPU_MPI_LOG_OUT();
