3f3cc82a6fe5fd0b919f8c6954ac8254b1fb3190
Nathalie Furmento <nathalie.furmento@labri.fr>
2026-08-13 08:47:26 +0200

    ci/scripts: fix path

--- ci/scripts/coverity.sh
+++ ci/scripts/coverity.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # StarPU --- Runtime system for heterogeneous multicore architectures.
 #
-# Copyright (C) 2020-2025   University of Bordeaux, CNRS (LaBRI UMR 5800), Inria
+# Copyright (C) 2020-2026   University of Bordeaux, CNRS (LaBRI UMR 5800), Inria
 #
 # StarPU is free software; you can redistribute it and/or modify
 # it under the terms of the GNU Lesser General Public License as published by
@@ -25,5 +25,5 @@ then
     BRANCH=$CI_MERGE_REQUEST_SOURCE_BRANCH_NAME
 fi
 
-./ci/scripts/check.sh -no-tarball -coverity $BRANCH
+PATH=$HOME/coverity/bin:$PATH ./ci/scripts/check.sh -no-tarball -coverity $BRANCH
 
