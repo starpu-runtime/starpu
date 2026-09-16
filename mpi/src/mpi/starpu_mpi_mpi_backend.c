a3633a4ebd591fef22addf4137b8b325f307e7b6
Tristan Riehs <tristan.riehs@inria.fr>
2026-09-16 11:57:24 +0200

    mpi: fix typo

--- mpi/src/mpi/starpu_mpi_mpi_backend.c
+++ mpi/src/mpi/starpu_mpi_mpi_backend.c
@@ -141,7 +141,7 @@ struct _starpu_mpi_backend _mpi_backend =
 	._starpu_mpi_backend_early_mem_reg = NULL,
 	._starpu_mpi_backend_early_mem_unreg = NULL,
 
-	._starpu_mpi_backend_send_notify_receiver = NULL;
+	._starpu_mpi_backend_send_notify_receiver = NULL,
 };
 
 #endif /* STARPU_USE_MPI_MPI*/
